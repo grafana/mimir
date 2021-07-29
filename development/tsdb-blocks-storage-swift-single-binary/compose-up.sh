5b1c3568dbfdd86895457ec0be3edeb0b94af057
Tyler Reid <tyler.reid@grafana.com>
2021-07-29 17:49:49 -0500

    integration image name change, start of rework for single binary swift
    Signed-off-by: Tyler Reid <tyler.reid@grafana.com>

--- development/tsdb-blocks-storage-swift-single-binary/compose-up.sh
+++ development/tsdb-blocks-storage-swift-single-binary/compose-up.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 
-CGO_ENABLED=0 GOOS=linux go build -o ${SCRIPT_DIR}/cortex ${SCRIPT_DIR}/../../cmd/cortex && \
-docker-compose -f ${SCRIPT_DIR}/docker-compose.yml build cortex-1 && \
+CGO_ENABLED=0 GOOS=linux go build -o ${SCRIPT_DIR}/mimir ${SCRIPT_DIR}/../../cmd/mimir && \
+docker-compose -f ${SCRIPT_DIR}/docker-compose.yml build mimir-1 && \
 docker-compose -f ${SCRIPT_DIR}/docker-compose.yml up $@
