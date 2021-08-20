734637a0e819f73b15cdb243a678a5d31864ef32
Marco Pracucci <marco@pracucci.com>
2021-08-20 16:47:22 +0200

    Do not use atomic because unnecessary
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/querier/astmapper/stats.go
+++ pkg/querier/astmapper/stats.go
@@ -2,10 +2,8 @@
 
 package astmapper
 
-import "go.uber.org/atomic"
-
 type MapperStats struct {
-	shardedQueries atomic.Int32
+	shardedQueries int
 }
 
 func NewMapperStats() *MapperStats {
@@ -14,10 +12,10 @@ func NewMapperStats() *MapperStats {
 
 // AddShardedQueries add num sharded queries to the counter.
 func (s *MapperStats) AddShardedQueries(num int) {
-	s.shardedQueries.Add(int32(num))
+	s.shardedQueries += num
 }
 
 // GetShardedQueries returns the number of sharded queries.
 func (s *MapperStats) GetShardedQueries() int {
-	return int(s.shardedQueries.Load())
+	return s.shardedQueries
 }
