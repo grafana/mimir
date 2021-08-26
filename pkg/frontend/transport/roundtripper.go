503482c8229586c645eff164d5700e069994fe58
Cyril Tovena <cyril.tovena@gmail.com>
2021-08-26 16:44:57 +0200

    Reuse the byte buffer from GRPC response in the frontend. (#4377)
    (cherry picked from Cortex commit e92f96f80fa4511257bc119de77c8bf46a9d6f83)
    
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/frontend/transport/roundtripper.go
+++ pkg/frontend/transport/roundtripper.go
@@ -8,6 +8,7 @@ package transport
 import (
 	"bytes"
 	"context"
+	"io"
 	"io/ioutil"
 	"net/http"
 
@@ -29,6 +30,15 @@ type grpcRoundTripperAdapter struct {
 	roundTripper GrpcRoundTripper
 }
 
+type buffer struct {
+	buff []byte
+	io.ReadCloser
+}
+
+func (b *buffer) Bytes() []byte {
+	return b.buff
+}
+
 func (a *grpcRoundTripperAdapter) RoundTrip(r *http.Request) (*http.Response, error) {
 	req, err := server.HTTPRequest(r)
 	if err != nil {
@@ -42,7 +52,7 @@ func (a *grpcRoundTripperAdapter) RoundTrip(r *http.Request) (*http.Response, er
 
 	httpResp := &http.Response{
 		StatusCode:    int(resp.Code),
-		Body:          ioutil.NopCloser(bytes.NewReader(resp.Body)),
+		Body:          &buffer{buff: resp.Body, ReadCloser: ioutil.NopCloser(bytes.NewReader(resp.Body))},
 		Header:        http.Header{},
 		ContentLength: int64(len(resp.Body)),
 	}
