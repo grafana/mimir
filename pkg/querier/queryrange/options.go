9a402d5d6b3119d749063c08ee59dae9dfca5c2c
Cyril Tovena <cyril.tovena@gmail.com>
2021-09-14 13:12:17 +0200

    Correct lint issues.
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

--- pkg/querier/queryrange/options.go
+++ pkg/querier/queryrange/options.go
@@ -2,14 +2,9 @@
 package queryrange
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
-
-	"github.com/go-kit/kit/log/level"
-
-	util_log "github.com/grafana/mimir/pkg/util/log"
 )
 
 var (
@@ -30,7 +25,6 @@ func DecodeOptions(r *http.Request, opts *Options) {
 	for _, value := range r.Header.Values(TotalShardsControlHeader) {
 		shards, err := strconv.ParseInt(value, 10, 32)
 		if err != nil {
-			level.Warn(util_log.Logger).Log("msg", fmt.Sprintf("failed to parse '%s' request header with value %s", TotalShardsControlHeader, value), "err", err)
 			break
 		}
 		opts.TotalShards = int32(shards)
