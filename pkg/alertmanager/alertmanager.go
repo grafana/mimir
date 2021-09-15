4c550f4fde62320912015d56dac9774f5cf4c323
Marco Pracucci <marco@pracucci.com>
2021-09-15 09:13:49 +0200

    Fixed alertmanager after upgrade
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/alertmanager/alertmanager.go
+++ pkg/alertmanager/alertmanager.go
@@ -207,7 +207,7 @@ func New(cfg *Config, reg *prometheus.Registry) (*Alertmanager, error) {
 	am.nflog, err = nflog.New(
 		nflog.WithRetention(cfg.Retention),
 		nflog.WithSnapshot(filepath.Join(cfg.TenantDataDir, notificationLogSnapshot)),
-		nflog.WithMaintenance(maintenancePeriod, am.stop, am.wg.Done),
+		nflog.WithMaintenance(maintenancePeriod, am.stop, am.wg.Done, nil),
 		nflog.WithMetrics(am.registry),
 		nflog.WithLogger(log.With(am.logger, "component", "nflog")),
 	)
@@ -251,7 +251,7 @@ func New(cfg *Config, reg *prometheus.Registry) (*Alertmanager, error) {
 
 	am.wg.Add(1)
 	go func() {
-		am.silences.Maintenance(maintenancePeriod, silencesFile, am.stop)
+		am.silences.Maintenance(maintenancePeriod, silencesFile, am.stop, nil)
 		am.wg.Done()
 	}()
 
