6803d0a9eede0d12902a79f9b597d71c3cfe2225
Marco Pracucci <marco@pracucci.com>
2021-09-15 17:23:29 +0200

    Allow to configure --step and benchmark more queries
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- tools/benchmark-query-sharding/benchmark.sh
+++ tools/benchmark-query-sharding/benchmark.sh
@@ -5,5 +5,15 @@ URL="http://localhost:8080/api/prom"
 TENANT_ID="9960"
 SHARDS="16"
 
-./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query "sum by(job) (rate(container_cpu_user_seconds_total[1m]))" --time-ranges "6H 24H 7d 30d" --shards "${SHARDS}"
-./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query "histogram_quantile(0.99, sum by(le) (rate(tempo_spanmetrics_latency_bucket[5m])))" --time-ranges "1H 6H 24H 2d" --shards "${SHARDS}"
+# Typical rate.
+#./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query "sum by(job) (rate(container_cpu_user_seconds_total[1m]))" --time-ranges "6H 24H 7d 30d" --shards "${SHARDS}"
+#./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query "sum by(job) (rate(container_cpu_user_seconds_total[1m]))" --time-ranges "10d" --shards "16"
+./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query "histogram_quantile(0.99, sum by(le) (rate(apiserver_request_duration_seconds_bucket[1m])))" --time-ranges "6H 24H 7d" --shards "16"
+./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query "histogram_quantile(0.99, sum by(verb, resource, le) (rate(apiserver_request_duration_seconds_bucket[1m])))" --time-ranges "6H 24H 7d" --shards "16"
+
+# Histograms.
+#./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query "histogram_quantile(0.99, sum by(le) (rate(tempo_spanmetrics_latency_bucket[5m])))" --time-ranges "1H 6H 24H 2d" --shards "${SHARDS}"
+
+# Analytics.
+#./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query 'count({__name__=~".+"})' --time-ranges "5M" --step "5m" --shards "${SHARDS}"
+#./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query 'topk(10, count by(__name__) ({__name__=~".+"}))' --time-ranges "5M" --step "5m" --shards "${SHARDS}"
--- tools/benchmark-query-sharding/run.sh
+++ tools/benchmark-query-sharding/run.sh
@@ -4,6 +4,7 @@
 URL=""
 TENANT_ID=""
 TEST_QUERY=""
+TEST_STEP=""
 TEST_SHARDS="4 8 16 32"
 TEST_TIME_RANGES="1H 6H 12H 24H"
 
@@ -22,6 +23,11 @@ do
         ;;
     --query)
         TEST_QUERY=$2
+        shift
+        shift
+        ;;
+    --step)
+        TEST_STEP=$2
         shift
         shift
         ;;
@@ -75,20 +81,24 @@ write_tsv_line() {
 # Params:
 # $1 - Query
 # $2 - Timerange
-# $3 - Sharding enabled
-# $4 - Shards size
+# $3 - Step (if empty it's auto computed based on timerange)
+# $4 - Sharding enabled
+# $5 - Shards size
 benchmark_query() {
   QUERY="$1"
   TIME_RANGE="$2"
-  SHARDING_ENABLED="$3"
-  SHARD_SIZE="$4"
+  STEP="$3"
+  SHARDING_ENABLED="$4"
+  SHARD_SIZE="$5"
   START_TIME="$(date -v -${TIME_RANGE} +%s)"
   END_TIME="$(date +%s)"
   HEADERS_FILE=".benchmark-response-headers"
 
   # Compute the step based on the query time range,
   # in order to have 1000 points in output.
-  STEP="$(((END_TIME-$START_TIME)/1000))s"
+  if [ -z "$STEP" ]; then
+    STEP="$(((END_TIME-$START_TIME)/1000))s"
+  fi
 
   # Cleanup any headers file left from a previous run.
   rm -f "${HEADERS_FILE}"
@@ -131,7 +141,7 @@ benchmark_query() {
 
 benchmark_query_with_multiple_runs() {
   for i in {1..3}; do
-   benchmark_query "$1" "$2" "$3" "$4"
+   benchmark_query "$1" "$2" "$3" "$4" "$5"
   done
 }
 
@@ -141,9 +151,9 @@ write_tsv_header
 for TIME_RANGE in $TEST_TIME_RANGES; do
   # Sharding enabled.
   for SHARD_SIZE in $TEST_SHARDS; do
-    benchmark_query_with_multiple_runs "$TEST_QUERY" "$TIME_RANGE" "yes" "${SHARD_SIZE}"
+    benchmark_query_with_multiple_runs "$TEST_QUERY" "$TIME_RANGE" "$TEST_STEP" "yes" "${SHARD_SIZE}"
   done
 
   # Sharding disabled.
-  benchmark_query_with_multiple_runs "$TEST_QUERY" "$TIME_RANGE" "no" ""
+  benchmark_query_with_multiple_runs "$TEST_QUERY" "$TIME_RANGE" "$TEST_STEP" "no" ""
 done
