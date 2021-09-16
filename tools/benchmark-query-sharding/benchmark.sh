35e27de4b9f03a8ff037d7fa63ddec9e2bae9f10
Marco Pracucci <marco@pracucci.com>
2021-09-16 09:30:28 +0200

    Allow to disable .tsv header
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- tools/benchmark-query-sharding/benchmark.sh
+++ tools/benchmark-query-sharding/benchmark.sh
@@ -6,14 +6,28 @@ TENANT_ID="9960"
 SHARDS="16"
 
 # Typical rate.
-#./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query "sum by(job) (rate(container_cpu_user_seconds_total[1m]))" --time-ranges "6H 24H 7d 30d" --shards "${SHARDS}"
-#./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query "sum by(job) (rate(container_cpu_user_seconds_total[1m]))" --time-ranges "10d" --shards "16"
-./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query "histogram_quantile(0.99, sum by(le) (rate(apiserver_request_duration_seconds_bucket[1m])))" --time-ranges "6H 24H 7d" --shards "16"
-./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query "histogram_quantile(0.99, sum by(verb, resource, le) (rate(apiserver_request_duration_seconds_bucket[1m])))" --time-ranges "6H 24H 7d" --shards "16"
+./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" \
+  --query "sum by(job) (rate(container_cpu_user_seconds_total[1m]))" --time-ranges "6H 24H 7d 30d" --shards "${SHARDS}"
+
+./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --tsv-no-header \
+  --query "sum by(job) (rate(container_cpu_user_seconds_total[1m]))" --time-ranges "10d" --shards "16"
+
+./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --tsv-no-header \
+  --query "histogram_quantile(0.99, sum by(le) (rate(apiserver_request_duration_seconds_bucket[1m])))" --time-ranges "6H 24H 7d" --shards "16"
+
+./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --tsv-no-header \
+  --query "histogram_quantile(0.99, sum by(verb, resource, le) (rate(apiserver_request_duration_seconds_bucket[1m])))" --time-ranges "6H 24H 7d" --shards "16"
+
 
 # Histograms.
-#./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query "histogram_quantile(0.99, sum by(le) (rate(tempo_spanmetrics_latency_bucket[5m])))" --time-ranges "1H 6H 24H 2d" --shards "${SHARDS}"
+./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --tsv-no-header \
+  --query "histogram_quantile(0.99, sum by(le) (rate(tempo_spanmetrics_latency_bucket[5m])))" --time-ranges "1H 6H 24H 2d" --shards "${SHARDS}"
+
 
 # Analytics.
-#./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query 'count({__name__=~".+"})' --time-ranges "5M" --step "5m" --shards "${SHARDS}"
-#./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --query 'topk(10, count by(__name__) ({__name__=~".+"}))' --time-ranges "5M" --step "5m" --shards "${SHARDS}"
+./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --tsv-no-header \
+  --query 'count({__name__=~".+"})' --time-ranges "5M" --step "5m" --shards "${SHARDS}"
+
+./run.sh --url "${URL}" --tenant-id "${TENANT_ID}" --tsv-no-header \
+  --query 'topk(10, count by(__name__) ({__name__=~".+"}))' --time-ranges "5M" --step "5m" --shards "${SHARDS}"
+
