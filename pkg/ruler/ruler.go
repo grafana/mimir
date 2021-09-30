44c862d643e0b36f7a8f3283963d784a58f2ebc4
Tyler Reid <tyler.reid@grafana.com>
2021-09-30 13:25:19 -0500

    Cherrypick of sharding changes from cortex #4466 (#287)
    * Cherrypick of sharding changes from cortex #4466
    
    * add mimir PR number
    
    Signed-off-by: Tyler Reid <tyler.reid@grafana.com>

--- pkg/ruler/ruler.go
+++ pkg/ruler/ruler.go
@@ -652,7 +652,7 @@ func (r *Ruler) GetRules(ctx context.Context) ([]*GroupStateDesc, error) {
 	}
 
 	if r.cfg.EnableSharding {
-		return r.getShardedRules(ctx)
+		return r.getShardedRules(ctx, userID)
 	}
 
 	return r.getLocalRules(userID)
@@ -745,8 +745,14 @@ func (r *Ruler) getLocalRules(userID string) ([]*GroupStateDesc, error) {
 	return groupDescs, nil
 }
 
-func (r *Ruler) getShardedRules(ctx context.Context) ([]*GroupStateDesc, error) {
-	rulers, err := r.ring.GetReplicationSetForOperation(RingOp)
+func (r *Ruler) getShardedRules(ctx context.Context, userID string) ([]*GroupStateDesc, error) {
+	ring := ring.ReadRing(r.ring)
+
+	if shardSize := r.limits.RulerTenantShardSize(userID); shardSize > 0 && r.cfg.ShardingStrategy == util.ShardingStrategyShuffle {
+		ring = r.ring.ShuffleShard(userID, shardSize)
+	}
+
+	rulers, err := ring.GetReplicationSetForOperation(RingOp)
 	if err != nil {
 		return nil, err
 	}
