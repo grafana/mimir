de6a94ed5f5ba218fb124969bbf8f55713ac28b9
Peter Štibraný <peter.stibrany@grafana.com>
2021-10-01 15:59:56 +0200

    Don't use block index. Show split count.

---
+++ tools/listblocks/README.md
@@ -0,0 +1,1 @@
+Listblocks can list blocks from bucket for a specific user. It can optionally use bucket index.
--- tools/listblocks/main.go
+++ tools/listblocks/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -15,10 +16,12 @@ import (
 	"time"
 
 	gokitlog "github.com/go-kit/kit/log"
+	"github.com/grafana/dskit/concurrency"
 	"github.com/oklog/ulid"
 	"github.com/prometheus/prometheus/pkg/labels"
+	"github.com/thanos-io/thanos/pkg/block"
 	"github.com/thanos-io/thanos/pkg/block/metadata"
-	"golang.org/x/sync/errgroup"
+	"github.com/thanos-io/thanos/pkg/objstore"
 
 	"github.com/grafana/mimir/pkg/storage/bucket"
 	"github.com/grafana/mimir/pkg/storage/tsdb"
@@ -33,6 +36,7 @@ func main() {
 		showDeleted      bool
 		showLabels       bool
 		showCreationTime bool
+		splitCount       int
 	}{}
 
 	cfg.bucket.RegisterFlags(flag.CommandLine)
@@ -40,6 +44,7 @@ func main() {
 	flag.BoolVar(&cfg.showDeleted, "show-deleted", false, "Show deleted blocks")
 	flag.BoolVar(&cfg.showLabels, "show-labels", false, "Show block labels")
 	flag.BoolVar(&cfg.showCreationTime, "show-creation-time", false, "Show when block was created (from ULID)")
+	flag.IntVar(&cfg.splitCount, "split-count", 0, "It not 0, shows split number that would be used for grouping blocks during split compaction")
 	flag.Parse()
 
 	if cfg.userID == "" {
@@ -55,103 +60,176 @@ func main() {
 		log.Fatalln("failed to create bucket:", err)
 	}
 
-	bi, err := bucketindex.ReadIndex(ctx, bkt, cfg.userID, nil, logger)
+	metas, deletedTimes, err := loadMetaFilesAndDeletionMarkers(ctx, bkt, cfg.userID, cfg.showDeleted)
 	if err != nil {
-		log.Fatalln("failed to read bucket index:", err)
+		log.Fatalln("failed to read block metadata:", err)
 	}
 
-	deletedTimes := map[ulid.ULID]time.Time{}
-	for _, dm := range bi.BlockDeletionMarks {
-		deletedTimes[dm.ID] = time.Unix(dm.DeletionTime, 0)
+	printMetas(metas, deletedTimes, cfg.showCreationTime, cfg.showDeleted, cfg.showLabels, cfg.splitCount)
+}
+
+func loadMetaFilesAndDeletionMarkers(ctx context.Context, bkt objstore.Bucket, user string, showDeleted bool) (map[ulid.ULID]*metadata.Meta, map[ulid.ULID]time.Time, error) {
+	deletedBlocks := map[ulid.ULID]bool{}
+	deletionMarkerFiles := []string(nil)
+
+	// Find blocks marked for deletion
+	err := bkt.Iter(ctx, path.Join(user, bucketindex.MarkersPathname), func(s string) error {
+		if id, ok := bucketindex.IsBlockDeletionMarkFilename(path.Base(s)); ok {
+			deletedBlocks[id] = true
+			deletionMarkerFiles = append(deletionMarkerFiles, s)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, nil, err
 	}
 
-	metasSync := sync.Mutex{}
-	metas := map[ulid.ULID]*metadata.Meta{}
+	metaPaths := []string(nil)
+	err = bkt.Iter(ctx, user, func(s string) error {
+		if id, ok := block.IsBlockDir(s); ok {
+			if !showDeleted && deletedBlocks[id] {
+				return nil
+			}
+			metaPaths = append(metaPaths, path.Join(s, "meta.json"))
+		}
+		return nil
+	})
 
-	// We only need metas when showing labels.
-	if cfg.showLabels {
-		g, gctx := errgroup.WithContext(ctx)
-		for _, b := range bi.Blocks {
-			if !cfg.showDeleted && !deletedTimes[b.ID].IsZero() {
-				continue
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var deletionTimes map[ulid.ULID]time.Time
+	if showDeleted {
+		deletionTimes, err = fetchDeletionTimes(ctx, bkt, deletionMarkerFiles)
+	}
+
+	metas, err := fetchMetas(ctx, bkt, metaPaths)
+	return metas, deletionTimes, err
+}
+
+const concurrencyLimit = 32
+
+func fetchDeletionTimes(ctx context.Context, bkt objstore.Bucket, deletionMarkers []string) (map[ulid.ULID]time.Time, error) {
+	mu := sync.Mutex{}
+	times := map[ulid.ULID]time.Time{}
+
+	return times, concurrency.ForEach(ctx, concurrency.CreateJobsFromStrings(deletionMarkers), concurrencyLimit, func(ctx context.Context, job interface{}) error {
+		r, err := bkt.Get(ctx, job.(string))
+		if err != nil {
+			if bkt.IsObjNotFoundErr(err) {
+				return nil
 			}
 
-			b := b
-			g.Go(func() error {
-				r, err := bkt.Get(gctx, path.Join(cfg.userID, b.ID.String(), "meta.json"))
-				if err != nil {
-					return err
-				}
+			return err
+		}
+		defer r.Close()
 
-				m, err := metadata.Read(r)
-				if err != nil {
-					if bkt.IsObjNotFoundErr(err) {
-						return nil
-					}
+		dec := json.NewDecoder(r)
 
-					return err
-				}
+		m := metadata.DeletionMark{}
+		if err := dec.Decode(&m); err != nil {
+			return err
+		}
 
-				metasSync.Lock()
-				defer metasSync.Unlock()
+		mu.Lock()
+		times[m.ID] = time.Unix(m.DeletionTime, 0)
+		mu.Unlock()
 
-				metas[b.ID] = m
+		return nil
+	})
+}
+
+func fetchMetas(ctx context.Context, bkt objstore.Bucket, metaFiles []string) (map[ulid.ULID]*metadata.Meta, error) {
+	mu := sync.Mutex{}
+	metas := map[ulid.ULID]*metadata.Meta{}
+
+	return metas, concurrency.ForEach(ctx, concurrency.CreateJobsFromStrings(metaFiles), concurrencyLimit, func(ctx context.Context, job interface{}) error {
+		r, err := bkt.Get(ctx, job.(string))
+		if err != nil {
+			if bkt.IsObjNotFoundErr(err) {
 				return nil
-			})
+			}
+
+			return err
+		}
+		defer r.Close()
+
+		m, err := metadata.Read(r)
+		if err != nil {
+			return err
 		}
 
-		if err := g.Wait(); err != nil {
-			log.Println("failed to fetch metas:", err)
-		}
+		mu.Lock()
+		metas[m.ULID] = m
+		mu.Unlock()
+
+		return nil
+	})
+}
+
+// nolint:errcheck
+//goland:noinspection GoUnhandledErrorResult
+func printMetas(metas map[ulid.ULID]*metadata.Meta, deletedTimes map[ulid.ULID]time.Time, showCreationTime, showDeleted, showLabels bool, splitCount int) {
+	var blocks []*metadata.Meta
+
+	for _, b := range metas {
+		blocks = append(blocks, b)
 	}
+
+	sort.Slice(blocks, func(i, j int) bool {
+		return blocks[i].MinTime < blocks[j].MinTime
+	})
 
 	tabber := tabwriter.NewWriter(os.Stdout, 1, 4, 3, ' ', 0)
 	defer tabber.Flush()
 
 	// Header
 	fmt.Fprintf(tabber, "Block ID\t")
-	if cfg.showCreationTime {
+	if splitCount > 0 {
+		fmt.Fprintf(tabber, "Split ID\t")
+	}
+	if showCreationTime {
 		fmt.Fprintf(tabber, "Created\t")
 	}
 	fmt.Fprintf(tabber, "Min Time\t")
 	fmt.Fprintf(tabber, "Max Time\t")
 	fmt.Fprintf(tabber, "Duration\t")
-	if cfg.showDeleted {
+	if showDeleted {
 		fmt.Fprintf(tabber, "Deletion Time\t")
 	}
-	if cfg.showLabels {
+	if showLabels {
 		fmt.Fprintf(tabber, "Labels (excl. "+tsdb.TenantIDExternalLabel+")\t")
 	}
 	fmt.Fprintln(tabber)
 
-	sort.Slice(bi.Blocks, func(i, j int) bool {
-		return bi.Blocks[i].MinTime < bi.Blocks[j].MinTime
-	})
-
-	for _, b := range bi.Blocks {
-		if !cfg.showDeleted && !deletedTimes[b.ID].IsZero() {
+	for _, b := range blocks {
+		if !showDeleted && !deletedTimes[b.ULID].IsZero() {
 			continue
 		}
 
-		fmt.Fprintf(tabber, "%v\t", b.ID)
-		if cfg.showCreationTime {
-			fmt.Fprintf(tabber, "%v\t", util.TimeFromMillis(int64(b.ID.Time())).UTC().Format(time.RFC3339))
+		fmt.Fprintf(tabber, "%v\t", b.ULID)
+		if splitCount > 0 {
+			fmt.Fprintf(tabber, "%d\t", tsdb.HashBlockID(b.ULID)%uint32(splitCount))
+		}
+		if showCreationTime {
+			fmt.Fprintf(tabber, "%v\t", util.TimeFromMillis(int64(b.ULID.Time())).UTC().Format(time.RFC3339))
 		}
 		fmt.Fprintf(tabber, "%v\t", util.TimeFromMillis(b.MinTime).UTC().Format(time.RFC3339))
 		fmt.Fprintf(tabber, "%v\t", util.TimeFromMillis(b.MaxTime).UTC().Format(time.RFC3339))
 		fmt.Fprintf(tabber, "%v\t", util.TimeFromMillis(b.MaxTime).Sub(util.TimeFromMillis(b.MinTime)))
 
-		if cfg.showDeleted {
-			if deletedTimes[b.ID].IsZero() {
+		if showDeleted {
+			if deletedTimes[b.ULID].IsZero() {
 				fmt.Fprintf(tabber, "\t") // no deletion time.
 			} else {
-				fmt.Fprintf(tabber, "%v\t", deletedTimes[b.ID].UTC().Format(time.RFC3339))
+				fmt.Fprintf(tabber, "%v\t", deletedTimes[b.ULID].UTC().Format(time.RFC3339))
 			}
 		}
 
-		if cfg.showLabels {
-			if m := metas[b.ID]; m != nil {
-				fmt.Fprintf(tabber, "%s\t", labels.FromMap(m.Thanos.Labels).WithoutLabels(tsdb.TenantIDExternalLabel))
+		if showLabels {
+			if m := b.Thanos.Labels; m != nil {
+				fmt.Fprintf(tabber, "%s\t", labels.FromMap(b.Thanos.Labels).WithoutLabels(tsdb.TenantIDExternalLabel))
 			} else {
 				fmt.Fprintf(tabber, "\t")
 			}
