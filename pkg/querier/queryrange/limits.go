182c73b2d01527085abbd18eff6affe9bd922e62
Cyril Tovena <cyril.tovena@gmail.com>
2021-10-11 16:18:18 +0200

    Correctly apply parallelism limits when doing querysharding. (#253)
    * Correctly apply parallelism limits when doing querysharding.
    
    Previously we were only splitting by day/time in the frontend, so applying the max parallelism was easy.
    Now we can also apply parallelism at the querysharding level, and this means we can easily by-pass the `MaxQueryParallelism` limits.
    
    This PR apply the limits at a lower level and so fixes the problem of overscheduling per query which we currently have since querysharding is activable.
    
    This is inspired by work we've done in Loki.
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>
    
    * Update pkg/querier/queryrange/limits.go
    
    Co-authored-by: Christian Simon <simon@swine.de>
    
    * Update pkg/querier/queryrange/limits_test.go
    
    Co-authored-by: Christian Simon <simon@swine.de>
    
    * Checks for error in tests.
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>
    
    * Improve concurrency handling.
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>
    
    * Added unit test on context cancellation
    
    Signed-off-by: Marco Pracucci <marco@pracucci.com>
    
    * Simplify the code.
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>
    
    Co-authored-by: Christian Simon <simon@swine.de>
    Co-authored-by: Marco Pracucci <marco@pracucci.com>

--- pkg/querier/queryrange/limits.go
+++ pkg/querier/queryrange/limits.go
@@ -8,9 +8,11 @@ package queryrange
 import (
 	"context"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/go-kit/kit/log/level"
+	"github.com/opentracing/opentracing-go"
 	"github.com/prometheus/prometheus/pkg/timestamp"
 	"github.com/weaveworks/common/httpgrpc"
 
@@ -102,4 +104,117 @@ func (l limitsMiddleware) Do(ctx context.Context, r Request) (Response, error) {
 	}
 
 	return l.next.Do(ctx, r)
+}
+
+type limitedRoundTripper struct {
+	downstream Handler
+	limits     Limits
+
+	codec      Codec
+	middleware Middleware
+}
+
+// NewLimitedRoundTripper creates a new roundtripper that enforces MaxQueryParallelism to the `next` roundtripper across `middlewares`.
+func NewLimitedRoundTripper(next http.RoundTripper, codec Codec, limits Limits, middlewares ...Middleware) http.RoundTripper {
+	transport := limitedRoundTripper{
+		downstream: roundTripperHandler{
+			next:  next,
+			codec: codec,
+		},
+		codec:      codec,
+		limits:     limits,
+		middleware: MergeMiddlewares(middlewares...),
+	}
+	return transport
+}
+
+type subRequest struct {
+	req    Request
+	ctx    context.Context
+	result chan result
+}
+
+type result struct {
+	response Response
+	err      error
+}
+
+func newSubRequest(ctx context.Context, req Request) subRequest {
+	return subRequest{
+		req:    req,
+		ctx:    ctx,
+		result: make(chan result, 1),
+	}
+}
+
+func (rt limitedRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	var (
+		wg           sync.WaitGroup
+		intermediate = make(chan subRequest)
+		ctx, cancel  = context.WithCancel(r.Context())
+	)
+	defer func() {
+		cancel()
+		wg.Wait()
+	}()
+
+	request, err := rt.codec.DecodeRequest(ctx, r)
+	if err != nil {
+		return nil, err
+	}
+
+	if span := opentracing.SpanFromContext(ctx); span != nil {
+		request.LogToSpan(span)
+	}
+
+	tenantIDs, err := tenant.TenantIDs(ctx)
+	if err != nil {
+		return nil, httpgrpc.Errorf(http.StatusBadRequest, err.Error())
+	}
+
+	// Creates workers that will process the sub-requests in parallel for this query.
+	// The amount of workers is limited by the MaxQueryParallelism tenant setting.
+	parallelism := validation.SmallestPositiveIntPerTenant(tenantIDs, rt.limits.MaxQueryParallelism)
+	for i := 0; i < parallelism; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				select {
+				case w := <-intermediate:
+					resp, err := rt.downstream.Do(w.ctx, w.req)
+					w.result <- result{response: resp, err: err}
+				case <-ctx.Done():
+					return
+				}
+			}
+		}()
+	}
+
+	// Wraps middlewares with a final handler, which will receive requests in
+	// parallel from upstream handlers. Then each requests gets scheduled to a
+	// different worker via the `intermediate` channel, so the maximum
+	// parallelism is limited. This worker will then call `Do` on the resulting
+	// handler.
+	response, err := rt.middleware.Wrap(
+		HandlerFunc(func(ctx context.Context, r Request) (Response, error) {
+			s := newSubRequest(ctx, r)
+			select {
+			case intermediate <- s:
+			case <-ctx.Done():
+				return nil, ctx.Err()
+			}
+
+			select {
+			case response := <-s.result:
+				return response.response, response.err
+			case <-ctx.Done():
+				return nil, ctx.Err()
+			}
+		})).Do(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return rt.codec.EncodeResponse(ctx, response)
 }
