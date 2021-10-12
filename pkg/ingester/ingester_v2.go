b59e96c9482b6bc661923b3519f99f4459389959
Marco Pracucci <marco@pracucci.com>
2021-10-12 14:59:00 +0200

    Revert "Revert "Distributor Push: re-use decompression buffers in a pool (#217)" (#341)"
    This reverts commit 9391ea4c8f205a5805ae38acc9b5cb84ee38df6e.

--- pkg/ingester/ingester_v2.go
+++ pkg/ingester/ingester_v2.go
@@ -775,8 +775,7 @@ func (i *Ingester) v2Push(ctx context.Context, req *mimirpb.WriteRequest) (*mimi
 	var firstPartialErr error
 
 	// NOTE: because we use `unsafe` in deserialisation, we must not
-	// retain anything from `req` past the call to ReuseSlice
-	defer mimirpb.ReuseSlice(req.Timeseries)
+	// retain anything from `req` past the exit from this function.
 
 	userID, err := tenant.TenantID(ctx)
 	if err != nil {
