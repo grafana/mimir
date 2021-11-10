b7c0ddb716da4f8268e113d3cc958f6c6b05730d
Marco Pracucci <marco@pracucci.com>
2021-11-10 11:10:28 +0100

    Added tsdb-index-toc tool
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

---
+++ tools/tsdb-index-toc/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/prometheus/prometheus/tsdb/fileutil"
+	"github.com/prometheus/prometheus/tsdb/index"
+)
+
+func main() {
+	flag.Parse()
+
+	filepath := flag.Arg(0)
+
+	f, err := fileutil.OpenMmapFile(filepath)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	toc, err := index.NewTOCFromByteSlice(realByteSlice(f.Bytes()))
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Println("Symbols table size:   ", toc.Series-toc.Symbols)
+	fmt.Println("Series size:          ", toc.LabelIndices-toc.Series)
+	fmt.Println("Label indices:        ", toc.Postings-toc.LabelIndices)
+	fmt.Println("Postings:             ", toc.LabelIndicesTable-toc.Postings)
+	fmt.Println("Label offset table:   ", toc.PostingsTable-toc.LabelIndicesTable)
+
+	// Requires the full index to be correct.
+	if uint64(len(f.Bytes())) > toc.PostingsTable {
+		fmt.Println("Postings offset table:", uint64(len(f.Bytes()))-toc.PostingsTable)
+	} else {
+		fmt.Println("Postings offset table: N/A")
+	}
+}
+
+type realByteSlice []byte
+
+func (b realByteSlice) Len() int {
+	return len(b)
+}
+
+func (b realByteSlice) Range(start, end int) []byte {
+	return b[start:end]
+}
