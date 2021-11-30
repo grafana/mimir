cc9a531d3b6830ebf51a75b952c0a7533dc195b5
Peter Štibraný <pstibrany@gmail.com>
2021-11-30 18:58:34 +0100

    Fix compilation error.
    Signed-off-by: Peter Štibraný <pstibrany@gmail.com>

--- pkg/compactor/bucket_compactor.go
+++ pkg/compactor/bucket_compactor.go
@@ -18,6 +18,7 @@ import (
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
+	"github.com/grafana/dskit/concurrency"
 	"github.com/oklog/ulid"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
