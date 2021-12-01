254fb2765c9da72ef01303a3dcb1eae445a779de
Peter Štibraný <pstibrany@gmail.com>
2021-12-01 11:58:25 +0100

    Added tool for listing split-and-merge compactor jobs based on blocks in bucket-index.
    Signed-off-by: Peter Štibraný <pstibrany@gmail.com>

--- pkg/compactor/job_sorting.go
+++ pkg/compactor/job_sorting.go
@@ -8,10 +8,10 @@ import (
 
 type jobsOrderFunc func(jobs []*Job) []*Job
 
-// sortJobsBySmallestRangeOldestBlocksFirst returns input jobs sorted by smallest range, oldest min time first.
+// SortJobsBySmallestRangeOldestBlocksFirst returns input jobs sorted by smallest range, oldest min time first.
 // The rationale of this sorting is that we may want to favor smaller ranges first (ie. to deduplicate samples
 // sooner than later) and older ones are more likely to be "complete" (no missing block still to be uploaded).
-func sortJobsBySmallestRangeOldestBlocksFirst(jobs []*Job) []*Job {
+func SortJobsBySmallestRangeOldestBlocksFirst(jobs []*Job) []*Job {
 	sort.SliceStable(jobs, func(i, j int) bool {
 		iLength := jobs[i].MaxTime() - jobs[i].MinTime()
 		jLength := jobs[j].MaxTime() - jobs[j].MinTime()
@@ -30,11 +30,11 @@ func sortJobsBySmallestRangeOldestBlocksFirst(jobs []*Job) []*Job {
 	return jobs
 }
 
-// sortJobsByNewestBlocksFirst returns input jobs sorted by most recent time ranges first
+// SortJobsByNewestBlocksFirst returns input jobs sorted by most recent time ranges first
 // (regardless of their compaction level). The rationale of this sorting is that in case the
 // compactor is lagging behind, we compact up to the largest range (eg. 24h) the most recent
 // blocks first and the move to older ones. Most recent blocks are the one more likely to be queried.
-func sortJobsByNewestBlocksFirst(jobs []*Job) []*Job {
+func SortJobsByNewestBlocksFirst(jobs []*Job) []*Job {
 	sort.SliceStable(jobs, func(i, j int) bool {
 		iMaxTime := jobs[i].MaxTime()
 		jMaxTime := jobs[j].MaxTime()
