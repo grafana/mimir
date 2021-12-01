a4950322f15b707f6cae09e1e215fc37bcebaf86
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2021-12-01 13:38:39 +0100

    Downgrade minio and update healthcheck EP
    Minio changed the semantics of the /ready endpoint, so we have to use
    /cluster to get write readiness. The cluster write readiness
    endpoint changed behaviour some time between feb and august this year
    (I think unintentionally, but don't have more time to spend on this),
    that's why the update is not to the latest image.

--- integration/e2e/images/images.go
+++ integration/e2e/images/images.go
@@ -12,7 +12,7 @@ package images
 
 var (
 	Memcached        = "memcached:1.6.12"
-	Minio            = "minio/minio:RELEASE.2021-11-09T03-21-45Z"
+	Minio            = "minio/minio:RELEASE.2021-02-19T04-38-02Z"
 	Consul           = "consul:1.8.15"
 	ETCD             = "gcr.io/etcd-development/etcd:v3.4.13"
 	DynamoDB         = "amazon/dynamodb-local:1.17.0"
