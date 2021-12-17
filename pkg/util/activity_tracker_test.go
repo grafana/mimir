2f60014324b92016b477a088dbffa82b89d043ec
Peter Štibraný <pstibrany@gmail.com>
2021-12-17 09:55:43 +0100

    Added activity tracker.
    Signed-off-by: Peter Štibraný <pstibrany@gmail.com>

---
+++ pkg/util/activity_tracker_test.go
@@ -0,0 +1,70 @@
+package util
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestActivityTracker(t *testing.T) {
+	file := filepath.Join(t.TempDir(), "activity")
+
+	const maxEntries = 5
+	tr, err := NewActivityTracker(file, 5, nil)
+	require.NoError(t, err)
+
+	defer func() {
+		require.NoError(t, tr.Close())
+	}()
+
+	var insertedActivities []string
+	for i := 0; i < maxEntries; i++ {
+		a := fmt.Sprintf("activity-%d", i)
+		aix := tr.InsertStatic(a)
+		require.True(t, aix >= 0)
+		insertedActivities = append(insertedActivities, a)
+	}
+
+	require.Equal(t, 0.0, testutil.ToFloat64(tr.failedInserts))
+
+	aix := tr.InsertStatic("extra")
+	require.True(t, aix < 0)
+
+	require.Equal(t, 1.0, testutil.ToFloat64(tr.failedInserts))
+
+	require.NoError(t, tr.flush())
+
+	activities := LoadUnfinishedEntries(file)
+	require.ElementsMatch(t, activities, insertedActivities)
+
+	for i := 0; i < maxEntries; i++ {
+		tr.Delete(i)
+		require.NoError(t, tr.flush())
+
+		insertedActivities = insertedActivities[1:]
+
+		require.ElementsMatch(t, LoadUnfinishedEntries(file), insertedActivities)
+	}
+}
+
+func BenchmarkName(b *testing.B) {
+	file := filepath.Join(b.TempDir(), "activity")
+
+	const maxEntries = 100
+	tr, err := NewActivityTracker(file, maxEntries, nil)
+	require.NoError(b, err)
+
+	defer func() {
+		require.NoError(b, tr.Close())
+	}()
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		ix := tr.InsertStatic("test")
+		tr.Delete(ix)
+	}
+}
