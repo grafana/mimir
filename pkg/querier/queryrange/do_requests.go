d48a93b7df34a4f02a526f2d9f20ea417d53b938
Oleg Zaytsev <mail@olegzaytsev.com>
2021-12-20 17:25:44 +0100

    Remove limiting logic from queryrange.DoRequests
    There's already limiting logic in the limitedRoundTripper that wraps all
    the tripperwares instantiated by queryrange, so there's no need to add
    the limiting twice.
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

---
+++ pkg/querier/queryrange/do_requests.go
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// Provenance-includes-location: https://github.com/cortexproject/cortex/blob/master/pkg/querier/queryrange/util.go
+// Provenance-includes-license: Apache-2.0
+// Provenance-includes-copyright: The Cortex Authors.
+
+package queryrange
+
+import (
+	"context"
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+
+	"github.com/opentracing/opentracing-go"
+)
+
+// RequestResponse contains a request response and the respective request that was used.
+type RequestResponse struct {
+	Request  Request
+	Response Response
+}
+
+// DoRequests executes a list of requests in parallel. The limits parameters is used to limit parallelism per single request.
+func DoRequests(ctx context.Context, downstream Handler, reqs []Request, recordSpan bool) ([]RequestResponse, error) {
+	g, ctx := errgroup.WithContext(ctx)
+	mtx := sync.Mutex{}
+	resps := make([]RequestResponse, 0, len(reqs))
+	for i := 0; i < len(reqs); i++ {
+		req := reqs[i]
+		g.Go(func() error {
+			var (
+				span     opentracing.Span
+				childCtx = ctx
+			)
+
+			if recordSpan {
+				span, childCtx = opentracing.StartSpanFromContext(ctx, "DoRequests")
+				req.LogToSpan(span)
+			}
+
+			resp, err := downstream.Do(childCtx, req)
+
+			if span != nil {
+				span.Finish()
+			}
+			if err != nil {
+				return err
+			}
+
+			mtx.Lock()
+			resps = append(resps, RequestResponse{req, resp})
+			mtx.Unlock()
+			return nil
+		})
+	}
+
+	return resps, g.Wait()
+}
