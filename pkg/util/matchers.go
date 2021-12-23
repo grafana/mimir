4264eb17e3402e7b68deebebaee95787c5dcba21
Oleg Zaytsev <mail@olegzaytsev.com>
2021-12-23 17:23:31 +0100

    Create util.MergeMatchers to merge matchers
    NotEqual and NotRegexp matchers for a given label will be merged into
    one to optimise querying.
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

--- pkg/util/matchers.go
+++ pkg/util/matchers.go
@@ -6,8 +6,13 @@
 package util
 
 import (
+	"fmt"
+	"regexp"
+	"sort"
 	"strings"
 
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/prometheus/prometheus/model/labels"
 )
 
@@ -60,3 +65,107 @@ func (s MatchersStringer) String() string {
 
 	return b.String()
 }
+
+// MergeMatchers merges matchers to optimise querying.
+func MergeMatchers(input []*labels.Matcher, logger log.Logger) []*labels.Matcher {
+	if len(input) == 0 {
+		return nil
+	}
+	ms := make([]*labels.Matcher, len(input))
+	copy(ms, input)
+	sort.Sort(sortedLabelMatchers(ms))
+
+	for i := 0; i < len(ms); i++ {
+		j := i
+		for j+1 < len(ms) && ms[i].Name == ms[j+1].Name {
+			j++
+		}
+		if j == i {
+			continue
+		}
+		span := j + 1 - i
+		merged, err := mergeSortedMatchersForSameLabelSpan(ms[i : i+span])
+		if err != nil {
+			level.Warn(logger).Log("msg", "can't merge matchers", "input", MatchersStringer(input), "err", err)
+			return input
+		}
+		if merged < span {
+			copy(ms[i+merged:], ms[i+span:])
+			shrinked := span - merged
+			ms = ms[:len(ms)-shrinked]
+		}
+		i += merged - 1
+	}
+
+	return ms
+}
+
+// mergeSortedMatchersForSameLabelSpan merges matchers in a span of matchers for same label.
+// This function expects matchers to be sorted as Equal, Regexp, NotEqual, NotRegexp.
+// The result integer is the length of the merged span, which has been replaced on the same slice.
+func mergeSortedMatchersForSameLabelSpan(span []*labels.Matcher) (int, error) {
+	out := 0
+	// Rewind trhough MatchEqual labels.
+	for out < len(span) && (span[out].Type == labels.MatchEqual || span[out].Type == labels.MatchRegexp) {
+		out++
+	}
+	// If none or only one left, return.
+	if len(span)-out <= 1 {
+		return len(span), nil
+	}
+
+	// Merge the rest of NotEqual and NotRegexp matchers into one.
+	sb := strings.Builder{}
+	seen := map[string]bool{}
+	for i := out; i < len(span); i++ {
+		m := span[i]
+		var val string
+		if m.Type == labels.MatchNotEqual {
+			if m.Value == "" {
+				val = ".+"
+			} else {
+				val = regexp.QuoteMeta(m.Value)
+			}
+		} else if m.Type == labels.MatchNotRegexp {
+			val = m.Value
+		} else {
+			return 0, fmt.Errorf("unsupported matcher type %d %q", m.Type, m.Type)
+		}
+
+		if !seen[val] {
+			if sb.Len() > 0 {
+				sb.WriteByte('|')
+			}
+			sb.WriteString(val)
+			seen[val] = true
+		}
+	}
+	m, err := labels.NewMatcher(labels.MatchNotRegexp, span[out].Name, sb.String())
+	if err != nil {
+		return 0, err
+	}
+	span[out] = m
+	return out + 1, nil
+}
+
+type sortedLabelMatchers []*labels.Matcher
+
+var matchersTypeOrder = [...]int{
+	labels.MatchEqual:     0,
+	labels.MatchRegexp:    1,
+	labels.MatchNotEqual:  2,
+	labels.MatchNotRegexp: 3,
+}
+
+func (c sortedLabelMatchers) Less(i, j int) bool {
+	if c[i].Name != c[j].Name {
+		return c[i].Name < c[j].Name
+	}
+	if c[i].Type != c[j].Type {
+		return matchersTypeOrder[c[i].Type] < matchersTypeOrder[c[j].Type]
+	}
+	return c[i].Value < c[j].Value
+}
+
+func (c sortedLabelMatchers) Len() int      { return len(c) }
+func (c sortedLabelMatchers) Swap(i, j int) { c[i], c[j] = c[j], c[i] }
--- pkg/util/matchers_test.go
+++ pkg/util/matchers_test.go
@@ -5,6 +5,7 @@ package util
 import (
 	"testing"
 
+	"github.com/go-kit/log"
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -72,4 +73,152 @@ func TestMatchersStringer(t *testing.T) {
 
 		assert.Equal(t, `name1="value1",name2="value2"`, got)
 	})
+}
+
+func TestMergeMatchers(t *testing.T) {
+	for _, tc := range []struct {
+		name     string
+		input    []*labels.Matcher
+		expected []*labels.Matcher
+	}{
+		{
+			name:     "empty input",
+			input:    nil,
+			expected: nil,
+		},
+		{
+			name: "one equals matcher",
+			input: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchEqual, "foo", "x"),
+			},
+			expected: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchEqual, "foo", "x"),
+			},
+		},
+		{
+			name: "one not equals matcher",
+			input: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchNotEqual, "foo", "x"),
+			},
+			expected: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchNotEqual, "foo", "x"),
+			},
+		},
+		{
+			name: "one not regexp matcher",
+			input: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchNotRegexp, "foo", "x.+"),
+			},
+			expected: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchNotRegexp, "foo", "x.+"),
+			},
+		},
+		{
+			name: "matchers for different names",
+			input: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchEqual, "foo", "x"),
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+			},
+			expected: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+				labels.MustNewMatcher(labels.MatchEqual, "foo", "x"),
+			},
+		},
+		{
+			name: "matchers for same label that are not merged",
+			input: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchEqual, "foo", "x"),
+				labels.MustNewMatcher(labels.MatchNotEqual, "foo", "y"),
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+			},
+			expected: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+				labels.MustNewMatcher(labels.MatchEqual, "foo", "x"),
+				labels.MustNewMatcher(labels.MatchNotEqual, "foo", "y"),
+			},
+		},
+		{
+			name: "merged not equal matchers for same label",
+			input: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchNotEqual, "foo", "x"),
+				labels.MustNewMatcher(labels.MatchNotEqual, "foo", "y"),
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+			},
+			expected: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+				labels.MustNewMatcher(labels.MatchNotRegexp, "foo", "x|y"),
+			},
+		},
+		{
+			name: "merged escaped not equal matchers for two different labels",
+			input: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchEqual, "moo", ""),
+				labels.MustNewMatcher(labels.MatchNotEqual, "foo", "x"),
+				labels.MustNewMatcher(labels.MatchNotEqual, "foo", "y"),
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+				labels.MustNewMatcher(labels.MatchNotEqual, "baz", ".*"),
+				labels.MustNewMatcher(labels.MatchNotEqual, "baz", "[1-2]{2}"),
+			},
+			expected: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+				labels.MustNewMatcher(labels.MatchNotRegexp, "baz", `\.\*|\[1-2\]\{2\}`),
+				labels.MustNewMatcher(labels.MatchNotRegexp, "foo", "x|y"),
+				labels.MustNewMatcher(labels.MatchEqual, "moo", ""),
+			},
+		},
+		{
+			name: "merged not regexp matchers for same label",
+			input: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchNotRegexp, "foo", "x.+"),
+				labels.MustNewMatcher(labels.MatchNotRegexp, "foo", ".+y"),
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+			},
+			expected: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+				labels.MustNewMatcher(labels.MatchNotRegexp, "foo", ".+y|x.+"),
+			},
+		},
+		{
+			name: "merged not regexp with not equal",
+			input: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchNotRegexp, "foo", "x.+"),
+				labels.MustNewMatcher(labels.MatchNotEqual, "foo", "|"),
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+			},
+			expected: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+				labels.MustNewMatcher(labels.MatchNotRegexp, "foo", `\||x.+`),
+			},
+		},
+		{
+			name: "merged not regexp with not equal slash",
+			input: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchNotRegexp, "foo", "x.+"),
+				labels.MustNewMatcher(labels.MatchNotEqual, "foo", `\`),
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+			},
+			expected: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+				labels.MustNewMatcher(labels.MatchNotRegexp, "foo", `\\|x.+`),
+			},
+		},
+		{
+			name: "merged not empty label with regexp not empty label deduplicates condition",
+			input: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchNotRegexp, "foo", ".+"),
+				labels.MustNewMatcher(labels.MatchNotEqual, "foo", ""),
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+			},
+			expected: []*labels.Matcher{
+				labels.MustNewMatcher(labels.MatchEqual, "bar", "1"),
+				labels.MustNewMatcher(labels.MatchNotRegexp, "foo", `.+`),
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			got := MergeMatchers(tc.input, log.NewNopLogger())
+			// We use MatcherStringer to compare as its easier to debug the test than seeing slices of references to label matchers.
+			require.Equal(t, MatchersStringer(tc.expected).String(), MatchersStringer(got).String())
+		})
+	}
 }
