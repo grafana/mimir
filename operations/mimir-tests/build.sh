cbf2d25434c1a1436c17e98cfe22f708cf6b67e7
Marco Pracucci <marco@pracucci.com>
2021-12-31 09:14:10 +0100

    Improved build.sh script
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- operations/mimir-tests/build.sh
+++ operations/mimir-tests/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # Start from a clean setup.
 rm -rf jsonnet-tests && mkdir jsonnet-tests
@@ -7,10 +8,8 @@ cd jsonnet-tests
 # Initialise the Tanka.
 tk init --k8s=1.18
 
-# Install Mimir jsonnet (so that we get dependencies) and then override it with this branch code.
-jb install github.com/grafana/mimir/operations/mimir@main
-rm -fr ./vendor/mimir
-cp -r ../operations/mimir ./vendor/mimir
+# Install Mimir jsonnet from this branch.
+jb install ../operations/mimir
 
 # Copy tests to dedicated environments and build them.
 export PAGER=cat
@@ -19,13 +18,15 @@ TESTS=$(ls -1 ../operations/mimir-tests/test*.jsonnet)
 for FILEPATH in $TESTS; do
   # Extract the filename (without extension).
   TEST_NAME=$(basename -s '.jsonnet' "$FILEPATH")
-  TEST_NAMES="${TEST_NAMES}${TEST_NAME}\n"
 
   echo "Importing $TEST_NAME"
   mkdir -p "environments/${TEST_NAME}"
   cp "$FILEPATH" "environments/${TEST_NAME}/main.jsonnet"
 
-  echo "Compiling $TEST_NAME"
+  # Copy spec.json from environments/default which is created by tk init.
+  # We just need the default spec.json to get tk compile the environment.
   cp environments/default/spec.json "environments/${TEST_NAME}/spec.json"
+
+  echo "Compiling $TEST_NAME"
   tk show --dangerous-allow-redirect "environments/${TEST_NAME}" > ../operations/mimir-tests/${TEST_NAME}-generated.yaml
 done
