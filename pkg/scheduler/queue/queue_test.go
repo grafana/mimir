649ae45cf61070abfdbe853b489f4e00cabcbe82
Oleg Zaytsev <mail@olegzaytsev.com>
2021-12-31 09:03:49 +0100

    Fix deadlock in disconnecting querier (#667)
    * Fix deadlock in disconnecting querier
    
    There was a race condition between the broadcast triggered by the
    canceled context and the condition waiting: if the broadcast happend
    before the wait, wait never receives any broadcast.
    
    Fixes https://github.com/grafana/mimir/issues/489
    
    The issue was very weird as this is a race between only two instructions
    and no other event (querier disconnecting, request incoming) should
    happen to see it.
    
    This implements a context-aware waiting mechanism for q.cond.Wait()
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>
    
    * Extract a contextCond and test it
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>
    
    * Remove duplicated test
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>
    
    * Update pkg/scheduler/queue/queue_test.go comment
    
    Co-authored-by: Mauro Stettler <mauro.stettler@gmail.com>
    
    Co-authored-by: Mauro Stettler <mauro.stettler@gmail.com>

--- pkg/scheduler/queue/queue_test.go
+++ pkg/scheduler/queue/queue_test.go
@@ -155,3 +155,154 @@ func TestRequestQueue_GetNextRequestForQuerier_ShouldGetRequestAfterReshardingBe
 	// We expect that querier-2 got the request only after querier-1 forget delay is passed.
 	assert.GreaterOrEqual(t, waitTime.Milliseconds(), forgetDelay.Milliseconds())
 }
+
+func TestContextCond(t *testing.T) {
+	t.Run("wait until broadcast", func(t *testing.T) {
+		t.Parallel()
+		mtx := &sync.Mutex{}
+		cond := contextCond{Cond: sync.NewCond(mtx)}
+
+		doneWaiting := make(chan struct{})
+
+		mtx.Lock()
+		go func() {
+			cond.Wait(context.Background())
+			mtx.Unlock()
+			close(doneWaiting)
+		}()
+
+		assertChanNotReceived(t, doneWaiting, 100*time.Millisecond, "cond.Wait returned, but it should not because we did not broadcast yet")
+
+		cond.Broadcast()
+		assertChanReceived(t, doneWaiting, 250*time.Millisecond, "cond.Wait did not return after broadcast")
+	})
+
+	t.Run("wait until context deadline", func(t *testing.T) {
+		t.Parallel()
+		mtx := &sync.Mutex{}
+		cond := contextCond{Cond: sync.NewCond(mtx)}
+		doneWaiting := make(chan struct{})
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		mtx.Lock()
+		go func() {
+			cond.Wait(ctx)
+			mtx.Unlock()
+			close(doneWaiting)
+		}()
+
+		assertChanNotReceived(t, doneWaiting, 100*time.Millisecond, "cond.Wait returned, but it should not because we did not broadcast yet and didn't cancel the context")
+
+		cancel()
+		assertChanReceived(t, doneWaiting, 250*time.Millisecond, "cond.Wait did not return after cancelling the context")
+	})
+
+	t.Run("wait on already canceled context", func(t *testing.T) {
+		// This test represents the racy real world scenario,
+		// we don't know whether it's going to wait before the broadcast triggered by the context cancellation.
+		t.Parallel()
+		mtx := &sync.Mutex{}
+		cond := contextCond{Cond: sync.NewCond(mtx)}
+		doneWaiting := make(chan struct{})
+
+		alreadyCanceledContext, cancel := context.WithCancel(context.Background())
+		cancel()
+
+		mtx.Lock()
+		go func() {
+			cond.Wait(alreadyCanceledContext)
+			mtx.Unlock()
+			close(doneWaiting)
+		}()
+
+		assertChanReceived(t, doneWaiting, 250*time.Millisecond, "cond.Wait did not return after cancelling the context")
+	})
+
+	t.Run("wait on already canceled context, but it takes a while to wait", func(t *testing.T) {
+		t.Parallel()
+		mtx := &sync.Mutex{}
+		cond := contextCond{
+			Cond: sync.NewCond(mtx),
+			testHookBeforeWaiting: func() {
+				// This makes the waiting goroutine so slow that out Wait(ctx) will need to broadcast once it sees it waiting.
+				time.Sleep(250 * time.Millisecond)
+			},
+		}
+		doneWaiting := make(chan struct{})
+
+		alreadyCanceledContext, cancel := context.WithCancel(context.Background())
+		cancel()
+
+		mtx.Lock()
+		go func() {
+			cond.Wait(alreadyCanceledContext)
+			mtx.Unlock()
+			close(doneWaiting)
+		}()
+
+		assertChanReceived(t, doneWaiting, time.Second, "cond.Wait did not return after 500ms")
+	})
+
+	t.Run("lots of goroutines waiting at the same time, none of them misses it's broadcast from cancel", func(t *testing.T) {
+		t.Parallel()
+		mtx := &sync.Mutex{}
+		cond := contextCond{
+			Cond: sync.NewCond(mtx),
+			testHookBeforeWaiting: func() {
+				// Wait just a little bit to create every goroutine
+				time.Sleep(time.Millisecond)
+			},
+		}
+		const goroutines = 100
+
+		doneWaiting := make(chan struct{}, goroutines)
+		release := make(chan struct{})
+
+		ctx, cancel := context.WithCancel(context.Background())
+		cancel()
+
+		for i := 0; i < goroutines; i++ {
+			go func() {
+				<-release
+
+				mtx.Lock()
+				cond.Wait(ctx)
+				mtx.Unlock()
+
+				doneWaiting <- struct{}{}
+			}()
+		}
+		go func() {
+			<-release
+			cancel()
+		}()
+
+		close(release)
+
+		assert.Eventually(t, func() bool {
+			return len(doneWaiting) == goroutines
+		}, time.Second, 10*time.Millisecond)
+	})
+}
+
+func assertChanReceived(t *testing.T, c chan struct{}, timeout time.Duration, msg string, args ...interface{}) {
+	t.Helper()
+
+	select {
+	case <-c:
+	case <-time.After(timeout):
+		t.Fatalf(msg, args...)
+	}
+}
+func assertChanNotReceived(t *testing.T, c chan struct{}, wait time.Duration, msg string, args ...interface{}) {
+	t.Helper()
+
+	select {
+	case <-c:
+		t.Fatalf(msg, args...)
+	case <-time.After(wait):
+		// OK!
+	}
+}
