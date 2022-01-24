9d6a23d13dc497e78813376a37756f8981ab0f06
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2022-01-24 19:54:27 +0100

    Update backward compatibility tests
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- integration/backward_compatibility.go
+++ integration/backward_compatibility.go
@@ -7,6 +7,7 @@ package integration
 var DefaultPreviousVersionImages = map[string]func(map[string]string) map[string]string{
 	"quay.io/cortexproject/cortex:v1.11.0": func(flags map[string]string) map[string]string {
 		flags["-store.engine"] = "blocks"
+		flags["-server.http-listen-port"] = "8080"
 		return flags
 	},
 }
