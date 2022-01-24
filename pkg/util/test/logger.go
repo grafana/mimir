a7f12536f6565327538e1bcc435baf2604f38442
Marco Pracucci <marco@pracucci.com>
2022-01-24 18:21:41 +0100

    Configure logger to investigate TestBlocksCleaner_ShouldRemoveBlocksOutsideRetentionPeriod (#867)
    * Configure logger to investigate TestBlocksCleaner_ShouldRemoveBlocksOutsideRetentionPeriod
    
    Signed-off-by: Marco Pracucci <marco@pracucci.com>
    
    * Fix
    
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

---
+++ pkg/util/test/logger.go
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package test
+
+import (
+	"testing"
+
+	"github.com/go-kit/log"
+)
+
+type testingLogger struct {
+	t *testing.T
+}
+
+func NewTestingLogger(t *testing.T) log.Logger {
+	return &testingLogger{
+		t: t,
+	}
+}
+
+func (l *testingLogger) Log(keyvals ...interface{}) error {
+	l.t.Log(keyvals...)
+	return nil
+}
