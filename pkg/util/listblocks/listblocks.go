c8c088f13c505185e45c3f1de7ef78c41e9ab104
Oleg Zaytsev <mail@olegzaytsev.com>
2022-01-26 21:16:13 +0100

    Extract listblocks business logic to a package
    So we can reuse it in store-gateway.
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

---
+++ pkg/util/listblocks/listblocks.go
@@ -0,0 +1,177 @@
+package listblocks
+
+import (
+	"context"
+	"encoding/json"
+	"path"
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/dustin/go-humanize"
+	"github.com/grafana/dskit/concurrency"
+	"github.com/oklog/ulid"
+	"github.com/thanos-io/thanos/pkg/block"
+	"github.com/thanos-io/thanos/pkg/block/metadata"
+	"github.com/thanos-io/thanos/pkg/objstore"
+
+	"github.com/grafana/mimir/pkg/storage/tsdb"
+	"github.com/grafana/mimir/pkg/storage/tsdb/bucketindex"
+)
+
+func LoadMetaFilesAndDeletionMarkers(ctx context.Context, bkt objstore.BucketReader, user string, showDeleted bool, minTime time.Time) (map[ulid.ULID]*metadata.Meta, map[ulid.ULID]time.Time, error) {
+	deletedBlocks := map[ulid.ULID]bool{}
+	deletionMarkerFiles := []string(nil)
+
+	// Find blocks marked for deletion
+	err := bkt.Iter(ctx, path.Join(user, bucketindex.MarkersPathname), func(s string) error {
+		if id, ok := bucketindex.IsBlockDeletionMarkFilename(path.Base(s)); ok {
+			deletedBlocks[id] = true
+			deletionMarkerFiles = append(deletionMarkerFiles, s)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	metaPaths := []string(nil)
+	err = bkt.Iter(ctx, user, func(s string) error {
+		if id, ok := block.IsBlockDir(s); ok {
+			if !showDeleted && deletedBlocks[id] {
+				return nil
+			}
+
+			// Block's ULID is typically higher than min/max time of the block,
+			// unless somebody was ingesting data with timestamps in the future.
+			if !minTime.IsZero() && ulid.Time(id.Time()).Before(minTime) {
+				return nil
+			}
+
+			metaPaths = append(metaPaths, path.Join(s, "meta.json"))
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var deletionTimes map[ulid.ULID]time.Time
+	if showDeleted {
+		deletionTimes, err = fetchDeletionTimes(ctx, bkt, deletionMarkerFiles)
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	metas, err := fetchMetas(ctx, bkt, metaPaths)
+	return metas, deletionTimes, err
+}
+
+const concurrencyLimit = 32
+
+func fetchDeletionTimes(ctx context.Context, bkt objstore.BucketReader, deletionMarkers []string) (map[ulid.ULID]time.Time, error) {
+	mu := sync.Mutex{}
+	times := map[ulid.ULID]time.Time{}
+
+	return times, concurrency.ForEachJob(ctx, len(deletionMarkers), concurrencyLimit, func(ctx context.Context, idx int) error {
+		r, err := bkt.Get(ctx, deletionMarkers[idx])
+		if err != nil {
+			if bkt.IsObjNotFoundErr(err) {
+				return nil
+			}
+
+			return err
+		}
+		defer r.Close()
+
+		dec := json.NewDecoder(r)
+
+		m := metadata.DeletionMark{}
+		if err := dec.Decode(&m); err != nil {
+			return err
+		}
+
+		mu.Lock()
+		times[m.ID] = time.Unix(m.DeletionTime, 0)
+		mu.Unlock()
+
+		return nil
+	})
+}
+
+func fetchMetas(ctx context.Context, bkt objstore.BucketReader, metaFiles []string) (map[ulid.ULID]*metadata.Meta, error) {
+	mu := sync.Mutex{}
+	metas := map[ulid.ULID]*metadata.Meta{}
+
+	return metas, concurrency.ForEachJob(ctx, len(metaFiles), concurrencyLimit, func(ctx context.Context, idx int) error {
+		r, err := bkt.Get(ctx, metaFiles[idx])
+		if err != nil {
+			if bkt.IsObjNotFoundErr(err) {
+				return nil
+			}
+
+			return err
+		}
+		defer r.Close()
+
+		m, err := metadata.Read(r)
+		if err != nil {
+			return err
+		}
+
+		mu.Lock()
+		metas[m.ULID] = m
+		mu.Unlock()
+
+		return nil
+	})
+}
+
+func SortedBlocks(metas map[ulid.ULID]*metadata.Meta) []*metadata.Meta {
+	var blocks []*metadata.Meta
+
+	for _, b := range metas {
+		blocks = append(blocks, b)
+	}
+
+	sort.Slice(blocks, func(i, j int) bool {
+		// By min-time
+		if blocks[i].MinTime != blocks[j].MinTime {
+			return blocks[i].MinTime < blocks[j].MinTime
+		}
+
+		// Duration
+		duri := blocks[i].MaxTime - blocks[i].MinTime
+		durj := blocks[j].MaxTime - blocks[j].MinTime
+		if duri != durj {
+			return duri < durj
+		}
+
+		// Compactor shard
+		shardi := blocks[i].Thanos.Labels[tsdb.CompactorShardIDExternalLabel]
+		shardj := blocks[j].Thanos.Labels[tsdb.CompactorShardIDExternalLabel]
+
+		if shardi != "" && shardj != "" && shardi != shardj {
+			return shardi < shardj
+		}
+
+		// ULID time.
+		return blocks[i].ULID.Time() < blocks[j].ULID.Time()
+	})
+	return blocks
+}
+
+func GetFormattedBlockSize(b *metadata.Meta) string {
+	if len(b.Thanos.Files) == 0 {
+		return ""
+	}
+
+	size := uint64(0)
+	for _, f := range b.Thanos.Files {
+		size += uint64(f.SizeBytes)
+	}
+
+	return humanize.IBytes(size)
+}
