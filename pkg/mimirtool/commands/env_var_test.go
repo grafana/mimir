4be43240641941c62e7b074ce25aa35594958e79
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2022-01-28 15:22:08 +0100

    Add a test for env_vars.go
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

---
+++ pkg/mimirtool/commands/env_var_test.go
@@ -0,0 +1,40 @@
+package commands
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewEnvVarsWithPrefix(t *testing.T) {
+	testCases := []struct {
+		prefix                 string
+		fieldsShouldMatchRegex string
+	}{
+		{
+			prefix:                 "PREFIX",
+			fieldsShouldMatchRegex: "^PREFIX_[A-Z].+",
+		},
+		{
+			prefix:                 "PREFIX_",
+			fieldsShouldMatchRegex: "^PREFIX_[A-Z].+",
+		},
+		{
+			prefix:                 "",
+			fieldsShouldMatchRegex: "^[A-Z].+",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.prefix, func(t *testing.T) {
+			vars := reflect.ValueOf(NewEnvVarsWithPrefix(tc.prefix))
+			for i := 0; i < vars.NumField(); i++ {
+				t.Run(vars.Type().Field(i).Name, func(t *testing.T) {
+					assert.Regexp(t, tc.fieldsShouldMatchRegex, vars.Field(i).Interface().(string))
+				})
+			}
+		})
+	}
+
+}
