5f0e164dc61400df946f183687d64f46b864c3cd
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2022-01-28 14:08:17 +0100

    Add forgotten UseLegacyRoutes to env_vars.go
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/mimirtool/commands/env_vars.go
+++ pkg/mimirtool/commands/env_vars.go
@@ -1,24 +1,26 @@
 package commands
 
 type EnvVarNames struct {
-	Address     string
-	APIKey      string
-	APIUser     string
-	TenantID    string
-	TLSCAPath   string
-	TLSCertPath string
-	TLSKeyPath  string
+	APIKey          string
+	APIUser         string
+	Address         string
+	TLSCAPath       string
+	TLSCertPath     string
+	TLSKeyPath      string
+	TenantID        string
+	UseLegacyRoutes string
 }
 
 func NewEnvVarsWithPrefix(prefix string) EnvVarNames {
 	const (
-		address     = "ADDRESS"
-		apiKey      = "API_KEY"
-		apiUser     = "API_USER"
-		tenantID    = "TENANT_ID"
-		tlsCAPath   = "TLS_CA_PATH"
-		tlsCertPath = "TLS_CERT_PATH"
-		tlsKeyPath  = "TLS_KEY_PATH"
+		address         = "ADDRESS"
+		apiKey          = "API_KEY"
+		apiUser         = "API_USER"
+		tenantID        = "TENANT_ID"
+		tlsCAPath       = "TLS_CA_PATH"
+		tlsCertPath     = "TLS_CERT_PATH"
+		tlsKeyPath      = "TLS_KEY_PATH"
+		useLegacyRoutes = "USE_LEGACY_ROUTES"
 	)
 
 	if len(prefix) < 1 || prefix[len(prefix)-1] != '_' {
@@ -26,12 +28,13 @@ func NewEnvVarsWithPrefix(prefix string) EnvVarNames {
 	}
 
 	return EnvVarNames{
-		Address:     prefix + address,
-		APIKey:      prefix + apiKey,
-		APIUser:     prefix + apiUser,
-		TenantID:    prefix + tenantID,
-		TLSCAPath:   prefix + tlsCAPath,
-		TLSCertPath: prefix + tlsCertPath,
-		TLSKeyPath:  prefix + tlsKeyPath,
+		APIKey:          prefix + apiKey,
+		APIUser:         prefix + apiUser,
+		Address:         prefix + address,
+		TLSCAPath:       prefix + tlsCAPath,
+		TLSCertPath:     prefix + tlsCertPath,
+		TLSKeyPath:      prefix + tlsKeyPath,
+		TenantID:        prefix + tenantID,
+		UseLegacyRoutes: prefix + useLegacyRoutes,
 	}
 }
