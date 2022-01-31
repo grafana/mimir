ba2106f848afdcb60815e43f224e17562407d4c8
Jack Baldry <jack.baldry@grafana.com>
2022-01-31 17:06:16 +0000

    Add integration test that validates `dev.yaml` configuration
    The test ensures that the process starts successfully and that all of
    the user operations in the _Getting started with Grafana Mimir_ guide
    can be performed. Primarily this is writing and querying back series.
    
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

---
+++ integration/getting_started_with_grafana_mimir_test.go
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+//go:build requires_docker
+// +build requires_docker
+
+package integration
+
+import (
+	"math"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/grafana/e2e"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/prompb"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/mimir/integration/e2emimir"
+)
+
+func TestGettingStartedWithGrafanaMimir(t *testing.T) {
+	s, err := e2e.NewScenario(networkName)
+	require.NoError(t, err)
+	defer s.Close()
+
+	require.NoError(t, copyFileToSharedDir(s, "docs/configurations/dev.yaml", "dev.yaml"))
+
+	mimir := e2emimir.NewSingleBinaryWithConfigFile("mimir", "dev.yaml", nil, "", 9009, 9095)
+	require.NoError(t, s.StartAndWaitReady(mimir))
+
+	c, err := e2emimir.NewClient(mimir.HTTPEndpoint(), mimir.HTTPEndpoint(), "", "", "user-1")
+	require.NoError(t, err)
+
+	// Push some series to Mimir.
+	now := time.Now()
+	series, expectedVector := generateSeries("series_1", now, prompb.Label{Name: "foo", Value: "bar"})
+
+	res, err := c.Push(series)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, res.StatusCode)
+
+	// Query the series.
+	result, err := c.Query("series_1", now)
+	require.NoError(t, err)
+	require.Equal(t, model.ValVector, result.Type())
+	assert.Equal(t, expectedVector, result.(model.Vector))
+
+	// Work round Prometheus client lib not having a way to omit the start&end params
+	minTime := time.Unix(math.MinInt64/1000+62135596801, 0).UTC()
+	maxTime := time.Unix(math.MaxInt64/1000-62135596801, 999999999).UTC()
+
+	labelValues, err := c.LabelValues("foo", minTime, maxTime, nil)
+	require.NoError(t, err)
+	require.Equal(t, model.LabelValues{"bar"}, labelValues)
+
+	labelNames, err := c.LabelNames(minTime, maxTime)
+	require.NoError(t, err)
+	require.Equal(t, []string{"__name__", "foo"}, labelNames)
+
+	// Check that a range query does not return an error to sanity check the queryrange tripperware.
+	_, err = c.QueryRange("series_1", now.Add(-15*time.Minute), now, 15*time.Second)
+	require.NoError(t, err)
+}
