0aa259c3f7579e1bcada96e3ff012b7c21fb3661
Peter Štibraný <pstibrany@gmail.com>
2022-02-02 14:52:09 +0100

    Comment.
    Signed-off-by: Peter Štibraný <pstibrany@gmail.com>

--- tools/trafficdump/linux_build.sh
+++ tools/trafficdump/linux_build.sh
@@ -8,6 +8,7 @@ MIMIR_ROOT="$(git rev-parse --show-toplevel)"
 
 # Build will be done inside mimir-trafficdump-build image, which installs libpcap-dev.
 
+# We use "--platform=linux/amd64" so that "apt-get install" inside Dockerfile installs lib for correct arch.
 docker build "$MIMIR_ROOT" --platform=linux/amd64 -f Dockerfile.build -t mimir-trafficdump-build
 
 docker run --rm -v "$MIMIR_ROOT":/mimir mimir-trafficdump-build
