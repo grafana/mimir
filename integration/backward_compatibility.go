d2a000a368ae5d671514f5585a3b97ff91e9f0dc
Oleg Zaytsev <mail@olegzaytsev.com>
2022-02-11 10:17:29 +0100

    Allow flag renaming in backwards (in)compatibility tests (#1166)
    Since some default flags are set when the service is being start, we
    need to pass the flag mapping function from the backward compatibility
    definition down to the service starting.
    
    I created an options-like scheme for services (currently supporting only
    flag mappers, but we could also move other options there, like
    WithImage() instead of passing in an empty string when we want the
    default image.
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

--- integration/backward_compatibility.go
+++ integration/backward_compatibility.go
@@ -2,13 +2,21 @@
 
 package integration
 
+import "github.com/grafana/mimir/integration/e2emimir"
+
 // DefaultPreviousVersionImages is used by `tools/pre-pull-images` so it needs
 // to be in a non `_test.go` file.
-var DefaultPreviousVersionImages = map[string]func(map[string]string) map[string]string{
-	"quay.io/cortexproject/cortex:v1.11.0": func(flags map[string]string) map[string]string {
-		flags["-store.engine"] = "blocks"
-		flags["-server.http-listen-port"] = "8080"
-		flags["-store-gateway.sharding-enabled"] = "true"
-		return flags
-	},
+var DefaultPreviousVersionImages = map[string]e2emimir.FlagMapper{
+	"quay.io/cortexproject/cortex:v1.11.0": e2emimir.ChainFlagMappers(
+		cortexFlagMapper,
+	),
 }
+
+var (
+	// cortexFlagMapper sets flags that are needed for cortex.
+	cortexFlagMapper = e2emimir.SetFlagMapper(map[string]string{
+		"-store.engine":                   "blocks",
+		"-server.http-listen-port":        "8080",
+		"-store-gateway.sharding-enabled": "true",
+	})
+)
