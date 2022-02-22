5da5bce050117834c7b4e494d0e798ddbc6b5083
Janos <janos.gub@grafana.com>
2022-02-22 09:27:40 +0100

    Adding more testcases

--- pkg/ingester/active_series_runtime_overrides_test.go
+++ pkg/ingester/active_series_runtime_overrides_test.go
@@ -5,6 +5,7 @@ package ingester
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v2"
 )
@@ -22,4 +23,77 @@ tenant_specific:
 `
 
 	require.NoError(t, yaml.UnmarshalStrict([]byte(input), &r))
+}
+
+func TestActiveSeriesCustomTrackersOverridesProvider(t *testing.T) {
+	overridesReference := &ActiveSeriesCustomTrackersOverrides{}
+	tests := map[string]struct {
+		provider *ActiveSeriesCustomTrackersOverridesProvider
+		expected *ActiveSeriesCustomTrackersOverrides
+	}{
+		"nil provider returns nil": {
+			provider: nil,
+			expected: nil,
+		},
+		"nil getter returns nil": {
+			provider: &ActiveSeriesCustomTrackersOverridesProvider{},
+			expected: nil,
+		},
+		"getter is called": {
+			provider: &ActiveSeriesCustomTrackersOverridesProvider{
+				Getter: func() *ActiveSeriesCustomTrackersOverrides {
+					return overridesReference
+				},
+			},
+			expected: overridesReference,
+		},
+	}
+
+	for name, testData := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, testData.expected, testData.provider.Get())
+		})
+	}
+}
+
+func TestMatchersForUser(t *testing.T) {
+	defaultMatchers, err := NewActiveSeriesMatchers(
+		map[string]string{
+			"foo": `{foo="bar"}`,
+			"bar": `{baz="bar"}`,
+		})
+
+	assert.NoError(t, err)
+	tenantSpecificMatchers, err := NewActiveSeriesMatchers(
+		map[string]string{
+			"team_a": `{team="team_a"}`,
+			"team_b": `{team="team_b"}`,
+		},
+	)
+	assert.NoError(t, err)
+	activeSeriesCustomTrackersOverrides := &ActiveSeriesCustomTrackersOverrides{
+		Default: defaultMatchers,
+		TenantSpecific: map[string]*ActiveSeriesMatchers{
+			"1": tenantSpecificMatchers,
+		},
+	}
+	tests := map[string]struct {
+		userID   string
+		expected *ActiveSeriesMatchers
+	}{
+		"User with no override should return deafult": {
+			userID:   "5",
+			expected: defaultMatchers,
+		},
+		"User with override should return override": {
+			userID:   "1",
+			expected: tenantSpecificMatchers,
+		},
+	}
+	for name, testData := range tests {
+		t.Run(name, func(t *testing.T) {
+			matchersForUser := activeSeriesCustomTrackersOverrides.MatchersForUser(testData.userID)
+			assert.True(t, testData.expected.Equals(matchersForUser))
+		})
+	}
 }
