405a6788357a468579e036e94399435d6f43c1e6
Peter Štibraný <pstibrany@gmail.com>
2022-03-31 12:58:35 +0200

    Split unit tests into groups to make them faster.
    Signed-off-by: Peter Štibraný <pstibrany@gmail.com>

---
+++ .github/workflows/scripts/run-unit-tests-group.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+MIMIR_DIR=$(realpath "${SCRIPT_DIR}/../../../")
+
+# Parse args.
+INDEX=""
+TOTAL=""
+
+while [[ $# -gt 0 ]]
+do
+  case "$1" in
+    --total)
+      TOTAL="$2"
+      shift # skip --total
+      shift # skip total value
+      ;;
+    --index)
+      INDEX="$2"
+      shift # skip --index
+      shift # skip index value
+      ;;
+    *)  break
+      ;;
+  esac
+done
+
+if [[ -z "$INDEX" ]]; then
+    echo "No --index provided."
+    exit 1
+fi
+
+if [[ -z "$TOTAL" ]]; then
+    echo "No --total provided."
+    exit 1
+fi
+
+# List all tests.
+ALL_TESTS=$(go list "${MIMIR_DIR}/..." | sort)
+
+# Filter tests by the requested group.
+GROUP_TESTS=$(echo "$ALL_TESTS" | awk -v TOTAL=$TOTAL -v INDEX=$INDEX 'NR % TOTAL == INDEX')
+
+echo "This group will run the following tests:"
+echo "$GROUP_TESTS"
+echo ""
+
+# shellcheck disable=SC2086 # we *want* word splitting of GROUP_TESTS.
+exec go test -tags=netgo -timeout 30m -race -count 1 ${GROUP_TESTS}
