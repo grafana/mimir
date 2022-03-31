d290d5d44836eb4f130ec9d0db5808473335ece5
Marco Pracucci <marco@pracucci.com>
2022-03-31 18:57:17 +0200

    Added query range support to write-read-series-test in mimir-continuous-test tool
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/continuoustest/metrics.go
+++ pkg/continuoustest/metrics.go
@@ -10,8 +10,12 @@ import (
 // TestMetrics holds generic metrics tracked by tests. The common metrics are used to enforce the same
 // metric names and labels to track the same information across different tests.
 type TestMetrics struct {
-	writesTotal       prometheus.Counter
-	writesFailedTotal *prometheus.CounterVec
+	writesTotal                  prometheus.Counter
+	writesFailedTotal            *prometheus.CounterVec
+	queriesTotal                 prometheus.Counter
+	queriesFailedTotal           prometheus.Counter
+	queryResultChecksTotal       prometheus.Counter
+	queryResultChecksFailedTotal prometheus.Counter
 }
 
 func NewTestMetrics(testName string, reg prometheus.Registerer) *TestMetrics {
@@ -26,5 +30,25 @@ func NewTestMetrics(testName string, reg prometheus.Registerer) *TestMetrics {
 			Help:        "Total number of failed write requests.",
 			ConstLabels: map[string]string{"test": testName},
 		}, []string{"status_code"}),
+		queriesTotal: promauto.With(reg).NewCounter(prometheus.CounterOpts{
+			Name:        "mimir_continuous_test_queries_total",
+			Help:        "Total number of attempted query requests.",
+			ConstLabels: map[string]string{"test": testName},
+		}),
+		queriesFailedTotal: promauto.With(reg).NewCounter(prometheus.CounterOpts{
+			Name:        "mimir_continuous_test_queries_failed_total",
+			Help:        "Total number of failed query requests.",
+			ConstLabels: map[string]string{"test": testName},
+		}),
+		queryResultChecksTotal: promauto.With(reg).NewCounter(prometheus.CounterOpts{
+			Name:        "mimir_continuous_test_query_result_checks_total",
+			Help:        "Total number of query results checked for correctness.",
+			ConstLabels: map[string]string{"test": testName},
+		}),
+		queryResultChecksFailedTotal: promauto.With(reg).NewCounter(prometheus.CounterOpts{
+			Name:        "mimir_continuous_test_query_result_checks_failed_total",
+			Help:        "Total number of query results failed when checking for correctness.",
+			ConstLabels: map[string]string{"test": testName},
+		}),
 	}
 }
--- pkg/continuoustest/util.go
+++ pkg/continuoustest/util.go
@@ -3,15 +3,41 @@
 package continuoustest
 
 import (
+	"fmt"
 	"math"
+	"math/rand"
 	"strconv"
 	"time"
 
+	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/prompb"
+)
+
+const (
+	maxComparisonDelta = 0.001
 )
 
 func alignTimestampToInterval(ts time.Time, interval time.Duration) time.Time {
 	return ts.Truncate(interval)
+}
+
+// getQueryStep returns the query step to use to run a test query. The returned step
+// is a guaranteed to be a multiple of alignInterval.
+func getQueryStep(start, end time.Time, alignInterval time.Duration) time.Duration {
+	const maxSamples = 1000
+
+	// Compute the number of samples that we would have if we query every single sample.
+	actualSamples := end.Sub(start) / alignInterval
+	if actualSamples <= maxSamples {
+		return alignInterval
+	}
+
+	// Adjust the query step based on the max steps spread over the query time range,
+	// rounding it to alignInterval.
+	step := end.Sub(start) / time.Duration(maxSamples)
+	step = ((step / alignInterval) + 1) * alignInterval
+
+	return step
 }
 
 func generateSineWaveSeries(name string, t time.Time, numSeries int) []prompb.TimeSeries {
@@ -41,4 +67,67 @@ func generateSineWaveValue(t time.Time) float64 {
 	period := 10 * time.Minute
 	radians := 2 * math.Pi * float64(t.UnixNano()) / float64(period.Nanoseconds())
 	return math.Sin(radians)
+}
+
+// verifySineWaveSamplesSum assumes the input matrix is the result of a range query summing the values
+// of expectedSeries sine wave series and checks whether the actual values match the expected ones.
+// Returns error if values don't match.
+func verifySineWaveSamplesSum(matrix model.Matrix, expectedSeries int, expectedStep time.Duration) error {
+	if len(matrix) != 1 {
+		return fmt.Errorf("expected 1 series in the result but got %d", len(matrix))
+	}
+
+	samples := matrix[0].Values
+
+	for idx, sample := range samples {
+		ts := time.UnixMilli(int64(sample.Timestamp)).UTC()
+
+		// Assert on value.
+		expectedValue := generateSineWaveValue(ts)
+		if !compareSampleValues(float64(sample.Value), expectedValue*float64(expectedSeries)) {
+			return fmt.Errorf("sample at timestamp %d (%s) has value %f while was expecting %f", sample.Timestamp, ts.String(), sample.Value, expectedValue)
+		}
+
+		// Assert on sample timestamp. We expect no gaps.
+		if idx > 0 {
+			prevTs := time.UnixMilli(int64(samples[idx-1].Timestamp)).UTC()
+			expectedTs := prevTs.Add(expectedStep)
+
+			if ts.UnixMilli() != expectedTs.UnixMilli() {
+				return fmt.Errorf("sample at timestamp %d (%s) was expected to have timestamp %d (%s) because previous sample had timestamp %d (%s)",
+					sample.Timestamp, ts.String(), expectedTs.UnixMilli(), expectedTs.String(), prevTs.UnixMilli(), prevTs.String())
+			}
+		}
+	}
+
+	return nil
+}
+
+func compareSampleValues(actual, expected float64) bool {
+	delta := math.Abs((actual - expected) / maxComparisonDelta)
+	return delta < maxComparisonDelta
+}
+
+func minTime(first, second time.Time) time.Time {
+	if first.After(second) {
+		return second
+	}
+	return first
+}
+
+func maxTime(first, second time.Time) time.Time {
+	if first.After(second) {
+		return first
+	}
+	return second
+}
+
+func randTime(min, max time.Time) time.Time {
+	delta := max.Unix() - min.Unix()
+	if delta <= 0 {
+		return min
+	}
+
+	sec := rand.Int63n(delta) + min.Unix()
+	return time.Unix(sec, 0)
 }
