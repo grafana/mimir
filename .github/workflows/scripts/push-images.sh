96aaae9984e0c0d140692296d99d2ad8833a4b23
Peter Štibraný <pstibrany@gmail.com>
2022-04-28 15:16:13 +0200

    Tag with extra tag, if pushing tagged commit or release.
    Signed-off-by: Peter Štibraný <pstibrany@gmail.com>

--- .github/workflows/scripts/push-images.sh
+++ .github/workflows/scripts/push-images.sh
@@ -8,6 +8,13 @@ IMAGES_DIR="$1"
 IMAGE_PREFIX="$2"
 TAG="$3"
 
+# If image is the latest stable git tag, also push :latest image.
+# Do not tag with latest any release candidate (tag ends with "-rc.*").
+EXTRA_TAG="test"
+if [[ "$(git tag | grep -E '^mimir-[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -n 1)" == "mimir-${TAG}" ]]; then
+  EXTRA_TAG="latest"
+fi
+
 # Push images from OCI archives to docker registry.
 for image in "$IMAGES_DIR"/*
 do
@@ -17,4 +24,9 @@ do
   echo "Uploading ${IMAGE_PREFIX}${NAME}:${TAG}"
   echo
   skopeo copy --all --retry-times 3 oci-archive:${image} "docker://${IMAGE_PREFIX}${NAME}:${TAG}"
+
+  if [[ -n "${EXTRA_TAG}" ]]; then
+    echo "Tagging with ${EXTRA_TAG}"
+    skopeo copy "docker://${IMAGE_PREFIX}${NAME}:${TAG}" "docker://${IMAGE_PREFIX}${NAME}:${EXTRA_TAG}"
+  fi
 done
