043d8bf0721196a60be8c1956d76aff684e74386
Marco Pracucci <marco@pracucci.com>
2022-04-29 16:15:11 +0200

    Remove unused metrics cortex_distributor_ingester_queries_total and cortex_distributor_ingester_query_failures_total
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/distributor/distributor.go
+++ pkg/distributor/distributor.go
@@ -322,16 +322,6 @@ func New(cfg Config, clientConfig ingester_client.Config, limits *validation.Ove
 			Name:      "distributor_ingester_append_failures_total",
 			Help:      "The total number of failed batch appends sent to ingesters.",
 		}, []string{"ingester", "type"}),
-		ingesterQueries: promauto.With(reg).NewCounterVec(prometheus.CounterOpts{
-			Namespace: "cortex",
-			Name:      "distributor_ingester_queries_total",
-			Help:      "The total number of queries sent to ingesters.",
-		}, []string{"ingester"}),
-		ingesterQueryFailures: promauto.With(reg).NewCounterVec(prometheus.CounterOpts{
-			Namespace: "cortex",
-			Name:      "distributor_ingester_query_failures_total",
-			Help:      "The total number of failed queries sent to ingesters.",
-		}, []string{"ingester"}),
 		replicationFactor: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
 			Namespace: "cortex",
 			Name:      "distributor_replication_factor",
