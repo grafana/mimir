d3104afff37465bff6d48035ed53979220c64588
Arve Knudsen <arve.knudsen@gmail.com>
2022-05-25 18:27:33 +0200

    Don't validate block on backfill completion
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/compactor/block_upload.go
+++ pkg/compactor/block_upload.go
@@ -7,34 +7,26 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
-	"os"
 	"path"
-	"path/filepath"
 	"strings"
 	"time"
 
 	"github.com/go-kit/log/level"
-	"github.com/gofrs/uuid"
 	"github.com/gorilla/mux"
 	"github.com/oklog/ulid"
 	"github.com/pkg/errors"
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/model/timestamp"
 	"github.com/prometheus/prometheus/tsdb/chunks"
-	"github.com/prometheus/prometheus/tsdb/index"
-	"github.com/thanos-io/thanos/pkg/block"
 	"github.com/thanos-io/thanos/pkg/block/metadata"
 	"github.com/thanos-io/thanos/pkg/objstore"
 
-	"github.com/grafana/dskit/runutil"
 	"github.com/grafana/dskit/tenant"
 	"github.com/grafana/regexp"
 
 	"github.com/grafana/mimir/pkg/storage/bucket"
-	"github.com/grafana/mimir/pkg/storage/sharding"
 	mimir_tsdb "github.com/grafana/mimir/pkg/storage/tsdb"
 )
 
@@ -46,7 +38,7 @@ func (c *MultitenantCompactor) CreateBlockUpload(w http.ResponseWriter, r *http.
 		http.Error(w, "missing block ID", http.StatusBadRequest)
 		return
 	}
-	_, err := ulid.Parse(blockID)
+	bULID, err := ulid.Parse(blockID)
 	if err != nil {
 		http.Error(w, "invalid block ID", http.StatusBadRequest)
 		return
@@ -60,11 +52,13 @@ func (c *MultitenantCompactor) CreateBlockUpload(w http.ResponseWriter, r *http.
 	level.Debug(c.logger).Log("msg", "creating block upload session", "user", tenantID, "block_id", blockID)
 
 	bkt := bucket.NewUserBucketClient(string(tenantID), c.bucketClient, c.cfgProvider)
+
 	exists := false
-	if err := bkt.Iter(ctx, blockID, func(pth string) error {
+	err = bkt.Iter(ctx, blockID, func(pth string) error {
 		exists = true
 		return nil
-	}); err != nil {
+	})
+	if err != nil {
 		level.Error(c.logger).Log("msg", "failed to iterate over block files", "user", tenantID,
 			"block_id", blockID, "err", err)
 		http.Error(w, "failed iterating over block files in object storage", http.StatusBadGateway)
@@ -77,32 +71,32 @@ func (c *MultitenantCompactor) CreateBlockUpload(w http.ResponseWriter, r *http.
 		return
 	}
 
-	rnd, err := uuid.NewV4()
-	if err != nil {
-		level.Error(c.logger).Log("msg", "failed to generate UUID", "err", err)
+	dec := json.NewDecoder(r.Body)
+	var meta metadata.Meta
+	if err := dec.Decode(&meta); err != nil {
+		http.Error(w, "malformed request body", http.StatusBadRequest)
+		return
+	}
+
+	if err := c.sanitizeMeta(tenantID, bULID, &meta); err != nil {
+		level.Error(c.logger).Log("msg", "failed to sanitize meta.json", "user", tenantID,
+			"block_id", blockID, "err", err)
 		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
-	lockName := fmt.Sprintf("%s.lock", rnd)
-	if err := bkt.Upload(ctx, lockName, bytes.NewBuffer(nil)); err != nil {
-		level.Error(c.logger).Log("msg", "failed to upload lock file to block dir", "user", tenantID,
+
+	if err := c.uploadMeta(ctx, w, meta, blockID, tenantID, "meta.json.temp", bkt); err != nil {
+		var eBadReq errBadRequest
+		if errors.As(err, &eBadReq) {
+			level.Warn(c.logger).Log("msg", eBadReq.message, "user", tenantID,
+				"block_id", blockID)
+			http.Error(w, eBadReq.message, http.StatusBadRequest)
+			return
+		}
+
+		level.Error(c.logger).Log("msg", "failed to upload meta.json", "user", tenantID,
 			"block_id", blockID, "err", err)
-		http.Error(w, "failed uploading lock file to block dir", http.StatusBadGateway)
-		return
-	}
-	if err := bkt.Iter(ctx, blockID, func(pth string) error {
-		exists = pth != lockName
-		return nil
-	}); err != nil {
-		level.Error(c.logger).Log("msg", "failed to iterate over block files", "user", tenantID,
-			"block_id", blockID, "err", err)
-		http.Error(w, "failed iterating over block files in object storage", http.StatusBadGateway)
-		return
-	}
-	if exists {
-		level.Debug(c.logger).Log("msg", "another file exists for block in object storage", "user", tenantID,
-			"block_id", blockID)
-		http.Error(w, "another file exists for block in object storage", http.StatusConflict)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
 
@@ -187,6 +181,8 @@ func (c *MultitenantCompactor) UploadBlockFile(w http.ResponseWriter, r *http.Re
 		return
 	}
 
+	level.Error(c.logger).Log("msg", "failed to decode meta.json",
+		"user", tenantID, "block_id", blockID, "err", err)
 	level.Debug(c.logger).Log("msg", "finished uploading block file to bucket",
 		"user", tenantID, "block_id", blockID, "path", pth)
 
@@ -216,13 +212,6 @@ func (c *MultitenantCompactor) CompleteBlockUpload(w http.ResponseWriter, r *htt
 	level.Debug(c.logger).Log("msg", "received request to complete block upload", "user", tenantID,
 		"block_id", blockID, "content_length", r.ContentLength)
 
-	dec := json.NewDecoder(r.Body)
-	var meta metadata.Meta
-	if err := dec.Decode(&meta); err != nil {
-		http.Error(w, "malformed request body", http.StatusBadRequest)
-		return
-	}
-
 	bkt := bucket.NewUserBucketClient(tenantID, c.bucketClient, c.cfgProvider)
 
 	exists := false
@@ -242,32 +231,27 @@ func (c *MultitenantCompactor) CompleteBlockUpload(w http.ResponseWriter, r *htt
 		return
 	}
 
+	rdr, err := bkt.Get(ctx, path.Join(blockID, "meta.json.temp"))
+	if err != nil {
+		level.Error(c.logger).Log("msg", "failed to download meta.json.temp from object storage",
+			"user", tenantID, "block_id", blockID, "err", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	dec := json.NewDecoder(rdr)
+	var meta metadata.Meta
+	if err := dec.Decode(&meta); err != nil {
+		level.Error(c.logger).Log("msg", "failed to decode meta.json",
+			"user", tenantID, "block_id", blockID, "err", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
 	level.Debug(c.logger).Log("msg", "completing block upload", "user",
 		tenantID, "block_id", blockID, "files", len(meta.Thanos.Files))
 
-	if err := c.sanitizeMeta(blockID, tenantID, &meta); err != nil {
-		level.Error(c.logger).Log("msg", "failed to upload meta.json", "user", tenantID,
-			"block_id", blockID, "err", err)
-		http.Error(w, "internal server error", http.StatusInternalServerError)
-		return
-	}
-
-	if err := c.validateBlock(ctx, w, blockID, tenantID, bkt, meta); err != nil {
-		level.Error(c.logger).Log("msg", "failed validating block", "user", tenantID,
-			"block_id", blockID, "err", err)
-		http.Error(w, "internal server error", http.StatusInternalServerError)
-		return
-	}
-
 	// Upload meta.json so block is considered complete
-	if err := c.uploadMeta(ctx, w, meta, blockID, tenantID, bkt); err != nil {
-		var eBadGW errBadGateway
-		if errors.As(err, &eBadGW) {
-			level.Error(c.logger).Log("msg", eBadGW.message, "user", tenantID,
-				"block_id", blockID, "err", err)
-			http.Error(w, eBadGW.message, http.StatusBadGateway)
-			return
-		}
+	if err := c.uploadMeta(ctx, w, meta, blockID, tenantID, "meta.json", bkt); err != nil {
 		var eBadReq errBadRequest
 		if errors.As(err, &eBadReq) {
 			level.Warn(c.logger).Log("msg", eBadReq.message, "user", tenantID,
@@ -282,28 +266,10 @@ func (c *MultitenantCompactor) CompleteBlockUpload(w http.ResponseWriter, r *htt
 		return
 	}
 
-	var lockFiles []string
-	if err := bkt.Iter(ctx, blockID, func(pth string) error {
-		if strings.HasSuffix(pth, ".lock") {
-			lockFiles = append(lockFiles, pth)
-		}
-		return nil
-	}); err != nil {
-		level.Error(c.logger).Log("msg", "failed to iterate over block files", "user", tenantID,
-			"block_id", blockID, "err", err)
-		http.Error(w, "failed iterating over block files in object storage", http.StatusBadGateway)
-		return
-	}
-	failed := false
-	for _, lf := range lockFiles {
-		if err := bkt.Delete(ctx, lf); err != nil {
-			level.Error(c.logger).Log("msg", "failed to delete lock file from block in object storage", "user", tenantID,
-				"block_id", blockID, "err", err)
-			failed = true
-		}
-	}
-	if failed {
-		http.Error(w, "failed deleting lock file(s) from object storage", http.StatusBadGateway)
+	if err := bkt.Delete(ctx, path.Join(blockID, "meta.json.temp")); err != nil {
+		level.Error(c.logger).Log("msg", "failed to delete meta.json.temp from block in object storage",
+			"user", tenantID, "block_id", blockID, "err", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 
@@ -312,248 +278,12 @@ func (c *MultitenantCompactor) CompleteBlockUpload(w http.ResponseWriter, r *htt
 	w.WriteHeader(http.StatusOK)
 }
 
-func (c *MultitenantCompactor) validateBlock(ctx context.Context, w http.ResponseWriter, blockID, tenantID string,
-	bkt objstore.Bucket, meta metadata.Meta) error {
-	blockDir, err := os.MkdirTemp("", "")
-	if err != nil {
-		return errors.Wrap(err, "failed to create temp dir")
-	}
-	defer func() {
-		if err := os.RemoveAll(blockDir); err != nil {
-			level.Warn(c.logger).Log("msg", "failed to remove temp dir", "path", blockDir, "err", err)
-		}
-	}()
-
-	if err := bkt.Iter(ctx, blockID, func(pth string) error {
-		if strings.HasSuffix(pth, ".lock") || pth == "meta.json" {
-			return nil
-		}
-
-		r, err := bkt.Get(ctx, pth)
-		if err != nil {
-			// TODO: Return error indicating bad gateway
-			return errBadGateway{message: fmt.Sprintf("failed to get object %q from object storage", pth), err: err}
-		}
-
-		f, err := os.Create(filepath.Join(blockDir, pth))
-		if err != nil {
-			return errors.Wrap(err, "failed creating temp file")
-		}
-		defer func() {
-			_ = f.Close()
-		}()
-		if _, err := io.Copy(f, r); err != nil {
-			return errors.Wrap(err, "failed writing to temp file")
-		}
-		if err := f.Close(); err != nil {
-			return errors.Wrap(err, "failed writing to temp file")
-		}
-
-		return nil
-	}); err != nil {
-		return errors.Wrapf(err, "failed to iterate block %s", blockID)
-	}
-
-	idxFi, err := analyzeBlockFile(blockDir, "index")
-	if err != nil {
-		return err
-	}
-	meta.Thanos.Files = []metadata.File{
-		idxFi,
-		{
-			// Size not stated for meta.json
-			RelPath: "meta.json",
-		},
-	}
-
-	meta.Thanos.SegmentFiles = []string{}
-	chunksDir := filepath.Join(blockDir, "chunks")
-	ces, err := os.ReadDir(chunksDir)
-	if err != nil {
-		return errors.Wrapf(err, "failed to read directory %s", chunksDir)
-	}
-	for _, e := range ces {
-		meta.Thanos.SegmentFiles = append(meta.Thanos.SegmentFiles, e.Name())
-		fi, err := analyzeBlockFile(blockDir, filepath.Join("chunks", e.Name()))
-		if err != nil {
-			return err
-		}
-
-		meta.Thanos.Files = append(meta.Thanos.Files, fi)
-	}
-
-	// Reset sources/parents since block history may be inconsistent with Mimir's
-	bULID, err := ulid.Parse(blockID)
-	if err != nil {
-		return errors.Wrap(err, "failed to parse block ID")
-	}
-	// Sources should always contain block ID itself
-	meta.Compaction.Sources = []ulid.ULID{
-		bULID,
-	}
-	meta.Compaction.Parents = nil
-
-	if err := c.scanBlock(blockDir, meta); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func analyzeBlockFile(relPath, blockDir string) (metadata.File, error) {
-	fi, err := os.Stat(filepath.Join(blockDir, relPath))
-	if err != nil {
-		return metadata.File{}, errors.Wrapf(err, "failed to stat %s", filepath.Join(blockDir, relPath))
-	}
-	return metadata.File{
-		RelPath:   relPath,
-		SizeBytes: fi.Size(),
-	}, nil
-}
-
-func (c *MultitenantCompactor) scanBlock(blockDir string, meta metadata.Meta) error {
-	// TODO: Count samples (check with Peter)
-
-	var cr *chunks.Reader
-	cr, err := chunks.NewDirReader(filepath.Join(blockDir, block.ChunksDirname), nil)
-	if err != nil {
-		return errors.Wrap(err, "open chunks dir")
-	}
-	defer runutil.CloseWithErrCapture(&err, cr, "closing chunks reader")
-
-	r, err := index.NewFileReader(filepath.Join(blockDir, block.IndexFilename))
-	if err != nil {
-		return errors.Wrap(err, "open index file")
-	}
-	defer runutil.CloseWithErrCapture(&err, r, "gather index issue file reader")
-
-	ps, err := r.Postings(index.AllPostingsKey())
-	if err != nil {
-		return errors.Wrap(err, "get all postings")
-	}
-
-	shard, shardCount, err := sharding.ParseShardIDLabelValue(meta.Thanos.Labels[mimir_tsdb.CompactorShardIDExternalLabel])
-	if err != nil {
-		return errors.Wrap(err, "parse shard ID label value")
-	}
-
-	var (
-		lastLset labels.Labels
-		lset     labels.Labels
-
-		outOfOrderLabels int
-	)
-	// Per series.
-	for ps.Next() {
-		lastLset = append(lastLset[:0], lset...)
-
-		id := ps.At()
-
-		var chnks []chunks.Meta
-		if err := r.Series(id, &lset, &chnks); err != nil {
-			return errors.Wrap(err, "read series")
-		}
-		if len(lset) == 0 {
-			return errors.Errorf("empty label set detected for series %d", id)
-		}
-		if lastLset != nil && labels.Compare(lastLset, lset) >= 0 {
-			return errors.Errorf("series %v out of order; previous: %v", lset, lastLset)
-		}
-		l0 := lset[0]
-		for _, l := range lset[1:] {
-			if l.Name < l0.Name {
-				outOfOrderLabels++
-				level.Warn(c.logger).Log("msg",
-					"out-of-order label set: known bug in Prometheus 2.8.0 and below",
-					"labelset", lset.String(),
-					"series", fmt.Sprintf("%d", id),
-				)
-			}
-			l0 = l
-		}
-		if len(chnks) == 0 {
-			return errors.Errorf("empty chunks for series %d", id)
-		}
-
-		if lset.Hash()%shardCount != shard {
-			return errBadRequest{message: fmt.Sprintf("series sharded incorrectly: %s", lset.String())}
-		}
-
-		ooo := 0
-		// Per chunk in series.
-		for i, chnk := range chnks {
-			if i == 0 {
-				continue
-			}
-
-			prev := chnks[i-1]
-
-			// Chunk order within block.
-			if chnk.MinTime > prev.MaxTime {
-				continue
-			}
-
-			if chnk.MinTime == prev.MinTime && chnk.MaxTime == prev.MaxTime {
-				// TODO(bplotka): Calc and check checksum from chunks itself.
-				// The chunks can overlap 1:1 in time, but does not have same data.
-				// We assume same data for simplicity, but it can be a symptom of error.
-				continue
-			}
-
-			// Chunks partly overlaps or out of order.
-			level.Debug(c.logger).Log("msg", "found out of order chunks",
-				"prev_ref", prev.Ref, "next_ref", chnk.Ref,
-				"prev_min_time", timestamp.Time(prev.MinTime).UTC().Format(time.RFC3339Nano),
-				"prev_max_time", timestamp.Time(prev.MaxTime).UTC().Format(time.RFC3339Nano),
-				"next_min_time", timestamp.Time(chnk.MinTime).UTC().Format(time.RFC3339Nano),
-				"next_max_time", timestamp.Time(chnk.MaxTime).UTC().Format(time.RFC3339Nano),
-				"labels", lset, "prev_chunk_index", i-1, "next_chunk_index", i, "chunks_for_series", len(chnks))
-
-			ooo++
-		}
-
-		/*
-			if ooo > 0 {
-				// TODO: Should this be an error?
-					stats.OutOfOrderSeries++
-					stats.OutOfOrderChunks += ooo
-			}
-		*/
-
-		if err := c.verifyChunks(cr, lset, chnks); err != nil {
-			return err
-		}
-	}
-	if ps.Err() != nil {
-		return errors.Wrap(err, "failed iterating over postings")
-	}
-
-	if outOfOrderLabels > 0 {
-		return errBadRequest{message: fmt.Sprintf("block has %d out of order labels", outOfOrderLabels)}
-	}
-
-	return nil
-}
-
 type errBadRequest struct {
 	message string
 }
 
 func (e errBadRequest) Error() string {
 	return e.message
-}
-
-type errBadGateway struct {
-	message string
-	err     error
-}
-
-func (e errBadGateway) Error() string {
-	return e.message
-}
-
-func (e errBadGateway) Unwrap() error {
-	return e.err
 }
 
 func (c *MultitenantCompactor) verifyChunks(cr *chunks.Reader, lset labels.Labels, chnks []chunks.Meta) error {
@@ -603,25 +333,16 @@ func (c *MultitenantCompactor) verifyChunks(cr *chunks.Reader, lset labels.Label
 	return nil
 }
 
-func (c *MultitenantCompactor) sanitizeMeta(tenantID, blockID string, meta *metadata.Meta) error {
+func (c *MultitenantCompactor) sanitizeMeta(tenantID string, blockID ulid.ULID, meta *metadata.Meta) error {
 	if meta.Thanos.Labels == nil {
 		meta.Thanos.Labels = map[string]string{}
 	}
 
-	metaULID := meta.ULID
-	if metaULID.String() != blockID {
-		level.Warn(c.logger).Log("msg", "updating meta.json block ID",
-			"old_value", metaULID.String(), "new_value", blockID, "user", tenantID)
-		var err error
-		meta.ULID, err = ulid.Parse(blockID)
-		if err != nil {
-			return errors.Wrapf(err, "couldn't parse block ID %q", blockID)
-		}
-	}
+	meta.ULID = blockID
 
 	metaTenantID := meta.Thanos.Labels[mimir_tsdb.TenantIDExternalLabel]
 	if metaTenantID != tenantID {
-		level.Warn(c.logger).Log("msg", "updating meta.json tenant label", "block_id", blockID,
+		level.Warn(c.logger).Log("msg", "updating meta.json tenant label", "block_id", blockID.String(),
 			"old_value", metaTenantID, "new_value", tenantID, "user", tenantID)
 		meta.Thanos.Labels[mimir_tsdb.TenantIDExternalLabel] = tenantID
 	}
@@ -632,7 +353,7 @@ func (c *MultitenantCompactor) sanitizeMeta(tenantID, blockID string, meta *meta
 		case mimir_tsdb.TenantIDExternalLabel, mimir_tsdb.CompactorShardIDExternalLabel:
 		case mimir_tsdb.IngesterIDExternalLabel, mimir_tsdb.DeprecatedShardIDExternalLabel:
 			level.Debug(c.logger).Log("msg", "removing unused external label from meta.json",
-				"block_id", blockID, "user", tenantID, "label", l, "value", v)
+				"block_id", blockID.String(), "user", tenantID, "label", l, "value", v)
 			delete(meta.Thanos.Labels, l)
 		default:
 			rejLbls = append(rejLbls, l)
@@ -641,7 +362,7 @@ func (c *MultitenantCompactor) sanitizeMeta(tenantID, blockID string, meta *meta
 
 	if len(rejLbls) > 0 {
 		level.Warn(c.logger).Log("msg", "rejecting unsupported external label(s) in meta.json",
-			"block_id", blockID, "user", tenantID, "labels", strings.Join(rejLbls, ","))
+			"block_id", blockID.String(), "user", tenantID, "labels", strings.Join(rejLbls, ","))
 		return errBadRequest{message: fmt.Sprintf("unsupported external label(s): %s", strings.Join(rejLbls, ","))}
 	}
 
@@ -651,17 +372,17 @@ func (c *MultitenantCompactor) sanitizeMeta(tenantID, blockID string, meta *meta
 	return nil
 }
 
-func (c *MultitenantCompactor) uploadMeta(ctx context.Context, w http.ResponseWriter, meta metadata.Meta, blockID, tenantID string,
-	bkt objstore.Bucket) error {
-	dst := path.Join(blockID, "meta.json")
-	level.Debug(c.logger).Log("msg", "uploading meta.json to bucket", "dst", dst)
+func (c *MultitenantCompactor) uploadMeta(ctx context.Context, w http.ResponseWriter, meta metadata.Meta,
+	blockID, tenantID, name string, bkt objstore.Bucket) error {
+	dst := path.Join(blockID, name)
+	level.Debug(c.logger).Log("msg", fmt.Sprintf("uploading %s to bucket", name), "dst", dst)
 	buf := bytes.NewBuffer(nil)
 	enc := json.NewEncoder(buf)
 	if err := enc.Encode(meta); err != nil {
-		return errors.Wrap(err, "failed to encode meta.json")
+		return errors.Wrap(err, "failed to encode block metadata")
 	}
 	if err := bkt.Upload(ctx, dst, buf); err != nil {
-		return errBadGateway{message: "failed uploading meta.json to bucket", err: err}
+		return errors.Wrapf(err, "failed uploading %s to bucket", name)
 	}
 
 	return nil
