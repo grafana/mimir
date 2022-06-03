c68edfe0212ee654115f4734b644d647468c6a16
Jesus Vazquez <jesus.vazquez@grafana.com>
2022-06-03 15:53:30 +0200

    Disable compacting overlapping blocks
    Prometheus has now this capability of compacting overlapping blocks when
    running internal compactions. This makes sense now that there are both
    in order and out of order blocks with samples.
    
    However Mimir only pushes level 1 compacted blocks to storage so we need
    to disable this by default.

--- pkg/compactor/split_merge_compactor.go
+++ pkg/compactor/split_merge_compactor.go
@@ -22,7 +22,7 @@ func splitAndMergeGrouperFactory(ctx context.Context, cfg Config, cfgProvider Co
 
 func splitAndMergeCompactorFactory(ctx context.Context, cfg Config, logger log.Logger, reg prometheus.Registerer) (Compactor, Planner, error) {
 	// We don't need to customise the TSDB compactor so we're just using the Prometheus one.
-	compactor, err := tsdb.NewLeveledCompactor(ctx, reg, logger, cfg.BlockRanges.ToMilliseconds(), downsample.NewPool(), nil)
+	compactor, err := tsdb.NewLeveledCompactor(ctx, reg, logger, cfg.BlockRanges.ToMilliseconds(), downsample.NewPool(), nil, false) // TODO(jesus.vazquez) Review this false enable overlapping compaction. I'm assuming it needs to be false because Mimir only pushes lvl 1 blocks to storage.
 	if err != nil {
 		return nil, nil, err
 	}
--- pkg/ingester/ingester.go
+++ pkg/ingester/ingester.go
@@ -1496,7 +1496,8 @@ func (i *Ingester) createTSDB(userID string) (*userTSDB, error) {
 		EnableMemorySnapshotOnShutdown: i.cfg.BlocksStorageConfig.TSDB.MemorySnapshotOnShutdown,
 		IsolationDisabled:              !i.cfg.BlocksStorageConfig.TSDB.IsolationEnabled,
 		HeadChunksWriteQueueSize:       i.cfg.BlocksStorageConfig.TSDB.HeadChunksWriteQueueSize,
-		AllowOverlappingBlocks:         i.cfg.BlocksStorageConfig.TSDB.OOOAllowance > 0, // always true if out of order support is enabled
+		AllowOverlappingQueries:        i.cfg.BlocksStorageConfig.TSDB.OOOAllowance > 0, // always true if out of order support is enabled
+		AllowOverlappingCompaction:     false,                                           // always false since Mimir only uploads lvl 1 compacted blocks
 		OOOAllowance:                   int64(i.cfg.BlocksStorageConfig.TSDB.OOOAllowance / time.Millisecond),
 		OOOCapMin:                      int64(i.cfg.BlocksStorageConfig.TSDB.OOOCapMin),
 		OOOCapMax:                      int64(i.cfg.BlocksStorageConfig.TSDB.OOOCapMax),
