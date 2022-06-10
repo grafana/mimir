5d91769fe8315c9f0cbb2a2a69ae03b37a1d0c5d
Janos <janos.gub@grafana.com>
2022-06-10 13:11:42 +0200

    Flag-checker checking if flag and config values are the same

--- tools/config-inspector/main_test.go
+++ tools/config-inspector/main_test.go
@@ -3,7 +3,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -22,4 +24,29 @@ func TestConfigDescriptorIsUpToDate(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.JSONEq(t, string(upToDateBytes), string(committedBytes), "config descriptor is not up to date; run `make reference-help`")
+}
+
+func TestFlagNamesEqualsConfigNames(t *testing.T) {
+	root, err := config.InspectConfig(&mimir.Config{})
+	require.NoError(t, err)
+
+	visit(t, root)
+}
+
+func visit(t *testing.T, entry *config.InspectedEntry) {
+	flagNameEqualsConfigName(t, entry)
+
+	for i := 0; i < len(entry.BlockEntries); i++ {
+		visit(t, entry.BlockEntries[i])
+	}
+
+}
+
+func flagNameEqualsConfigName(t *testing.T, entry *config.InspectedEntry) {
+	if entry.FieldFlag == "" {
+		return
+	}
+	flagConfigPart := entry.FieldFlag[strings.LastIndex(entry.FieldFlag, ".")+1:]
+
+	assert.Equal(t, entry.Name, strings.ReplaceAll(flagConfigPart, "-", "_"), fmt.Sprintf("Flag value is: [%s], config value is: [%s]", entry.FieldFlag, entry.Name))
 }
