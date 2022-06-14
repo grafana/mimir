ac7b8a02781ee7ecf33966650919675174b0973e
György Krajcsovits <gyorgy.krajcsovits@grafana.com>
2022-06-14 10:11:51 +0200

    Do not include subcharts
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- operations/helm/tests/build.sh
+++ operations/helm/tests/build.sh
@@ -22,6 +22,6 @@ for FILEPATH in $TESTS; do
   echo "Templating $TEST_NAME"
   helm template "${TEST_NAME}" ${CHART_PATH} -f "${FILEPATH}" --output-dir "${OUTPUT_DIR}" --namespace citestns
 
-  echo "Removing mutable config checksum and helm chart version for clarity in ${OUTPUT_DIR}"
-  find "${OUTPUT_DIR}" -type f -print0 | xargs -0 sed -E -i -- "/^[ ]+(checksum\/config|(helm.sh\/)?chart):/d"
+  echo "Removing mutable config checksum and helm chart version for clarity"
+  find "${OUTPUT_DIR}/$(basename ${CHART_PATH})/templates" -type f -print0 | xargs -0 sed -E -i -- "/^[ ]+(checksum\/config|(helm.sh\/)?chart):/d"
 done
