a435803d74b4fd1cd8e42c52dfce7a9969681257
George Krajcsovits <krajorama@users.noreply.github.com>
2022-06-14 10:24:48 +0200

    Helm: remove checksum and helm chart version from golden record (#2092)
    * Helm: remove checksum and helm chart version from golden record
    
    Simple changes create too big diffs without this.
    Do not include subcharts.
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- operations/helm/tests/build.sh
+++ operations/helm/tests/build.sh
@@ -17,7 +17,11 @@ TESTS=$(ls -1 ${CHART_PATH}/ci/*values.yaml)
 for FILEPATH in $TESTS; do
   # Extract the filename (without extension).
   TEST_NAME=$(basename -s '.yaml' "$FILEPATH")
+  OUTPUT_DIR="operations/helm/tests/${TEST_NAME}-generated"
 
   echo "Templating $TEST_NAME"
-  helm template "${TEST_NAME}" ${CHART_PATH} -f "${FILEPATH}" --output-dir "operations/helm/tests/${TEST_NAME}-generated" --namespace citestns
+  helm template "${TEST_NAME}" ${CHART_PATH} -f "${FILEPATH}" --output-dir "${OUTPUT_DIR}" --namespace citestns
+
+  echo "Removing mutable config checksum and helm chart version for clarity"
+  find "${OUTPUT_DIR}/$(basename ${CHART_PATH})/templates" -type f -print0 | xargs -0 sed -E -i -- "/^[ ]+(checksum\/config|(helm.sh\/)?chart):/d"
 done
