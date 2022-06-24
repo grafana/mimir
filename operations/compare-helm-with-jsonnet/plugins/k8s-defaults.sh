ab77f284200ddf0c4e9ec081fd3b0d61a962f948
Patrick Oyarzun <patrick.oyarzun@grafana.com>
2022-06-24 09:34:50 -0500

    Add License header

--- operations/compare-helm-with-jsonnet/plugins/k8s-defaults.sh
+++ operations/compare-helm-with-jsonnet/plugins/k8s-defaults.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# SPDX-License-Identifier: AGPL-3.0-only
 
 # Here we use kubectl's server-side dry run feature to apply all k8s default values
 # This process sets things like default storage class, default image pull policy, etc.
