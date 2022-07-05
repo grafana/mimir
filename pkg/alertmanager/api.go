23b507ac0d9b58521335780489c71c2f1c3b9970
Marco Pracucci <marco@pracucci.com>
2022-07-05 09:20:59 +0200

    Removed unused variable errProxyURLNotAllowed
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/alertmanager/api.go
+++ pkg/alertmanager/api.go
@@ -49,7 +49,6 @@ const (
 var (
 	errPasswordFileNotAllowed           = errors.New("setting password_file, bearer_token_file and credentials_file is not allowed")
 	errOAuth2SecretFileNotAllowed       = errors.New("setting OAuth2 client_secret_file is not allowed")
-	errProxyURLNotAllowed               = errors.New("setting proxy_url is not allowed")
 	errTLSFileNotAllowed                = errors.New("setting TLS ca_file, cert_file and key_file is not allowed")
 	errSlackAPIURLFileNotAllowed        = errors.New("setting Slack api_url_file and global slack_api_url_file is not allowed")
 	errVictorOpsAPIKeyFileNotAllowed    = errors.New("setting VictorOps api_key_file is not allowed")
