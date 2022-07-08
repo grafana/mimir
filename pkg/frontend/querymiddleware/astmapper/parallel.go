8d59ccc9765820550c44a5a29082760a423006a8
Oleg Zaytsev <mail@olegzaytsev.com>
2022-07-08 16:45:53 +0200

    Fix sharding of `vector` and `time` functions
    Functions `vector` and `time` can't be sharded, as they produce same set
    of series (with zero labels) and can't be concatenated later.
    
    Additionally, functions that have a default `time()` argument,
    like `month()`, were not propagating the shardeability checks properly.
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

--- pkg/frontend/querymiddleware/astmapper/parallel.go
+++ pkg/frontend/querymiddleware/astmapper/parallel.go
@@ -29,6 +29,21 @@ var NonParallelFuncs = []string{
 	"histogram_quantile",
 	"sort_desc",
 	"sort",
+	"time",
+	"vector",
+}
+
+// FuncsWithDefaultTimeArg is the list of functions that extract date information from a variadic list of params,
+// which defaults to be just time() otherwise.
+var FuncsWithDefaultTimeArg = []string{
+	"day_of_month",
+	"day_of_week",
+	"day_of_year",
+	"days_in_month",
+	"hour",
+	"minute",
+	"month",
+	"year",
 }
 
 // CanParallelize tests if a subtree is parallelizable.
@@ -81,7 +96,7 @@ func CanParallelize(node parser.Node, logger log.Logger) bool {
 			return false
 		}
 
-		for _, e := range n.Args {
+		for _, e := range argsWithDefaults(n) {
 			if !CanParallelize(e, logger) {
 				return false
 			}
@@ -125,6 +140,28 @@ func isAggregateExpr(n parser.Node) (bool, error) {
 
 // ParallelizableFunc ensures that a promql function can be part of a parallel query.
 func ParallelizableFunc(f parser.Function) bool {
+	for _, v := range NonParallelFuncs {
+		if v == f.Name {
+			return false
+		}
+	}
+	return true
+}
+
+// argsWithDefaults returns the arguments of the call, including the omitted defaults.
+func argsWithDefaults(call *parser.Call) parser.Expressions {
+	for _, fn := range FuncsWithDefaultTimeArg {
+		if fn == call.Func.Name && len(call.Args) == 0 {
+			return parser.Expressions{
+				&parser.Call{Func: parser.Functions["time"]},
+			}
+		}
+	}
+	return call.Args
+}
+
+// VariadicDateFunc retuns true if .
+func VariadicDateFunc(f parser.Function) bool {
 	for _, v := range NonParallelFuncs {
 		if v == f.Name {
 			return false
