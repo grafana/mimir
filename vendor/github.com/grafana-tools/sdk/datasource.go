707ffdb8a32c85750006d7e67dd5be538b4e8172
Oleg Zaytsev <mail@olegzaytsev.com>
2022-07-12 15:07:54 +0200

    Remove replace directive for grafana-tools/sdk
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

--- vendor/github.com/grafana-tools/sdk/datasource.go
+++ vendor/github.com/grafana-tools/sdk/datasource.go
@@ -1,9 +1,5 @@
 package sdk
 
-import (
-	"encoding/json"
-)
-
 /*
    Copyright 2016 Alexander I.Grafov <grafov@gmail.com>
    Copyright 2016-2019 The Grafana SDK authors
@@ -28,17 +24,14 @@ import (
 type Datasource struct {
 	ID                uint        `json:"id"`
 	OrgID             uint        `json:"orgId"`
-	UID               string      `json:"uid"`
 	Name              string      `json:"name"`
 	Type              string      `json:"type"`
-	TypeLogoURL       string      `json:"typeLogoUrl"`
 	Access            string      `json:"access"` // direct or proxy
 	URL               string      `json:"url"`
 	Password          *string     `json:"password,omitempty"`
 	User              *string     `json:"user,omitempty"`
 	Database          *string     `json:"database,omitempty"`
 	BasicAuth         *bool       `json:"basicAuth,omitempty"`
-	ReadOnly          *bool       `json:"readOnly,omitempty"`
 	BasicAuthUser     *string     `json:"basicAuthUser,omitempty"`
 	BasicAuthPassword *string     `json:"basicAuthPassword,omitempty"`
 	IsDefault         bool        `json:"isDefault"`
@@ -58,38 +51,4 @@ type DatasourceType struct {
 	PluginType  string `json:"pluginType"`
 	ServiceName string `json:"serviceName"`
 	Type        string `json:"type"`
-}
-
-// DatasourceRef is used to reference a datasource from panels, queries, etc.
-type DatasourceRef struct {
-	// Type describes the type of the datasource, like "prometheus", "graphite", etc.
-	// Datasources of the same type should support same queries.
-	// If Type is empty in an unmarshaled DatasourceRef, check the LegacyName field.
-	Type string `json:"type"`
-	// UID is the uid of the specific datasource this references to.
-	UID string `json:"UID"`
-	// LegacyName is the old way of referencing a datasource by its name, replaced in Grafana v8.4.3 by Type and UID referencing.
-	// If datasource is encoded as a string, then it's unmarshaled into this LegacyName field (Type and UID will be empty).
-	// If LegacyName is not empty, then this DatasourceRef will be marshaled as a string, ignoring the values of Type and UID.
-	LegacyName string `json:"-"`
-}
-
-func (ref DatasourceRef) MarshalJSON() ([]byte, error) {
-	if ref.LegacyName != "" {
-		return json.Marshal(ref.LegacyName)
-	}
-	type plain DatasourceRef
-	return json.Marshal(plain(ref))
-}
-
-func (ref *DatasourceRef) UnmarshalJSON(data []byte) error {
-	type plain DatasourceRef
-	err := json.Unmarshal(data, (*plain)(ref))
-	if err != nil {
-		if err := json.Unmarshal(data, &ref.LegacyName); err == nil {
-			// We could check here if it's `-- Mixed --` and in that case set ref.Type="mixed".
-			return nil
-		}
-	}
-	return err
 }
--- vendor/github.com/grafana-tools/sdk/panel.go
+++ vendor/github.com/grafana-tools/sdk/panel.go
@@ -39,7 +39,6 @@ const (
 	RowType
 	BarGaugeType
 	HeatmapType
-	TimeseriesType
 )
 
 const MixedSource = "-- Mixed --"
@@ -61,14 +60,13 @@ type (
 		*AlertlistPanel
 		*BarGaugePanel
 		*HeatmapPanel
-		*TimeseriesPanel
 		*CustomPanel
 	}
 	panelType   int8
 	CommonPanel struct {
-		Datasource *DatasourceRef `json:"datasource,omitempty"` // metrics
-		Editable   bool           `json:"editable"`
-		Error      bool           `json:"error"`
+		Datasource *string `json:"datasource,omitempty"` // metrics
+		Editable   bool    `json:"editable"`
+		Error      bool    `json:"error"`
 		GridPos    struct {
 			H *int `json:"h,omitempty"`
 			W *int `json:"w,omitempty"`
@@ -166,7 +164,17 @@ type (
 		FieldConfig     *FieldConfig     `json:"fieldConfig,omitempty"`
 	}
 	FieldConfig struct {
-		Defaults FieldConfigDefaults `json:"defaults"`
+		Defaults struct {
+			Unit      string `json:"unit"`
+			Threshold struct {
+				Mode  string `json:"mode"`
+				Steps []struct {
+					Color string `json:"color"`
+					Value string `json:"value"`
+				} `json:"steps"`
+			} `json:"threshold"`
+			Links []Link `json:"links,omitempty"`
+		} `json:"defaults"`
 	}
 	Options struct {
 		Orientation   string `json:"orientation"`
@@ -353,80 +361,6 @@ type (
 		YBucketNumber *float64 `json:"yBucketNumber"`
 		YBucketSize   *float64 `json:"yBucketSize"`
 	}
-	TimeseriesPanel struct {
-		Targets     []Target          `json:"targets,omitempty"`
-		Options     TimeseriesOptions `json:"options"`
-		FieldConfig FieldConfig       `json:"fieldConfig"`
-	}
-	TimeseriesOptions struct {
-		Legend  TimeseriesLegendOptions  `json:"legend,omitempty"`
-		Tooltip TimeseriesTooltipOptions `json:"tooltip,omitempty"`
-	}
-	TimeseriesLegendOptions struct {
-		Calcs       []string `json:"calcs"`
-		DisplayMode string   `json:"displayMode"`
-		Placement   string   `json:"placement"`
-	}
-	TimeseriesTooltipOptions struct {
-		Mode string `json:"mode"`
-	}
-	FieldConfigDefaults struct {
-		Unit       string            `json:"unit"`
-		Decimals   *int              `json:"decimals,omitempty"`
-		Min        *float64          `json:"min,omitempty"`
-		Max        *float64          `json:"max,omitempty"`
-		Color      FieldConfigColor  `json:"color"`
-		Thresholds Thresholds        `json:"thresholds"`
-		Custom     FieldConfigCustom `json:"custom"`
-		Links      []Link            `json:"links,omitempty"`
-	}
-	FieldConfigCustom struct {
-		AxisLabel         string `json:"axisLabel,omitempty"`
-		AxisPlacement     string `json:"axisPlacement"`
-		AxisSoftMin       *int   `json:"axisSoftMin,omitempty"`
-		AxisSoftMax       *int   `json:"axisSoftMax,omitempty"`
-		BarAlignment      int    `json:"barAlignment"`
-		DrawStyle         string `json:"drawStyle"`
-		FillOpacity       int    `json:"fillOpacity"`
-		GradientMode      string `json:"gradientMode"`
-		LineInterpolation string `json:"lineInterpolation"`
-		LineWidth         int    `json:"lineWidth"`
-		PointSize         int    `json:"pointSize"`
-		ShowPoints        string `json:"showPoints"`
-		SpanNulls         bool   `json:"spanNulls"`
-		HideFrom          struct {
-			Legend  bool `json:"legend"`
-			Tooltip bool `json:"tooltip"`
-			Viz     bool `json:"viz"`
-		} `json:"hideFrom"`
-		LineStyle struct {
-			Fill string `json:"fill"`
-		} `json:"lineStyle"`
-		ScaleDistribution struct {
-			Type string `json:"type"`
-			Log  int    `json:"log,omitempty"`
-		} `json:"scaleDistribution"`
-		Stacking struct {
-			Group string `json:"group"`
-			Mode  string `json:"mode"`
-		} `json:"stacking"`
-		ThresholdsStyle struct {
-			Mode string `json:"mode"`
-		} `json:"thresholdsStyle"`
-	}
-	Thresholds struct {
-		Mode  string          `json:"mode"`
-		Steps []ThresholdStep `json:"steps"`
-	}
-	ThresholdStep struct {
-		Color string   `json:"color"`
-		Value *float64 `json:"value"`
-	}
-	FieldConfigColor struct {
-		Mode       string `json:"mode"`
-		FixedColor string `json:"fixedColor,omitempty"`
-		SeriesBy   string `json:"seriesBy,omitempty"`
-	}
 	CustomPanel map[string]interface{}
 )
 
@@ -549,9 +483,9 @@ type (
 
 // for an any panel
 type Target struct {
-	RefID      string         `json:"refId"`
-	Datasource *DatasourceRef `json:"datasource,omitempty"`
-	Hide       bool           `json:"hide,omitempty"`
+	RefID      string `json:"refId"`
+	Datasource string `json:"datasource,omitempty"`
+	Hide       bool   `json:"hide,omitempty"`
 
 	// For PostgreSQL
 	Table        string `json:"table,omitempty"`
@@ -707,34 +641,6 @@ func NewGraph(title string) *Panel {
 		}}
 }
 
-// NewTimeseries initializes panel with a timeseries panel.
-func NewTimeseries(title string) *Panel {
-	if title == "" {
-		title = "Panel Title"
-	}
-
-	return &Panel{
-		CommonPanel: CommonPanel{
-			OfType: TimeseriesType,
-			Title:  title,
-			Type:   "timeseries",
-			Span:   12,
-			IsNew:  true,
-		},
-		TimeseriesPanel: &TimeseriesPanel{
-			FieldConfig: FieldConfig{
-				Defaults: FieldConfigDefaults{
-					Color: FieldConfigColor{
-						Mode:       "palette-classic",
-						FixedColor: "green",
-						SeriesBy:   "last",
-					},
-				},
-			},
-		},
-	}
-}
-
 // NewTable initializes panel with a table panel.
 func NewTable(title string) *Panel {
 	if title == "" {
@@ -876,8 +782,6 @@ func (p *Panel) ResetTargets() {
 		p.BarGaugePanel.Targets = nil
 	case HeatmapType:
 		p.HeatmapPanel.Targets = nil
-	case TimeseriesType:
-		p.TimeseriesPanel.Targets = nil
 	}
 }
 
@@ -897,8 +801,6 @@ func (p *Panel) AddTarget(t *Target) {
 		p.TablePanel.Targets = append(p.TablePanel.Targets, *t)
 	case HeatmapType:
 		p.HeatmapPanel.Targets = append(p.HeatmapPanel.Targets, *t)
-	case TimeseriesType:
-		p.TimeseriesPanel.Targets = append(p.TimeseriesPanel.Targets, *t)
 	}
 	// TODO check for existing refID
 }
@@ -926,8 +828,6 @@ func (p *Panel) SetTarget(t *Target) {
 		setTarget(t, &p.TablePanel.Targets)
 	case HeatmapType:
 		setTarget(t, &p.HeatmapPanel.Targets)
-	case TimeseriesType:
-		setTarget(t, &p.TimeseriesPanel.Targets)
 	}
 }
 
@@ -942,7 +842,7 @@ func (p *Panel) RepeatDatasourcesForEachTarget(dsNames ...string) {
 			for _, ds := range dsNames {
 				newTarget := target
 				newTarget.RefID = refID
-				newTarget.Datasource = &DatasourceRef{LegacyName: ds}
+				newTarget.Datasource = ds
 				refID = incRefID(refID)
 				*targets = append(*targets, newTarget)
 			}
@@ -959,8 +859,6 @@ func (p *Panel) RepeatDatasourcesForEachTarget(dsNames ...string) {
 		repeatDS(dsNames, &p.TablePanel.Targets)
 	case HeatmapType:
 		repeatDS(dsNames, &p.HeatmapPanel.Targets)
-	case TimeseriesType:
-		repeatDS(dsNames, &p.TimeseriesPanel.Targets)
 	}
 }
 
@@ -973,13 +871,13 @@ func (p *Panel) RepeatTargetsForDatasources(dsNames ...string) {
 		lenTargets := len(*targets)
 		for i, name := range dsNames {
 			if i < lenTargets {
-				(*targets)[i].Datasource = &DatasourceRef{LegacyName: name}
+				(*targets)[i].Datasource = name
 				lastRefID = (*targets)[i].RefID
 			} else {
 				newTarget := (*targets)[i%lenTargets]
 				lastRefID = incRefID(lastRefID)
 				newTarget.RefID = lastRefID
-				newTarget.Datasource = &DatasourceRef{LegacyName: name}
+				newTarget.Datasource = name
 				*targets = append(*targets, newTarget)
 			}
 		}
@@ -995,8 +893,6 @@ func (p *Panel) RepeatTargetsForDatasources(dsNames ...string) {
 		repeatTarget(dsNames, &p.TablePanel.Targets)
 	case HeatmapType:
 		repeatTarget(dsNames, &p.HeatmapPanel.Targets)
-	case TimeseriesType:
-		repeatTarget(dsNames, &p.TimeseriesPanel.Targets)
 	}
 }
 
@@ -1016,8 +912,6 @@ func (p *Panel) GetTargets() *[]Target {
 		return &p.BarGaugePanel.Targets
 	case HeatmapType:
 		return &p.HeatmapPanel.Targets
-	case TimeseriesType:
-		return &p.TimeseriesPanel.Targets
 	default:
 		return nil
 	}
@@ -1080,12 +974,6 @@ func (p *Panel) UnmarshalJSON(b []byte) (err error) {
 			p.OfType = HeatmapType
 			if err = json.Unmarshal(b, &heatmap); err == nil {
 				p.HeatmapPanel = &heatmap
-			}
-		case "timeseries":
-			var timeseries TimeseriesPanel
-			p.OfType = TimeseriesType
-			if err = json.Unmarshal(b, &timeseries); err == nil {
-				p.TimeseriesPanel = &timeseries
 			}
 		case "row":
 			var rowpanel RowPanel
@@ -1172,12 +1060,6 @@ func (p *Panel) MarshalJSON() ([]byte, error) {
 			HeatmapPanel
 		}{p.CommonPanel, *p.HeatmapPanel}
 		return json.Marshal(outHeatmap)
-	case TimeseriesType:
-		var outTimeseries = struct {
-			CommonPanel
-			TimeseriesPanel
-		}{p.CommonPanel, *p.TimeseriesPanel}
-		return json.Marshal(outTimeseries)
 	case CustomType:
 		var outCustom = customPanelOutput{
 			p.CommonPanel,
