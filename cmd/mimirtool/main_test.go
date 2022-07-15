616c6a630a58416ff0e787c913fb2fa407ee57cf
Arve Knudsen <arve.knudsen@gmail.com>
2022-07-15 18:59:04 +0200

    Add test skeleton
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- cmd/mimirtool/main_test.go
+++ cmd/mimirtool/main_test.go
@@ -2,7 +2,48 @@
 
 package main
 
-import "testing"
+import (
+	"net"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/weaveworks/common/server"
+)
 
 func TestUploadBlock(t *testing.T) {
+	serverCfg := getServerConfig(t)
+	srv, err := server.New(serverCfg)
+	require.NoError(t, err)
+	go func() { _ = srv.Run() }()
+	t.Cleanup(srv.Stop)
+}
+
+func getServerConfig(t *testing.T) server.Config {
+	t.Helper()
+
+	grpcHost, grpcPortNum := getHostnameAndRandomPort(t)
+	httpHost, httpPortNum := getHostnameAndRandomPort(t)
+	return server.Config{
+		HTTPListenAddress:        httpHost,
+		HTTPListenPort:           httpPortNum,
+		GRPCListenAddress:        grpcHost,
+		GRPCListenPort:           grpcPortNum,
+		GPRCServerMaxRecvMsgSize: 1024,
+	}
+}
+
+func getHostnameAndRandomPort(t *testing.T) (string, int) {
+	t.Helper()
+
+	listen, err := net.Listen("tcp", "localhost:0")
+	require.NoError(t, err)
+
+	host, port, err := net.SplitHostPort(listen.Addr().String())
+	require.NoError(t, err)
+	require.NoError(t, listen.Close())
+
+	portNum, err := strconv.Atoi(port)
+	require.NoError(t, err)
+	return host, portNum
 }
