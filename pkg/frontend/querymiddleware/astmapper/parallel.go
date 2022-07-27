c414eba2b1d9db428dfd62b54f6eb47233265888
Oleg Zaytsev <mail@olegzaytsev.com>
2022-07-27 15:46:26 +0200

    Update the comment
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

--- pkg/frontend/querymiddleware/astmapper/parallel.go
+++ pkg/frontend/querymiddleware/astmapper/parallel.go
@@ -66,12 +66,16 @@ func CanParallelize(expr parser.Expr, logger log.Logger) bool {
 		return err == nil && !nestedAggrs && CanParallelize(e.Expr, logger)
 
 	case *parser.BinaryExpr:
-		// Binary expressions can be parallelised when one of the sides is a constant scalar value
-		// and the other one can be parallelised and does not contain agggregations.
+		// Binary expressions can be parallelised when:
+		// - It's not a bool expr: bool expression should yield only one result, but sharding would provide many.
+		// - One of the sides is a constant scalar value
+		// - The other side:
+		//   - Is not a constant scalar value (because why would we shard then?)
+		//   - Does not contain aggregations
+		//
 		// If one side contained aggregations, like sum(foo) > 0, then aggregated values from different shards can cancel
 		// each other, like foo{shard="1"}=1 and foo{shard="2"}=-1: aggregated sum is zero, but if we concat results from different shards it's not.
-		// Both comparison and arithmetic binary operations _can_ be parallelised, but not all of them are worth parallelising,
-		// this function doesn't decide that.
+		//
 		// Since we don't care about the order in which binary op is written, we extract the condition into a lambda and check both ways.
 		parallelisable := func(a, b parser.Expr) bool {
 			return CanParallelize(a, logger) && noAggregates(a) && !isConstantScalar(a) && isConstantScalar(b)
