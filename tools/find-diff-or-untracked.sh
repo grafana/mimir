4ec827402a1a2c8761919c77f368afd76b5c8671
György Krajcsovits <gyorgy.krajcsovits@grafana.com>
2022-07-28 08:58:27 +0200

    Simplify algorithm
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- tools/find-diff-or-untracked.sh
+++ tools/find-diff-or-untracked.sh
@@ -3,24 +3,13 @@
 
 set -eu -o pipefail
 
-function join_to_regex {
-    for i in "$@" ; do
-        echo -n "$i|"
-    done
-}
-
 function find_diff_or_untracked {
     git diff --exit-code -- "$@"
 
-    local regex=$(join_to_regex "$@")
-    regex=${regex::-1}
-    local untracked_files=$(git ls-files --others --exclude-standard)
-    set +e
-	echo "${untracked_files}" | grep -E "^(${regex})"
-    local rc=$?
-    set -e
-    if [ "${rc}" -eq 0 ] ; then  # flip the grep result
-      echo "$0: Untracked files found"
+    local untracked_files=$(git ls-files --others --exclude-standard "$@")
+    if [ -n "${untracked_files}" ] ; then
+      echo "$0: FAIL: untracked files found:"
+      echo "${untracked_files}"
       return 1
     fi
 }
