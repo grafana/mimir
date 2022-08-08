1837e074221846f6b30bd09595987aa48ba0f4e3
Mauro Stettler <mauro.stettler@gmail.com>
2022-08-08 13:07:54 -0400

    remove confusing comment (#2602)
    * remove incorrect comment
    
    Signed-off-by: Mauro Stettler <mauro.stettler@gmail.com>
    
    * better comment
    
    Signed-off-by: Mauro Stettler <mauro.stettler@gmail.com>
    
    * remove "only"
    
    Signed-off-by: Mauro Stettler <mauro.stettler@gmail.com>

--- pkg/distributor/distributor.go
+++ pkg/distributor/distributor.go
@@ -547,7 +547,8 @@ func (d *Distributor) checkSample(ctx context.Context, userID, cluster, replica 
 	// At this point we know we have both HA labels, we should lookup
 	// the cluster/instance here to see if we want to accept this sample.
 	err := d.HATracker.checkReplica(ctx, userID, cluster, replica, time.Now())
-	// checkReplica should only have returned an error if there was a real error talking to Consul, or if the replica labels don't match.
+	// checkReplica would have returned an error if there was a real error talking to Consul,
+	// or if the replica is not the currently elected replica.
 	if err != nil { // Don't accept the sample.
 		return false, err
 	}
