63bb6b0fc4d4b88d34d0e01524f199a08983d88e
Peter Štibraný <pstibrany@gmail.com>
2022-08-12 11:01:27 +0200

    Don't build list of ranges first.
    Signed-off-by: Peter Štibraný <pstibrany@gmail.com>

--- pkg/util/slice_operations.go
+++ pkg/util/slice_operations.go
@@ -2,21 +2,6 @@
 
 package util
 
-import (
-	"sync"
-)
-
-type idxRange struct {
-	from int
-	to   int
-}
-
-var idxRangesPool = sync.Pool{
-	New: func() interface{} {
-		return &[]idxRange{}
-	},
-}
-
 // RemoveSliceIndexes takes a slice of elements of any type and a sorted slice of indexes of elements in the former slice.
 // It removes the elements at the given indexes from the given slice, while minimizing the number of copy operations
 // that are required to do so by grouping consecutive indexes into ranges and removing the ranges in single operations.
@@ -27,62 +12,54 @@ var idxRangesPool = sync.Pool{
 // - The number of elements that have been removed.
 // - The number of ranges of consecutive elements that have been removed.
 func RemoveSliceIndexes[T any](data []T, indexes []int) ([]T, int, int) {
-	ranges := idxRangesPool.Get().(*[]idxRange)
-	none := idxRange{-1, -1}
-	currentRange := none
+	rangeStart := -1
+	rangeEnd := -1
+	rangeCount := 0
+	removed := 0
 
 	// Keep track of the last index to filter duplicates.
-	lastIndex := -1
 	for _, index := range indexes {
-		if index < 0 || index >= len(data) {
+		if index < 0 || index >= len(data)+removed {
 			// Invalid indexes given, ignore them.
 			continue
 		}
 
-		// Skip duplicates.
-		if index == lastIndex {
+		// Skip duplicates or unsorted indexes.
+		if rangeEnd >= 0 && index <= rangeEnd {
 			continue
 		}
-		lastIndex = index
 
-		// Initialize the current range variable.
-		if currentRange == none {
-			currentRange.from = index
-			currentRange.to = index
+		// Initialize the current range.
+		if rangeStart < 0 {
+			rangeStart = index
+			rangeEnd = index
 			continue
 		}
 
 		// Extend the current range by one, because this index connected to it.
-		if currentRange.to+1 == index {
-			currentRange.to = index
+		if rangeEnd+1 == index {
+			rangeEnd = index
 			continue
 		}
 
-		// The current range is finished, add it to the ranges list.
-		*ranges = append(*ranges, currentRange)
-
-		// Initialize the next range.
-		currentRange.from = index
-		currentRange.to = index
-	}
-
-	if currentRange != none {
-		// Add the last range.
-		*ranges = append(*ranges, currentRange)
-	}
-
-	removed := 0
-	for _, r := range *ranges {
-		// Remove range while offsetting by the number of already removed elements.
-		data = append(data[:r.from-removed], data[r.to+1-removed:]...)
+		// Otherwise we can remove the previous range
+		rangeCount++
+		data = append(data[:rangeStart-removed], data[rangeEnd+1-removed:]...)
 
 		// Update the number of removed elements.
-		removed += r.to - r.from + 1
+		removed += rangeEnd - rangeStart + 1
+
+		// Initialize the next range.
+		rangeStart = index
+		rangeEnd = index
 	}
 
-	rangeCount := len(*ranges)
-	*ranges = (*ranges)[:0]
-	idxRangesPool.Put(ranges)
+	// Remove last range.
+	if rangeStart >= 0 {
+		rangeCount++
+		data = append(data[:rangeStart-removed], data[rangeEnd+1-removed:]...)
+		removed += rangeEnd - rangeStart + 1
+	}
 
 	return data, removed, rangeCount
 }
