972147b14411b55eb28d93cd7d2463d8eb503b17
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2022-09-29 11:10:37 +0200

    Update pkg/mimirtool/commands/alerts.go
    Co-authored-by: Ursula Kallio <ursula.kallio@grafana.com>

--- pkg/mimirtool/commands/alerts.go
+++ pkg/mimirtool/commands/alerts.go
@@ -74,7 +74,7 @@ func (a *AlertmanagerCommand) Register(app *kingpin.Application, envVars EnvVarN
 
 	alertCmd.Command("delete", "Delete the Alertmanager configuration that is currently in the Grafana Mimir Alertmanager.").Action(a.deleteConfig)
 
-	loadalertCmd := alertCmd.Command("load", "Load Alertmanager tenant configuration and template files to Grafana Mimir.").Action(a.loadConfig)
+	loadalertCmd := alertCmd.Command("load", "Load Alertmanager tenant configuration and template files into Grafana Mimir.").Action(a.loadConfig)
 	loadalertCmd.Arg("config", "Alertmanager configuration to load").Required().StringVar(&a.AlertmanagerConfigFile)
 	loadalertCmd.Arg("template-files", "The template files to load").ExistingFilesVar(&a.TemplateFiles)
 }
