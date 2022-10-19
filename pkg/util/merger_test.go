2414906b4adcf0e15183f2451270ab7139c89a06
Bryan Boreham <bjboreham@gmail.com>
2022-10-19 15:13:40 -0400

    Add a test for util.MergeSlices

--- pkg/util/merger_test.go
+++ pkg/util/merger_test.go
@@ -84,3 +84,52 @@ func TestMergeNSampleSets(t *testing.T) {
 		require.Equal(t, c.expected, samples)
 	}
 }
+
+func TestMergeSlices(t *testing.T) {
+	tests := []struct {
+		name string
+		args [][]string
+		want []string
+	}{
+		{
+			name: "empty input",
+			want: nil,
+		},
+		{
+			name: "single input",
+			args: [][]string{{"a", "b", "c"}},
+			want: []string{"a", "b", "c"},
+		},
+		{
+			name: "two inputs same",
+			args: [][]string{{"a", "b", "c"}, {"a", "b", "c"}},
+			want: []string{"a", "b", "c"},
+		},
+		{
+			name: "two inputs interleaved",
+			args: [][]string{{"a", "c", "e"}, {"b", "d", "f"}},
+			want: []string{"a", "b", "c", "d", "e", "f"},
+		},
+		{
+			name: "first input short",
+			args: [][]string{{"a"}, {"b", "d", "f"}},
+			want: []string{"a", "b", "d", "f"},
+		},
+		{
+			name: "second input short",
+			args: [][]string{{"a", "c", "e"}, {"b"}},
+			want: []string{"a", "b", "c", "e"},
+		},
+		{
+			name: "some duplicates",
+			args: [][]string{{"a", "c", "e"}, {"b", "c"}},
+			want: []string{"a", "b", "c", "e"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := MergeSlices(tt.args...)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
