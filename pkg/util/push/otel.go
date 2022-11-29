4fcf6fd7a7ce7e16afa959e25c2a5fe0687399e1
Mauro Stettler <mauro.stettler@gmail.com>
2022-11-29 17:58:17 +0000

    Revert "[otlp] Fix panic in dropped count (again!) (#3538)"
    This reverts commit 3744cb501309039eaad84b66501e84e96cec2510.

--- pkg/util/push/otel.go
+++ pkg/util/push/otel.go
@@ -22,7 +22,6 @@ import (
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/pdata/pmetric/pmetricotlp"
-	"go.uber.org/multierr"
 
 	"github.com/grafana/mimir/pkg/mimirpb"
 	"github.com/grafana/mimir/pkg/util"
@@ -133,7 +132,7 @@ func otelMetricsToTimeseries(ctx context.Context, discardedDueToOtelParseError *
 			return nil, err
 		}
 
-		dropped := len(multierr.Errors(errs))
+		dropped := md.DataPointCount() - sampleCountInMap(tsMap)
 		discardedDueToOtelParseError.WithLabelValues(userID).Add(float64(dropped))
 
 		parseErrs := errs.Error()
@@ -228,4 +227,13 @@ func TimeseriesToOTLPRequest(timeseries []prompb.TimeSeries) pmetricotlp.Request
 	}
 
 	return pmetricotlp.NewRequestFromMetrics(d)
+}
+
+func sampleCountInMap(tsMap map[string]*prompb.TimeSeries) int {
+	count := 0
+	for _, ts := range tsMap {
+		count += len(ts.Samples)
+	}
+
+	return count
 }
