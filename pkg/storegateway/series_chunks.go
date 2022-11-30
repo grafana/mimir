f715c241e3ba01e4b9b824c51aa411f1207e0a00
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2022-11-30 14:10:01 +0100

    Update pkg/storegateway/series_chunks.go
    Co-authored-by: Marco Pracucci <marco@pracucci.com>

--- pkg/storegateway/series_chunks.go
+++ pkg/storegateway/series_chunks.go
@@ -15,7 +15,7 @@ type seriesChunksSetIterator interface {
 //
 //nolint:unused // dead code while we are working on PR 3355
 type seriesChunksSet struct {
-	series []seriesEntry // this should ideally be its own type that doesn't have the refs
+	series []seriesEntry
 
 	bytesReleaser releaser
 }
