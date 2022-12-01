3ba7b69e4c3f607e4a07d4f0adeb73fa2333682e
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2022-12-01 18:51:28 +0100

    store-gateway: add iterators for seriesChunks and seriesChunkRefs
    This is the next step to introducing our changes from PR 3355. This
    commit adds the iterators (and tests for them) used over the data
    structures we added in PR 3587.
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
    Co-authored-by: Marco Pracucci <marco@pracucci.com>
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/storegateway/series_chunks_test.go
+++ pkg/storegateway/series_chunks_test.go
@@ -2,6 +2,302 @@
 
 package storegateway
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/tsdb/chunks"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
+
+	"github.com/grafana/mimir/pkg/storegateway/storepb"
+	"github.com/grafana/mimir/pkg/util/test"
+)
+
+func TestSeriesChunksSeriesSet(t *testing.T) {
+	c := generateAggrChunk(6)
+
+	series1 := labels.FromStrings(labels.MetricName, "metric_1")
+	series2 := labels.FromStrings(labels.MetricName, "metric_2")
+	series3 := labels.FromStrings(labels.MetricName, "metric_3")
+	series4 := labels.FromStrings(labels.MetricName, "metric_4")
+	series5 := labels.FromStrings(labels.MetricName, "metric_4")
+
+	// Utility function to create sets, so that each test starts from a clean setup (e.g. releaser is not released).
+	createSets := func() (sets []seriesChunksSet, releasers []*releaserMock) {
+		for i := 0; i < 3; i++ {
+			releasers = append(releasers, newReleaserMock())
+		}
+
+		sets = append(sets,
+			seriesChunksSet{
+				chunksReleaser: releasers[0],
+				series: []seriesEntry{
+					{lset: series1, chks: []storepb.AggrChunk{c[1]}},
+					{lset: series2, chks: []storepb.AggrChunk{c[2]}},
+				}},
+			seriesChunksSet{
+				chunksReleaser: releasers[1],
+				series: []seriesEntry{
+					{lset: series3, chks: []storepb.AggrChunk{c[3]}},
+					{lset: series4, chks: []storepb.AggrChunk{c[4]}},
+				}},
+			seriesChunksSet{
+				chunksReleaser: releasers[2],
+				series: []seriesEntry{
+					{lset: series5, chks: []storepb.AggrChunk{c[5]}},
+				}},
+		)
+
+		return
+	}
+
+	t.Run("should iterate over a single set and release it once done", func(t *testing.T) {
+		sets, releasers := createSets()
+		source := newSliceSeriesChunksSetIterator(sets[0])
+		it := newSeriesChunksSeriesSet(source)
+
+		lbls, chks := it.At()
+		require.Zero(t, lbls)
+		require.Zero(t, chks)
+		require.NoError(t, it.Err())
+
+		require.True(t, it.Next())
+		lbls, chks = it.At()
+		require.Equal(t, series1, lbls)
+		require.Equal(t, []storepb.AggrChunk{c[1]}, chks)
+		require.NoError(t, it.Err())
+		require.False(t, releasers[0].isReleased())
+
+		require.True(t, it.Next())
+		lbls, chks = it.At()
+		require.Equal(t, series2, lbls)
+		require.Equal(t, []storepb.AggrChunk{c[2]}, chks)
+		require.NoError(t, it.Err())
+		require.False(t, releasers[0].isReleased())
+
+		require.False(t, it.Next())
+		lbls, chks = it.At()
+		require.Zero(t, lbls)
+		require.Zero(t, chks)
+		require.NoError(t, it.Err())
+		require.True(t, releasers[0].isReleased())
+	})
+
+	t.Run("should iterate over a multiple sets and release each set once we begin to iterate the next one", func(t *testing.T) {
+		sets, releasers := createSets()
+		source := newSliceSeriesChunksSetIterator(sets[0], sets[1])
+		it := newSeriesChunksSeriesSet(source)
+
+		lbls, chks := it.At()
+		require.Zero(t, lbls)
+		require.Zero(t, chks)
+		require.NoError(t, it.Err())
+
+		// Set 1.
+		require.True(t, it.Next())
+		lbls, chks = it.At()
+		require.Equal(t, series1, lbls)
+		require.Equal(t, []storepb.AggrChunk{c[1]}, chks)
+		require.NoError(t, it.Err())
+		require.False(t, releasers[0].isReleased())
+		require.False(t, releasers[1].isReleased())
+
+		require.True(t, it.Next())
+		lbls, chks = it.At()
+		require.Equal(t, series2, lbls)
+		require.Equal(t, []storepb.AggrChunk{c[2]}, chks)
+		require.NoError(t, it.Err())
+		require.False(t, releasers[0].isReleased())
+		require.False(t, releasers[1].isReleased())
+
+		// Set 2.
+		require.True(t, it.Next())
+		lbls, chks = it.At()
+		require.Equal(t, series3, lbls)
+		require.Equal(t, []storepb.AggrChunk{c[3]}, chks)
+		require.NoError(t, it.Err())
+		require.True(t, releasers[0].isReleased())
+		require.False(t, releasers[1].isReleased())
+
+		require.True(t, it.Next())
+		lbls, chks = it.At()
+		require.Equal(t, series4, lbls)
+		require.Equal(t, []storepb.AggrChunk{c[4]}, chks)
+		require.NoError(t, it.Err())
+		require.True(t, releasers[0].isReleased())
+		require.False(t, releasers[1].isReleased())
+
+		require.False(t, it.Next())
+		lbls, chks = it.At()
+		require.Zero(t, lbls)
+		require.Zero(t, chks)
+		require.NoError(t, it.Err())
+		require.True(t, releasers[0].isReleased())
+	})
+
+	t.Run("should release the current set on error", func(t *testing.T) {
+		expectedErr := errors.New("mocked error")
+
+		sets, releasers := createSets()
+		source := newSliceSeriesChunksSetIteratorWithError(expectedErr, 1, sets[0], sets[1], sets[2])
+		it := newSeriesChunksSeriesSet(source)
+
+		lbls, chks := it.At()
+		require.Zero(t, lbls)
+		require.Zero(t, chks)
+		require.NoError(t, it.Err())
+
+		require.True(t, it.Next())
+		lbls, chks = it.At()
+		require.Equal(t, series1, lbls)
+		require.Equal(t, []storepb.AggrChunk{c[1]}, chks)
+		require.NoError(t, it.Err())
+		require.False(t, releasers[0].isReleased())
+
+		require.True(t, it.Next())
+		lbls, chks = it.At()
+		require.Equal(t, series2, lbls)
+		require.Equal(t, []storepb.AggrChunk{c[2]}, chks)
+		require.NoError(t, it.Err())
+		require.False(t, releasers[0].isReleased())
+
+		require.False(t, it.Next())
+		lbls, chks = it.At()
+		require.Zero(t, lbls)
+		require.Zero(t, chks)
+		require.Equal(t, expectedErr, it.Err())
+
+		// The current set is released.
+		require.True(t, releasers[0].isReleased())
+
+		// Can't release the next ones because can't move forward with the iteration (due to the error).
+		require.False(t, releasers[1].isReleased())
+		require.False(t, releasers[2].isReleased())
+	})
+}
+
+func TestPreloadingSeriesChunkSetIterator(t *testing.T) {
+	test.VerifyNoLeak(t)
+
+	const delay = 10 * time.Millisecond
+
+	// Create some sets, each set containing 1 series.
+	sets := make([]seriesChunksSet, 0, 10)
+	for i := 0; i < 10; i++ {
+		sets = append(sets, seriesChunksSet{
+			series: []seriesEntry{{
+				lset: labels.FromStrings("__name__", fmt.Sprintf("metric_%d", i)),
+				refs: []chunks.ChunkRef{chunks.ChunkRef(i)},
+			}},
+		})
+	}
+
+	t.Run("should iterate all sets if no error occurs", func(t *testing.T) {
+		for preloadSize := 1; preloadSize <= len(sets)+1; preloadSize++ {
+			preloadSize := preloadSize
+
+			t.Run(fmt.Sprintf("preload size: %d", preloadSize), func(t *testing.T) {
+				t.Parallel()
+
+				source := newSliceSeriesChunksSetIterator(sets...)
+				source = newDelayedSeriesChunksSetIterator(delay, source)
+
+				preloading := newPreloadingSeriesChunkSetIterator(context.Background(), preloadSize, source)
+
+				// Ensure expected sets are returned in order.
+				expectedIdx := 0
+				for preloading.Next() {
+					require.NoError(t, preloading.Err())
+					require.Equal(t, sets[expectedIdx], preloading.At())
+					expectedIdx++
+				}
+
+				// Ensure all sets have been returned.
+				require.NoError(t, preloading.Err())
+				require.Equal(t, len(sets), expectedIdx)
+			})
+		}
+	})
+
+	t.Run("should stop iterating once an error is found", func(t *testing.T) {
+		for preloadSize := 1; preloadSize <= len(sets)+1; preloadSize++ {
+			preloadSize := preloadSize
+
+			t.Run(fmt.Sprintf("preload size: %d", preloadSize), func(t *testing.T) {
+				t.Parallel()
+
+				source := newSliceSeriesChunksSetIteratorWithError(errors.New("mocked error"), len(sets), sets...)
+				source = newDelayedSeriesChunksSetIterator(delay, source)
+
+				preloading := newPreloadingSeriesChunkSetIterator(context.Background(), preloadSize, source)
+
+				// Ensure expected sets are returned in order.
+				expectedIdx := 0
+				for preloading.Next() {
+					require.NoError(t, preloading.Err())
+					require.Equal(t, sets[expectedIdx], preloading.At())
+					expectedIdx++
+				}
+
+				// Ensure an error is returned at the end.
+				require.Error(t, preloading.Err())
+				require.Equal(t, len(sets), expectedIdx)
+			})
+		}
+	})
+
+	t.Run("should not leak preloading goroutine if caller doesn't iterated until the end of sets but context is canceled", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancelCtx := context.WithCancel(context.Background())
+
+		source := newSliceSeriesChunksSetIteratorWithError(errors.New("mocked error"), len(sets), sets...)
+		source = newDelayedSeriesChunksSetIterator(delay, source)
+
+		preloading := newPreloadingSeriesChunkSetIterator(ctx, 1, source)
+
+		// Just call Next() once.
+		require.True(t, preloading.Next())
+		require.NoError(t, preloading.Err())
+		require.Equal(t, sets[0], preloading.At())
+
+		// Cancel the context.
+		cancelCtx()
+
+		// Give a short time to the preloader goroutine to react to the context cancellation.
+		// This is required to avoid a flaky test.
+		time.Sleep(100 * time.Millisecond)
+
+		// At this point we expect Next() to return false.
+		require.False(t, preloading.Next())
+		require.NoError(t, preloading.Err())
+	})
+
+	t.Run("should not leak preloading goroutine if caller doesn't call Next() until false but context is canceled", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, cancelCtx := context.WithCancel(context.Background())
+
+		source := newSliceSeriesChunksSetIteratorWithError(errors.New("mocked error"), len(sets), sets...)
+		source = newDelayedSeriesChunksSetIterator(delay, source)
+
+		preloading := newPreloadingSeriesChunkSetIterator(ctx, 1, source)
+
+		// Just call Next() once.
+		require.True(t, preloading.Next())
+		require.NoError(t, preloading.Err())
+		require.Equal(t, sets[0], preloading.At())
+
+		// Cancel the context. Do NOT call Next() after canceling the context.
+		cancelCtx()
+	})
+}
+
 // sliceSeriesChunksSetIterator implements seriesChunksSetIterator and
 // returns the provided err when the sets are exhausted
 //
@@ -10,21 +306,37 @@ type sliceSeriesChunksSetIterator struct {
 	current int
 	sets    []seriesChunksSet
 
-	err error
+	err   error
+	errAt int
 }
 
 //nolint:unused // dead code while we are working on PR 3355
-func newSliceSeriesChunksSetIterator(err error, sets ...seriesChunksSet) seriesChunksSetIterator {
+func newSliceSeriesChunksSetIterator(sets ...seriesChunksSet) seriesChunksSetIterator {
+	return &sliceSeriesChunksSetIterator{
+		current: -1,
+		sets:    sets,
+	}
+}
+
+//nolint:unused // dead code while we are working on PR 3355
+func newSliceSeriesChunksSetIteratorWithError(err error, errAt int, sets ...seriesChunksSet) seriesChunksSetIterator {
 	return &sliceSeriesChunksSetIterator{
 		current: -1,
 		sets:    sets,
 		err:     err,
+		errAt:   errAt,
 	}
 }
 
 //nolint:unused // dead code while we are working on PR 3355
 func (s *sliceSeriesChunksSetIterator) Next() bool {
 	s.current++
+
+	// If the next item should fail, we return false. The Err() function will return the error.
+	if s.err != nil && s.current >= s.errAt {
+		return false
+	}
+
 	return s.current < len(s.sets)
 }
 
@@ -35,8 +347,85 @@ func (s *sliceSeriesChunksSetIterator) At() seriesChunksSet {
 
 //nolint:unused // dead code while we are working on PR 3355
 func (s *sliceSeriesChunksSetIterator) Err() error {
-	if s.current >= len(s.sets) {
+	if s.err != nil && s.current >= s.errAt {
 		return s.err
 	}
 	return nil
+}
+
+// delayedSeriesChunksSetIterator implements seriesChunksSetIterator and
+// introduces an artificial delay before returning from Next() and At().
+type delayedSeriesChunksSetIterator struct {
+	wrapped seriesChunksSetIterator
+	delay   time.Duration
+}
+
+func newDelayedSeriesChunksSetIterator(delay time.Duration, wrapped seriesChunksSetIterator) seriesChunksSetIterator {
+	return &delayedSeriesChunksSetIterator{
+		wrapped: wrapped,
+		delay:   delay,
+	}
+}
+
+func (s *delayedSeriesChunksSetIterator) Next() bool {
+	time.Sleep(s.delay)
+	return s.wrapped.Next()
+}
+
+func (s *delayedSeriesChunksSetIterator) At() seriesChunksSet {
+	time.Sleep(s.delay)
+	return s.wrapped.At()
+}
+
+func (s *delayedSeriesChunksSetIterator) Err() error {
+	return s.wrapped.Err()
+}
+
+func generateAggrChunk(num int) []storepb.AggrChunk {
+	out := make([]storepb.AggrChunk, 0, num)
+
+	for i := 0; i < num; i++ {
+		out = append(out, storepb.AggrChunk{
+			MinTime: int64(i),
+			MaxTime: int64(i),
+		})
+	}
+
+	return out
+}
+
+type releaserMock struct {
+	released *atomic.Bool
+}
+
+func newReleaserMock() *releaserMock {
+	return &releaserMock{
+		released: atomic.NewBool(false),
+	}
+}
+
+func (r *releaserMock) Release() {
+	r.released.Store(true)
+}
+
+func (r *releaserMock) isReleased() bool {
+	return r.released.Load()
+}
+
+//nolint:unused // dead code while we are working on PR 3355
+func readAllSeriesChunksSets(it seriesChunksSetIterator) []seriesChunksSet {
+	var out []seriesChunksSet
+	for it.Next() {
+		out = append(out, it.At())
+	}
+	return out
+}
+
+func readAllSeriesLabels(it storepb.SeriesSet) []labels.Labels {
+	var out []labels.Labels
+	for it.Next() {
+		lbls, _ := it.At()
+		out = append(out, lbls)
+	}
+	return out
 }
