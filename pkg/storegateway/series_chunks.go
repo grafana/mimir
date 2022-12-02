2dde45d683a91470f1f0eb9f09772a5c2a2363f2
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2022-12-02 12:22:38 +0100

    store-gateway: add iterators for seriesChunks and seriesChunkRefs (#3620)
    This is the next step to introducing our changes from PR 3355. This
    commit adds the iterators (and tests for them) used over the data
    structures we added in PR 3587.
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
    Co-authored-by: Marco Pracucci <marco@pracucci.com>

--- pkg/storegateway/series_chunks.go
+++ pkg/storegateway/series_chunks.go
@@ -2,6 +2,14 @@
 
 package storegateway
 
+import (
+	"context"
+
+	"github.com/prometheus/prometheus/model/labels"
+
+	"github.com/grafana/mimir/pkg/storegateway/storepb"
+)
+
 // seriesChunksSetIterator is the interface implemented by an iterator returning a sequence of seriesChunksSet.
 //
 //nolint:unused // dead code while we are working on PR 3355
@@ -17,26 +25,134 @@ type seriesChunksSetIterator interface {
 type seriesChunksSet struct {
 	series []seriesEntry
 
-	bytesReleaser releaser
+	// chunksReleaser releases the memory used to allocate series chunks.
+	chunksReleaser chunksReleaser
 }
 
 //nolint:unused // dead code while we are working on PR 3355
-type releaser interface{ Release() }
+type chunksReleaser interface {
+	// Release the memory used to allocate series chunks.
+	Release()
+}
 
 //nolint:unused // dead code while we are working on PR 3355
 func (b *seriesChunksSet) release() {
-	if len(b.series) == 0 {
-		// There's nothing to release, just return; this also allows to call release() on a zero-valued seriesChunksSet.
-		return
+	if b.chunksReleaser != nil {
+		b.chunksReleaser.Release()
+		b.chunksReleaser = nil
 	}
 
-	b.bytesReleaser.Release()
-
-	// Make it harder to do a "use after free".
 	b.series = nil
 }
 
 //nolint:unused // dead code while we are working on PR 3355
 func (b seriesChunksSet) len() int {
 	return len(b.series)
+}
+
+type seriesChunksSeriesSet struct {
+	from seriesChunksSetIterator
+
+	currSet    seriesChunksSet
+	currOffset int
+}
+
+func newSeriesChunksSeriesSet(from seriesChunksSetIterator) storepb.SeriesSet {
+	return &seriesChunksSeriesSet{
+		from: from,
+	}
+}
+
+// Next advances to the next item. Once the underlying seriesChunksSet has been fully consumed
+// (which means the call to Next moves to the next set), the seriesChunksSet is released. This
+// means that it's not safe to read from the values returned by At() after Next() is called again.
+func (b *seriesChunksSeriesSet) Next() bool {
+	b.currOffset++
+	if b.currOffset >= b.currSet.len() {
+		if !b.from.Next() {
+			b.currSet.release()
+			return false
+		}
+		b.currSet.release()
+		b.currSet = b.from.At()
+		b.currOffset = 0
+	}
+	return true
+}
+
+// At returns the current series. The result from At() MUST not be retained after calling Next()
+func (b *seriesChunksSeriesSet) At() (labels.Labels, []storepb.AggrChunk) {
+	if b.currOffset >= b.currSet.len() {
+		return nil, nil
+	}
+
+	return b.currSet.series[b.currOffset].lset, b.currSet.series[b.currOffset].chks
+}
+
+func (b *seriesChunksSeriesSet) Err() error {
+	return b.from.Err()
+}
+
+// preloadedSeriesChunksSet holds the result of preloading the next set. It can either contain
+// the preloaded set or an error, but not both.
+type preloadedSeriesChunksSet struct {
+	set seriesChunksSet
+	err error
+}
+
+type preloadingSeriesChunkSetIterator struct {
+	ctx     context.Context
+	from    seriesChunksSetIterator
+	current seriesChunksSet
+
+	preloaded chan preloadedSeriesChunksSet
+	err       error
+}
+
+func newPreloadingSeriesChunkSetIterator(ctx context.Context, preloadedSetsCount int, from seriesChunksSetIterator) *preloadingSeriesChunkSetIterator {
+	preloadedSet := &preloadingSeriesChunkSetIterator{
+		ctx:       ctx,
+		from:      from,
+		preloaded: make(chan preloadedSeriesChunksSet, preloadedSetsCount-1), // one will be kept outside the channel when the channel blocks
+	}
+	go preloadedSet.preload()
+	return preloadedSet
+}
+
+func (p *preloadingSeriesChunkSetIterator) preload() {
+	defer close(p.preloaded)
+
+	for p.from.Next() {
+		select {
+		case <-p.ctx.Done():
+			// If the context is done, we should just stop the preloading goroutine.
+			return
+		case p.preloaded <- preloadedSeriesChunksSet{set: p.from.At()}:
+		}
+	}
+
+	if p.from.Err() != nil {
+		p.preloaded <- preloadedSeriesChunksSet{err: p.from.Err()}
+	}
+}
+
+func (p *preloadingSeriesChunkSetIterator) Next() bool {
+	preloaded, ok := <-p.preloaded
+	if !ok {
+		// Iteration reached the end or context has been canceled.
+		return false
+	}
+
+	p.current = preloaded.set
+	p.err = preloaded.err
+
+	return p.err == nil
+}
+
+func (p *preloadingSeriesChunkSetIterator) At() seriesChunksSet {
+	return p.current
+}
+
+func (p *preloadingSeriesChunkSetIterator) Err() error {
+	return p.err
 }
