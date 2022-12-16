94b3648b92caba957078b8d76c7e10bc11d870da
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2022-12-16 15:34:56 +0100

    Remove unnecessary test
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/storegateway/snappy_gob_codec_test.go
+++ pkg/storegateway/snappy_gob_codec_test.go
@@ -5,7 +5,6 @@ package storegateway
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/prometheus/prometheus/model/labels"
@@ -57,36 +56,5 @@ func TestSnappyGobSeriesCacheEntryCodec(t *testing.T) {
 		var decoded testType
 		err = decodeSnappyGob(data, &decoded)
 		require.Error(t, err)
-	})
-
-	t.Run("encoding and decoding in different order", func(t *testing.T) {
-		entry2 := testType{
-			LabelSets: []labels.Labels{
-				labels.FromStrings("a", "1"),
-				labels.FromStrings("a", "2"),
-			},
-			MatchersKey: indexcache.CanonicalLabelMatchersKey([]*labels.Matcher{labels.MustNewMatcher(labels.MatchRegexp, "foo", "bar")}),
-		}
-
-		var (
-			data [3][]byte
-			err  error
-		)
-		data[0], err = encodeSnappyGob(entry)
-		assert.NoError(t, err)
-		data[1], err = encodeSnappyGob(entry2)
-		assert.NoError(t, err)
-		data[2], err = encodeSnappyGob(entry)
-		assert.NoError(t, err)
-
-		var decoded testType
-		assert.NoError(t, decodeSnappyGob(data[1], &decoded))
-		assert.Equal(t, entry2, decoded)
-
-		assert.NoError(t, decodeSnappyGob(data[0], &decoded))
-		assert.Equal(t, entry, decoded)
-
-		assert.NoError(t, decodeSnappyGob(data[2], &decoded))
-		assert.Equal(t, entry, decoded)
 	})
 }
