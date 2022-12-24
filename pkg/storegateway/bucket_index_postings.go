41803af08d0163ba0c15211987b02b26acfe0749
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2022-12-24 01:21:20 +0100

    WIP
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/storegateway/bucket_index_postings.go
+++ pkg/storegateway/bucket_index_postings.go
@@ -69,6 +69,8 @@ func toPostingGroup(lvr labelValuesReader, m *labels.Matcher) (*postingGroup, er
 		// Fast-path for equal matching.
 		// Works for every case except for `foo=""`, which is a special case, see below.
 		if m.Type == labels.MatchEqual {
+			// TODO we only need to know whether there are any values that match. We don't care what they are,
+			// 		so we can probably avoid doing some allocations in the postings offset table
 			matchingVals, err := lvr.LabelValues(m.Name, m.Matches)
 			if err != nil {
 				return nil, err
