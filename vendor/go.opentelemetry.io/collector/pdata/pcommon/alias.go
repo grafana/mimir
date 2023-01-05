58d8a3fd6c4e5272375307b321acf5b5871c7545
Johanna Ratliff <jmarg.smith@gmail.com>
2023-01-04 18:09:46 -0700

    Cherry-pick https://github.com/grafana/mimir/pull/3853/files to r217

--- vendor/go.opentelemetry.io/collector/pdata/pcommon/alias.go
+++ vendor/go.opentelemetry.io/collector/pdata/pcommon/alias.go
@@ -75,12 +75,6 @@ var (
 
 	// NewValueBytes creates a new Value with the given ImmutableByteSlice value.
 	NewValueBytes = internal.NewValueBytes
-
-	// NewValueMBytes creates a new Value with the given []byte value.
-	// The caller must ensure the []byte passed in is not modified after the call is made, sharing the data
-	// across multiple attributes is forbidden.
-	// Deprecated: [0.54.0] Use NewValueBytes instead.
-	NewValueMBytes = internal.NewValueMBytes
 )
 
 // Map stores a map of string keys to elements of Value type.
@@ -93,3 +87,6 @@ var (
 	// NewMapFromRaw creates a Map with values from the given map[string]interface{}.
 	NewMapFromRaw = internal.NewMapFromRaw
 )
+
+// NewSliceFromRaw creates a Slice with values from the given []interface{}.
+var NewSliceFromRaw = internal.NewSliceFromRaw
