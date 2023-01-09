d08d9b83da81aaa4f374a61ed12c87f12eef65bc
Marco Pracucci <marco@pracucci.com>
2023-01-09 13:02:19 +0100

    Remove omitempty to get Helm comparison work correctly
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/util/validation/limits.go
+++ pkg/util/validation/limits.go
@@ -117,7 +117,7 @@ type Limits struct {
 	SplitInstantQueriesByInterval  model.Duration `yaml:"split_instant_queries_by_interval" json:"split_instant_queries_by_interval" category:"experimental"`
 
 	// Query-frontend limits.
-	MaxTotalQueryLength model.Duration `yaml:"max_total_query_length,omitempty" json:"max_total_query_length,omitempty" category:"experimental"`
+	MaxTotalQueryLength model.Duration `yaml:"max_total_query_length" json:"max_total_query_length" category:"experimental"`
 
 	// Cardinality
 	CardinalityAnalysisEnabled                    bool `yaml:"cardinality_analysis_enabled" json:"cardinality_analysis_enabled"`
