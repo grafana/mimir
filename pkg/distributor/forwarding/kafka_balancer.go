c2873b570ffec7f6c825286a253501f1241374a7
Mauro Stettler <mauro.stettler@gmail.com>
2023-01-23 20:37:55 +0100

    delete unused file
    Signed-off-by: Mauro Stettler <mauro.stettler@gmail.com>

--- pkg/distributor/forwarding/kafka_balancer.go
+++
@@ -1,5 +0,0 @@
-package forwarding
-
-// kafkaBalancer is a load balancer for kafka which distributes the messages in such a way that
-// samples which should be aggregated together get written into the same partition.
-type kafkaBalancer struct{}
