4d0dde4189cdd31da6410dddb9ec7ba3f4c103a2
Mauro Stettler <mauro.stettler@gmail.com>
2023-01-30 17:19:41 +0100

    add benchmark to compose kafka key
    Signed-off-by: Mauro Stettler <mauro.stettler@gmail.com>

--- pkg/util/kafka/kafka_test.go
+++ pkg/util/kafka/kafka_test.go
@@ -8,88 +8,101 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestComposeKafkaKey(t *testing.T) {
-	type testCase struct {
-		name      string
-		buf       []byte
-		user      []byte
-		labels    []mimirpb.LabelAdapter
-		rules     validation.ForwardingRules
-		expectKey []byte
-	}
+type kafkaKeyTestCase struct {
+	name      string
+	buf       []byte
+	user      []byte
+	labels    []mimirpb.LabelAdapter
+	rules     validation.ForwardingRules
+	expectKey []byte
+}
 
-	testCases := []testCase{
-		{
-			name: "normal happy case",
-			buf:  nil,
-			user: []byte("user"),
-			labels: []mimirpb.LabelAdapter{
-				{
-					Name:  "__name__",
-					Value: "test_metric",
-				}, {
-					Name:  "foo",
-					Value: "bar",
-				}, {
-					Name:  "baz",
-					Value: "qux",
-				},
+var kafkaKeyTestCases = []kafkaKeyTestCase{
+	{
+		name: "normal happy case",
+		buf:  nil,
+		user: []byte("user"),
+		labels: []mimirpb.LabelAdapter{
+			{
+				Name:  "__name__",
+				Value: "test_metric",
+			}, {
+				Name:  "foo",
+				Value: "bar",
+			}, {
+				Name:  "baz",
+				Value: "qux",
 			},
-			rules: validation.ForwardingRules{
-				"test_metric": {
-					DropLabels: []string{"baz"},
-				},
-			},
-			expectKey: []byte("user\xfe{__dropped_labels__=\"baz\", __name__=\"test_metric\", foo=\"bar\"}"),
-		}, {
-			name: "dropping all labels",
-			buf:  nil,
-			user: []byte("user"),
-			labels: []mimirpb.LabelAdapter{
-				{
-					Name:  "__name__",
-					Value: "test_metric",
-				}, {
-					Name:  "foo",
-					Value: "bar",
-				},
-			},
-			rules: validation.ForwardingRules{
-				"test_metric": {
-					DropLabels: []string{"foo"},
-				},
-			},
-			expectKey: []byte("user\xfe{__dropped_labels__=\"foo\", __name__=\"test_metric\"}"),
-		}, {
-			name: "dropping multiple labels",
-			buf:  nil,
-			user: []byte("user"),
-			labels: []mimirpb.LabelAdapter{
-				{
-					Name:  "__name__",
-					Value: "test_metric",
-				}, {
-					Name:  "foo",
-					Value: "bar",
-				}, {
-					Name:  "baz",
-					Value: "qux",
-				},
-			},
-			rules: validation.ForwardingRules{
-				"test_metric": {
-					DropLabels: []string{"foo", "baz"},
-				},
-			},
-			expectKey: []byte("user\xfe{__dropped_labels__=\"baz,foo\", __name__=\"test_metric\"}"),
 		},
-	}
+		rules: validation.ForwardingRules{
+			"test_metric": {
+				DropLabels: []string{"baz"},
+			},
+		},
+		expectKey: []byte("user\xfe{__dropped_labels__=\"baz\", __name__=\"test_metric\", foo=\"bar\"}"),
+	}, {
+		name: "dropping all labels",
+		buf:  nil,
+		user: []byte("user"),
+		labels: []mimirpb.LabelAdapter{
+			{
+				Name:  "__name__",
+				Value: "test_metric",
+			}, {
+				Name:  "foo",
+				Value: "bar",
+			},
+		},
+		rules: validation.ForwardingRules{
+			"test_metric": {
+				DropLabels: []string{"foo"},
+			},
+		},
+		expectKey: []byte("user\xfe{__dropped_labels__=\"foo\", __name__=\"test_metric\"}"),
+	}, {
+		name: "dropping multiple labels",
+		buf:  nil,
+		user: []byte("user"),
+		labels: []mimirpb.LabelAdapter{
+			{
+				Name:  "__name__",
+				Value: "test_metric",
+			}, {
+				Name:  "foo",
+				Value: "bar",
+			}, {
+				Name:  "baz",
+				Value: "qux",
+			},
+		},
+		rules: validation.ForwardingRules{
+			"test_metric": {
+				DropLabels: []string{"foo", "baz"},
+			},
+		},
+		expectKey: []byte("user\xfe{__dropped_labels__=\"baz,foo\", __name__=\"test_metric\"}"),
+	},
+}
 
-	for _, tc := range testCases {
+func TestComposeKafkaKey(t *testing.T) {
+	for _, tc := range kafkaKeyTestCases {
 		t.Run(tc.name, func(t *testing.T) {
 			gotKey, err := ComposeKafkaKey(tc.buf, tc.user, tc.labels, tc.rules)
 			require.NoError(t, err)
 			require.Equal(t, tc.expectKey, gotKey)
+		})
+	}
+}
+
+func BenchmarkComposeKafkaKey(b *testing.B) {
+	for _, tc := range kafkaKeyTestCases {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_, err := ComposeKafkaKey(tc.buf, tc.user, tc.labels, tc.rules)
+				if err != nil {
+					b.Fatal(err)
+				}
+			}
 		})
 	}
 }
