50f828fc0d67eca1c70269f3f7b5dd89fc04d4d3
George Krajcsovits <krajorama@users.noreply.github.com>
2023-02-01 11:30:13 +0100

    Add histogram generator functions for e2e tests (#4128)
    A version of these have been tested on sparsehistogram branch.
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- integration/util.go
+++ integration/util.go
@@ -9,12 +9,19 @@ package integration
 import (
 	"bytes"
 	"math"
+	"math/rand"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"time"
 
 	"github.com/pkg/errors"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/model/histogram"
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/prompb"
+	"github.com/prometheus/prometheus/storage/remote"
+	"github.com/prometheus/prometheus/tsdb"
 
 	"github.com/grafana/e2e"
 )
@@ -107,4 +114,167 @@ func getTLSFlagsWithPrefix(prefix string, servername string, http bool) map[stri
 	}
 
 	return flags
+}
+
+func GenerateTestHistogram(i int) *histogram.Histogram {
+	return tsdb.GenerateTestHistograms(i + 1)[i]
+}
+
+func GenerateTestFloatHistogram(i int) *histogram.FloatHistogram {
+	return tsdb.GenerateTestFloatHistograms(i + 1)[i]
+}
+
+// explicit decoded version of GenerateTestHistogram and GenerateTestFloatHistogram
+func GenerateTestSampleHistogram(i int) *model.SampleHistogram {
+	return &model.SampleHistogram{
+		Count: model.FloatString(10 + i*8),
+		Sum:   model.FloatString(18.4 * float64(i+1)),
+		Buckets: model.HistogramBuckets{
+			&model.HistogramBucket{
+				Boundaries: 1,
+				Lower:      -4,
+				Upper:      -2.82842712474619,
+				Count:      model.FloatString(1 + i),
+			},
+			&model.HistogramBucket{
+				Boundaries: 1,
+				Lower:      -2.82842712474619,
+				Upper:      -2,
+				Count:      model.FloatString(1 + i),
+			},
+			&model.HistogramBucket{
+				Boundaries: 1,
+				Lower:      -1.414213562373095,
+				Upper:      -1,
+				Count:      model.FloatString(2 + i),
+			},
+			&model.HistogramBucket{
+				Boundaries: 1,
+				Lower:      -1,
+				Upper:      -0.7071067811865475,
+				Count:      model.FloatString(1 + i),
+			},
+			&model.HistogramBucket{
+				Boundaries: 3,
+				Lower:      -0.001,
+				Upper:      0.001,
+				Count:      model.FloatString(2 + i),
+			},
+			&model.HistogramBucket{
+				Boundaries: 0,
+				Lower:      0.7071067811865475,
+				Upper:      1,
+				Count:      model.FloatString(1 + i),
+			},
+			&model.HistogramBucket{
+				Boundaries: 0,
+				Lower:      1,
+				Upper:      1.414213562373095,
+				Count:      model.FloatString(2 + i),
+			},
+			&model.HistogramBucket{
+				Boundaries: 0,
+				Lower:      2,
+				Upper:      2.82842712474619,
+				Count:      model.FloatString(1 + i),
+			},
+			&model.HistogramBucket{
+				Boundaries: 0,
+				Lower:      2.82842712474619,
+				Upper:      4,
+				Count:      model.FloatString(1 + i),
+			},
+		},
+	}
+}
+
+func GenerateHistogramSeries(name string, ts time.Time, additionalLabels ...prompb.Label) (series []prompb.TimeSeries, vector model.Vector, matrix model.Matrix) {
+	tsMillis := e2e.TimeToMilliseconds(ts)
+
+	value := rand.Intn(1000)
+
+	lbls := append(
+		[]prompb.Label{
+			{Name: labels.MetricName, Value: name},
+		},
+		additionalLabels...,
+	)
+
+	// Generate the series
+	series = append(series, prompb.TimeSeries{
+		Labels: lbls,
+		Exemplars: []prompb.Exemplar{
+			{Value: float64(value), Timestamp: tsMillis, Labels: []prompb.Label{
+				{Name: "trace_id", Value: "1234"},
+			}},
+		},
+		Histograms: []prompb.Histogram{remote.HistogramToHistogramProto(tsMillis, GenerateTestHistogram(value))},
+	})
+
+	// Generate the expected vector and matrix when querying it
+	metric := model.Metric{}
+	metric[labels.MetricName] = model.LabelValue(name)
+	for _, lbl := range additionalLabels {
+		metric[model.LabelName(lbl.Name)] = model.LabelValue(lbl.Value)
+	}
+
+	vector = append(vector, &model.Sample{
+		Metric:    metric,
+		Timestamp: model.Time(tsMillis),
+		Histogram: GenerateTestSampleHistogram(value),
+	})
+
+	matrix = append(matrix, &model.SampleStream{
+		Metric: metric,
+		Histograms: []model.SampleHistogramPair{
+			{
+				Timestamp: model.Time(tsMillis),
+				Histogram: GenerateTestSampleHistogram(value),
+			},
+		},
+	})
+
+	return
+}
+
+func GenerateNHistogramSeries(nSeries, nExemplars int, name func() string, ts time.Time, additionalLabels func() []prompb.Label) (series []prompb.TimeSeries, vector model.Vector) {
+	tsMillis := e2e.TimeToMilliseconds(ts)
+
+	// Generate the series
+	for i := 0; i < nSeries; i++ {
+		lbls := []prompb.Label{
+			{Name: labels.MetricName, Value: name()},
+		}
+		if additionalLabels != nil {
+			lbls = append(lbls, additionalLabels()...)
+		}
+
+		exemplars := []prompb.Exemplar{}
+		if i < nExemplars {
+			exemplars = []prompb.Exemplar{
+				{Value: float64(i), Timestamp: tsMillis, Labels: []prompb.Label{{Name: "trace_id", Value: "1234"}}},
+			}
+		}
+
+		series = append(series, prompb.TimeSeries{
+			Labels:     lbls,
+			Histograms: []prompb.Histogram{remote.HistogramToHistogramProto(tsMillis, GenerateTestHistogram(i))},
+			Exemplars:  exemplars,
+		})
+	}
+
+	// Generate the expected vector when querying it
+	for i := 0; i < nSeries; i++ {
+		metric := model.Metric{}
+		for _, lbl := range series[i].Labels {
+			metric[model.LabelName(lbl.Name)] = model.LabelValue(lbl.Value)
+		}
+
+		vector = append(vector, &model.Sample{
+			Metric:    metric,
+			Timestamp: model.Time(tsMillis),
+			Histogram: GenerateTestSampleHistogram(i),
+		})
+	}
+	return
 }
