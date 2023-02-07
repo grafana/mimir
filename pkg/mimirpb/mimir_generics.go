3d55c06ff0394c0d6c57f53b3d9e73de5a2f5e83
György Krajcsovits <gyorgy.krajcsovits@grafana.com>
2023-02-07 09:15:42 +0100

    Remove provenance from mimir_generics.go
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- pkg/mimirpb/mimir_generics.go
+++ pkg/mimirpb/mimir_generics.go
@@ -1,7 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-// Provenance-includes-location: https://github.com/cortexproject/cortex/blob/master/pkg/querier/queryrange/query_range.go
-// Provenance-includes-license: Apache-2.0
-// Provenance-includes-copyright: The Cortex Authors.
 
 package mimirpb
 
