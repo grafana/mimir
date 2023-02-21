2879bf499988c6a48a346b3a22b3e70de3ddf414
Charles Korn <charles.korn@grafana.com>
2023-02-21 14:27:57 +1100

    Log org ID when comparison fails.
    This makes it easier to manually examine mismatched queries.

--- tools/querytee/proxy_endpoint.go
+++ tools/querytee/proxy_endpoint.go
@@ -165,7 +165,7 @@ func (p *ProxyEndpoint) executeBackendRequests(r *http.Request, resCh chan *back
 		err := p.compareResponses(expectedResponse, actualResponse)
 		if err != nil {
 			level.Error(util_log.Logger).Log("msg", "response comparison failed", "route-name", p.routeName,
-				"query", r.URL.RawQuery, "err", err)
+				"query", r.URL.RawQuery, "org-id", r.Header.Get("X-Scope-OrgID"), "err", err)
 			result = comparisonFailed
 		}
 
