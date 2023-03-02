cec0b52ed66613ad34e843c33d7aa71dda397d77
ying-jeanne <74549700+ying-jeanne@users.noreply.github.com>
2023-03-02 22:22:16 +0100

    Update integration/configs.go
    Co-authored-by: Marco Pracucci <marco@pracucci.com>

--- integration/configs.go
+++ integration/configs.go
@@ -117,7 +117,7 @@ var (
 	RulerFlags = func() map[string]string {
 		return map[string]string{
 			"-ruler.poll-interval":             "2s",
-			"-ruler.evaluation_delay_duration": "0",
+			"-ruler.evaluation-delay-duration": "0",
 		}
 	}
 
