b8925a1c7db820bf804e8ee082dc95c15d62d46b
György Krajcsovits <gyorgy.krajcsovits@grafana.com>
2023-03-08 12:34:06 +0100

    Rework TestChunkMergeIteratorSeek to remove duplications
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- pkg/querier/iterators/chunk_merge_iterator_test.go
+++ pkg/querier/iterators/chunk_merge_iterator_test.go
@@ -202,89 +202,62 @@ func TestChunkMergeIteratorMixed(t *testing.T) {
 
 func TestChunkMergeIteratorSeek(t *testing.T) {
 	for i, tc := range []struct {
-		chunks     []chunk.Chunk
-		mint, maxt int64
-		enc        chunkenc.ValueType
+		enc          chunk.Encoding
+		assertSample func(t *testing.T, iter chunkenc.Iterator, i int64, valueType chunkenc.ValueType)
 	}{
 		{
-			chunks: []chunk.Chunk{
-				mkChunk(t, 0, 100, 1*time.Millisecond, chunk.PrometheusXorChunk),
-				mkChunk(t, 50, 150, 1*time.Millisecond, chunk.PrometheusXorChunk),
-				mkChunk(t, 100, 200, 1*time.Millisecond, chunk.PrometheusXorChunk),
+			enc: chunk.PrometheusXorChunk,
+			assertSample: func(t *testing.T, iter chunkenc.Iterator, i int64, valueType chunkenc.ValueType) {
+				require.Equal(t, chunkenc.ValFloat, valueType)
+				ts, s := iter.At()
+				assert.Equal(t, i, ts)
+				assert.Equal(t, float64(i), s)
+				assert.NoError(t, iter.Err())
 			},
-			maxt: 200,
-			enc:  chunkenc.ValFloat,
 		},
 		{
-			chunks: []chunk.Chunk{
-				mkChunk(t, 0, 100, 1*time.Millisecond, chunk.PrometheusHistogramChunk),
-				mkChunk(t, 50, 150, 1*time.Millisecond, chunk.PrometheusHistogramChunk),
-				mkChunk(t, 100, 200, 1*time.Millisecond, chunk.PrometheusHistogramChunk),
+			enc: chunk.PrometheusHistogramChunk,
+			assertSample: func(t *testing.T, iter chunkenc.Iterator, i int64, valueType chunkenc.ValueType) {
+				require.Equal(t, chunkenc.ValHistogram, valueType)
+				ts, h := iter.AtHistogram()
+				assert.Equal(t, i, ts)
+				test.RequireHistogramEqual(t, generateTestHistogram(int(i)), h)
+				assert.NoError(t, iter.Err())
+				// check auto type conversion for PromQL
+				ts2, fh := iter.AtFloatHistogram()
+				assert.Equal(t, i, ts2)
+				test.RequireFloatHistogramEqual(t, generateTestHistogram(int(i)).ToFloat(), fh)
+				assert.NoError(t, iter.Err())
 			},
-			maxt: 200,
-			enc:  chunkenc.ValHistogram,
 		},
 		{
-			chunks: []chunk.Chunk{
-				mkChunk(t, 0, 100, 1*time.Millisecond, chunk.PrometheusFloatHistogramChunk),
-				mkChunk(t, 50, 150, 1*time.Millisecond, chunk.PrometheusFloatHistogramChunk),
-				mkChunk(t, 100, 200, 1*time.Millisecond, chunk.PrometheusFloatHistogramChunk),
+			enc: chunk.PrometheusFloatHistogramChunk,
+			assertSample: func(t *testing.T, iter chunkenc.Iterator, i int64, valueType chunkenc.ValueType) {
+				require.Equal(t, chunkenc.ValFloatHistogram, valueType)
+				ts, h := iter.AtFloatHistogram()
+				assert.Equal(t, i, ts)
+				test.RequireFloatHistogramEqual(t, generateTestFloatHistogram(int(i)), h)
+				assert.NoError(t, iter.Err())
 			},
-			maxt: 200,
-			enc:  chunkenc.ValFloatHistogram,
 		},
 	} {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
-			for i := int64(tc.mint); i < tc.maxt; i += 20 {
-				iter := NewChunkMergeIterator(tc.chunks, 0, 0)
+			chunks := []chunk.Chunk{
+				mkChunk(t, 0, 100, 1*time.Millisecond, tc.enc),
+				mkChunk(t, 50, 150, 1*time.Millisecond, tc.enc),
+				mkChunk(t, 100, 200, 1*time.Millisecond, tc.enc),
+			}
+			mint := int64(0)
+			maxt := int64(200)
 
-				require.Equal(t, tc.enc, iter.Seek(i))
-				switch tc.enc {
-				case chunkenc.ValFloat:
-					ts, s := iter.At()
-					assert.Equal(t, i, ts)
-					assert.Equal(t, float64(i), s)
-					assert.NoError(t, iter.Err())
-				case chunkenc.ValHistogram:
-					ts, h := iter.AtHistogram()
-					assert.Equal(t, i, ts)
-					test.RequireHistogramEqual(t, generateTestHistogram(int(i)), h)
-					assert.NoError(t, iter.Err())
-					// check auto type conversion for PromQL
-					ts2, fh := iter.AtFloatHistogram()
-					assert.Equal(t, i, ts2)
-					test.RequireFloatHistogramEqual(t, generateTestHistogram(int(i)).ToFloat(), fh)
-					assert.NoError(t, iter.Err())
-				case chunkenc.ValFloatHistogram:
-					ts, h := iter.AtFloatHistogram()
-					assert.Equal(t, i, ts)
-					test.RequireFloatHistogramEqual(t, generateTestFloatHistogram(int(i)), h)
-					assert.NoError(t, iter.Err())
-				default:
-					require.FailNowf(t, "Unexpected encoding", "%v", tc.enc)
-				}
+			for i := mint; i < maxt; i += 20 {
+				iter := NewChunkMergeIterator(chunks, 0, 0)
+				valueType := iter.Seek(i)
+				tc.assertSample(t, iter, i, valueType)
 
-				for j := i + 1; j < tc.maxt; j++ {
-					require.Equal(t, tc.enc, iter.Next())
-					switch tc.enc {
-					case chunkenc.ValFloat:
-						ts, s := iter.At()
-						assert.Equal(t, j, ts)
-						assert.Equal(t, float64(j), s)
-						assert.NoError(t, iter.Err())
-					case chunkenc.ValHistogram:
-						ts, h := iter.AtHistogram()
-						assert.Equal(t, j, ts)
-						test.RequireHistogramEqual(t, generateTestHistogram(int(j)), h)
-						assert.NoError(t, iter.Err())
-					case chunkenc.ValFloatHistogram:
-						ts, h := iter.AtFloatHistogram()
-						assert.Equal(t, j, ts)
-						test.RequireFloatHistogramEqual(t, generateTestFloatHistogram(int(j)), h)
-						assert.NoError(t, iter.Err())
-					default:
-						require.FailNowf(t, "Unexpected encoding", "%v", tc.enc)
-					}
+				for j := i + 1; j < maxt; j++ {
+					valueType := iter.Next()
+					tc.assertSample(t, iter, j, valueType)
 				}
 				assert.Equal(t, chunkenc.ValNone, iter.Next())
 			}
