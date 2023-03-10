88e0e1e2829ce18b13e7287a99dcf39881f6e1d3
inbarpatashnik <75210443+inbarpatashnik@users.noreply.github.com>
2023-03-10 11:35:02 +0000

    get the S3 SDK

--- vendor/github.com/aws/aws-sdk-go-v2/doc.go
+++ vendor/github.com/aws/aws-sdk-go-v2/doc.go
@@ -10,6 +10,7 @@
 // 	go get github.com/aws/aws-sdk-go-v2
 //	go get github.com/aws/aws-sdk-go-v2/config
 //	go get github.com/aws/aws-sdk-go-v2/service/dynamodb
+//	go get github.com/aws/aws-sdk-go
 //
 // Hello AWS
 //
