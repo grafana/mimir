43699007fd6d06af75aaecc3cee75c14511417a5
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2023-03-27 15:13:31 +0200

    store-gateway: Add BenchmarkOpenBlockSeriesChunkRefsSetsIterator
    This PR add a benchmark for BenchmarkOpenBlockSeriesChunkRefsSetsIterator.
    It also includes an internal refactor to allow reusing the test cases
    that BenchmarkBucketIndexReader_ExpandedPostings uses.
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/storegateway/series_refs_test.go
+++ pkg/storegateway/series_refs_test.go
@@ -1630,6 +1630,62 @@ func TestOpenBlockSeriesChunkRefsSetsIterator(t *testing.T) {
 	}
 }
 
+func BenchmarkOpenBlockSeriesChunkRefsSetsIterator(b *testing.B) {
+	tb := test.NewTB(b)
+	const series = 5e6
+
+	newTestBlock := prepareTestBlockWithBinaryReader(tb, appendTestSeries(series))
+
+	testSetups := map[string]struct {
+		indexCache indexcache.IndexCache
+	}{
+		"without index cache": {indexCache: noopCache{}},
+		"with index cache":    {indexCache: newInMemoryIndexCache(tb)},
+	}
+
+	for name, setup := range testSetups {
+		tb.Run(name, func(tb test.TB) {
+			seriesSelectionTestCases(tb, series, func(tb test.TB, testCase seriesSelectionTestCase) {
+				ctx, cancel := context.WithCancel(context.Background())
+				tb.Cleanup(cancel)
+
+				var block = newTestBlock()
+				indexReader := block.indexReader()
+				tb.Cleanup(func() { require.NoError(tb, indexReader.Close()) })
+
+				hashCache := hashcache.NewSeriesHashCache(1024 * 1024).GetBlockCache(block.meta.ULID.String())
+
+				tb.ResetTimer()
+
+				for i := 0; i < tb.N(); i++ {
+					iterator, err := openBlockSeriesChunkRefsSetsIterator(
+						ctx,
+						5000,
+						"",
+						indexReader,
+						setup.indexCache,
+						block.meta,
+						testCase.matchers,
+						nil,
+						cachedSeriesHasher{hashCache},
+						false, // we don't skip chunks, so we can measure impact in loading chunk refs too
+						block.meta.MinTime,
+						block.meta.MaxTime,
+						2,
+						newSafeQueryStats(),
+						nil,
+					)
+					require.NoError(tb, err)
+
+					actualSeriesSets := readAllSeriesChunkRefs(newFlattenedSeriesChunkRefsIterator(iterator))
+					assert.Len(tb, actualSeriesSets, testCase.expectedSeriesLen)
+					assert.NoError(tb, iterator.Err())
+				}
+			})
+		})
+	}
+}
+
 func TestMetasToRanges(t *testing.T) {
 	blockID := ulid.MustNew(1, nil)
 	testCases := map[string]struct {
