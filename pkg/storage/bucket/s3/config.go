5a8afc7fef89fb5f8193cdc68e4d31f1342e24c9
inbarpatashnik <75210443+inbarpatashnik@users.noreply.github.com>
2023-03-27 22:14:49 +0300

    removed unecessary space
    Co-authored-by: Charles Korn <charleskorn@users.noreply.github.com>

--- pkg/storage/bucket/s3/config.go
+++ pkg/storage/bucket/s3/config.go
@@ -42,7 +42,7 @@ var (
 	supportedStorageClasses        = s3_service.ObjectStorageClass_Values()
 	errUnsupportedSignatureVersion = fmt.Errorf("unsupported signature version (supported values: %s)", strings.Join(supportedSignatureVersions, ", "))
 	errUnsupportedSSEType          = errors.New("unsupported S3 SSE type")
-	errUnsupportedStorageClass     = fmt.Errorf("unsupported S3 storage class(supported values: %s)", strings.Join(supportedStorageClasses, ", "))
+	errUnsupportedStorageClass     = fmt.Errorf("unsupported S3 storage class (supported values: %s)", strings.Join(supportedStorageClasses, ", "))
 	errInvalidSSEContext           = errors.New("invalid S3 SSE encryption context")
 	errInvalidEndpointPrefix       = errors.New("the endpoint must not prefixed with the bucket name")
 )
