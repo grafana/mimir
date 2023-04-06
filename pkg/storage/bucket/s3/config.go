c4c098580c4cf0e93cb573456caa9522a870172e
Nick Pillitteri <nick.pillitteri@grafana.com>
2023-04-06 09:59:24 -0400

    Lint fix
    Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

--- pkg/storage/bucket/s3/config.go
+++ pkg/storage/bucket/s3/config.go
@@ -15,10 +15,11 @@ import (
 
 	s3_service "github.com/aws/aws-sdk-go/service/s3"
 	"github.com/grafana/dskit/flagext"
-	"github.com/grafana/mimir/pkg/util"
 	"github.com/minio/minio-go/v7/pkg/encrypt"
 	"github.com/pkg/errors"
 	"github.com/thanos-io/objstore/providers/s3"
+
+	"github.com/grafana/mimir/pkg/util"
 )
 
 const (
