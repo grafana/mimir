5a3e1b0d50705fa7abf3dcfc2f1e0ff5371a098a
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2023-04-10 14:17:26 +0200

    store-gateway: handle deferred matchers for Series()
    This PR adds deferred matchers to the exported interface of the
    bucketIndexReader and adds an iterator which filters out series that do
    not match the deferred matchers.
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/storegateway/series_refs.go
+++ pkg/storegateway/series_refs.go
@@ -702,12 +702,13 @@ func openBlockSeriesChunkRefsSetsIterator(
 		return nil, errors.New("set size must be a positive number")
 	}
 
-	ps, err := indexr.ExpandedPostings(ctx, matchers, stats)
+	ps, deferredMatchers, err := indexr.ExpandedPostings(ctx, matchers, stats)
 	if err != nil {
 		return nil, errors.Wrap(err, "expanded matching postings")
 	}
 
-	iterator := newLoadingSeriesChunkRefsSetIterator(
+	var iterator seriesChunkRefsSetIterator
+	iterator = newLoadingSeriesChunkRefsSetIterator(
 		ctx,
 		newPostingsSetsIterator(ps, batchSize),
 		indexr,
@@ -723,6 +724,9 @@ func openBlockSeriesChunkRefsSetsIterator(
 		chunkRangesPerSeries,
 		logger,
 	)
+	if len(deferredMatchers) > 0 {
+		iterator = &matchingSeriesChunkRefsSetIterator{from: iterator, matchers: deferredMatchers}
+	}
 
 	// Track the time spent loading series and chunk refs.
 	return newNextDurationMeasuringIterator[seriesChunkRefsSet](iterator, func(duration time.Duration, _ bool) {
@@ -1028,6 +1032,52 @@ func (s *loadingSeriesChunkRefsSetIterator) loadSeries(ref storage.SeriesRef, lo
 	}
 
 	return lset, s.chunkMetasBuffer, nil
+}
+
+type matchingSeriesChunkRefsSetIterator struct {
+	from     seriesChunkRefsSetIterator
+	matchers []*labels.Matcher
+
+	current seriesChunkRefsSet
+}
+
+func (m *matchingSeriesChunkRefsSetIterator) Next() bool {
+	if !m.from.Next() {
+		return false
+	}
+
+	next := m.from.At()
+	writeIdx := 0
+
+	for _, series := range next.series {
+		matches := true
+		for _, matcher := range m.matchers {
+			if !matcher.Matches(series.lset.Get(matcher.Name)) {
+				matches = false
+				break
+			}
+		}
+		if matches {
+			next.series[writeIdx] = series
+			writeIdx++
+		}
+	}
+	next.series = next.series[:writeIdx]
+
+	if next.len() == 0 {
+		next.release()
+		return m.Next()
+	}
+	m.current = next
+	return true
+}
+
+func (m *matchingSeriesChunkRefsSetIterator) At() seriesChunkRefsSet {
+	return m.current
+}
+
+func (m *matchingSeriesChunkRefsSetIterator) Err() error {
+	return m.from.Err()
 }
 
 // cachedSeriesForPostingsID contains enough information to be able to tell whether a cache entry
