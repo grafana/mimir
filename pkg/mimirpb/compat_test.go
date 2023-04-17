9a47183d49bea6dabf0c67a36c724497ad265e1a
George Krajcsovits <krajorama@users.noreply.github.com>
2023-04-17 08:16:04 +0200

    Add unit tests to verify Prometheus and Mimir are in sync on SampleHistogram and Label types (#4729)
    * Move shape testing code to pkg/util/test
    
    Want to reuse for testing other types as well.
    
    * Handle pointer in shape test
    
    Both as top level type and as a field type.
    
    * Unit test to check that Prometheus and Mimir agree on SampleHistogram type
    * Unit test Prometheus and Mimir agree on Label type
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- pkg/mimirpb/compat_test.go
+++ pkg/mimirpb/compat_test.go
@@ -8,6 +8,7 @@ package mimirpb
 import (
 	stdlibjson "encoding/json"
 	"math"
+	"reflect"
 	"strconv"
 	"testing"
 	"unsafe"
@@ -555,4 +556,22 @@ func TestFromFloatHistogramToPromHistogram(t *testing.T) {
 			require.Equal(t, c.exp, *FromFloatHistogramToPromHistogram(&c.h))
 		})
 	}
+}
+
+// Check that Prometheus and Mimir SampleHistogram types converted
+// into each other with unsafe.Pointer are compatible
+func TestPrometheusSampleHistogramInSyncWithMimirPbSampleHistogram(t *testing.T) {
+	protoType := reflect.TypeOf(SampleHistogram{})
+	prometheusType := reflect.TypeOf(model.SampleHistogram{})
+
+	test.RequireSameShape(t, prometheusType, protoType)
+}
+
+// Check that Promtheus Label and MimirPb LabelAdapter types converted
+// into each other with unsafe.Pointer are compatible
+func TestPrometheusLabelsInSyncWithMimirPbLabelAdapter(t *testing.T) {
+	protoType := reflect.TypeOf(LabelAdapter{})
+	prometheusType := reflect.TypeOf(labels.Label{})
+
+	test.RequireSameShape(t, prometheusType, protoType)
 }
