05ab5d327afa485bb563a82b53c2fb25863e830f
Yuri Nikolic <durica.nikolic@grafana.com>
2023-04-17 07:04:40 +0200

    Adding tools for per zone analysis of ingesters in the ring

--- tools/analyse-unbalanced-series/main.go
+++ tools/analyse-unbalanced-series/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"os"
 	"time"
@@ -33,11 +34,71 @@ func main() {
 		numTokensPerIngester = 512
 	)
 
-	ringDesc := generateRingWithPerfectlySpacedTokens(numIngesters, numZones, numTokensPerIngester, math.MaxUint32, time.Now())
-	analyseRing("simulated-ingesters-ring-with-perfectly-spaced-tokens", ringDesc, logger)
+	//ringDesc := generateRingWithPerfectlySpacedTokens(numIngesters, numZones, numTokensPerIngester, math.MaxUint32, time.Now())
+	//analyseRing("simulated-ingesters-ring-with-perfectly-spaced-tokens", ringDesc, logger)
 
-	ringDesc = generateRingWithRandomTokens(numIngesters, numZones, numTokensPerIngester, math.MaxUint32, time.Now())
-	analyseRing("simulated-ingesters-ring-with-random-tokens", ringDesc, logger)
+	//ringDesc = generateRingWithRandomTokens(numIngesters, numZones, numTokensPerIngester, math.MaxUint32, time.Now())
+	//analyseRing("simulated-ingesters-ring-with-random-tokens", ringDesc, logger)
 
-	analyzeRingOwnershipSpreadOnDifferentTokensPerIngester(numIngesters, numZones, []int{64, 128, 256, 512, 1024, 2048, 4096, 8192}, logger)
+	//analyzeRingOwnershipSpreadOnDifferentTokensPerIngester(numIngesters, numZones, []int{64, 128, 256, 512, 1024, 2048, 4096, 8192}, logger)
+
+	analysesPerZone(63, numZones, 64, logger)
+
+}
+
+func analysesPerZone(numIngesters, numZones, numTokensPerIngester int, logger log.Logger) {
+	// analyze ring creation by optimizing token distribution per-zone with zone-awareness disabled by considering different numbers of tokens per ingester
+	analyzeRingOwnershipSpreadOnDifferentTokensPerZone(numIngesters, 1, []int{4, 16, 64, 128, 256, 512}, logger)
+
+	// analyze ring creation by optimizing token distribution per-zone with zone-awareness enabled by considering different numbers of tokens per ingester
+	analyzeRingOwnershipSpreadOnDifferentTokensPerZone(numIngesters, numZones, []int{4, 16, 64, 128, 256, 512}, logger)
+
+	// simulate traffic in a ring created by optimizing token distribution per zone with zone-awareness disabled by considering 512 ingesters and 64 tokens per ingester
+	//analysesPerZoneSingleZone(numIngesters, numTokensPerIngester, logger)
+
+	// simulate traffic in a ring created by optimizing token distribution per zone with zone-awareness disabled by considering 512 ingesters and 64 tokens per ingester
+	analysesPerZoneMultiZone(numIngesters, numZones, numTokensPerIngester, logger)
+}
+
+func analysesPerZoneSingleZone(numIngesters, numTokensPerIngester int, logger log.Logger) error {
+	var (
+		ringDesc, _         = generateRingWithEvenlyDistributedTokensPerZone(numIngesters, 1, numTokensPerIngester, math.MaxUint32, time.Now())
+		ringTokens          = ringDesc.GetTokens()
+		ringInstanceByToken = getRingInstanceByToken(ringDesc)
+	)
+
+	// Analyze owned tokens.
+	if err := analyzeActualTokensOwnership(fmt.Sprintf("simulated-ingesters-ring-with-evenly-distributed-tokens-per-zone-analysis-ingesters-%d-tokens-per-ingester-%d-rf-%d-zone-aware-%s", numIngesters, numTokensPerIngester, 1, "disabled"), ringDesc, ringTokens, ringInstanceByToken, 1, false, logger); err != nil {
+		return err
+	}
+	if err := analyzeActualTokensOwnership(fmt.Sprintf("simulated-ingesters-ring-with-evenly-distributed-tokens-per-zone-analysis-ingesters-%d-tokens-per-ingester-%d-rf-%d-zone-aware-%s", numIngesters, numTokensPerIngester, 3, "disabled"), ringDesc, ringTokens, ringInstanceByToken, 3, false, logger); err != nil {
+		return err
+	}
+
+	// Analyze the registered tokens percentage (no replication factor or zone-aware replication
+	// taken in account).
+	if err := analyzeRegisteredTokensOwnership(fmt.Sprintf("simulated-ingesters-ring-with-evenly-distributed-tokens-per-zone-ingesters-%d-tokens-per-ingester-%d-zone-aware-disabled", numIngesters, numTokensPerIngester), ringTokens, ringInstanceByToken, logger); err != nil {
+		return err
+	}
+	return nil
+}
+
+func analysesPerZoneMultiZone(numIngesters, numZones, numTokensPerIngester int, logger log.Logger) error {
+
+	var (
+		ringDesc, _         = generateRingWithEvenlyDistributedTokensPerZone(numIngesters, 1, numTokensPerIngester, math.MaxUint32, time.Now())
+		ringTokens          = ringDesc.GetTokens()
+		ringInstanceByToken = getRingInstanceByToken(ringDesc)
+	)
+
+	if err := analyzeActualTokensOwnership(fmt.Sprintf("simulated-ingesters-ring-with-evenly-distributed-tokens-per-zone-analysis-ingesters-%d-tokens-per-ingester-%d-rf-%d-zone-aware-%s", numIngesters, numTokensPerIngester, numZones, "enabled"), ringDesc, ringTokens, ringInstanceByToken, numZones, true, logger); err != nil {
+		return err
+	}
+
+	// Analyze the registered tokens percentage (no replication factor or zone-aware replication
+	// taken in account).
+	if err := analyzeRegisteredTokensOwnership(fmt.Sprintf("simulated-ingesters-ring-with-evenly-distributed-tokens-per-zone-ingesters-%d-tokens-per-ingester-%d-zone-aware-enabled", numIngesters, numTokensPerIngester), ringTokens, ringInstanceByToken, logger); err != nil {
+		return err
+	}
+	return nil
 }
--- tools/analyse-unbalanced-series/ring_simulation_test.go
+++ tools/analyse-unbalanced-series/ring_simulation_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"testing"
 	"time"
 
@@ -55,6 +56,12 @@ func TestGenerateRingWithBucketedFillerTokens(t *testing.T) {
 	printRingTokens(actual)
 }
 
+func TestGenerateRingWithEvenlyDistributedTokensPerZone(t *testing.T) {
+	ringDesc, tokenInfoByZone := generateRingWithEvenlyDistributedTokensPerZone(6, 3, 4, math.MaxUint32, time.Now())
+
+	printRingTokensPerZone(ringDesc, tokenInfoByZone)
+}
+
 func printRingTokens(ringDesc *ring.Desc) {
 	const verbose = true
 
@@ -77,4 +84,35 @@ func printRingTokens(ringDesc *ring.Desc) {
 	min, max, spread := getRegisteredTokensOwnershipStatistics(ringDesc.GetTokens(), getRingInstanceByToken(ringDesc))
 	fmt.Println("")
 	fmt.Println(fmt.Sprintf("min: %.3f max: %.3f spread: %.2f", min, max, spread))
+}
+
+func printRingTokensPerZone(ringDesc *ring.Desc, tokenInfoByZone map[string]*tokenInfo) {
+	const verbose = true
+
+	if verbose {
+		// Print the tokens for each ingester.
+		for id, ingester := range ringDesc.Ingesters {
+			fmt.Println(id, "\t", ingester.Tokens)
+		}
+
+		fmt.Println("")
+		for zone, tokenInfo := range tokenInfoByZone {
+			fmt.Println(zone)
+			first := tokenInfo
+			if first != nil {
+				fmt.Printf("%d (+%d)\n", first.token, first.registeredOwnership)
+				for curr := tokenInfo.next; curr != first; curr = curr.next {
+					fmt.Printf("%d (+%d)\n", curr.token, curr.registeredOwnership)
+				}
+			}
+		}
+	}
+
+	// Print stats.
+	statistics := getTokenInfoByZoneStatistics(tokenInfoByZone)
+	fmt.Println("")
+	for zone, tokenInfoStatistics := range statistics {
+		fmt.Println(zone)
+		fmt.Printf("min: %d max: %d spread: %.2f\n", tokenInfoStatistics.min, tokenInfoStatistics.max, tokenInfoStatistics.spread)
+	}
 }
