88b913297be6cf98b835b44d537bfe239e4128b7
Patrick Oyarzun <patrick.oyarzun@grafana.com>
2023-04-18 21:03:23 -0500

    Return HTTP 204 when no metadata is found

--- pkg/querier/metadata_handler.go
+++ pkg/querier/metadata_handler.go
@@ -61,6 +61,10 @@ func NewMetadataHandler(m MetadataSupplier) http.Handler {
 			metrics[m.Metric] = append(ms, metricMetadata{Type: string(m.Type), Help: m.Help, Unit: m.Unit})
 		}
 
+		if len(metrics) == 0 {
+			w.WriteHeader(http.StatusNoContent)
+		}
+
 		util.WriteJSONResponse(w, metadataResult{Status: statusSuccess, Data: metrics})
 	})
 }
