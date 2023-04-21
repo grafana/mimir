f5d668ebb890e6b404fa14ffe1bd35d763c2009a
Jon Kartago Lamida <me@lamida.net>
2023-04-21 18:31:15 +0800

    Accidentally removed flag
    Signed-off-by: Jon Kartago Lamida <me@lamida.net>

--- pkg/storage/bucket/azure/config.go
+++ pkg/storage/bucket/azure/config.go
@@ -33,5 +33,6 @@ func (cfg *Config) RegisterFlagsWithPrefix(prefix string, f *flag.FlagSet, logge
 	f.Var(&cfg.StorageAccountKey, prefix+"azure.account-key", "Azure storage account key")
 	f.StringVar(&cfg.ContainerName, prefix+"azure.container-name", "", "Azure storage container name")
 	f.StringVar(&cfg.Endpoint, prefix+"azure.endpoint-suffix", "", "Azure storage endpoint suffix without schema. The account name will be prefixed to this value to create the FQDN. If set to empty string, default endpoint suffix is used.")
+	f.IntVar(&cfg.MaxRetries, prefix+"azure.max-retries", 20, "Number of retries for recoverable errors")
 	f.StringVar(&cfg.UserAssignedID, prefix+"azure.user-assigned-id", "", "User assigned identity. If empty, then System assigned identity is used.")
 }
