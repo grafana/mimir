83f81916019e2d623c40d9eea6d13df8fb1561fc
Fiona Liao <fiona.y.liao@gmail.com>
2023-04-24 14:46:32 +0100

    Fix reference to tsdb retention
    Previous field used was set by tsdbIngesterConfig() rather than in
    yaml/command line.

--- pkg/mimir/modules.go
+++ pkg/mimir/modules.go
@@ -343,7 +343,7 @@ func (t *Mimir) initDistributorService() (serv services.Service, err error) {
 	t.Cfg.Distributor.InstanceLimitsFn = distributorInstanceLimits(t.RuntimeConfig)
 
 	if t.Cfg.Querier.ShuffleShardingIngestersEnabled {
-		t.Cfg.Distributor.ShuffleShardingLookbackPeriod = t.Cfg.Ingester.BlocksStorageConfig.TSDB.Retention
+		t.Cfg.Distributor.ShuffleShardingLookbackPeriod = t.Cfg.BlocksStorage.TSDB.Retention
 	}
 
 	// Check whether the distributor can join the distributors ring, which is
