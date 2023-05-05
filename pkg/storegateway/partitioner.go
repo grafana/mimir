8fc262c5a350a225783a41d254c481eb5539187c
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2023-05-05 10:29:00 +0200

    Move offsetTrackingReader
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/storegateway/partitioner.go
+++ pkg/storegateway/partitioner.go
@@ -9,10 +9,6 @@
 package storegateway
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
@@ -133,45 +129,4 @@ func (g *gapBasedPartitioner) partition(length int, rng func(int) (uint64, uint6
 		parts = append(parts, p)
 	}
 	return
-}
-
-type offsetTrackingReader struct {
-	offset uint64
-	r      *bufio.Reader
-}
-
-// SkipTo skips to the provided offset.
-func (r *offsetTrackingReader) SkipTo(at uint64) error {
-	if diff := int(at - r.offset); diff < 0 {
-		return fmt.Errorf("cannot reverse reader: offset %d, at %d", r.offset, at)
-	} else if diff > 0 {
-		n, err := r.r.Discard(diff)
-		r.offset += uint64(n)
-		if err != nil {
-			return fmt.Errorf("discarding sequence reader: %w", err)
-		}
-	}
-	return nil
-}
-
-// ReadByte implement io.ByteReader for compatibility with binary.ReadUvarint.
-func (r *offsetTrackingReader) ReadByte() (byte, error) {
-	r.offset++
-	return r.r.ReadByte()
-}
-
-// Read implements io.Reader.
-func (r *offsetTrackingReader) Read(p []byte) (int, error) {
-	n, err := r.r.Read(p)
-	r.offset += uint64(n)
-	return n, err
-}
-
-func (r *offsetTrackingReader) Reset(offset uint64, reader io.Reader) {
-	r.r.Reset(reader)
-	r.offset = offset
-}
-
-func (r *offsetTrackingReader) Release() {
-	r.Reset(0, nil)
 }
