a280bc57cdc67f505e5d0d75bab24e499259d9fe
Patryk Prus <patryk.prus@grafana.com>
2023-05-17 15:59:04 -0400

    Add test

--- pkg/frontend/transport/handler_test.go
+++ pkg/frontend/transport/handler_test.go
@@ -378,3 +378,18 @@ func (t *testLogger) Log(keyvals ...interface{}) error {
 	t.logMessages = append(t.logMessages, msg)
 	return nil
 }
+
+func TestFormatRequestHeaders(t *testing.T) {
+	h := http.Header{}
+	h.Add("X-Header-To-Log", "i should be logged!")
+	h.Add("X-Header-To-Not-Log", "i shouldn't be logged!")
+
+	fields := formatRequestHeaders(&h, []string{"X-Header-To-Log", "X-Header-Not-Present"})
+
+	expected := []interface{}{
+		"header_x_header_to_log",
+		"i should be logged!",
+	}
+
+	assert.Equal(t, expected, fields)
+}
