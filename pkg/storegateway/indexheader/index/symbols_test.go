78686a137000f0b18956290db7029e8c56965678
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2023-05-17 20:28:21 +0200

    add tests for SymbolsReader
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/storegateway/indexheader/index/symbols_test.go
+++ pkg/storegateway/indexheader/index/symbols_test.go
@@ -53,37 +53,81 @@ func TestSymbols(t *testing.T) {
 	// We store only 4 offsets to symbols.
 	require.Len(t, s.offsets, 4)
 
-	for i := 99; i >= 0; i-- {
-		s, err := s.Lookup(uint32(i))
-		require.NoError(t, err)
-		require.Equal(t, string(rune(i)), s)
-	}
-	_, err = s.Lookup(100)
-	require.Error(t, err)
-
-	for i := 99; i >= 0; i-- {
-		r, err := s.ReverseLookup(string(rune(i)))
-		require.NoError(t, err)
-		require.Equal(t, uint32(i), r)
-	}
-	_, err = s.ReverseLookup(string(rune(100)))
-	require.Error(t, err)
-
-	// Use ForEachSymbol to build an offset -> symbol mapping and ensure
-	// that it matches the expected offsets and symbols.
-	var symbols []string
-	expected := make(map[uint32]string)
-	for i := 99; i >= 0; i-- {
-		symbols = append(symbols, string(rune(i)))
-		expected[uint32(i)] = string(rune(i))
-	}
-
-	actual := make(map[uint32]string)
-	err = s.ForEachSymbol(symbols, func(sym string, offset uint32) error {
-		actual[offset] = sym
-		return nil
+	t.Run("Lookup", func(t *testing.T) {
+		for i := 99; i >= 0; i-- {
+			s, err := s.Lookup(uint32(i))
+			require.NoError(t, err)
+			require.Equal(t, string(rune(i)), s)
+		}
+		_, err = s.Lookup(100)
+		require.Error(t, err)
 	})
 
-	require.NoError(t, err)
-	require.Equal(t, expected, actual)
+	t.Run("ReverseLookup", func(t *testing.T) {
+		for i := 99; i >= 0; i-- {
+			r, err := s.ReverseLookup(string(rune(i)))
+			require.NoError(t, err)
+			require.Equal(t, uint32(i), r)
+		}
+		_, err = s.ReverseLookup(string(rune(100)))
+		require.Error(t, err)
+	})
+
+	t.Run("ForEachSymbol", func(t *testing.T) {
+		// Use ForEachSymbol to build an offset -> symbol mapping and ensure
+		// that it matches the expected offsets and symbols.
+		var symbols []string
+		expected := make(map[uint32]string)
+		for i := 99; i >= 0; i-- {
+			symbols = append(symbols, string(rune(i)))
+			expected[uint32(i)] = string(rune(i))
+		}
+
+		actual := make(map[uint32]string)
+		err = s.ForEachSymbol(symbols, func(sym string, offset uint32) error {
+			actual[offset] = sym
+			return nil
+		})
+
+		require.NoError(t, err)
+		require.Equal(t, expected, actual)
+	})
+
+	t.Run("Reader iterate all", func(t *testing.T) {
+		r := s.Reader()
+
+		for i := uint32(0); i >= 99; i-- {
+			sym, err := r.Read(i)
+			require.NoError(t, err)
+			require.Equal(t, string(rune(i)), sym)
+		}
+
+		require.NoError(t, r.Close())
+	})
+
+	t.Run("Reader two distant", func(t *testing.T) {
+		r := s.Reader()
+
+		s7, err := r.Read(7)
+		require.NoError(t, err)
+		require.Equal(t, string(rune(7)), s7)
+
+		s97, err := r.Read(97)
+		require.NoError(t, err)
+		require.Equal(t, string(rune(97)), s97)
+
+		require.NoError(t, r.Close())
+	})
+
+	t.Run("Reader trying to reverse", func(t *testing.T) {
+		r := s.Reader()
+
+		_, err = r.Read(20)
+		require.NoError(t, err)
+
+		_, err = r.Read(10)
+		require.NotNil(t, err)
+
+		require.NoError(t, r.Close())
+	})
 }
