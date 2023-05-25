e100f1a925a179a7342638dffda88c9a8bf0196a
Charles Korn <charles.korn@grafana.com>
2023-05-25 15:29:01 +1000

    Address PR feedback: add note about where query stats are handled for streaming series.

--- pkg/distributor/query.go
+++ pkg/distributor/query.go
@@ -377,6 +377,8 @@ func (d *Distributor) queryIngesterStream(ctx context.Context, replicationSet ri
 	}
 
 	reqStats.AddFetchedSeries(uint64(len(resp.Chunkseries) + len(resp.Timeseries) + len(resp.StreamingSeries)))
+
+	// Stats for streaming series are handled in streamingChunkSeries.
 	reqStats.AddFetchedChunkBytes(uint64(ingester_client.ChunksSize(resp.Chunkseries)))
 	reqStats.AddFetchedChunks(uint64(ingester_client.ChunksCount(resp.Chunkseries)))
 
