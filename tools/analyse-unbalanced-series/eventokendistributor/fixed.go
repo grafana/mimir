9979493045c4c720ab3561efc5431a3b83cd0c40
Oleg Zaytsev <mail@olegzaytsev.com>
2023-05-30 10:57:50 +0200

    Slow token distribution
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

--- tools/analyse-unbalanced-series/eventokendistributor/fixed.go
+++ tools/analyse-unbalanced-series/eventokendistributor/fixed.go
@@ -1,79 +1,133 @@
 package eventokendistributor
 
 import (
-	"fmt"
 	"math"
 
 	"github.com/grafana/dskit/ring"
+	"golang.org/x/exp/slices"
 )
 
-type Fixed struct{}
+type token struct {
+	start    uint32
+	next     *token
+	instance int
+	other    *token
+}
+
+func (t *token) size() uint32 {
+	end := t.end()
+	return end - t.start
+}
+
+func (t *token) end() uint32 {
+	if t.next != nil {
+		return t.next.start
+	}
+	return math.MaxUint32
+}
+
+type Fixed struct {
+	zoneOffset int
+}
 
 func (f Fixed) CalculateTokens(instanceID, zoneID int, _ ring.Tokens) (ring.Tokens, error) {
-	if instanceID == 0 {
-		return ring.Tokens{0 + uint32(zoneID)*100}, nil
+	first := make([]*token, 0, instanceID)
+	first = append(first, &token{start: 0, instance: 0})
+	first = append(first, &token{start: math.MaxUint32 / 2, instance: 1})
+	first[0].next = first[1]
+
+	for instance := 2; instance <= instanceID; instance++ {
+		fraction := uint32(math.MaxUint32 / (instance * (instance + 1)))
+		first = append(first, nil)
+		var last *token
+
+		for stolen := 0; stolen < instance; stolen++ {
+			assigned := false
+			for candidate := first[stolen]; !assigned && candidate != nil; candidate = candidate.other {
+				if candidate.size() > fraction {
+					assigned = true
+					token := &token{start: candidate.end() - fraction, next: candidate.next, instance: instance}
+					candidate.next = token
+					if first[instance] == nil {
+						first[instance] = token
+					} else {
+						last.other = token
+					}
+					last = token
+				}
+			}
+			if !assigned {
+				panic("not assigned")
+			}
+		}
 	}
 
-	// = 1 - 1/2 -  1/6 - 1/12 - 1/20 - 1/30 - 1/42
-	// = 1 - 1/2 -  1/6 - 1/12 - 1/20 - 1/42
-	// = 1 - 1/2 -  1/6 - 1/42
-	// = 1 - 1/6 - 1/12 - 1/20 - 1/30 - 1/42
-	// = 1 - 1/6 - 1/42
-	// = 1 - 1/6 - 1/42
-
-	// = 1 - 1/2 -  1/6 - 1/12 - 1/20 - 1/30 - 1/42
-	// = 1       -  1/6 - 1/12 - 1/20 - 1/30 - 1/42
-	// = 1 - 1/2 -  1/6 - 1/12 - 1/20        - 1/42
-	// = 1                     - 1/20        - 1/42
-	// = 1 - 1/2 -  1/6                      - 1/42
-	// = 1       -  1/6                      - 1/42
-
-	// =1 - 1/2  -  1/6 - 1/12 - 1/20 - 1/30
-	// =1         - 1/6 - 1/12 - 1/20 - 1/30
-	// =1 - 1/2  -  1/6 - 1/12 - 1/20
-	// =1 - 1/2  -  1/6
-	// =1 - 1/6
-	// =1                      - 1/20
-
-	// First token
-	// = 1 - 1/2
-	// = 1 - 1/2 - 1/6
-	// = 1 - 1/2 - 1/6 - 1/12
-	// = 1 - 1/2 - 1/6 - 1/12 - 1/20
-	// = 1 - 1/2 - 1/6 - 1/12 - 1/20 - 1/30
-	// = 1 - 1/2 - 1/6 - 1/12 - 1/20 - 1/30 - 1/42
-
-	// Second token
-	// = 1       - 1/6
-	// = 1 - 1/2       - 1/12
-	// = 1 - 1/2 - 1/6        - 1/20
-	// = 1 - 1/2 - 1/6 - 1/12        - 1/30
-	// = 1 - 1/2 - 1/6 - 1/12 - 1/20         - 1/42
-
-	// Third token
-	// = 1       - 1/6 - 1/12
-	// = 1       - 1/6 - 1/12 - 1/20
-	// = 1 - 1/2       - 1/12         - 1/30
-	// = 1 - 1/2 - 1/6                       - 1/42
-
-	// Fourth token
-	// = 1              - 1/20
-	// = 1 - 1/6 - 1/12 - 1/20 - 1/30
-	// = 1 - 1/6 - 1/12 - 1/20 - 1/30 - 1/42
-
-	tokens := make(ring.Tokens, 0, instanceID+1)
-	token := uint32(math.MaxUint32)
-	str := ""
-	for n := instanceID; n >= 1; n-- {
-		str = fmt.Sprintf("%s-1/%d", str, n*(n+1))
-		token = token - uint32((math.MaxUint32)/float64(n*(n+1)))
-		tokens = append(tokens, token)
-		fmt.Printf("token: %10d 1%s\n", token, str)
+	ret := make([]uint32, 0, instanceID)
+	for token := first[instanceID]; token != nil; token = token.other {
+		ret = append(ret, (token.start+uint32(zoneID*f.zoneOffset))%math.MaxUint32)
 	}
-	// Reverse tokens list, because we generated it from the end.
-	for i, j := 0, len(tokens)-1; i < j; i, j = i+1, j-1 {
-		tokens[i], tokens[j] = tokens[j], tokens[i]
-	}
-
-	return tokens, nil
+	slices.Sort(ret)
+	return ret, nil
 }
+
+// = 1 - 1/2 -  1/6 - 1/12 - 1/20 - 1/30 - 1/42
+// = 1 - 1/2 -  1/6 - 1/12 - 1/20 - 1/42
+// = 1 - 1/2 -  1/6 - 1/42
+// = 1 - 1/6 - 1/12 - 1/20 - 1/30 - 1/42
+// = 1 - 1/6 - 1/42
+// = 1 - 1/6 - 1/42
+
+// = 1 - 1/2 -  1/6 - 1/12 - 1/20 - 1/30 - 1/42
+// = 1       -  1/6 - 1/12 - 1/20 - 1/30 - 1/42
+// = 1 - 1/2 -  1/6 - 1/12 - 1/20        - 1/42
+// = 1                     - 1/20        - 1/42
+// = 1 - 1/2 -  1/6                      - 1/42
+// = 1       -  1/6                      - 1/42
+
+// =1 - 1/2  -  1/6 - 1/12 - 1/20 - 1/30
+// =1         - 1/6 - 1/12 - 1/20 - 1/30
+// =1 - 1/2  -  1/6 - 1/12 - 1/20
+// =1 - 1/2  -  1/6
+// =1 - 1/6
+// =1                      - 1/20
+
+// First token
+// = 1 - 1/2
+// = 1 - 1/2 - 1/6
+// = 1 - 1/2 - 1/6 - 1/12
+// = 1 - 1/2 - 1/6 - 1/12 - 1/20
+// = 1 - 1/2 - 1/6 - 1/12 - 1/20 - 1/30
+// = 1 - 1/2 - 1/6 - 1/12 - 1/20 - 1/30 - 1/42
+
+// Second token
+// = 1       - 1/6
+// = 1 - 1/2       - 1/12
+// = 1 - 1/2 - 1/6        - 1/20
+// = 1 - 1/2 - 1/6 - 1/12        - 1/30
+// = 1 - 1/2 - 1/6 - 1/12 - 1/20         - 1/42
+
+// 2=     0     1
+// 3=     1     0     1
+// 4=     1     1     0     1
+// 5=     1     1     1     0     1
+// 6=     1     1     1     1     0     1
+
+// Third token
+// = 1       - 1/6 - 1/12
+// = 1       - 1/6 - 1/12 - 1/20
+// = 1 - 1/2       - 1/12         - 1/30
+// = 1 - 1/2 - 1/6                       - 1/42
+
+// 3=      0     1     1
+// 4=      0     1     1     1
+// 5=      1     0     1     0     1
+// 6=      1     1     0     1     0
+
+// Fourth token
+// = 1                    - 1/20
+// = 1       - 1/6 - 1/12 - 1/20 - 1/30
+// = 1       - 1/6 - 1/12 - 1/20 - 1/30 - 1/42
+
+// 4=      0     0     0     1
+// 5=      0     1     1     1     1
+// 6=      0     1     1     1     1     1
--- tools/analyse-unbalanced-series/eventokendistributor/fixed_test.go
+++ tools/analyse-unbalanced-series/eventokendistributor/fixed_test.go
@@ -14,60 +14,80 @@ func TestFixed(t *testing.T) {
 	t.Run("generates a linearly incrementing amount of tokens", func(t *testing.T) {
 		f := Fixed{}
 
-		for i := 0; i < 10; i++ {
+		for i := 0; i < 2; i++ {
 			tokens, err := f.CalculateTokens(i, 0, nil)
 			require.NoError(t, err)
-			require.Equal(t, i+1, len(tokens))
+			require.Equal(t, 1, len(tokens))
+		}
+		for i := 2; i < 100; i++ {
+			t.Run(fmt.Sprintf("%d instances", i), func(t *testing.T) {
+				tokens, err := f.CalculateTokens(i, 0, nil)
+				require.NoError(t, err)
+				require.Equal(t, i, len(tokens))
+			})
 		}
 	})
 	t.Run("equally distributed", func(t *testing.T) {
-		const instances = 7
-		type tokenOwner struct {
-			token    uint32
-			instance int
-		}
-		f := Fixed{}
-		var ring []tokenOwner
-		for i := 0; i < instances; i++ {
-			tokens, err := f.CalculateTokens(i, 0, nil)
-			require.NoError(t, err)
-			for _, t := range tokens {
-				ring = append(ring, tokenOwner{t, i})
-			}
-		}
-		sort.Slice(ring, func(i, j int) bool { return ring[i].token < ring[j].token })
-		ownership := map[int]uint32{}
-		tokensOwned := map[int]int{}
-		for i, t := range ring {
-			nextToken := uint32(math.MaxUint32)
-			if i < len(ring)-1 {
-				nextToken = ring[i+1].token
-			}
-			fmt.Println(t.token, nextToken)
-			ownership[t.instance] += nextToken - t.token
-			tokensOwned[t.instance]++
-		}
-		expectedOwnership := uint32(math.MaxUint32) / uint32(instances)
-		t.Logf("Expected ownership: %d", expectedOwnership)
-		for instance := 0; instance < instances; instance++ {
-			owns := ownership[instance]
-			t.Logf("Instance %d: %d in %d tokens", instance, owns, tokensOwned[instance])
-			assert.InEpsilon(t, expectedOwnership, owns, 0.01, "instance %d", instance)
+		for _, instances := range []int{2, 10, 20, 50, 66, 100} {
+			t.Run(fmt.Sprintf("%d instances", instances), func(t *testing.T) {
+				type tokenOwner struct {
+					token    uint32
+					instance int
+				}
+				f := Fixed{}
+				var ring []tokenOwner
+				for i := 0; i < instances; i++ {
+					tokens, err := f.CalculateTokens(i, 0, nil)
+					require.NoError(t, err)
+					for _, t := range tokens {
+						ring = append(ring, tokenOwner{t, i})
+					}
+				}
+				sort.Slice(ring, func(i, j int) bool { return ring[i].token < ring[j].token })
+				ownership := map[int]uint32{}
+				tokensOwned := map[int]int{}
+				for i, t := range ring {
+					nextToken := uint32(math.MaxUint32)
+					if i < len(ring)-1 {
+						nextToken = ring[i+1].token
+					}
+					fmt.Println(t.token, nextToken)
+					ownership[t.instance] += nextToken - t.token
+					tokensOwned[t.instance]++
+				}
+				expectedOwnership := uint32(math.MaxUint32) / uint32(instances)
+				t.Logf("Expected ownership: %d", expectedOwnership)
+				for instance := 0; instance < instances; instance++ {
+					owns := ownership[instance]
+					t.Logf("Instance %d: %d in %d tokens", instance, owns, tokensOwned[instance])
+					assert.InEpsilon(t, expectedOwnership, owns, 0.01, "instance %d", instance)
+				}
+			})
 		}
 	})
 	t.Run("collisions", func(t *testing.T) {
-		f := Fixed{}
-		assigned := make(map[uint32]int)
-
-		for i := 0; i < 10; i++ {
-			tokens, err := f.CalculateTokens(i, 0, nil)
-			require.NoError(t, err)
-			for _, token := range tokens {
-				if instance, ok := assigned[token]; ok {
-					t.Fatalf("instance %d: token %d already assigned to instance %d", i, token, instance)
-				}
-				assigned[token] = i
-			}
+		type instanceInZone struct {
+			instance, zone int
 		}
+		for offset := range [512]int{} {
+			t.Run(fmt.Sprintf("offset %d", offset), func(t *testing.T) {
+				f := Fixed{zoneOffset: offset}
+				assigned := make(map[uint32]instanceInZone)
+
+				for _, zone := range []int{0, 1, 2} {
+					for i := 0; i < 256; i++ {
+						tokens, err := f.CalculateTokens(i, zone, nil)
+						require.NoError(t, err)
+						for _, token := range tokens {
+							if repeated, ok := assigned[token]; ok {
+								t.Fatalf("instance %d in zone %d: token %d already assigned to instance %d in zone %d", i, zone, token, repeated.instance, repeated.zone)
+							}
+							assigned[token] = instanceInZone{i, zone}
+						}
+					}
+				}
+			})
+		}
+
 	})
 }
