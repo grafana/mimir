81f0f1c3dba191215bd67e22ce57b00c37867714
Patrick Oyarzun <patrick.oyarzun@grafana.com>
2023-06-01 15:06:58 -0500

    Add activeseries.Postings
    This type is a wrapper around an index.Postings that filters out
    inactive series.

---
+++ pkg/ingester/activeseries/active_postings_test.go
@@ -0,0 +1,103 @@
+package activeseries
+
+import (
+	"testing"
+	"time"
+
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/prometheus/prometheus/storage"
+	"github.com/prometheus/prometheus/tsdb/index"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPostings_Expand(t *testing.T) {
+	ttl := 3
+	mockedTime := time.Unix(int64(ttl), 0)
+	series := []labels.Labels{
+		labels.FromStrings("a", "1"),
+		labels.FromStrings("a", "2"),
+		labels.FromStrings("a", "3"),
+		labels.FromStrings("a", "4"),
+		labels.FromStrings("a", "5"),
+	}
+	allStorageRefs := []storage.SeriesRef{1, 2, 3, 4, 5}
+	storagePostings := index.NewListPostings(allStorageRefs)
+	activeSeries := NewActiveSeries(&Matchers{}, time.Duration(ttl))
+
+	// Update each series at a different time according to its index.
+	for i := range allStorageRefs {
+		activeSeries.UpdateSeries(series[i], uint64(allStorageRefs[i]), time.Unix(int64(i), 0), copyFn)
+	}
+
+	allActive, _, valid := activeSeries.Active(mockedTime)
+	require.True(t, valid)
+	require.Equal(t, 2, allActive)
+
+	activeSeriesPostings := NewPostings(activeSeries, storagePostings)
+
+	activeRefs, err := index.ExpandPostings(activeSeriesPostings)
+	require.NoError(t, err)
+
+	require.Equal(t, allStorageRefs[ttl:], activeRefs)
+}
+
+func TestPostings_Seek(t *testing.T) {
+	ttl := 3
+	mockedTime := time.Unix(int64(ttl), 0)
+	series := []labels.Labels{
+		labels.FromStrings("a", "1"),
+		labels.FromStrings("a", "2"),
+		labels.FromStrings("a", "3"),
+		labels.FromStrings("a", "4"),
+		labels.FromStrings("a", "5"),
+	}
+	allStorageRefs := []storage.SeriesRef{1, 2, 3, 4, 5}
+	storagePostings := index.NewListPostings(allStorageRefs)
+	activeSeries := NewActiveSeries(&Matchers{}, time.Duration(ttl))
+
+	// Update each series at a different time according to its index.
+	for i := range allStorageRefs {
+		activeSeries.UpdateSeries(series[i], uint64(allStorageRefs[i]), time.Unix(int64(i), 0), copyFn)
+	}
+
+	allActive, _, valid := activeSeries.Active(mockedTime)
+	require.True(t, valid)
+	require.Equal(t, 2, allActive)
+
+	activeSeriesPostings := NewPostings(activeSeries, storagePostings)
+
+	// See to a series that is not active.
+	require.True(t, activeSeriesPostings.Seek(3))
+	// The next active series is 4.
+	require.Equal(t, storage.SeriesRef(4), activeSeriesPostings.At())
+}
+
+func TestPostings_SeekToEnd(t *testing.T) {
+	ttl := 5
+	mockedTime := time.Unix(int64(ttl), 0)
+	series := []labels.Labels{
+		labels.FromStrings("a", "1"),
+		labels.FromStrings("a", "2"),
+		labels.FromStrings("a", "3"),
+		labels.FromStrings("a", "4"),
+		labels.FromStrings("a", "5"),
+	}
+	allStorageRefs := []storage.SeriesRef{1, 2, 3, 4, 5}
+	storagePostings := index.NewListPostings(allStorageRefs)
+	activeSeries := NewActiveSeries(&Matchers{}, time.Duration(ttl))
+
+	// Update each series at a different time according to its index.
+	for i := range allStorageRefs {
+		activeSeries.UpdateSeries(series[i], uint64(allStorageRefs[i]), time.Unix(int64(i), 0), copyFn)
+	}
+
+	allActive, _, valid := activeSeries.Active(mockedTime)
+	require.True(t, valid)
+	require.Equal(t, 0, allActive)
+
+	activeSeriesPostings := NewPostings(activeSeries, storagePostings)
+
+	// See to a series that is not active.
+	// There are no active series after 3, so Seek should return false.
+	require.False(t, activeSeriesPostings.Seek(3))
+}
