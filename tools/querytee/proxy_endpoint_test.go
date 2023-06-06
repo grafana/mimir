bd2c939f47e04b2984b388902926651372969975
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2023-06-06 18:43:19 +0200

    query-tee: proxy Content-Type response header
    The query-tee doesn't proxy the content-type HTTP header. This breaks some integrations, such as Grafana Alerting.
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- tools/querytee/proxy_endpoint_test.go
+++ tools/querytee/proxy_endpoint_test.go
@@ -331,6 +331,7 @@ func Test_ProxyEndpoint_Comparison(t *testing.T) {
 			endpoint.ServeHTTP(resp, req)
 			require.Equal(t, "preferred response", resp.Body.String())
 			require.Equal(t, scenario.preferredResponseStatusCode, resp.Code)
+			require.Equal(t, scenario.preferredResponseContentType, resp.Header().Get("Content-Type"))
 
 			// The HTTP request above will return as soon as the primary response is received, but this doesn't guarantee that the response comparison has been completed.
 			// Wait for the response comparison to complete before checking the logged messages.
