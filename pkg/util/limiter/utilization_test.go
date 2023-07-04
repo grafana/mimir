1d248fc07cd00f304da448076350a7dc39654f1d
Marco Pracucci <marco@pracucci.com>
2023-07-04 12:24:00 +0200

    Allow to enable CPU and/or memory read-path limits individually (#5392)
    * Allow to enable CPU and/or memory read-path limits independently
    
    Signed-off-by: Marco Pracucci <marco@pracucci.com>
    
    * Update doc
    
    Signed-off-by: Marco Pracucci <marco@pracucci.com>
    
    ---------
    
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/util/limiter/utilization_test.go
+++ pkg/util/limiter/utilization_test.go
@@ -7,22 +7,27 @@ import (
 	"time"
 
 	"github.com/go-kit/log"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestUtilizationBasedLimiter(t *testing.T) {
-	const cpuLimit = 0.11
-	// 1 GB
-	const memLimit = 1_073_741_824
+	const gigabyte = 1024 * 1024 * 1024
 
-	fakeScanner := &fakeUtilizationScanner{}
-	lim := NewUtilizationBasedLimiter(cpuLimit, memLimit, log.NewNopLogger())
-	lim.utilizationScanner = fakeScanner
-	require.Empty(t, lim.LimitingReason(), "Limiting should initially be disabled")
+	setup := func(t *testing.T, cpuLimit float64, memoryLimit uint64) (*UtilizationBasedLimiter, *fakeUtilizationScanner) {
+		fakeScanner := &fakeUtilizationScanner{}
+		lim := NewUtilizationBasedLimiter(cpuLimit, memoryLimit, log.NewNopLogger())
+		lim.utilizationScanner = fakeScanner
+		require.Empty(t, lim.LimitingReason(), "Limiting should initially be disabled")
+
+		return lim, fakeScanner
+	}
 
 	tim := time.Now()
 
-	t.Run("CPU based limiting", func(t *testing.T) {
+	t.Run("CPU based limiting should be enabled if set to a value greater than 0", func(t *testing.T) {
+		lim, _ := setup(t, 0.11, gigabyte)
+
 		// The fake utilization scanner linearly increases CPU usage for a minute
 		for i := 0; i < 59; i++ {
 			lim.compute(tim)
@@ -39,16 +44,69 @@ func TestUtilizationBasedLimiter(t *testing.T) {
 		require.Empty(t, lim.LimitingReason(), "Limiting should be disabled again")
 	})
 
-	t.Run("memory based limiting", func(t *testing.T) {
-		fakeScanner.memoryUtilization = memLimit
+	t.Run("CPU based limiting should be disabled if set to 0", func(t *testing.T) {
+		lim, _ := setup(t, 0, gigabyte)
+
+		for i := 0; i < 60; i++ {
+			lim.compute(tim)
+			tim = tim.Add(time.Second)
+			require.Empty(t, lim.LimitingReason(), "Limiting should be disabled")
+		}
+	})
+
+	t.Run("memory based limiting should be enabled if set to a value greater than 0", func(t *testing.T) {
+		lim, fakeScanner := setup(t, 0.11, gigabyte)
+
+		// Compute the utilization a first time to warm up the limiter.
+		lim.compute(tim)
+
+		fakeScanner.memoryUtilization = gigabyte
 		lim.compute(tim)
 		tim = tim.Add(time.Second)
 		require.Equal(t, "memory", lim.LimitingReason(), "Limiting should be enabled due to memory")
 
-		fakeScanner.memoryUtilization = memLimit - 1
+		fakeScanner.memoryUtilization = gigabyte - 1
 		lim.compute(tim)
 		require.Empty(t, lim.LimitingReason(), "Limiting should be disabled again")
 	})
+
+	t.Run("memory based limiting should be disabled if set to 0", func(t *testing.T) {
+		lim, fakeScanner := setup(t, 0.11, 0)
+
+		// Compute the utilization a first time to warm up the limiter.
+		lim.compute(tim)
+
+		fakeScanner.memoryUtilization = gigabyte
+		lim.compute(tim)
+		tim = tim.Add(time.Second)
+		require.Empty(t, lim.LimitingReason(), "Limiting should be disabled")
+	})
+}
+
+func TestFormatCPU(t *testing.T) {
+	assert.Equal(t, "0.00", formatCPU(0))
+	assert.Equal(t, "0.11", formatCPU(0.11))
+	assert.Equal(t, "0.11", formatCPU(0.111))
+	assert.Equal(t, "0.12", formatCPU(0.115))
+	assert.Equal(t, "2.10", formatCPU(2.1))
+}
+
+func TestFormatCPULimit(t *testing.T) {
+	assert.Equal(t, "disabled", formatCPULimit(0))
+	assert.Equal(t, "0.11", formatCPULimit(0.111))
+	assert.Equal(t, "0.12", formatCPULimit(0.115))
+}
+
+func TestFormatMemory(t *testing.T) {
+	assert.Equal(t, "0", formatMemory(0))
+	assert.Equal(t, "1073741824", formatMemory(1024*1024*1024))
+	assert.Equal(t, "1073741825", formatMemory((1024*1024*1024)+1))
+}
+
+func TestFormatMemoryLimit(t *testing.T) {
+	assert.Equal(t, "disabled", formatMemoryLimit(0))
+	assert.Equal(t, "1073741824", formatMemoryLimit(1024*1024*1024))
+	assert.Equal(t, "1073741825", formatMemoryLimit((1024*1024*1024)+1))
 }
 
 type fakeUtilizationScanner struct {
