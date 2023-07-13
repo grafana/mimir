79589d5b2c1405a60dd2f04114e82daaf174f87d
Bryan Boreham <bjboreham@gmail.com>
2023-07-13 11:34:30 +0000

    ingester: don't log errors that cause OOMs
    Not-ready and over-max-inflight can be emitted when the server is
    overloaded, in which case we don't want to spend more resource logging
    the fact. The problem will be visible in metrics.
    
    Temporarily using a branch from upstream weaveworks/common, to check
    Mimir still passes CI.

--- pkg/ingester/ingester.go
+++ pkg/ingester/ingester.go
@@ -47,6 +47,7 @@ import (
 	"github.com/prometheus/prometheus/util/zeropool"
 	"github.com/thanos-io/objstore"
 	"github.com/weaveworks/common/httpgrpc"
+	"github.com/weaveworks/common/middleware"
 	"go.uber.org/atomic"
 	"golang.org/x/exp/slices"
 	"golang.org/x/sync/errgroup"
@@ -707,7 +708,7 @@ func (i *Ingester) PushWithCleanup(ctx context.Context, pushReq *push.Request) (
 	defer pushReq.CleanUp()
 
 	if err := i.checkRunning(); err != nil {
-		return nil, err
+		return nil, middleware.DoNotLogError{Err: err}
 	}
 
 	// We will report *this* request in the error too.
