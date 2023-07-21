c81f82137d6d1511dea067ee9a738f80319b8272
Peter Štibraný <pstibrany@gmail.com>
2023-07-21 13:21:56 +0200

    fix comma
    Signed-off-by: Peter Štibraný <pstibrany@gmail.com>

--- pkg/util/ballast.go
+++ pkg/util/ballast.go
@@ -1,6 +1,6 @@
 package util
 
-// AllocateBallast allocates ballast of given size. to alter GC behaviour. See https://github.com/golang/go/issues/23044
+// AllocateBallast allocates ballast of given size, to alter GC behaviour. See https://github.com/golang/go/issues/23044
 // Instead of allocating one big slice, we allocate many small ones to avoid keeping too much data in memory when Go runtime
 // decides that it needs to zero the slice.
 // Returned value should not be used.
