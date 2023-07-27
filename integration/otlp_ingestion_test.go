d84bb8554de25c1e2a106c07729abbfc2d235027
Marco Pracucci <marco@pracucci.com>
2023-07-27 10:12:19 +0200

    Chore: use upstream Prometheus API v1 MinTime and MaxTime
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- integration/otlp_ingestion_test.go
+++ integration/otlp_ingestion_test.go
@@ -11,11 +11,11 @@ import (
 	e2edb "github.com/grafana/e2e/db"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/prompb"
+	v1 "github.com/prometheus/prometheus/web/api/v1"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/mimir/integration/e2emimir"
-	"github.com/grafana/mimir/pkg/util"
 )
 
 func TestOTLPIngestion(t *testing.T) {
@@ -58,11 +58,11 @@ func TestOTLPIngestion(t *testing.T) {
 	require.Equal(t, model.ValVector, result.Type())
 	assert.Equal(t, expectedVector, result.(model.Vector))
 
-	labelValues, err := c.LabelValues("foo", util.PrometheusMinTime, util.PrometheusMaxTime, nil)
+	labelValues, err := c.LabelValues("foo", v1.MinTime, v1.MaxTime, nil)
 	require.NoError(t, err)
 	require.Equal(t, model.LabelValues{"bar"}, labelValues)
 
-	labelNames, err := c.LabelNames(util.PrometheusMinTime, util.PrometheusMaxTime)
+	labelNames, err := c.LabelNames(v1.MinTime, v1.MaxTime)
 	require.NoError(t, err)
 	require.Equal(t, []string{"__name__", "foo"}, labelNames)
 
