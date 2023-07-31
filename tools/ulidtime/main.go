7aced3b6c3f889de212eb068b711db007f5826a1
Peter Štibraný <pstibrany@gmail.com>
2023-07-31 09:32:14 +0200

    Add -seconds flag to ulidtime. Exit with non-zero if any ulid was wrong.
    Signed-off-by: Peter Štibraný <pstibrany@gmail.com>

--- tools/ulidtime/main.go
+++ tools/ulidtime/main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -12,17 +13,29 @@ import (
 )
 
 func main() {
-	if len(os.Args) == 1 {
+	seconds := flag.Bool("seconds", false, "Print timestamp as unix timestamp in seconds")
+	flag.Parse()
+
+	if len(flag.Args()) == 0 {
 		fmt.Println("Usage:", os.Args[0], "[ulid ...]")
+		os.Exit(1)
 		return
 	}
 
-	for _, v := range os.Args[1:] {
+	exit := 0
+	for _, v := range flag.Args() {
 		id, err := ulid.Parse(v)
 		if err != nil {
 			log.Printf("failed to parse %q: %v", v, err)
+			exit = 1
 		} else {
-			fmt.Println(id.String(), ulid.Time(id.Time()).UTC().Format(time.RFC3339Nano))
+			if *seconds {
+				fmt.Println(id.String(), ulid.Time(id.Time()).UTC().Unix())
+			} else {
+				fmt.Println(id.String(), ulid.Time(id.Time()).UTC().Format(time.RFC3339Nano))
+			}
 		}
 	}
+
+	os.Exit(exit)
 }
