2cc6827680d4a9f99028706686d7635f820672e7
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2023-08-02 19:09:11 +0300

    Add BenchmarkIngesterClient_ConcurrentStreams
    benchstat 100-max-concurrent-streams.txt 10000-max-concurrent-streams.txt
    100-max-concurrent-streams.txt:4: missing iteration count
    10000-max-concurrent-streams.txt:4: missing iteration count
    goos: darwin
    goarch: arm64
    pkg: github.com/grafana/mimir/pkg/ingester/client
                                        │ 100-max-concurrent-streams.txt │ 10000-max-concurrent-streams.txt │
                                        │             sec/op             │     sec/op      vs base          │
    IngesterClient_ConcurrentStreams-10                      5.909 ± 14%      6.066 ± 13%  ~ (p=0.529 n=10)
    
                                        │ 100-max-concurrent-streams.txt │  10000-max-concurrent-streams.txt  │
                                        │            max-heap            │  max-heap    vs base               │
    IngesterClient_ConcurrentStreams-10                      1.942G ± 4%   2.072G ± 1%  +6.69% (p=0.001 n=10)
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/ingester/client/client_test.go
+++ pkg/ingester/client/client_test.go
@@ -7,14 +7,25 @@ package client
 
 import (
 	"context"
+	"errors"
 	"net/http/httptest"
+	"runtime"
 	"strconv"
+	"sync"
 	"testing"
+	"time"
 
+	"github.com/grafana/dskit/flagext"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"github.com/weaveworks/common/server"
+	"github.com/weaveworks/common/user"
 
 	"github.com/grafana/mimir/pkg/mimirpb"
 	"github.com/grafana/mimir/pkg/util"
+	util_math "github.com/grafana/mimir/pkg/util/math"
 	"github.com/grafana/mimir/pkg/util/test"
 )
 
@@ -55,5 +66,74 @@ func TestMarshall(t *testing.T) {
 		_, err = util.ParseProtoReader(context.Background(), recorder.Body, recorder.Body.Len(), plentySize, nil, &req, util.RawSnappy)
 		require.NoError(t, err)
 		require.Equal(t, numSeries, len(req.Timeseries))
+	}
+}
+
+func BenchmarkIngesterClient_ConcurrentStreams(b *testing.B) {
+	serverCfg := server.Config{}
+	flagext.DefaultValues(&serverCfg)
+	require.NoError(b, serverCfg.LogLevel.Set("error"))
+	serverCfg.GPRCServerMaxConcurrentStreams = 100
+	//serverCfg.GPRCServerMaxConcurrentStreams = 10000
+	serverCfg.Registerer = prometheus.NewRegistry()
+	server, err := server.New(serverCfg)
+	require.NoError(b, err)
+	b.Cleanup(server.Shutdown)
+	go func() {
+		_ = server.Run()
+	}()
+	b.Cleanup(server.Stop)
+
+	ingServ := &IngesterServerMock{}
+	RegisterIngesterServer(server.GRPC, ingServ)
+	noopLock := &sync.Mutex{}
+	ingServ.On("Push", mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+		bytes := make([]byte, 1024)
+		noopLock.Lock()
+		noopLock.Unlock()
+		bytes[4] = byte(time.Now().Second()) // use the slice to retain the memory during the Sleep
+	}).Return(&mimirpb.WriteResponse{}, errors.New("overwhelmed"))
+
+	client, err := MakeIngesterClient(server.GRPCListenAddr().String(), Config{}, NewMetrics(prometheus.NewRegistry()))
+	require.NoError(b, err)
+	b.Cleanup(func() { _ = client.Close() })
+
+	wg := &sync.WaitGroup{}
+	reportingWG := &sync.WaitGroup{}
+	ctx, err := user.InjectIntoGRPCRequest(user.InjectOrgID(context.Background(), "test"))
+	require.NoError(b, err)
+	b.ResetTimer()
+
+	for j := 0; j < b.N; j++ {
+		var maxHeap uint64
+		stopReporting := make(chan struct{})
+		reportingWG.Add(1)
+		go func() {
+			memStats := &runtime.MemStats{}
+			defer reportingWG.Done()
+			t := time.NewTicker(time.Millisecond)
+			defer t.Stop()
+			for {
+				select {
+				case <-stopReporting:
+					return
+				case <-t.C:
+					runtime.ReadMemStats(memStats)
+					maxHeap = util_math.Max(memStats.HeapInuse, maxHeap)
+				}
+			}
+		}()
+		for i := 0; i < 100_000; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				_, err := client.Push(ctx, &mimirpb.WriteRequest{})
+				assert.ErrorContains(b, err, "overwhelmed")
+			}()
+		}
+		wg.Wait()
+		close(stopReporting)
+		reportingWG.Wait()
+		b.ReportMetric(float64(maxHeap), "max-heap")
 	}
 }
