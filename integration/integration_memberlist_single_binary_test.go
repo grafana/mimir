57fcf2218de9dc923a83ecc8e7e3f426a32f8205
Peter Štibraný <pstibrany@gmail.com>
2023-08-25 15:16:32 +0200

    Try for longer.
    Signed-off-by: Peter Štibraný <pstibrany@gmail.com>

--- integration/integration_memberlist_single_binary_test.go
+++ integration/integration_memberlist_single_binary_test.go
@@ -194,6 +194,8 @@ func TestSingleBinaryWithMemberlistScaling(t *testing.T) {
 	// Sanity check the ring membership and give each instance time to see every other instance.
 
 	for _, c := range instances {
+		c.SetBackoff(backoff.Config{MinBackoff: 250 * time.Millisecond, MaxBackoff: 1 * time.Second, MaxRetries: 50})
+
 		// we expect 5*maxMimir to account for ingester, distributor, compactor, store-gateway and store-gateway-client rings
 		require.NoError(t, c.WaitSumMetrics(e2e.Equals(float64(maxMimir*5)), "cortex_ring_members"))
 		require.NoError(t, c.WaitSumMetrics(e2e.Equals(0), "memberlist_client_kv_store_value_tombstones"))
