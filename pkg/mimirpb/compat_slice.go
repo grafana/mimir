f3d8d7ee75488cddb6bba9fd4e2505b9665504c5
Marco Pracucci <marco@pracucci.com>
2023-08-25 16:07:42 +0200

    Add unit test on CompareLabelAdapters()
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/mimirpb/compat_slice.go
+++ pkg/mimirpb/compat_slice.go
@@ -86,7 +86,7 @@ func FromLabelsToLabelAdapters(ls labels.Labels) []LabelAdapter {
 	return *(*[]LabelAdapter)(unsafe.Pointer(&ls))
 }
 
-// The result will be 0 if a==b, <0 if a < b, and >0 if a > b.
+// CompareLabelAdapters returns 0 if a==b, <0 if a < b, and >0 if a > b.
 func CompareLabelAdapters(a, b []LabelAdapter) int {
 	return labels.Compare(FromLabelAdaptersToLabels(a), FromLabelAdaptersToLabels(b))
 }
