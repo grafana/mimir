63f6df065aa6f838dd00ce6414ff9688a20508fc
Jack Baldry <jack.baldry@grafana.com>
2023-09-01 12:05:12 +0100

    Refactor workflow to make it clearer which versions are being built together
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- docs/variables.mk
+++ docs/variables.mk
@@ -10,8 +10,8 @@ endif
 # List of projects to provide to the make-docs script.
 PROJECTS := mimir:$(MIMIR_DOCS_VERSION):mimir-$(MIMIR_DOCS_VERSION) helm-charts/mimir-distributed
 
+# Parent directory of the current git repository.
 GIT_ROOT_PARENT := $(realpath $(GIT_ROOT)/..)
-export REPOS_PATH := $(GIT_ROOT_PARENT)
 
 # Use the doc-validator image defined in CI by default.
 export DOC_VALIDATOR_IMAGE := $(shell sed -n 's, *image: \(grafana/doc-validator.*\),\1,p' "$(GIT_ROOT)/.github/workflows/test-build-deploy.yml")
