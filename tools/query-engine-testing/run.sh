fe934100ad079412d0fed58e8677f800ef7ff5b6
Charles Korn <charles.korn@grafana.com>
2023-09-07 16:40:40 +1000

    Add todo

--- tools/query-engine-testing/run.sh
+++ tools/query-engine-testing/run.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 TARGET_PATH="prometheus/api/v1/query_range?query=up{}&start=2023-09-07T05:00:00Z&end=2023-09-07T05:30:00Z&step=15"
 
+# TODO: run one query against both engines and check results are the same
+
 for target in standard streaming; do
   "$SCRIPT_DIR/run-one.sh" "$target" "$TARGET_PATH" "result-$target-up-$(date -u +%Y-%m-%dT%H-%M-%SZ).json"
 done
