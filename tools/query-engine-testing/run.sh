4146014dc3fcfe1cd8cb6c446ea8900b82d69c06
Charles Korn <charles.korn@grafana.com>
2023-09-13 13:05:01 +1000

    Record test config for fifth test

--- tools/query-engine-testing/run.sh
+++ tools/query-engine-testing/run.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-QUERY="test_metric{index=~\"1\\\\\\d\\\\\\d\\\\\\d\"}" # Select all series with index 1000-1999 (ie. 1000 series)
+QUERY="test_metric{index=~\"1\\\\\\d\\\\\\d\\\\\\d\\\\\\d\"}" # Select all series with index 10000-19999 (ie. 10000 series)
 START="2023-09-13T00:03:00Z"
 END="2023-09-13T01:03:00Z"
 ENCODED_QUERY=$(python3 -c "import urllib.parse; print(urllib.parse.quote('''$QUERY'''))")
