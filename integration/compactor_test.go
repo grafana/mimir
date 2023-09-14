a5fcd9683938df8d210caf9f4234a4dd049b5044
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2023-09-14 17:53:33 +0200

    Update mimir-prometheus to `16653689899c` (2023-09-01) (#5927)
    * Update mimir-prometheus to `b641b78d9` (2023-09-01)
    
    This is a draft PR to showcase the changes from mimir-prometheus PR XXX. This is the diff in mimir-prometheus github.com/grafana/mimir-prometheus/compare/002ae0aa1d7e...77ac7ad40 and these are the corresponding upstream changes github.com/prometheus/prometheus/compare/76dd9b547...b6f903b5f
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
    
    * Update usages of tsdbutil with chunks
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
    
    * Update usages of tsdbutil with chunks
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
    
    * Fix tests relying on prometheus test setup code
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
    
    * Fix tests relying on prometheus test setup code
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
    
    * Follow up change coming from Prometheus PR 12739
    
    https://github.com/prometheus/prometheus/pull/12739
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    * Add histogram_stddev and histogram_stdvar to sharding tests
    
    Followup to https://github.com/prometheus/prometheus/pull/12614
    
    Created issue to double check my patches:
    https://github.com/grafana/mimir/issues/5929
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    * Follow up change coming from Prometheus PR 12739
    
    https://github.com/prometheus/prometheus/pull/12739
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    * Fix block TestUpload
    
    Follow up change coming from Prometheus PR 12739
    
    https://github.com/prometheus/prometheus/pull/12739
    
    The change means that more chunks are created, thus altering the tsdb index
    size.
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    * Remove WIP temporary code
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    * Emit `cortex_ingester_tsdb_mmap_chunks_total`
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
    
    * Update prometheus to main branch
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
    
    ---------
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    Co-authored-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- integration/compactor_test.go
+++ integration/compactor_test.go
@@ -23,7 +23,6 @@ import (
 	"github.com/prometheus/prometheus/tsdb/chunkenc"
 	"github.com/prometheus/prometheus/tsdb/chunks"
 	"github.com/prometheus/prometheus/tsdb/index"
-	"github.com/prometheus/prometheus/tsdb/tsdbutil"
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/mimir/integration/e2emimir"
@@ -71,15 +70,15 @@ func TestCompactBlocksContainingNativeHistograms(t *testing.T) {
 		spec := block.SeriesSpec{
 			Labels: labels.FromStrings("case", "native_histogram", "i", strconv.Itoa(i)),
 			Chunks: []chunks.Meta{
-				must(tsdbutil.ChunkFromSamples([]tsdbutil.Sample{
+				must(chunks.ChunkFromSamples([]chunks.Sample{
 					sample{10, 0, test.GenerateTestHistogram(1), nil},
 					sample{20, 0, test.GenerateTestHistogram(2), nil},
 				})),
-				must(tsdbutil.ChunkFromSamples([]tsdbutil.Sample{
+				must(chunks.ChunkFromSamples([]chunks.Sample{
 					sample{30, 0, test.GenerateTestHistogram(3), nil},
 					sample{40, 0, test.GenerateTestHistogram(4), nil},
 				})),
-				must(tsdbutil.ChunkFromSamples([]tsdbutil.Sample{
+				must(chunks.ChunkFromSamples([]chunks.Sample{
 					sample{50, 0, test.GenerateTestHistogram(5), nil},
 					sample{2*time.Hour.Milliseconds() - 1, 0, test.GenerateTestHistogram(6), nil},
 				})),
