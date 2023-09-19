84d3cb28febab56ecce40ce3cc31be1a7433d639
Oleg Zaytsev <mail@olegzaytsev.com>
2023-09-19 09:35:35 +0200

    Remove backwards compatibility tests for >1y (#6056)
    2.3.0 was released one year ago, so we're removing backwards
    compatibility tests for versions older than that.
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

--- integration/backward_compatibility.go
+++ integration/backward_compatibility.go
@@ -7,18 +7,6 @@ import "github.com/grafana/mimir/integration/e2emimir"
 // DefaultPreviousVersionImages is used by `tools/pre-pull-images` so it needs
 // to be in a non `_test.go` file.
 var DefaultPreviousVersionImages = map[string]e2emimir.FlagMapper{
-	"grafana/mimir:2.0.0": e2emimir.ChainFlagMappers(
-		e2emimir.SetFlagMapper(map[string]string{"-ingester.ring.readiness-check-ring-health": "false"}),
-		e2emimir.RemoveFlagMapper([]string{"-ingester.native-histograms-ingestion-enabled"}),
-	),
-	"grafana/mimir:2.1.0": e2emimir.ChainFlagMappers(
-		e2emimir.SetFlagMapper(map[string]string{"-ingester.ring.readiness-check-ring-health": "false"}),
-		e2emimir.RemoveFlagMapper([]string{"-ingester.native-histograms-ingestion-enabled"}),
-	),
-	"grafana/mimir:2.2.0": e2emimir.ChainFlagMappers(
-		e2emimir.SetFlagMapper(map[string]string{"-ingester.ring.readiness-check-ring-health": "false"}),
-		e2emimir.RemoveFlagMapper([]string{"-ingester.native-histograms-ingestion-enabled"}),
-	),
 	"grafana/mimir:2.3.1": e2emimir.ChainFlagMappers(
 		e2emimir.SetFlagMapper(map[string]string{"-ingester.ring.readiness-check-ring-health": "false"}),
 		e2emimir.RemoveFlagMapper([]string{"-ingester.native-histograms-ingestion-enabled"}),
