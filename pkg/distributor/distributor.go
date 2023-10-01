165ef01563d34d967a62b6b6c87c2f3ee053bb9f
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2023-10-01 19:11:28 +0200

    Add comments
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/distributor/distributor.go
+++ pkg/distributor/distributor.go
@@ -762,7 +762,7 @@ func (d *Distributor) prePushHaDedupeMiddleware(next push.Func) push.Func {
 
 		var getReplicaForSample func(int) haReplica
 		{
-			var replicaKey haReplica
+			var replicaKey haReplica // TODO dimitarvdimitrov - do we need this closure or a regular func also works?
 			getReplicaForSample = func(i int) haReplica {
 				replicaKey.cluster, replicaKey.replica = findHALabels(haReplicaLabel, haClusterLabel, req.Timeseries[i].Labels)
 				return replicaKey
