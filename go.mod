d8336b9c55af48f9ea86afed4a056b0eb3b09b10
Peter Štibraný <pstibrany@gmail.com>
2023-10-06 14:25:47 +0200

    Update dskit, fix grpc push check.
    Signed-off-by: Peter Štibraný <pstibrany@gmail.com>

--- go.mod
+++ go.mod
@@ -20,7 +20,7 @@ require (
 	github.com/golang/snappy v0.0.4
 	github.com/google/gopacket v1.1.19
 	github.com/gorilla/mux v1.8.0
-	github.com/grafana/dskit v0.0.0-20231021095337-736bca5d13d8
+	github.com/grafana/dskit v0.0.0-20231023092801-7982d8ce862f
 	github.com/grafana/e2e v0.1.1
 	github.com/hashicorp/golang-lru v1.0.2 // indirect
 	github.com/json-iterator/go v1.1.12
@@ -47,7 +47,7 @@ require (
 	golang.org/x/net v0.17.0
 	golang.org/x/sync v0.4.0
 	golang.org/x/time v0.3.0
-	google.golang.org/grpc v1.58.3
+	google.golang.org/grpc v1.59.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -91,6 +91,7 @@ require (
 	github.com/cenkalti/backoff/v3 v3.2.2 // indirect
 	github.com/go-test/deep v1.1.0 // indirect
 	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
+	github.com/golang/glog v1.1.2 // indirect
 	github.com/google/gnostic-models v0.6.8 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.4 // indirect
@@ -156,7 +157,6 @@ require (
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/gofrs/uuid v4.4.0+incompatible // indirect
 	github.com/gogo/googleapis v1.4.1 // indirect
-	github.com/golang/glog v1.1.1 // indirect
 	github.com/google/btree v1.0.1 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/pprof v0.0.0-20230926050212-f7f687d19a98 // indirect
--- pkg/mimir/grpc_push_check_test.go
+++ pkg/mimir/grpc_push_check_test.go
@@ -3,6 +3,7 @@
 package mimir
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,15 +13,17 @@ func TestGrpcInflightMethodLimiter(t *testing.T) {
 	t.Run("nil push receiver", func(t *testing.T) {
 		l := newGrpcInflightMethodLimiter(func() pushReceiver { return nil })
 
-		require.NoError(t, l.RPCCallStarting("test"))
+		ctx, err := l.RPCCallStarting(context.Background(), "test", nil)
+		require.NoError(t, err)
 		require.NotPanics(t, func() {
-			l.RPCCallFinished("test")
+			l.RPCCallFinished(ctx)
 		})
 
-		require.ErrorIs(t, l.RPCCallStarting(ingesterPushMethod), errNoIngester)
+		ctx, err = l.RPCCallStarting(context.Background(), ingesterPushMethod, nil)
+		require.ErrorIs(t, err, errNoIngester)
 		require.Panics(t, func() {
 			// In practice, this will not be called, since l.RPCCallStarting(ingesterPushMethod) returns error.
-			l.RPCCallFinished(ingesterPushMethod)
+			l.RPCCallFinished(context.WithValue(ctx, ingesterPushStarted, true))
 		})
 	})
 
@@ -28,19 +31,22 @@ func TestGrpcInflightMethodLimiter(t *testing.T) {
 		m := &mockPushReceiver{startErr: nil}
 
 		l := newGrpcInflightMethodLimiter(func() pushReceiver { return m })
-		require.NoError(t, l.RPCCallStarting("test"))
+
+		ctx, err := l.RPCCallStarting(context.Background(), "test", nil)
+		require.NoError(t, err)
 		require.NotPanics(t, func() {
-			l.RPCCallFinished("test")
+			l.RPCCallFinished(ctx)
 		})
 		require.Equal(t, 0, m.startCalls)
 		require.Equal(t, 0, m.finishCalls)
 
-		require.NoError(t, l.RPCCallStarting(ingesterPushMethod))
+		ctx, err = l.RPCCallStarting(context.Background(), ingesterPushMethod, nil)
+		require.NoError(t, err)
 		require.Equal(t, 1, m.startCalls)
 		require.Equal(t, 0, m.finishCalls)
 
 		require.NotPanics(t, func() {
-			l.RPCCallFinished(ingesterPushMethod)
+			l.RPCCallFinished(ctx)
 		})
 		require.Equal(t, 1, m.startCalls)
 		require.Equal(t, 1, m.finishCalls)
