4a18a5967e77a67100d2517e58a3e7d2882ebfca
Marco Pracucci <marco@pracucci.com>
2023-10-26 14:53:41 +0200

    Added compression analysis
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- tools/read-write-request-sampling/main.go
+++ tools/read-write-request-sampling/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"compress/flate"
+	"compress/gzip"
+	"compress/zlib"
 	"encoding/binary"
 	"flag"
 	"fmt"
 	"io"
 	"os"
 
+	"github.com/golang/snappy"
 	"github.com/grafana/dskit/flagext"
 	"github.com/pkg/errors"
 
@@ -41,6 +46,7 @@ func main() {
 	// Analysis.
 	analyzeLabelsRepetitionFromRequests(reqs)
 	analyzeLabelsSizeFromRequests(reqs)
+	analyzeCompression(reqs)
 }
 
 func readRequestsFromFile(file string) ([]*mimirpb.WriteRequest, error) {
@@ -141,7 +147,7 @@ func analyzeLabelsSizeFromRequest(req *mimirpb.WriteRequest) (origSize, origWith
 
 	// Decode the request to get a clone.
 	clone := &mimirpb.WriteRequest{}
-	_ = clone.Unmarshal(origEncoded)
+	panicOnError(clone.Unmarshal(origEncoded))
 
 	// Remove all labels.
 	for i := 0; i < len(clone.Timeseries); i++ {
@@ -149,11 +155,72 @@ func analyzeLabelsSizeFromRequest(req *mimirpb.WriteRequest) (origSize, origWith
 	}
 
 	// Re-encode the version without labels.
-	cloneEncoded, _ := clone.Marshal()
+	cloneEncoded, _ := clone.Marshal() // TODO check error
 
 	origSize = len(origEncoded)
 	origWithoutLabelsSize = len(cloneEncoded)
 	return
 }
 
-// TODO try various compression algorithms
+func analyzeCompression(reqs []*mimirpb.WriteRequest) {
+	var (
+		totalOrigSize   int
+		totalGzipSize   int
+		totalSnappySize int
+		totalZlibSize   int
+		totalFlateSize  int
+		buffer          = bytes.NewBuffer(nil)
+	)
+
+	for _, req := range reqs {
+		// Encode the request as is.
+		origEncoded, _ := req.Marshal() // TODO error
+		totalOrigSize += len(origEncoded)
+
+		// Gzip.
+		totalGzipSize += compressAndReturnSize(origEncoded, buffer, func(buffer *bytes.Buffer) io.WriteCloser {
+			return gzip.NewWriter(buffer)
+		})
+
+		// Snappy.
+		totalSnappySize += compressAndReturnSize(origEncoded, buffer, func(buffer *bytes.Buffer) io.WriteCloser {
+			return snappy.NewBufferedWriter(buffer)
+		})
+
+		// Zlib.
+		totalZlibSize += compressAndReturnSize(origEncoded, buffer, func(buffer *bytes.Buffer) io.WriteCloser {
+			return zlib.NewWriter(buffer)
+		})
+
+		// Flate.
+		totalFlateSize += compressAndReturnSize(origEncoded, buffer, func(buffer *bytes.Buffer) io.WriteCloser {
+			writer, err := flate.NewWriter(buffer, flate.DefaultCompression)
+			panicOnError(err)
+			return writer
+		})
+	}
+
+	gzipRatio := float64(totalOrigSize) / float64(totalGzipSize)
+	snappyRatio := float64(totalOrigSize) / float64(totalSnappySize)
+	zlibRatio := float64(totalOrigSize) / float64(totalZlibSize)
+	flateRatio := float64(totalOrigSize) / float64(totalFlateSize)
+
+	fmt.Println(fmt.Sprintf("Per-request compression ratios: gzip = %.2f snappy = %.2f zlib = %.2f flate = %.2f", gzipRatio, snappyRatio, zlibRatio, flateRatio))
+}
+
+func compressAndReturnSize(data []byte, tmpBuffer *bytes.Buffer, getCompressor func(buffer *bytes.Buffer) io.WriteCloser) int {
+	tmpBuffer.Reset()
+	writer := getCompressor(tmpBuffer)
+	panicOnError(writer.Write(data))
+	panicOnError(writer.Close())
+
+	return tmpBuffer.Len()
+}
+
+func panicOnError(args ...any) {
+	for _, arg := range args {
+		if err, ok := arg.(error); ok && err != nil {
+			panic(err)
+		}
+	}
+}
