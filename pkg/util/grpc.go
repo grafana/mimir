db0858c5303555191b5af419ba3aefe0d0650021
Charles Korn <charles.korn@grafana.com>
2023-10-30 11:21:08 +1100

    Add documentation to CloseAndExhaust.

--- pkg/util/grpc.go
+++ pkg/util/grpc.go
@@ -7,15 +7,17 @@ type Stream[T any] interface {
 	Recv() (T, error)
 }
 
+// CloseAndExhaust closes and exhausts stream to ensure:
+// - the gRPC library can release any resources associated with the stream (see https://pkg.go.dev/google.golang.org/grpc#ClientConn.NewStream)
+// - instrumentation middleware correctly observes the end of the stream, rather than reporting it as "context canceled"
+//
+// Note that this method may block if the stream has not already been exhausted (successfully or otherwise).
 func CloseAndExhaust[T any](stream Stream[T]) error {
 	err := stream.CloseSend() //nolint:forbidigo // This is the one place we want to call this method.
 	if err != nil {
 		return err
 	}
 
-	// Exhaust the stream to ensure:
-	// - the gRPC library can release any resources associated with the stream (see https://pkg.go.dev/google.golang.org/grpc#ClientConn.NewStream)
-	// - instrumentation middleware correctly observes the end of the stream, rather than reporting it as "context canceled"
 	for err == nil {
 		_, err = stream.Recv()
 	}
