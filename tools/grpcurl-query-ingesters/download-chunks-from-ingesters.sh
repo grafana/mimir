0a2399ca2615e4981a930b31d7af726e7f0c09f0
Marco Pracucci <marco@pracucci.com>
2023-10-31 16:50:44 +0100

    Add tooling to easily download chunks from ingesters and parse it
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

---
+++ tools/grpcurl-query-ingesters/download-chunks-from-ingesters.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Configuration.
+K8S_CONTEXT=""
+K8S_NAMESPACE=""
+MIMIR_TENANT_ID=""
+
+mkdir -p chunks-dump/
+
+# Get list of pods.
+PODS=$(kubectl --context "$K8S_CONTEXT" -n "$K8S_NAMESPACE" get pods --no-headers | grep ingester | awk '{print $1}')
+
+for POD in $PODS; do
+  echo "Querying $POD"
+
+  # Open port-forward
+  kubectl port-forward --context "$K8S_CONTEXT" -n "$K8S_NAMESPACE" "$POD" 9095:9095 &
+  KUBECTL_PID=$!
+
+  # Wait some time
+  sleep 5
+
+  cat query.json | grpcurl \
+    -d @ \
+    -H "X-Scope-OrgID: $MIMIR_TENANT_ID" \
+  	-proto pkg/ingester/client/ingester.proto \
+  	-import-path . \
+  	-import-path ./vendor \
+  	-plaintext \
+  	localhost:9095 "cortex.Ingester/QueryStream" > "chunks-dump/$POD"
+
+  kill $KUBECTL_PID
+  wait $KUBECTL_PID
+done
---
+++ tools/grpcurl-query-ingesters/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/grafana/dskit/flagext"
+	"github.com/prometheus/prometheus/tsdb/chunkenc"
+)
+
+func main() {
+	args, err := flagext.ParseFlagsAndArguments(flag.CommandLine)
+	if err != nil {
+		fmt.Println("Failed to parse CLI arguments:", err.Error())
+		os.Exit(1)
+	}
+
+	for _, arg := range args {
+		res, err := parseFile(arg)
+		if err != nil {
+			fmt.Println("Failed to parse file:", err.Error())
+			os.Exit(1)
+		}
+
+		dumpResponse(res)
+	}
+}
+
+func parseFile(file string) (QueryStreamResponse, error) {
+	res := QueryStreamResponse{}
+
+	fileData, err := os.ReadFile(file)
+	if err != nil {
+		return res, err
+	}
+
+	// Decode file.
+	decoder := json.NewDecoder(bytes.NewReader(fileData))
+	if err := decoder.Decode(&res); err != nil {
+		return res, err
+	}
+
+	return res, nil
+}
+
+func dumpResponse(res QueryStreamResponse) {
+	for _, series := range res.Chunkseries {
+		fmt.Println(series.LabelSet().String())
+
+		for _, chunk := range series.Chunks {
+			fmt.Println(fmt.Sprintf(
+				"- Chunk: %s - %s",
+				chunk.StartTime().Format(time.TimeOnly),
+				chunk.EndTime().Format(time.TimeOnly)))
+
+			chunkIterator := chunk.EncodedChunk().NewIterator(nil)
+			for {
+				sampleType := chunkIterator.Scan()
+				if sampleType == chunkenc.ValNone {
+					break
+				}
+
+				switch sampleType {
+				case chunkenc.ValFloat:
+					fmt.Println("  - Sample:", sampleType.String(), "ts:", chunkIterator.Timestamp(), "value:", chunkIterator.Value().Value)
+				case chunkenc.ValHistogram:
+					ts, value := chunkIterator.AtHistogram()
+					fmt.Println("  - Sample:", sampleType.String(), "ts:", ts, "value:", value)
+				case chunkenc.ValFloatHistogram:
+					ts, value := chunkIterator.AtFloatHistogram()
+					fmt.Println("  - Sample:", sampleType.String(), "ts:", ts, "value:", value)
+				default:
+					panic(fmt.Errorf("unknown sample type %s", sampleType.String()))
+				}
+			}
+
+			if chunkIterator.Err() != nil {
+				panic(chunkIterator.Err())
+			}
+		}
+	}
+}
