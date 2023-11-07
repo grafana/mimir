b4aae8205f49386ebd607d80ed87e5b756f18d92
francoposa <franco@francoposa.io>
2023-11-07 14:05:20 -0800

    create clearer tests for tree queue using balanced tree cases

--- pkg/scheduler/queue/tree_queue_test.go
+++ pkg/scheduler/queue/tree_queue_test.go
@@ -13,97 +13,93 @@ import (
 
 const maxTestQueueLen = 8
 
-func TestTreeQueue(t *testing.T) {
+// TestDequeueBalancedTree checks dequeuing behavior from a balanced tree.
+//
+// Dequeuing from a balanced tree allows the test to have a simple looped structures
+// while running checks to ensure that round-robin order is respected.
+func TestDequeueBalancedTree(t *testing.T) {
+	firstDimensions := []string{"0", "1", "2"}
+	secondDimensions := []string{"a", "b", "c"}
+	itemsPerDimension := 5
+	root := makeBalancedTreeQueue(t, firstDimensions, secondDimensions, itemsPerDimension)
+	assert.NotNil(t, root)
 
-	root := NewTreeQueue("root", maxTestQueueLen) // creates path: root
+	count := 0
+	// tree queue will fairly dequeue from all levels of the tree
+	rotationsBeforeRepeat := len(firstDimensions) * len(secondDimensions)
+	// track dequeued paths to ensure round-robin dequeuing does not repeat before expected
+	dequeuedPathCache := make([]QueuePath, rotationsBeforeRepeat)
 
-	type queuePathItem struct {
-		path QueuePath
-		item any
-	}
+	for !root.IsEmpty() {
+		dequeuedPath, v := root.Dequeue()
+		assertDequeuedPathValueMatch(t, dequeuedPath, v)
 
-	// note no queue at a given level is dequeued from twice in a row
-	// unless all others at the same level are empty down to the leaf node
-	queuePathItems := []queuePathItem{
-		{QueuePath{"root", "0"}, "root:0:val0"},
-		{QueuePath{"root", "1"}, "root:1:val0"},
-		{QueuePath{"root", "2"}, "root:2:val0"},
-		{QueuePath{"root", "1", "0"}, "root:1:0:val0"},
-		{QueuePath{"root", "2", "0"}, "root:2:0:val0"},
-		{QueuePath{"root", "1"}, "root:1:val1"},
-		{QueuePath{"root", "2", "1"}, "root:2:1:val0"},
-		{QueuePath{"root", "1", "0"}, "root:1:0:val1"},
-		{QueuePath{"root", "2", "0"}, "root:2:0:val1"},
-		{QueuePath{"root", "2", "1"}, "root:2:1:val1"},
-		{QueuePath{"root", "2", "1"}, "root:2:1:val2"},
-	}
-
-	// build tree by enqueueing in order
-	for _, pathItem := range queuePathItems {
-		childPath := pathItem.path[1:]
-		require.Nil(t, root.EnqueueBackByPath(childPath, pathItem.item))
-	}
-
-	// assert all expected child paths have been created; empty child path {} serves as root path
-	childQueuePaths := []QueuePath{
-		{}, {"0"}, {"1"}, {"1", "0"}, {"2"}, {"2", "0"}, {"2", "1"},
-	}
-	assert.Equal(t, len(childQueuePaths), root.NodeCount())
-	for _, childQueuePath := range childQueuePaths {
-		assert.NotNil(t, root.getNode(childQueuePath))
-	}
-
-	// check some nonexistent paths
-	assert.Nil(t, root.getNode(QueuePath{"3"}))
-	assert.Nil(t, root.getNode(QueuePath{"1", "1"}))
-	assert.Nil(t, root.getNode(QueuePath{"2", "2"}))
-
-	// queuePathItems, plus an extra item which will be enqueued during dequeueing
-	// note no queue at a given level is dequeued from twice in a row
-	// unless all others at the same level are empty down to the leaf node
-	expectedQueueOutput := []queuePathItem{
-		{QueuePath{"root", "0"}, "root:0:val0"}, // root:0:localQueue is done
-		{QueuePath{"root", "1"}, "root:1:val0"},
-		{QueuePath{"root", "2"}, "root:2:val0"}, // root:2:localQueue is done
-		{QueuePath{"root", "1", "0"}, "root:1:0:val0"},
-		{QueuePath{"root", "2", "0"}, "root:2:0:val0"},
-		{QueuePath{"root", "1"}, "root:1:val1"}, // root:1:localQueue is done
-		{QueuePath{"root", "2", "1"}, "root:2:1:val0"},
-		{QueuePath{"root", "1", "0"}, "root:1:0:val1"}, // root:1:0:localQueue is done; no other queues in root:1, so root:1 is done as well
-		{QueuePath{"root", "2", "0"}, "root:2:0:val1"}, // root:2:0:localQueue is done
-		{QueuePath{"root", "1", "0"}, "root:1:0:val2"}, // this was enqueued during dequeueing, which re-creates the deleted root:1 and its child root:1:0
-		{QueuePath{"root", "2", "1"}, "root:2:1:val1"}, // once again root:1:0:localQueue is done; no other queues in root:1, so root:1 is done as well
-		{QueuePath{"root", "2", "1"}, "root:2:1:val2"}, // root:2:1:localQueue is done; no other queues in root:2, so root:2 is done as well
-		// back up to root; its local queue is done and all childQueueOrder are done, so the full tree is done
-	}
-
-	var queueOutput []queuePathItem
-	for range expectedQueueOutput {
-		path, v := root.Dequeue()
-		if v == nil {
-			fmt.Println(path)
-			break
+		// assert dequeued path has not repeated before the expected number of rotations
+		for _, previousDequeuedPath := range dequeuedPathCache {
+			assert.NotEqual(t, previousDequeuedPath, dequeuedPath)
 		}
-		queueOutput = append(queueOutput, queuePathItem{path, v})
-		if v == "root:1:0:val1" {
-			// root:1 and all subqueues are completely exhausted;
-			// root:2 will be next in the rotation
-			// here we insert something new into root:1 to test that:
-			//  - the new root:1 insert does not jump the line in front of root:2
-			//  - root:2 will not be dequeued from twice in a row now that there is a item in root:1 again
-			require.NoError(t, root.EnqueueBackByPath(QueuePath{"1", "0"}, "root:1:0:val2"))
-		}
-	}
-	assert.Equal(t, expectedQueueOutput, queueOutput)
 
-	// Dequeue one more time;
-	path, v := root.Dequeue()
-	assert.Nil(t, v) // assert we get nil back,
-	assert.Nil(t, path)
-	assert.True(t, root.IsEmpty()) // assert nothing in local or child queues
+		dequeuedPathCache = append(dequeuedPathCache[1:], dequeuedPath)
+		count++
+	}
+
+	// count items enqueued to nodes at depth 1
+	expectedFirstDimensionCount := len(firstDimensions) * itemsPerDimension
+	// count items enqueued to nodes at depth 2
+	expectedSecondDimensionCount := len(firstDimensions) * len(secondDimensions) * itemsPerDimension
+
+	assert.Equal(t, expectedFirstDimensionCount+expectedSecondDimensionCount, count)
 }
 
-func TestDequeue(t *testing.T) {
+// TestDequeueBalancedTree checks dequeuing behavior by path from a balanced tree.
+//
+// Dequeuing from a balanced tree allows the test to have a simple looped structures
+// while running checks to ensure that round-robin order is respected.
+func TestDequeueByPathBalancedTree(t *testing.T) {
+	firstDimensions := []string{"0", "1", "2"}
+	secondDimensions := []string{"a", "b", "c"}
+	itemsPerDimension := 5
+	root := makeBalancedTreeQueue(t, firstDimensions, secondDimensions, itemsPerDimension)
+	assert.NotNil(t, root)
+
+	count := 0
+	// tree queue will fairly dequeue from all levels of the tree below the path provided;
+	// dequeuing by path skips the top level the tree and rotates only through the
+	// second-layer subtrees of the first layer node selected by the queue path
+	rotationsBeforeRepeat := len(secondDimensions)
+	// track dequeued paths to ensure round-robin dequeuing does not repeat before expected
+	dequeuedPathCache := make([]QueuePath, rotationsBeforeRepeat)
+
+	for _, firstDimName := range firstDimensions {
+		firstDimPath := QueuePath{firstDimName}
+		for root.getNode(firstDimPath) != nil {
+			dequeuedPath, v := root.DequeueByPath(firstDimPath)
+			assertDequeuedPathValueMatch(t, dequeuedPath, v)
+
+			// assert dequeued path has not repeated before the expected number of rotations
+			for _, previousDequeuedPath := range dequeuedPathCache {
+				assert.NotEqual(t, previousDequeuedPath, dequeuedPath)
+			}
+
+			dequeuedPathCache = append(dequeuedPathCache[1:], dequeuedPath)
+			count++
+		}
+	}
+
+	// count items enqueued to nodes at depth 1
+	expectedFirstDimensionCount := len(firstDimensions) * itemsPerDimension
+	// count items enqueued to nodes at depth 2
+	expectedSecondDimensionCount := len(firstDimensions) * len(secondDimensions) * itemsPerDimension
+
+	assert.Equal(t, expectedFirstDimensionCount+expectedSecondDimensionCount, count)
+}
+
+// TestDequeuePathUnbalancedTree checks dequeuing behavior from an unbalanced tree.
+//
+// Assertions are done one by one to illustrate and check the behaviors of dequeuing from
+// an unbalanced tree, where the same node will be dequeued from twice if the node remains
+// nonempty while its sibling nodes have been exhausted and deleted from the tree.
+func TestDequeueUnbalancedTree(t *testing.T) {
 	root := makeUnbalancedTreeQueue(t)
 
 	// dequeue from root until exhausted
@@ -179,7 +175,12 @@ func TestDequeue(t *testing.T) {
 	assert.True(t, root.IsEmpty())
 }
 
-func TestDequeuePath(t *testing.T) {
+// TestDequeueByPathUnbalancedTree checks dequeuing behavior from an unbalanced tree by path.
+//
+// Assertions are done one by one to illustrate and check the behaviors of dequeuing from
+// an unbalanced tree, where the same node will be dequeued from twice if the node remains
+// nonempty while its sibling nodes have been exhausted and deleted from the tree.
+func TestDequeueByPathUnbalancedTree(t *testing.T) {
 	root := makeUnbalancedTreeQueue(t)
 
 	// dequeue from root:2 until exhausted
@@ -337,6 +338,33 @@ func TestEnqueueDuringDequeueRespectsRoundRobin(t *testing.T) {
 
 	// assert nothing in local or child queues
 	assert.True(t, root.IsEmpty())
+}
+
+func makeBalancedTreeQueue(t *testing.T, firstDimensions, secondDimensions []string, itemsPerDimensions int) *TreeQueue {
+	root := NewTreeQueue("root", maxTestQueueLen)
+	require.Equal(t, 1, root.NodeCount())
+	require.Equal(t, 0, root.ItemCount())
+
+	cache := map[string]struct{}{}
+
+	for _, firstDimName := range firstDimensions {
+		// insert first dimension local queue items
+		for k := 0; k < itemsPerDimensions; k++ {
+			childPath := QueuePath{firstDimName}
+			item := makeQueueItemForChildPath(root, childPath, cache)
+			require.NoError(t, root.EnqueueBackByPath(childPath, item))
+		}
+		for _, secondDimName := range secondDimensions {
+			// insert second dimension local queue items
+			for k := 0; k < itemsPerDimensions; k++ {
+				childPath := QueuePath{firstDimName, secondDimName}
+				item := makeQueueItemForChildPath(root, childPath, cache)
+				require.NoError(t, root.EnqueueBackByPath(childPath, item))
+			}
+		}
+	}
+
+	return root
 }
 
 func makeUnbalancedTreeQueue(t *testing.T) *TreeQueue {
