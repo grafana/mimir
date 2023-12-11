12c91ff6556a42b4b3dac735c86477c86e27b3c5
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2023-12-11 17:03:36 +0100

    Update docs on CacheKeyGenerator
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/frontend/querymiddleware/results_cache.go
+++ pkg/frontend/querymiddleware/results_cache.go
@@ -191,10 +191,14 @@ type CacheKeyGenerator interface {
 
 	// LabelValues should return a cache key for a label values request. The cache key does not need to contain the tenant ID.
 	// LabelValues can return ErrUnsupportedRequest, in which case the response won't be treated as an error, but the item will still not be cached.
+	// LabelValues should return a nil *GenericQueryCacheKey when it returns an error and
+	// should always return nil *GenericQueryCacheKey when the returned error is non-nil.
 	LabelValues(ctx context.Context, path string, values url.Values) (*GenericQueryCacheKey, error)
 
 	// LabelValuesCardinality should return a cache key for a label values cardinality request. The cache key does not need to contain the tenant ID.
 	// LabelValuesCardinality can return ErrUnsupportedRequest, in which case the response won't be treated as an error, but the item will still not be cached.
+	// LabelValuesCardinality should return a nil *GenericQueryCacheKey when it returns an error and
+	// should always return nil *GenericQueryCacheKey when the returned error is non-nil.
 	LabelValuesCardinality(ctx context.Context, path string, values url.Values) (*GenericQueryCacheKey, error)
 }
 
