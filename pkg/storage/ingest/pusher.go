dbab1eb2084e11ff1b5745a00322754a1b334a2b
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2023-12-13 19:45:24 +0100

    Record cortex_ingest_storage_reader_processing_time_seconds
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/storage/ingest/pusher.go
+++ pkg/storage/ingest/pusher.go
@@ -2,6 +2,7 @@ package ingest
 
 import (
 	"context"
+	"time"
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
@@ -18,7 +19,9 @@ type Pusher interface {
 
 type pusherConsumer struct {
 	p Pusher
-	l log.Logger
+
+	metrics readerMetrics
+	l       log.Logger
 }
 
 type parsedRecord struct {
@@ -27,10 +30,11 @@ type parsedRecord struct {
 	err      error
 }
 
-func newPusherConsumer(p Pusher, l log.Logger) *pusherConsumer {
+func newPusherConsumer(p Pusher, metrics readerMetrics, l log.Logger) *pusherConsumer {
 	return &pusherConsumer{
-		p: p,
-		l: l,
+		p:       p,
+		metrics: metrics,
+		l:       l,
 	}
 }
 
@@ -40,21 +44,25 @@ func (c pusherConsumer) Consume(ctx context.Context, records []Record) error {
 	defer cancel(cancellation.NewErrorf("done consuming records"))
 
 	go c.unmarshalRequests(ctx, records, recC)
-	err := c.consumeRequests(ctx, recC)
+	err := c.pushRequests(ctx, recC)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (c pusherConsumer) consumeRequests(ctx context.Context, reqC <-chan parsedRecord) error {
+func (c pusherConsumer) pushRequests(ctx context.Context, reqC <-chan parsedRecord) error {
 	for wr := range reqC {
 		if wr.err != nil {
 			level.Error(c.l).Log("msg", "failed to parse write request; skipping", "err", wr.err)
 			continue
 		}
+		processingStart := time.Now()
+
 		ctx := user.InjectOrgID(ctx, wr.tenantId)
 		_, err := c.p.Push(ctx, wr.WriteRequest)
+
+		c.metrics.processingTime.Observe(time.Since(processingStart).Seconds())
 		if err != nil {
 			level.Error(c.l).Log("msg", "failed to push write request; skipping", "err", err)
 			// TODO move distributor's isClientError to a separate package and use that here to swallow only client errors and abort on others
