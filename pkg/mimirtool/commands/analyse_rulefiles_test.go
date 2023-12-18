e4f98e0423194b7de0a0f77cbad02af3cf3089a6
Oleg Zaytsev <mail@olegzaytsev.com>
2023-12-18 13:51:28 +0100

    Fix .metricsUsed in mimirtool analyze rule-file
    This field wasn't populated.
    
    Fixes #6952
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

---
+++ pkg/mimirtool/commands/analyse_rulefiles_test.go
@@ -0,0 +1,41 @@
+package commands
+
+import (
+	"testing"
+
+	"github.com/prometheus/common/model"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAnalyzeRuleFiles(t *testing.T) {
+	mir, err := AnalyzeRuleFiles([]string{"testdata/prometheus_rules.yaml"})
+	require.NoError(t, err)
+	require.Equal(t, 24, len(mir.MetricsUsed))
+	expectedMetrics := model.LabelValues{
+		"apiserver_request_duration_seconds_bucket",
+		"apiserver_request_duration_seconds_count",
+		"apiserver_request_total",
+		"container_memory_cache",
+		"container_memory_rss",
+		"container_memory_swap",
+		"container_memory_working_set_bytes",
+		"kube_pod_container_resource_limits",
+		"kube_pod_container_resource_requests",
+		"kube_pod_info",
+		"kube_pod_owner",
+		"kube_pod_status_phase",
+		"kube_replicaset_owner",
+		"kubelet_node_name",
+		"kubelet_pleg_relist_duration_seconds_bucket",
+		"node_cpu_seconds_total",
+		"node_memory_Buffers_bytes",
+		"node_memory_Cached_bytes",
+		"node_memory_MemAvailable_bytes",
+		"node_memory_MemFree_bytes",
+		"node_memory_Slab_bytes",
+		"scheduler_binding_duration_seconds_bucket",
+		"scheduler_e2e_scheduling_duration_seconds_bucket",
+		"scheduler_scheduling_algorithm_duration_seconds_bucket",
+	}
+	require.Equal(t, expectedMetrics, mir.MetricsUsed)
+}
