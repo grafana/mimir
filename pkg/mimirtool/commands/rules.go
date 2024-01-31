e90af351f633531409d83bf91e95b4fd00d2a994
Edwin Tye <edwin.tye@gmail.com>
2024-01-31 13:23:36 +0000

    fix: remove redundant --save-file flag

--- pkg/mimirtool/commands/rules.go
+++ pkg/mimirtool/commands/rules.go
@@ -72,8 +72,6 @@ type RuleCommand struct {
 	// Get Rule Groups Configs
 	Namespace string
 	RuleGroup string
-	// Used for both Get and Print
-	SaveFile  bool
 	OutputDir string
 
 	// Load Rules Config
@@ -212,8 +210,7 @@ func (r *RuleCommand) Register(app *kingpin.Application, envVars EnvVarNames, re
 	getRuleGroupCmd.Arg("namespace", "Namespace of the rulegroup to retrieve.").Required().StringVar(&r.Namespace)
 	getRuleGroupCmd.Arg("group", "Name of the rulegroup to retrieve.").Required().StringVar(&r.RuleGroup)
 	getRuleGroupCmd.Flag("disable-color", "disable colored output").BoolVar(&r.DisableColor)
-	getRuleGroupCmd.Flag("save-file", "save file to directory").Default("false").BoolVar(&r.SaveFile)
-	getRuleGroupCmd.Flag("output-dir", "The directory where the rules will be written to.").Default(".").ExistingDirVar(&r.OutputDir)
+	getRuleGroupCmd.Flag("output-dir", "The directory where the rules will be written to.").ExistingDirVar(&r.OutputDir)
 
 	// Delete RuleGroup Command
 	deleteRuleGroupCmd.Arg("namespace", "Namespace of the rulegroup to delete.").Required().StringVar(&r.Namespace)
@@ -448,7 +445,7 @@ func (r *RuleCommand) getRuleGroup(_ *kingpin.ParseContext) error {
 		log.Fatalf("Unable to read rules from Grafana Mimir, %v", err)
 	}
 
-	if r.SaveFile {
+	if r.OutputDir != "" {
 		log.Debugf("Writing to file %s.yaml", r.Namespace)
 		err := saveNamespaceRuleGroup(r.Namespace, []rwrulefmt.RuleGroup{*group}, r.OutputDir)
 		if err != nil {
