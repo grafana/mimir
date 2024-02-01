358b38b62111ac83e73d74100eac1a168fc35095
francoposa <franco@francoposa.io>
2024-01-31 17:16:44 -0800

    WIP Mimir Debug Service

--- integration/e2emimir/services.go
+++ integration/e2emimir/services.go
@@ -21,8 +21,8 @@ const (
 	delveBinaryName = "dlv"
 )
 
-// GetDefaultImage returns the Docker image to use to run Mimir.
-func GetDefaultImage() string {
+// GetMimirImageOrDefault returns the Docker image to use to run Mimir.
+func GetMimirImageOrDefault() string {
 	// Get the mimir image from the MIMIR_IMAGE env variable,
 	// falling back to grafana/mimir:latest"
 	if img := os.Getenv("MIMIR_IMAGE"); img != "" {
@@ -70,9 +70,9 @@ func getExtraFlags() map[string]string {
 	return extraArgs
 }
 
-func newMimirServiceFromOptions(name string, defaultFlags, flags map[string]string, options ...Option) *MimirService {
+func newMimirServiceFromOptions(name string, defaultFlags, overrideFlags map[string]string, options ...Option) *MimirService {
 	o := newOptions(options)
-	serviceFlags := o.MapFlags(e2e.MergeFlags(defaultFlags, flags, getExtraFlags()))
+	serviceFlags := o.MapFlags(e2e.MergeFlags(defaultFlags, overrideFlags, getExtraFlags()))
 
 	return NewMimirService(
 		name,
@@ -83,6 +83,42 @@ func newMimirServiceFromOptions(name string, defaultFlags, flags map[string]stri
 		o.HTTPPort,
 		o.GRPCPort,
 		o.OtherPorts...,
+	)
+}
+
+func newMimirDebugServiceFromOptions(name string, defaultFlags, flags map[string]string, options ...Option) *MimirService {
+	o := newOptions(options)
+	serviceFlags := o.MapFlags(e2e.MergeFlags(defaultFlags, flags, getExtraFlags()))
+	serviceArgs := e2e.BuildArgs(serviceFlags)
+
+	delveListenPort := o.HTTPPort + 10000
+
+	// delve ars must be ordered correctly due to the use of `--`
+	// to delineate the end of delve args and beginning of the binary's args;
+	// do not use any interface utilizing a map structure as order will not be preserved
+	delveArgs := []string{
+		"--log",
+		"exec",
+		"mimir",
+		fmt.Sprintf("--listen=:%d", delveListenPort),
+		"--headless=true",
+		"--api-version=2",
+		"--accept-multiclient",
+		"--",
+	}
+
+	cmd := e2e.NewCommandWithoutEntrypoint(delveBinaryName, append(delveArgs, serviceArgs...)...)
+
+	return NewMimirService(
+		name,
+		o.Image,
+		cmd,
+		e2e.NewHTTPReadinessProbe(o.HTTPPort, "/ready", 200, 299),
+		o.Environment,
+		o.HTTPPort,
+		o.GRPCPort,
+		//otherPorts,
+		//map[int]int{delveListenPort: delveListenPort, o.HTTPPort: o.HTTPPort},
 	)
 }
 
@@ -214,20 +250,31 @@ func NewCompactor(name string, consulAddress string, flags map[string]string, op
 	)
 }
 
-func NewSingleBinary(name string, flags map[string]string, options ...Option) *MimirService {
+var singleBinaryDefaultFlags = map[string]string{
+	// Do not pass any extra default flags (except few used to speed up the test)
+	// because the config could be driven by the config file.
+	"-target":    "all",
+	"-log.level": "warn",
+	// Speed up the startup.
+	"-ingester.ring.min-ready-duration": "0s",
+	// Enable native histograms
+	"-ingester.native-histograms-ingestion-enabled": "true",
+}
+
+func NewSingleBinary(name string, overrideFlags map[string]string, options ...Option) *MimirService {
 	return newMimirServiceFromOptions(
 		name,
-		map[string]string{
-			// Do not pass any extra default flags (except few used to speed up the test)
-			// because the config could be driven by the config file.
-			"-target":    "all",
-			"-log.level": "warn",
-			// Speed up the startup.
-			"-ingester.ring.min-ready-duration": "0s",
-			// Enable native histograms
-			"-ingester.native-histograms-ingestion-enabled": "true",
-		},
-		flags,
+		singleBinaryDefaultFlags,
+		overrideFlags,
+		options...,
+	)
+}
+
+func NewDebugSingleBinary(name string, overrideFlags map[string]string, options ...Option) *MimirService {
+	return newMimirDebugServiceFromOptions(
+		name,
+		singleBinaryDefaultFlags,
+		overrideFlags,
 		options...,
 	)
 }
@@ -349,7 +396,7 @@ type Option func(*Options)
 // newOptions creates an Options with default values and applies the options provided.
 func newOptions(options []Option) *Options {
 	o := &Options{
-		Image:       GetDefaultImage(),
+		Image:       GetMimirImageOrDefault(),
 		MapFlags:    NoopFlagMapper,
 		HTTPPort:    httpPort,
 		GRPCPort:    grpcPort,
