92a48b11f85f94d11e855a9d37b8788140cc8326
Felix Beuke <felix.j.beuke@gmail.com>
2024-02-26 09:04:40 +0100

    Revert "Update github.com/thanos-io/objstore digest to a8d75c5 (#7351)" (#7459)
    This reverts commit 101bfc2d7d6277b40b0236a47c14e15d019d3106.
    
    (cherry picked from commit 1c4164b8eb47297f86364183e63a597e7fe9aacc)

--- vendor/github.com/thanos-io/objstore/providers/s3/s3.go
+++ vendor/github.com/thanos-io/objstore/providers/s3/s3.go
@@ -14,6 +14,7 @@ import (
 	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/efficientgo/core/logerrcapture"
 	"github.com/go-kit/log"
@@ -22,6 +23,7 @@ import (
 	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/minio/minio-go/v7/pkg/encrypt"
 	"github.com/pkg/errors"
+	"github.com/prometheus/common/model"
 	"github.com/prometheus/common/version"
 	"gopkg.in/yaml.v2"
 
@@ -99,8 +101,16 @@ const (
 )
 
 var DefaultConfig = Config{
-	PutUserMetadata:  map[string]string{},
-	HTTPConfig:       exthttp.DefaultHTTPConfig,
+	PutUserMetadata: map[string]string{},
+	HTTPConfig: exthttp.HTTPConfig{
+		IdleConnTimeout:       model.Duration(90 * time.Second),
+		ResponseHeaderTimeout: model.Duration(2 * time.Minute),
+		TLSHandshakeTimeout:   model.Duration(10 * time.Second),
+		ExpectContinueTimeout: model.Duration(1 * time.Second),
+		MaxIdleConns:          100,
+		MaxIdleConnsPerHost:   100,
+		MaxConnsPerHost:       0,
+	},
 	PartSize:         1024 * 1024 * 64, // 64MB.
 	BucketLookupType: AutoLookup,
 	SendContentMd5:   true, // Default to using MD5.
