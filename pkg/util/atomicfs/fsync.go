1a5f555c43142753a06c75a4e0b53ad78bc4e2f6
JB3523 Henrik Søndergaard Christensen <jb3523@jyskebank.dk>
2024-03-01 20:38:23 +0100

    index headerss write windows support

--- pkg/util/atomicfs/fsync.go
+++ pkg/util/atomicfs/fsync.go
@@ -7,47 +7,85 @@ import (
 	"io"
 	"os"
 	"path"
-
-	"github.com/grafana/dskit/multierror"
+	"runtime"
 )
 
-// CreateFile creates a file in the filePath, write the data into the file and then execute
-// fsync operation to make sure the file and its content are stored atomically. If the file already
-// exists, it will be overwritten.
+// // CreateFile creates a file in the filePath, write the data into the file and then execute
+// // fsync operation to make sure the file and its content are stored atomically. If the file already
+// // exists, it will be overwritten.
+// func CreateFile(filePath string, data io.Reader) error {
+// 	// Write the file, fsync it, then fsync the containing directory in order to guarantee
+// 	// it is persisted to disk. From https://man7.org/linux/man-pages/man2/fsync.2.html
+// 	//
+// 	// > Calling fsync() does not necessarily ensure that the entry in the
+// 	// > directory containing the file has also reached disk.  For that an
+// 	// > explicit fsync() on a file descriptor for the directory is also
+// 	// > needed.
+// 	file, err := os.Create(filePath)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	merr := multierror.New()
+// 	buf := new(bytes.Buffer)
+// 	_, err = buf.ReadFrom(data)
+// 	merr.Add(err)
+// 	_, err = file.Write(buf.Bytes())
+// 	merr.Add(err)
+// 	merr.Add(file.Sync())
+// 	merr.Add(file.Close())
+
+// 	if err := merr.Err(); err != nil {
+// 		return err
+// 	}
+
+// 	dir, err := os.OpenFile(path.Dir(file.Name()), os.O_RDONLY, 0777)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	merr.Add(dir.Sync())
+// 	merr.Add(dir.Close())
+// 	return merr.Err()
+// }
+
 func CreateFile(filePath string, data io.Reader) error {
-	// Write the file, fsync it, then fsync the containing directory in order to guarantee
-	// it is persisted to disk. From https://man7.org/linux/man-pages/man2/fsync.2.html
-	//
-	// > Calling fsync() does not necessarily ensure that the entry in the
-	// > directory containing the file has also reached disk.  For that an
-	// > explicit fsync() on a file descriptor for the directory is also
-	// > needed.
 	file, err := os.Create(filePath)
 	if err != nil {
 		return err
 	}
+	defer file.Close() // Ensure file is closed regardless of the errors below
 
-	merr := multierror.New()
+	// Read data into buffer before writing to file
 	buf := new(bytes.Buffer)
-	_, err = buf.ReadFrom(data)
-	merr.Add(err)
-	_, err = file.Write(buf.Bytes())
-	merr.Add(err)
-	merr.Add(file.Sync())
-	merr.Add(file.Close())
-
-	if err := merr.Err(); err != nil {
+	if _, err := buf.ReadFrom(data); err != nil {
 		return err
 	}
 
-	dir, err := os.OpenFile(path.Dir(file.Name()), os.O_RDONLY, 0777)
-	if err != nil {
+	// Write buffer to file
+	if _, err := file.Write(buf.Bytes()); err != nil {
 		return err
 	}
 
-	merr.Add(dir.Sync())
-	merr.Add(dir.Close())
-	return merr.Err()
+	// Sync file to ensure data is written to disk
+	if err := file.Sync(); err != nil {
+		return err
+	}
+
+	// Skip directory sync on Windows
+	if runtime.GOOS != "windows" {
+		dir, err := os.OpenFile(path.Dir(filePath), os.O_RDONLY, 0777)
+		if err != nil {
+			return err
+		}
+		defer dir.Close() // Ensure directory is closed regardless of the errors below
+
+		if err := dir.Sync(); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // CreateFileAndMove creates a file in the tmpPath, write the data into the file and then execute
