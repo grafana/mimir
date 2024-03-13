777be761354b28c3e5003a84be37e111138685d6
Vladimir Varankin <vladimir.varankin@grafana.com>
2024-03-13 12:09:00 +0100

    distributor, ruler: expect METHOD_NOT_ALLOWED from pusher
    Signed-off-by: Vladimir Varankin <vladimir.varankin@grafana.com>

--- pkg/distributor/push.go
+++ pkg/distributor/push.go
@@ -228,6 +228,9 @@ func toHTTPStatus(ctx context.Context, pushErr error, limits *validation.Overrid
 			return http.StatusServiceUnavailable
 		case mimirpb.CIRCUIT_BREAKER_OPEN:
 			return http.StatusServiceUnavailable
+		case mimirpb.METHOD_NOT_ALLOWED:
+			// Return a 501 (and not 405) to explicitly signal a misconfiguration and to possibly track that amongst other 5xx errors.
+			return http.StatusNotImplemented
 		}
 	}
 
