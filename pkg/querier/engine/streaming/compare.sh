61d22a1caacca467a7c1003af33b6054820002b1
Charles Korn <charles.korn@grafana.com>
2024-03-14 16:37:42 +1100

    Improve engine benchmark comparison script: fix issue with package name containing "streaming", clearly label results

--- pkg/querier/engine/streaming/compare.sh
+++ pkg/querier/engine/streaming/compare.sh
@@ -4,7 +4,10 @@ set -euo pipefail
 
 RESULTS_FILE="$1"
 
-STANDARD_RESULTS=$(grep --invert-match "streaming" "$RESULTS_FILE" | sed 's#/standard##g')
-STREAMING_RESULTS=$(grep --invert-match "standard" "$RESULTS_FILE" | sed 's#/streaming##g')
+STANDARD_RESULTS_FILE=$(mktemp /tmp/standard.XXXX)
+STREAMING_RESULTS_FILE=$(mktemp /tmp/streaming.XXXX)
 
-benchstat <(echo "$STANDARD_RESULTS") <(echo "$STREAMING_RESULTS")
+grep --invert-match "streaming-" "$RESULTS_FILE" | sed -E 's#/standard-[0-9]+##g' > "$STANDARD_RESULTS_FILE"
+grep --invert-match "standard-" "$RESULTS_FILE" | sed -E 's#/streaming-[0-9]+##g' > "$STREAMING_RESULTS_FILE"
+
+benchstat "$STANDARD_RESULTS_FILE" "$STREAMING_RESULTS_FILE"
