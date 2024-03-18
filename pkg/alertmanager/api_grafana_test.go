d3826b4f8b5b826af0b8dd217998ae6b52bb7e23
Santiago <santiagohernandez.1997@gmail.com>
2024-03-18 11:44:10 +0100

    remove IDs from api_grafana_test.go

--- pkg/alertmanager/api_grafana_test.go
+++ pkg/alertmanager/api_grafana_test.go
@@ -187,8 +187,7 @@ func TestMultitenantAlertmanager_GetUserGrafanaConfig(t *testing.T) {
 				 "configuration": "a grafana config",
 				 "configuration_hash": "bb788eaa294c05ec556c1ed87546b7a9",
 				 "created": %d,
-				 "default": false,
-				 "id": 1
+				 "default": false
 			},
 			"status": "success"
 		}
@@ -276,7 +275,6 @@ func TestMultitenantAlertmanager_SetUserGrafanaConfig(t *testing.T) {
 		rec := httptest.NewRecorder()
 		json := `
 		{
-			"id": 124,
 			"configuration_hash": "",
 			"created": 12312414343,
 			"default": false
@@ -300,7 +298,6 @@ func TestMultitenantAlertmanager_SetUserGrafanaConfig(t *testing.T) {
 		rec = httptest.NewRecorder()
 		json = `
 		{
-			"id": 124,
 			"configuration": "a grafana configuration",
 			"configuration_hash": "ChEKBW5mbG9nEghzb21lZGF0YQ==",
 			"created": 12312414343,
