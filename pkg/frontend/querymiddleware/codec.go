a5de2e83866585b632afe7a33df5039065e65d68
francoposa <franco@francoposa.io>
2024-04-08 12:33:30 -0700

    integration test fix attempt: convert to decorateWithParamName

--- pkg/frontend/querymiddleware/codec.go
+++ pkg/frontend/querymiddleware/codec.go
@@ -288,7 +288,7 @@ func (c prometheusCodec) decodeRangeQueryRequest(r *http.Request) (MetricsQueryR
 	query := reqValues.Get("query")
 	queryExpr, err := parser.ParseExpr(query)
 	if err != nil {
-		return nil, apierror.New(apierror.TypeBadData, err.Error())
+		return nil, decorateWithParamName(err, "query")
 	}
 	result.Query = query
 	result.QueryExpr = queryExpr
@@ -316,7 +316,7 @@ func (c prometheusCodec) decodeInstantQueryRequest(r *http.Request) (MetricsQuer
 	query := reqValues.Get("query")
 	queryExpr, err := parser.ParseExpr(query)
 	if err != nil {
-		return nil, apierror.New(apierror.TypeBadData, err.Error())
+		return nil, decorateWithParamName(err, "query")
 	}
 	result.Query = query
 	result.QueryExpr = queryExpr
