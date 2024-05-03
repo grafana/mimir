de7a8e27d9af19d1a93fc724954fdbaff65e731b
Santiago <santiagohernandez.1997@gmail.com>
2024-05-03 11:15:06 +0200

    pass bucket config from mimir package

--- pkg/alertmanager/alertstore/store.go
+++ pkg/alertmanager/alertstore/store.go
@@ -73,7 +73,7 @@ type AlertStore interface {
 }
 
 // NewAlertStore returns a alertmanager store backend client based on the provided cfg.
-func NewAlertStore(ctx context.Context, cfg Config, cfgProvider bucket.TenantConfigProvider, fetchGrafanaCfg bool, logger log.Logger, reg prometheus.Registerer) (AlertStore, error) {
+func NewAlertStore(ctx context.Context, cfg Config, cfgProvider bucket.TenantConfigProvider, bucketConfig bucketclient.BucketAlertStoreConfig, logger log.Logger, reg prometheus.Registerer) (AlertStore, error) {
 	if cfg.Backend == local.Name {
 		level.Warn(logger).Log("msg", "-alertmanager-storage.backend=local is not suitable for persisting alertmanager state between replicas (silences, notifications); you should switch to an external object store for production use")
 		return local.NewStore(cfg.Local)
@@ -88,8 +88,5 @@ func NewAlertStore(ctx context.Context, cfg Config, cfgProvider bucket.TenantCon
 		return nil, err
 	}
 
-	bCfg := bucketclient.BucketAlertStoreConfig{
-		FetchGrafanaConfig: fetchGrafanaCfg,
-	}
-	return bucketclient.NewBucketAlertStore(bCfg, bucketClient, cfgProvider, logger), nil
+	return bucketclient.NewBucketAlertStore(bucketConfig, bucketClient, cfgProvider, logger), nil
 }
