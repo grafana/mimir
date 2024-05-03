2b92a2db17f92c66f1716c309edef9cb7dab83cf
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2024-05-03 18:49:29 +0200

    Add comment to TestOwnedSeriesIngesterRingStrategyRingChanged (#8050)
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/ingester/owned_series_test.go
+++ pkg/ingester/owned_series_test.go
@@ -1447,6 +1447,8 @@ func TestOwnedSeriesIngesterRingStrategyRingChanged(t *testing.T) {
 
 	t.Run("change of state is not interesting", func(t *testing.T) {
 		updateRingAndWaitForWatcherToReadUpdate(t, wkv, func(desc *ring.Desc) {
+			// Change only the state of the ingester without registering it again in the ring.
+			// We only want to change the state and not any other field.
 			ingester2 := desc.Ingesters[instanceID2]
 			ingester2.State = ring.LEAVING
 			desc.Ingesters[instanceID2] = ingester2
