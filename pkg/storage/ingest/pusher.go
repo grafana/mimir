26d88be3fb8474cde8e76755fb837ed63b9a9035
Vladimir Varankin <vladimir.varankin@grafana.com>
2024-05-06 13:26:49 +0200

    storage/ingest: trace producer to consumer
    Signed-off-by: Vladimir Varankin <vladimir.varankin@grafana.com>

--- pkg/storage/ingest/pusher.go
+++ pkg/storage/ingest/pusher.go
@@ -11,11 +11,11 @@ import (
 	"github.com/go-kit/log/level"
 	"github.com/grafana/dskit/cancellation"
 	"github.com/grafana/dskit/user"
-	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 
 	"github.com/grafana/mimir/pkg/mimirpb"
+	"github.com/grafana/mimir/pkg/util/spanlogger"
 )
 
 type Pusher interface {
@@ -34,6 +34,7 @@ type pusherConsumer struct {
 
 type parsedRecord struct {
 	*mimirpb.WriteRequest
+	ctx      context.Context
 	tenantID string
 	err      error
 }
@@ -65,20 +66,21 @@ func newPusherConsumer(p Pusher, reg prometheus.Registerer, l log.Logger) *pushe
 }
 
 func (c pusherConsumer) consume(ctx context.Context, records []record) error {
-	recC := make(chan parsedRecord)
 	ctx, cancel := context.WithCancelCause(ctx)
 	defer cancel(cancellation.NewErrorf("done consuming records"))
 
+	recC := make(chan parsedRecord)
+
 	// Speed up consumption by unmarhsalling the next request while the previous one is being pushed.
 	go c.unmarshalRequests(ctx, records, recC)
-	err := c.pushRequests(ctx, recC)
+	err := c.pushRequests(recC)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (c pusherConsumer) pushRequests(ctx context.Context, reqC <-chan parsedRecord) error {
+func (c pusherConsumer) pushRequests(reqC <-chan parsedRecord) error {
 	recordIdx := -1
 	for wr := range reqC {
 		recordIdx++
@@ -86,30 +88,45 @@ func (c pusherConsumer) pushRequests(ctx context.Context, reqC <-chan parsedReco
 			level.Error(c.l).Log("msg", "failed to parse write request; skipping", "err", wr.err)
 			continue
 		}
-		processingStart := time.Now()
 
-		ctx := user.InjectOrgID(ctx, wr.tenantID)
-		err := c.p.PushToStorage(ctx, wr.WriteRequest)
-
-		c.processingTimeSeconds.Observe(time.Since(processingStart).Seconds())
-		c.totalRequests.Inc()
-
+		ctx := user.InjectOrgID(wr.ctx, wr.tenantID)
+		err := c.pushToStorage(ctx, wr.WriteRequest)
 		if err != nil {
-			if !mimirpb.IsClientError(err) {
-				c.serverErrRequests.Inc()
-				return fmt.Errorf("consuming record at index %d for tenant %s: %w", recordIdx, wr.tenantID, err)
-			}
-			c.clientErrRequests.Inc()
+			return fmt.Errorf("consuming record at index %d for tenant %s: %w", recordIdx, wr.tenantID, err)
+		}
+	}
+	return nil
+}
 
-			// The error could be sampled or marked to be skipped in logs, so we check whether it should be
-			// logged before doing it.
-			if keep, reason := shouldLog(ctx, err); keep {
-				if reason != "" {
-					err = fmt.Errorf("%w (%s)", err, reason)
-				}
+func (c pusherConsumer) pushToStorage(ctx context.Context, req *mimirpb.WriteRequest) error {
+	spanLog, spanCtx := spanlogger.NewWithLogger(ctx, c.l, "pusherConsumer.pushToStorage")
+	defer spanLog.Finish()
 
-				level.Warn(c.l).Log("msg", "detected a client error while ingesting write request (the request may have been partially ingested)", "err", err, "user", wr.tenantID)
+	processingStart := time.Now()
+
+	err := c.p.PushToStorage(spanCtx, req)
+
+	c.processingTimeSeconds.Observe(time.Since(processingStart).Seconds())
+	c.totalRequests.Inc()
+
+	if err != nil {
+		// Only return non-client errors; these will stop the processing of the current Kafka fetches and retry (possibly).
+		if !mimirpb.IsClientError(err) {
+			c.serverErrRequests.Inc()
+			return spanLog.Error(err)
+		}
+
+		c.clientErrRequests.Inc()
+
+		// The error could be sampled or marked to be skipped in logs, so we check whether it should be
+		// logged before doing it.
+		if keep, reason := shouldLog(ctx, err); keep {
+			if reason != "" {
+				err = fmt.Errorf("%w (%s)", err, reason)
 			}
+
+			tenantID, _ := user.ExtractOrgID(spanCtx)
+			level.Warn(spanLog).Log("msg", "detected a client error while ingesting write request (the request may have been partially ingested)", "err", err, "user", tenantID)
 		}
 	}
 	return nil
@@ -119,16 +136,23 @@ func (c pusherConsumer) unmarshalRequests(ctx context.Context, records []record,
 	defer close(recC)
 	done := ctx.Done()
 
-	for _, record := range records {
+	for _, rec := range records {
+		// rec.ctx contains the tracing baggage for this record, which we propagate down the call tree.
+		// But this context's cancellation is disjointed from the top-most ctx. The context.AfterFunc below,
+		// is what fuses the two lifetimes together.
+		recCtx, cancel := context.WithCancelCause(rec.ctx)
+		context.AfterFunc(ctx, func() {
+			cancel(context.Cause(ctx))
+		})
 		pRecord := parsedRecord{
-			tenantID:     record.tenantID,
+			ctx:          recCtx,
+			tenantID:     rec.tenantID,
 			WriteRequest: &mimirpb.WriteRequest{},
 		}
 		// We don't free the WriteRequest slices because they are being freed by the Pusher.
-		err := pRecord.WriteRequest.Unmarshal(record.content)
+		err := pRecord.WriteRequest.Unmarshal(rec.content)
 		if err != nil {
-			err = errors.Wrap(err, "parsing ingest consumer write request")
-			pRecord.err = err
+			pRecord.err = fmt.Errorf("parsing ingest consumer write request: %w", err)
 		}
 		select {
 		case <-done:
