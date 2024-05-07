0b2a0b93c31d9e52fbca2f2c517b7b580574b864
David Grant <seizethedave@gmail.com>
2024-05-07 12:23:41 -0700

    Don't give UpdateIndex a timeout.

--- pkg/compactor/blocks_cleaner.go
+++ pkg/compactor/blocks_cleaner.go
@@ -495,13 +495,7 @@ func (c *BlocksCleaner) cleanUser(ctx context.Context, userID string, userLogger
 
 	// Generate an updated in-memory version of the bucket index.
 	w := bucketindex.NewUpdater(c.bucketClient, userID, c.cfgProvider, userLogger)
-
-	var partials map[ulid.ULID]error
-	err = withRetries(ctx, 5*time.Minute, c.retryConfig, log.With(userLogger, "op", "updateIndex"), func(ctx context.Context) error {
-		var err error
-		idx, partials, err = w.UpdateIndex(ctx, idx)
-		return err
-	})
+	idx, partials, err := w.UpdateIndex(ctx, idx)
 	if err != nil {
 		return fmt.Errorf("update index: %w", err)
 	}
