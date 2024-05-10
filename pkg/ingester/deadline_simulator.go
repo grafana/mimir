c5323adef0bd4843f4267c2055915daf5981031a
Yuri Nikolic <durica.nikolic@grafana.com>
2024-05-10 11:57:39 +0200

    ingester/deadline-exceeded: allow percentage of failures
    Signed-off-by: Yuri Nikolic <durica.nikolic@grafana.com>

--- pkg/ingester/deadline_simulator.go
+++ pkg/ingester/deadline_simulator.go
@@ -3,6 +3,7 @@ package ingester
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -18,29 +19,76 @@ var (
 	deadlineEnd           = make(chan chan int64)
 	count                 = atomic.NewInt64(0)
 	deadlineExceedEnabled = atomic.NewBool(false)
+	frequency             = atomic.NewInt64(1)
+	pushCounter           = atomic.NewInt64(0)
 )
 
-func DeadlineExceededHandler(w http.ResponseWriter, req *http.Request) {
-	load := make(chan int64)
-	moreLoad <- load
-	currentLoad := <-load
-	w.Write([]byte(fmt.Sprintf("Load created. Total running: %d", currentLoad)))
-
+func getDuration(req *http.Request) (time.Duration, string) {
 	vars := mux.Vars(req)
 	durationArg := vars["duration"]
 	var (
 		duration = 10 * time.Second
-		response = make([]string, 0, 5)
+		response string
 		err      error
 	)
 	if durationArg == "" {
-		response = append(response, "DeadlineExceededHandler: duration has not been specified, so the default duration of 10s will be used")
+		response = fmt.Sprintf("DeadlineExceededHandler: duration has not been specified, so the default duration of %s will be used", duration.String())
 	} else {
 		duration, err = time.ParseDuration(durationArg)
-		if err != nil {
-			response = append(response, fmt.Sprintf("DeadlineExceededHandler: invalid duration '%s' has been specified, so the default duration of 10s will be used", durationArg))
+		if err == nil {
+			response = fmt.Sprintf("DeadlineExceededHandler: duration of %s will be used", duration.String())
+		} else {
+			response = fmt.Sprintf("DeadlineExceededHandler: invalid duration '%s' has been specified, so the default duration of 10s will be used", durationArg)
 		}
 	}
+	return duration, response
+}
+
+func getFailureFrequency(req *http.Request) (int, string) {
+	vars := mux.Vars(req)
+	failurePercentageArg := vars["failurePercentage"]
+	var (
+		failureFrequency = 1
+		response         string
+	)
+	if failurePercentageArg == "" {
+		response = fmt.Sprintf("DeadlineExceededHandler: failure percentage has not been specified, so the default failure frequency of %d will be used", failureFrequency)
+	} else {
+		failurePercentage, err := strconv.Atoi(failurePercentageArg)
+		if err == nil {
+			failureFrequency = 100 / failurePercentage
+			response = fmt.Sprintf("DeadlineExceededHandler: failure frequency of %d corresponding to failure percentage of %d will be used", failureFrequency, failurePercentage)
+		} else {
+			response = fmt.Sprintf("DeadlineExceededHandler: invalid failure percentage '%s' has been specified, so the default failure frequency %d will be used", failurePercentageArg, failureFrequency)
+		}
+	}
+	return failureFrequency, response
+}
+
+func shouldDelayPushRequest() bool {
+	if !deadlineExceedEnabled.Load() {
+		return false
+	}
+	pushCounter.Inc()
+	pushCount := pushCounter.Load()
+	freq := frequency.Load()
+	return pushCount%freq == 0
+}
+
+func DeadlineExceededHandler(w http.ResponseWriter, req *http.Request) {
+	if deadlineExceedEnabled.Load() {
+		util.WriteTextResponse(w, "DeadlineExceededHandler is already active. This request will fail.")
+		return
+	}
+
+	response := make([]string, 0, 4)
+
+	duration, status := getDuration(req)
+	response = append(response, status)
+
+	failureFrequency, status := getFailureFrequency(req)
+	response = append(response, status)
+	frequency.Store(int64(failureFrequency))
 
 	start := make(chan int64)
 	deadlineStart <- start
@@ -62,6 +110,7 @@ func init() {
 		for started := range deadlineStart {
 			go func(started chan int64) {
 				count.Inc()
+				pushCounter.Store(0)
 				started <- count.Load()
 				deadlineExceedEnabled.Store(true)
 				for {
