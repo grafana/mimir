1f113ebfeae922c9ba128db611bce3b842d71083
George Krajcsovits <krajorama@users.noreply.github.com>
2024-05-15 21:47:21 +0200

    Remove label selector string quoting from mimir-prometheus (#8148)
    Reverting https://github.com/prometheus/prometheus/pull/14068
    because it is causing unforseen complications with LBAC in GEM.
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- vendor/github.com/prometheus/prometheus/model/labels/matcher.go
+++ vendor/github.com/prometheus/prometheus/model/labels/matcher.go
@@ -14,8 +14,7 @@
 package labels
 
 import (
-	"bytes"
-	"strconv"
+	"fmt"
 )
 
 // MatchType is an enum for label matching types.
@@ -79,29 +78,7 @@ func MustNewMatcher(mt MatchType, name, val string) *Matcher {
 }
 
 func (m *Matcher) String() string {
-	// Start a buffer with a pre-allocated size on stack to cover most needs.
-	var bytea [1024]byte
-	b := bytes.NewBuffer(bytea[:0])
-
-	if m.shouldQuoteName() {
-		b.Write(strconv.AppendQuote(b.AvailableBuffer(), m.Name))
-	} else {
-		b.WriteString(m.Name)
-	}
-	b.WriteString(m.Type.String())
-	b.Write(strconv.AppendQuote(b.AvailableBuffer(), m.Value))
-
-	return b.String()
-}
-
-func (m *Matcher) shouldQuoteName() bool {
-	for i, c := range m.Name {
-		if c == '_' || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (i > 0 && c >= '0' && c <= '9') {
-			continue
-		}
-		return true
-	}
-	return false
+	return fmt.Sprintf("%s%s%q", m.Name, m.Type, m.Value)
 }
 
 // Matches returns whether the matcher matches the given string value.
