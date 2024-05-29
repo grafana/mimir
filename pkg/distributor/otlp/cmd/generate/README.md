c4b0f9fcd4d7aa8de6c102582b9f769a415dbf21
Arve Knudsen <arve.knudsen@gmail.com>
2024-05-29 07:16:27 +0200

    Use vendored Prometheus sources
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/distributor/otlp/cmd/generate/README.md
+++ pkg/distributor/otlp/cmd/generate/README.md
@@ -1,8 +1,4 @@
 # OTLP translation code generator
 
 This command line utility ("generate") generates code for translating from an OTLP write request to a Mimir remote write request.
-It does so by downloading corresponding mimir-prometheus Go files, and transforming them through the
-[gopatch](https://github.com/uber-go/gopatch) tool.
-
-The utility maintains the source mimir-prometheus Git reference hash, from which the current sources were generated, in the file .source-ref in this directory.
-If the mimir-prometheus version currently depended on hasn't changed from that, no source code is re-generated.
+It does so by transforming corresponding vendored Prometheus Go files through the [gopatch](https://github.com/uber-go/gopatch) tool.
--- pkg/distributor/otlp/cmd/generate/main.go
+++ pkg/distributor/otlp/cmd/generate/main.go
@@ -4,54 +4,30 @@ package main
 
 import (
 	"bufio"
-	"context"
 	"errors"
 	"fmt"
 	"os"
 	"os/exec"
-	"path"
 	"path/filepath"
 	"runtime"
 	"strings"
 
 	"github.com/alecthomas/kingpin/v2"
-	"github.com/grafana/regexp"
 
-	"github.com/google/go-github/v57/github"
-)
-
-const (
-	dpath = "storage/remote/otlptranslator/prometheusremotewrite"
-	owner = "grafana"
-	repo  = "mimir-prometheus"
+	// Dummy dependency to ensure these files get vendored and we can use them as transformation sources.
+	_ "github.com/prometheus/prometheus/storage/remote/otlptranslator/prometheusremotewrite"
 )
 
 func main() {
 	app := kingpin.New("generate", "Generate OTLP translation code.")
-	checkFlag := app.Flag("check", "Verify that generated OTLP translation code corresponds to mimir-prometheus version.").Bool()
 
 	generate := func(*kingpin.ParseContext) error {
-		sourceRefPath := filepath.Join("cmd", "generate", ".source-ref")
-
-		if *checkFlag {
-			return check(sourceRefPath)
-		}
-
-		sources, sourceRef, err := downloadSources(sourceRefPath)
+		sources, err := copySources()
 		if err != nil {
 			return err
 		}
-		if sourceRef == "" {
-			// The source hasn't changed since last time
-			return nil
-		}
-
 		if err := patchSources(sources); err != nil {
 			return err
-		}
-
-		if err := os.WriteFile(sourceRefPath, []byte(sourceRef+"\n"), 0644); err != nil {
-			return fmt.Errorf("couldn't write to %s: %w", sourceRefPath, err)
 		}
 
 		return nil
@@ -84,138 +60,59 @@ func patchSources(sources []string) error {
 	return nil
 }
 
-// getSourceRef gets the mimir-prometheus source GitHub reference hash,
-// and whether it's different from the source ref the code was generated from.
-func getSourceRef(sourceRefPath string) (string, bool, error) {
-	var curSourceRef string
-	data, err := os.ReadFile(sourceRefPath)
+func copySources() ([]string, error) {
+	dpath := filepath.Join("..", "..", "..", "vendor", "github.com", "prometheus", "prometheus", "storage", "remote", "otlptranslator", "prometheusremotewrite")
+	entries, err := os.ReadDir(dpath)
 	if err != nil {
-		if !errors.Is(err, os.ErrNotExist) {
-			return "", false, fmt.Errorf("couldn't read file %s: %w", sourceRefPath, err)
-		}
-	} else {
-		curSourceRef = strings.TrimSpace(string(data))
-	}
-
-	f, err := os.Open(filepath.Join("..", "..", "..", "go.mod"))
-	if err != nil {
-		return "", false, err
-	}
-	defer f.Close()
-
-	reReplace := regexp.MustCompile(`^replace github.com/prometheus/prometheus => github.com/grafana/mimir-prometheus .+-([^-]+)$`)
-	var ref string
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		l := scanner.Text()
-		ms := reReplace.FindStringSubmatch(l)
-		if ms == nil {
-			continue
-		}
-
-		ref = ms[1]
-	}
-	if err := scanner.Err(); err != nil {
-		return "", false, err
-	}
-
-	if ref == "" {
-		return "", false, fmt.Errorf("couldn't find mimir-prometheus replace directive in go.mod")
-	}
-
-	return ref, ref != curSourceRef, nil
-}
-
-func downloadSources(sourceRefPath string) ([]string, string, error) {
-	ref, changed, err := getSourceRef(sourceRefPath)
-	if err != nil {
-		return nil, "", err
-	}
-	if !changed {
-		return nil, "", nil
-	}
-
-	ctx := context.Background()
-
-	token := os.Getenv("GITHUB_TOKEN")
-	if token == "" {
-		return nil, "", fmt.Errorf("$GITHUB_TOKEN must be set")
-	}
-	client := github.NewClient(nil).WithAuthToken(token)
-	_, contents, _, err := client.Repositories.GetContents(
-		ctx, owner, repo, dpath, &github.RepositoryContentGetOptions{
-			Ref: ref,
-		},
-	)
-	if err != nil {
-		return nil, "", fmt.Errorf("getting repo dir contents: %w", err)
+		return nil, fmt.Errorf("list files in %q: %w", dpath, err)
 	}
 
 	var sources []string
-	for _, c := range contents {
-		if *c.Type != "file" || !strings.HasSuffix(*c.Name, ".go") || *c.Name == "timeseries.go" || strings.HasSuffix(*c.Name, "_test.go") {
+	for _, e := range entries {
+		if e.IsDir() || !strings.HasSuffix(e.Name(), ".go") || e.Name() == "timeseries.go" || strings.HasSuffix(e.Name(), "_test.go") {
 			continue
 		}
 
-		fname, err := downloadFile(ctx, client, ref, *c.Name)
-		if err != nil {
-			return nil, "", err
+		dst := fmt.Sprintf("%s_generated.go", e.Name()[:len(e.Name())-3])
+		if err := copyFile(filepath.Join(dpath, e.Name()), dst); err != nil {
+			return nil, err
 		}
-		sources = append(sources, fname)
-	}
 
-	return sources, ref, nil
+		sources = append(sources, dst)
+	}
+	return sources, nil
 }
 
-func downloadFile(ctx context.Context, client *github.Client, ref, fname string) (string, error) {
-	ghFpath := path.Join(dpath, fname)
-	r, _, err := client.Repositories.DownloadContents(
-		ctx, owner, repo, ghFpath, &github.RepositoryContentGetOptions{
-			Ref: ref,
-		},
-	)
+func copyFile(srcPath, dstPath string) (err error) {
+	src, err := os.Open(srcPath)
 	if err != nil {
-		return "", fmt.Errorf("download %s: %w", ghFpath, err)
-	}
-	defer r.Close()
-
-	reFilename := regexp.MustCompile(`^(.+)\.go$`)
-	ms := reFilename.FindStringSubmatch(fname)
-	outputFname := fmt.Sprintf("%s_generated.go", ms[1])
-
-	f, err := os.OpenFile(outputFname, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	if err != nil {
-		return "", fmt.Errorf("open file %s: %w", outputFname, err)
+		return fmt.Errorf("open %q: %w", srcPath, err)
 	}
 	defer func() {
-		if closeErr := f.Close(); closeErr != nil && err == nil {
-			err = fmt.Errorf("close %s: %w", outputFname, closeErr)
+		if cErr := src.Close(); cErr != nil {
+			err = errors.Join(fmt.Errorf("close %q: %w", srcPath, cErr), err)
 		}
 	}()
 
-	wr := bufio.NewWriter(f)
-	if _, err := wr.WriteString("// Code generated from Prometheus sources - DO NOT EDIT.\n\n"); err != nil {
-		return "", fmt.Errorf("write to %s: %w", outputFname, err)
+	dst, err := os.OpenFile(dstPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return fmt.Errorf("create %q: %w", dstPath, err)
 	}
-	if _, err := wr.ReadFrom(r); err != nil {
-		return "", fmt.Errorf("read from GitHub repo file %s: %w", ghFpath, err)
+	defer func() {
+		if cErr := dst.Close(); cErr != nil {
+			err = errors.Join(fmt.Errorf("close %q: %w", dstPath, cErr), err)
+		}
+	}()
+
+	wr := bufio.NewWriter(dst)
+	if _, err := wr.WriteString("// Code generated from Prometheus sources - DO NOT EDIT.\n\n"); err != nil {
+		return fmt.Errorf("write to %q: %w", dstPath, err)
+	}
+	if _, err := wr.ReadFrom(src); err != nil {
+		return fmt.Errorf("read from file %q: %w", srcPath, err)
 	}
 	if err := wr.Flush(); err != nil {
-		return "", fmt.Errorf("write to file %s: %w", outputFname, err)
-	}
-
-	return outputFname, nil
-}
-
-// check verifies that the generated OTLP translation sources correspond to the current mimir-promethus version depended on.
-func check(sourceRefPath string) error {
-	_, changed, err := getSourceRef(sourceRefPath)
-	if err != nil {
-		return err
-	}
-	if changed {
-		return fmt.Errorf("the OTLP translation code was generated from an older version of mimir-prometheus - " +
-			"please regenerate and commit: make generate-otlp")
+		return fmt.Errorf("write to file %q: %w", dstPath, err)
 	}
 
 	return nil
