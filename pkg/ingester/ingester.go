69671afb8fb772273f8dde62ea894c7981172c16
Bryan Boreham <bjboreham@gmail.com>
2024-05-31 20:00:04 +0100

    Ingester: bridge tracing spans to Otel
    So we see spans and events inside Prometheus.
    
    The bridge added in #2704 works when Prometheus creates a new Otel span,
    but when Prometheus calls `SpanFromContext` we need to have already set
    up a link from the current OpenTracing span to Otel.

--- pkg/ingester/ingester.go
+++ pkg/ingester/ingester.go
@@ -68,6 +68,7 @@ import (
 	util_math "github.com/grafana/mimir/pkg/util/math"
 	"github.com/grafana/mimir/pkg/util/shutdownmarker"
 	"github.com/grafana/mimir/pkg/util/spanlogger"
+	"github.com/grafana/mimir/pkg/util/tracing"
 	"github.com/grafana/mimir/pkg/util/validation"
 )
 
@@ -1550,6 +1551,7 @@ func (i *Ingester) QueryExemplars(ctx context.Context, req *client.ExemplarQuery
 
 	spanlog, ctx := spanlogger.NewWithLogger(ctx, i.logger, "Ingester.QueryExemplars")
 	defer spanlog.Finish()
+	ctx = tracing.BridgeOpenTracingToOtel(ctx)
 
 	userID, err := tenant.TenantID(ctx)
 	if err != nil {
@@ -1996,6 +1998,7 @@ func (i *Ingester) QueryStream(req *client.QueryRequest, stream client.Ingester_
 
 	spanlog, ctx := spanlogger.NewWithLogger(stream.Context(), i.logger, "Ingester.QueryStream")
 	defer spanlog.Finish()
+	ctx = tracing.BridgeOpenTracingToOtel(ctx)
 
 	userID, err := tenant.TenantID(ctx)
 	if err != nil {
