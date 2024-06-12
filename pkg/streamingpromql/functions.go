b9e766bab01587b31d0bb08b2a71d894b874f000
Charles Korn <charleskorn@users.noreply.github.com>
2024-06-12 16:00:34 +1000

    Mimir query engine: complete renaming from #8256 (#8348)
    * Complete renaming from https://github.com/grafana/mimir/pull/8256/commits/75506d68c00799569b524ebc7313448942c81327
    
    * Add changelog entry

--- pkg/streamingpromql/functions.go
+++ pkg/streamingpromql/functions.go
@@ -13,14 +13,14 @@ import (
 
 type InstantVectorFunctionOperatorFactory func(args []types.Operator, pool *pooling.LimitingPool) (types.InstantVectorOperator, error)
 
-// SingleInputVectorFunctionOperator creates an InstantVectorFunctionOperatorFactory for functions
+// SingleInputVectorFunctionOperatorFactory creates an InstantVectorFunctionOperatorFactory for functions
 // that have exactly 1 argument (v instant-vector).
 //
 // Parameters:
 //   - name: The name of the function.
 //   - metadataFunc: The function for handling metadata
 //   - seriesDataFunc: The function to handle series data
-func SingleInputVectorFunctionOperator(name string, metadataFunc functions.SeriesMetadataFunction, seriesDataFunc functions.InstantVectorFunction) InstantVectorFunctionOperatorFactory {
+func SingleInputVectorFunctionOperatorFactory(name string, metadataFunc functions.SeriesMetadataFunction, seriesDataFunc functions.InstantVectorFunction) InstantVectorFunctionOperatorFactory {
 	return func(args []types.Operator, pool *pooling.LimitingPool) (types.InstantVectorOperator, error) {
 		if len(args) != 1 {
 			// Should be caught by the PromQL parser, but we check here for safety.
@@ -43,17 +43,17 @@ func SingleInputVectorFunctionOperator(name string, metadataFunc functions.Serie
 	}
 }
 
-// TransformationFunctionOperator creates an InstantVectorFunctionOperatorFactory for functions
+// TransformationFunctionOperatorFactory creates an InstantVectorFunctionOperatorFactory for functions
 // that have exactly 1 argument (v instant-vector), and drop the series __name__ label.
 //
 // Parameters:
 //   - name: The name of the function.
 //   - seriesDataFunc: The function to handle series data
-func TransformationFunctionOperator(name string, seriesDataFunc functions.InstantVectorFunction) InstantVectorFunctionOperatorFactory {
-	return SingleInputVectorFunctionOperator(name, functions.DropSeriesName, seriesDataFunc)
+func TransformationFunctionOperatorFactory(name string, seriesDataFunc functions.InstantVectorFunction) InstantVectorFunctionOperatorFactory {
+	return SingleInputVectorFunctionOperatorFactory(name, functions.DropSeriesName, seriesDataFunc)
 }
 
-// LabelManipulationFunctionOperator creates an InstantVectorFunctionOperator for functions
+// LabelManipulationFunctionOperatorFactory creates an InstantVectorFunctionOperator for functions
 // that have exactly 1 argument (v instant-vector), and need to manipulate the labels of
 // each series without manipulating the returned samples.
 // The values of v are passed through.
@@ -65,11 +65,11 @@ func TransformationFunctionOperator(name string, seriesDataFunc functions.Instan
 // Returns:
 //
 //	An InstantVectorFunctionOperator.
-func LabelManipulationFunctionOperator(name string, metadataFunc functions.SeriesMetadataFunction) InstantVectorFunctionOperatorFactory {
-	return SingleInputVectorFunctionOperator(name, metadataFunc, functions.Passthrough)
+func LabelManipulationFunctionOperatorFactory(name string, metadataFunc functions.SeriesMetadataFunction) InstantVectorFunctionOperatorFactory {
+	return SingleInputVectorFunctionOperatorFactory(name, metadataFunc, functions.Passthrough)
 }
 
-func rateFunctionOperator(args []types.Operator, pool *pooling.LimitingPool) (types.InstantVectorOperator, error) {
+func createRateFunctionOperator(args []types.Operator, pool *pooling.LimitingPool) (types.InstantVectorOperator, error) {
 	if len(args) != 1 {
 		// Should be caught by the PromQL parser, but we check here for safety.
 		return nil, fmt.Errorf("expected exactly 1 argument for rate, got %v", len(args))
@@ -89,17 +89,17 @@ func rateFunctionOperator(args []types.Operator, pool *pooling.LimitingPool) (ty
 
 // These functions return an instant-vector.
 var instantVectorFunctionOperatorFactories = map[string]InstantVectorFunctionOperatorFactory{
-	"acos":            TransformationFunctionOperator("acos", functions.Acos),
-	"histogram_count": TransformationFunctionOperator("histogram_count", functions.HistogramCount),
-	"histogram_sum":   TransformationFunctionOperator("histogram_sum", functions.HistogramSum),
-	"rate":            rateFunctionOperator,
+	"acos":            TransformationFunctionOperatorFactory("acos", functions.Acos),
+	"histogram_count": TransformationFunctionOperatorFactory("histogram_count", functions.HistogramCount),
+	"histogram_sum":   TransformationFunctionOperatorFactory("histogram_sum", functions.HistogramSum),
+	"rate":            createRateFunctionOperator,
 }
 
-func RegisterInstantVectorFunctionOperator(functionName string, functionOperator InstantVectorFunctionOperatorFactory) error {
+func RegisterInstantVectorFunctionOperatorFactory(functionName string, factory InstantVectorFunctionOperatorFactory) error {
 	if _, exists := instantVectorFunctionOperatorFactories[functionName]; exists {
 		return fmt.Errorf("function '%s' has already been registered", functionName)
 	}
 
-	instantVectorFunctionOperatorFactories[functionName] = functionOperator
+	instantVectorFunctionOperatorFactories[functionName] = factory
 	return nil
 }
