7b7f169b197549b1d2b66e2433a563d1957e3d14
Joshua Hesketh <joshua.hesketh@grafana.com>
2024-06-12 13:42:00 +1000

    Make sure tests tidy themselves up

--- pkg/streamingpromql/functions_test.go
+++ pkg/streamingpromql/functions_test.go
@@ -26,4 +26,7 @@ func TestRegisterInstantVectorFunctionOperator(t *testing.T) {
 	err = RegisterInstantVectorFunctionOperator("new_function", newFunc)
 	require.Error(t, err)
 	require.Equal(t, "function 'new_function' has already been registered", err.Error())
+
+	// Cleanup changes to instantVectorFunctionOperatorFactories
+	delete(instantVectorFunctionOperatorFactories, "new_function")
 }
