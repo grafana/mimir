b0ab5ed8661d247532f84a53aabadb5d589e9d74
Marco Pracucci <marco@pracucci.com>
2024-06-26 12:39:07 +0200

    Add tool to estimate rule evaluations duration after strong ready consistency
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- vendor/github.com/prometheus/prometheus/rules/manager.go
+++ vendor/github.com/prometheus/prometheus/rules/manager.go
@@ -467,6 +467,10 @@ type ruleDependencyController struct{}
 
 // AnalyseRules implements RuleDependencyController.
 func (c ruleDependencyController) AnalyseRules(rules []Rule) {
+	AnalyseRulesDependencies(rules)
+}
+
+func AnalyseRulesDependencies(rules []Rule) {
 	depMap := buildDependencyMap(rules)
 	for _, r := range rules {
 		r.SetNoDependentRules(depMap.dependents(r) == 0)
