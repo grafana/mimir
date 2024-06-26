698fe7ee229755e4fe1f2d4b2b31f9207df35cc6
fayzal-g <fayzal.ghantiwala@grafana.com>
2024-06-26 14:20:09 +0100

    Reintroduce remove from rebase

--- pkg/alertmanager/multitenant.go
+++ pkg/alertmanager/multitenant.go
@@ -719,6 +719,13 @@ func (am *MultitenantAlertmanager) computeConfig(cfgs alertspb.AlertConfigDescs)
 // setConfig applies the given configuration to the alertmanager for `userID`,
 // creating an alertmanager if it doesn't already exist.
 func (am *MultitenantAlertmanager) setConfig(cfg alertspb.AlertConfigDesc) error {
+	// Instead of using "config" as the origin, as in Prometheus Alertmanager, we use "tenant".
+	// The reason for this that the config.Load function uses the origin "config",
+	// which is correct, but Mimir uses config.Load to validate both API requests and tenant
+	// configurations. This means metrics from API requests are confused with metrics from
+	// tenant configurations. To avoid this confusion, we use a different origin.
+	validateMatchersInConfigDesc(am.logger, "tenant", cfg)
+
 	level.Debug(am.logger).Log("msg", "setting config", "user", cfg.User)
 
 	am.alertmanagersMtx.Lock()
