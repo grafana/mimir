5f75a48e0461c9864a34213a8404c1dd863d4685
francoposa <franco@francoposa.io>
2024-07-08 18:25:06 -0700

    fix test

--- pkg/scheduler/queue/querier_worker_group_queue_algorithm_test.go
+++ pkg/scheduler/queue/querier_worker_group_queue_algorithm_test.go
@@ -9,69 +9,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestQuerierWorkerGroupDequeue_DefaultRoundRobin(t *testing.T) {
-	type opType string
-	enqueue := opType("enqueue")
-	dequeue := opType("dequeue")
-
-	type op struct {
-		kind opType
-		path QueuePath
-		obj  any
-	}
-
-	operationOrder := []op{
-		// enqueue 2 objects each to 3 different children;
-		// nodes are only added to a rotation on first enqueue,
-		// so only order of first enqueue sets the dequeue order
-		{enqueue, QueuePath{"child-1"}, "obj-1"}, // child-1 node created
-		{enqueue, QueuePath{"child-2"}, "obj-2"}, // child-2 node created
-		{enqueue, QueuePath{"child-3"}, "obj-3"}, // child-3 node created
-		// order of nodes is set, further enqueues in a different order will not change it
-		{enqueue, QueuePath{"child-3"}, "obj-4"},
-		{enqueue, QueuePath{"child-2"}, "obj-5"},
-		{enqueue, QueuePath{"child-1"}, "obj-6"},
-
-		// dequeue proceeds in order of first enqueue until a node is emptied and removed from rotation
-		{dequeue, QueuePath{"child-1"}, "obj-1"},
-		{dequeue, QueuePath{"child-2"}, "obj-2"},
-		{dequeue, QueuePath{"child-3"}, "obj-3"},
-		{dequeue, QueuePath{"child-1"}, "obj-6"},
-		// child-1 is now empty and removed from rotation
-		{dequeue, QueuePath{"child-2"}, "obj-5"},
-		// child-2 is now empty and removed from rotation
-
-		// enqueue for child-1 again to verify it is added back to rotation
-		{enqueue, QueuePath{"child-1"}, "obj-7"},
-
-		// child-3 is still next; child-1 was added back to rotation
-		{dequeue, QueuePath{"child-3"}, "obj-4"},
-		// child-3 is now empty and removed from rotation; only child-1 remains
-		{dequeue, QueuePath{"child-1"}, "obj-7"},
-		// nothing left to dequeue
-		{dequeue, QueuePath{}, nil},
-	}
-
-	querierWorkerPrioritizationQueueAlgo := &querierWorkerPrioritizationQueueAlgo{
-		currentQuerierWorker: 0,
-	}
-
-	tree, err := NewTree(querierWorkerPrioritizationQueueAlgo, &roundRobinState{})
-	require.NoError(t, err)
-
-	for _, operation := range operationOrder {
-		if operation.kind == enqueue {
-			err = tree.EnqueueBackByPath(operation.path, operation.obj)
-			require.NoError(t, err)
-		}
-		if operation.kind == dequeue {
-			path, obj := tree.Dequeue()
-			require.Equal(t, operation.path, path)
-			require.Equal(t, operation.obj, obj)
-		}
-	}
-}
-
 func TestQuerierWorkerGroupDequeue_StartPositionByQuerierWorker(t *testing.T) {
 	querierWorkerPrioritizationQueueAlgo := &querierWorkerPrioritizationQueueAlgo{}
 
@@ -150,4 +87,68 @@ func TestQuerierWorkerGroupDequeue_StartPositionByQuerierWorker(t *testing.T) {
 	path, obj = tree.Dequeue()
 	assert.Equal(t, QueuePath{}, path)
 	assert.Nil(t, obj)
+}
+
+func TestQuerierWorkerGroupDequeue_SingleWorkerID(t *testing.T) {
+	type opType string
+	enqueue := opType("enqueue")
+	dequeue := opType("dequeue")
+
+	type op struct {
+		kind opType
+		path QueuePath
+		obj  any
+	}
+
+	operationOrder := []op{
+		// enqueue 2 objects each to 3 different children;
+		// nodes are only added to a rotation on first enqueue,
+		// so only order of first enqueue sets the dequeue order
+		{enqueue, QueuePath{"child-1"}, "obj-1"}, // child-1 node created
+		{enqueue, QueuePath{"child-2"}, "obj-2"}, // child-2 node created
+		{enqueue, QueuePath{"child-3"}, "obj-3"}, // child-3 node created
+		// order of nodes is set, further enqueues in a different order will not change it
+		{enqueue, QueuePath{"child-3"}, "obj-4"},
+		{enqueue, QueuePath{"child-2"}, "obj-5"},
+		{enqueue, QueuePath{"child-1"}, "obj-6"},
+
+		// without changing worker ID, dequeue will clear full queue nodes,
+		// starting with the first node in the order then moving on to the next empty queue
+		{dequeue, QueuePath{"child-1"}, "obj-1"},
+		{dequeue, QueuePath{"child-1"}, "obj-6"},
+		// child-1 is now empty and removed from rotation
+		{dequeue, QueuePath{"child-2"}, "obj-2"},
+		{dequeue, QueuePath{"child-2"}, "obj-5"},
+		// child-2 is now empty and removed from rotation
+		{dequeue, QueuePath{"child-3"}, "obj-3"},
+
+		// enqueue for child-1 again to verify it is added back to rotation
+		{enqueue, QueuePath{"child-1"}, "obj-7"},
+
+		// child-3 is still next; child-1 was added back to rotation
+		{dequeue, QueuePath{"child-3"}, "obj-4"},
+		// child-3 is now empty and removed from rotation; only child-1 remains
+		{dequeue, QueuePath{"child-1"}, "obj-7"},
+		// nothing left to dequeue
+		{dequeue, QueuePath{}, nil},
+	}
+
+	querierWorkerPrioritizationQueueAlgo := &querierWorkerPrioritizationQueueAlgo{
+		currentQuerierWorker: 0,
+	}
+
+	tree, err := NewTree(querierWorkerPrioritizationQueueAlgo, &roundRobinState{})
+	require.NoError(t, err)
+
+	for _, operation := range operationOrder {
+		if operation.kind == enqueue {
+			err = tree.EnqueueBackByPath(operation.path, operation.obj)
+			require.NoError(t, err)
+		}
+		if operation.kind == dequeue {
+			path, obj := tree.Dequeue()
+			require.Equal(t, operation.path, path)
+			require.Equal(t, operation.obj, obj)
+		}
+	}
 }
