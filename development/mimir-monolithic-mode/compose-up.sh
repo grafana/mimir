7d3df969f9f7fafd4eeb15ce3e157f96c283e777
Owen Williams <owen.williams@grafana.com>
2024-07-23 15:48:15 -0400

    revert my changes

--- development/mimir-monolithic-mode/compose-up.sh
+++ development/mimir-monolithic-mode/compose-up.sh
@@ -17,19 +17,6 @@ docker_compose() {
 
 SCRIPT_DIR=$(cd "$(dirname -- "$0")" && pwd)
 
-# Optionally Build prometheus images
-if [ "$1"x == "fullx" ] ; then
-    shift
-    cd /home/owilliams/src/grafana/prometheus
-    go mod vendor
-    nice -n 10 make build
-    mkdir -p .build/linux-amd64/
-    cp promtool .build/linux-amd64/
-    cp prometheus .build/linux-amd64/
-    cd -
-    go mod vendor
-fi
-
 PROFILES=()
 ARGS=()
 while [[ $# -gt 0 ]]; do
@@ -53,24 +40,9 @@ if [ ${#PROFILES[@]} -eq 0 ]; then
     PROFILES=("${DEFAULT_PROFILES[@]}")
 fi
 
-cd /home/owilliams/src/grafana/prom-for-agent
-go mod vendor
-cd -
-
-cd /home/owilliams/src/grafana/agent
-go mod vendor
-cd -
-
-cd /home/owilliams/src/grafana/mimir
-go mod vendor
-cd -
-
 # -gcflags "all=-N -l" disables optimizations that allow for better run with combination with Delve debugger.
 # GOARCH is not changed.
 CGO_ENABLED=0 GOOS=linux go build -mod=vendor -tags=netgo,stringlabels -gcflags "all=-N -l" -o "${SCRIPT_DIR}"/mimir "${SCRIPT_DIR}"/../../cmd/mimir
 
-docker_compose -f "${SCRIPT_DIR}"/docker-compose.yml build prometheus && \
-docker_compose -f "${SCRIPT_DIR}"/docker-compose.yml build grafana-agent && \
 docker_compose -f "${SCRIPT_DIR}"/docker-compose.yml build mimir-1 && \
-docker_compose -f "${SCRIPT_DIR}"/docker-compose.yml up -d "$@"
-# docker_compose -f "${SCRIPT_DIR}"/docker-compose.yml "${PROFILES[@]}" up "${ARGS[@]}"
+docker_compose -f "${SCRIPT_DIR}"/docker-compose.yml "${PROFILES[@]}" up "${ARGS[@]}"
