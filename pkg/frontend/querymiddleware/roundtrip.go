eac8ee38208d397f621cb97976410d91f2e83a4c
Marco Pracucci <marco@pracucci.com>
2024-07-25 08:31:49 +0200

    Added TODO
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/frontend/querymiddleware/roundtrip.go
+++ pkg/frontend/querymiddleware/roundtrip.go
@@ -266,6 +266,7 @@ func newQueryTripperware(
 		}
 
 		// Enforce read consistency after caching.
+		// TODO unit test
 		if ingestStorageTopicOffsetsReader != nil {
 			queryrange = newReadConsistencyRoundTripper(queryrange, ingestStorageTopicOffsetsReader, limits, log)
 			instant = newReadConsistencyRoundTripper(instant, ingestStorageTopicOffsetsReader, limits, log)
