c5584984a95b2891341f42e99f08d7169a9c9092
Ganesh Vernekar <ganeshvern@gmail.com>
2024-08-13 18:21:02 -0400

    Fix mergeIterator.Seek()
    Signed-off-by: Ganesh Vernekar <ganeshvern@gmail.com>

--- pkg/querier/batch/merge.go
+++ pkg/querier/batch/merge.go
@@ -78,7 +78,7 @@ func (c *mergeIterator) Seek(t int64, size int) chunkenc.ValueType {
 found:
 	for c.batches.len() > 0 {
 		batch := c.batches.curr()
-		if t >= batch.Timestamps[0] && t <= batch.Timestamps[batch.Length-1] {
+		if t <= batch.Timestamps[batch.Length-1] {
 			batch.Index = 0
 			for batch.Index < batch.Length && t > batch.Timestamps[batch.Index] {
 				batch.Index++
