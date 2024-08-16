3b790da19458a4b8c33ad7a2c1265b436327dde1
Peter Štibraný <pstibrany@gmail.com>
2024-08-16 17:23:40 +0200

    Update dskit.
    Signed-off-by: Peter Štibraný <pstibrany@gmail.com>

--- vendor/github.com/grafana/dskit/ring/ring.go
+++ vendor/github.com/grafana/dskit/ring/ring.go
@@ -697,11 +697,11 @@ func (r *Ring) updateRingMetrics(compareResult CompareResult) {
 //
 // Subring returned by this method does not contain instances that have read-only field set.
 func (r *Ring) ShuffleShard(identifier string, size int) ReadRing {
-	// Use all instances if shuffle sharding is disabled, or it covers all instances anyway.
+	// Use all possible instances if shuffle sharding is disabled. We don't set size to r.InstancesCount(), because
+	// that could lead to not all instances being returned when ring zones are unbalanced.
 	// Reason for not returning entire ring directly is that we need to filter out read-only instances.
-	instances := r.InstancesCount()
-	if size <= 0 || instances <= size {
-		size = instances
+	if size <= 0 {
+		size = math.MaxInt
 	}
 
 	if cached := r.getCachedShuffledSubring(identifier, size); cached != nil {
@@ -771,7 +771,7 @@ func (r *Ring) shuffleShard(identifier string, size int, lookbackPeriod time.Dur
 		actualZones = []string{""}
 	}
 
-	shard := make(map[string]InstanceDesc, size)
+	shard := make(map[string]InstanceDesc, min(len(r.ringDesc.Ingesters), size))
 
 	// We need to iterate zones always in the same order to guarantee stability.
 	for _, zone := range actualZones {
