35c113b98882af1e1318250a0c47e5672da9a1d5
Alex Weaver <weaver.alex.d@gmail.com>
2024-08-23 10:50:53 -0500

    Reverse cancellation.NewError one

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -405,7 +405,7 @@ func (s *Scheduler) cancelRequestAndRemoveFromPending(key queue.RequestKey, reas
 
 	req := s.schedulerInflightRequests[key]
 	if req != nil {
-		req.CancelFunc(cancellation.NewError(reason))
+		req.CancelFunc(cancellation.NewErrorf("%s", reason))
 	}
 
 	delete(s.schedulerInflightRequests, key)
