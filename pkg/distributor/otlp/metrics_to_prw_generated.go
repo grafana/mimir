5dc459302c5b7a438923e45bb7bca268b8db2632
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2024-09-02 14:42:15 +0100

    update prometheus no client golang at grafana/mimir-prometheus@fdf902d (#9156) (#9176)
    * Update mimir-prometheus
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    * DefaultChunkedReadLimit was moved from storage to config
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    * Adopt to new remote read client interface
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    * Update otlp generate code
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    * OTLP: ignore annotations for now
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    * Remote read Content-Type unset in tests
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    * Use client_golang 1.19.1
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    * Update engine test cases from upstream
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    * Mark unsupported engine expressions
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    * Disable test that assumes the newly added `promql-delayed-name-removal` flag is enabled
    
    * Re-enable running MQE test cases against Prometheus' engine now that https://github.com/prometheus/prometheus/pull/14611/ has been merged
    
    * Updates due to review
    
    Add changelog.
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    
    ---------
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>
    Co-authored-by: Charles Korn <charles.korn@grafana.com>
    (cherry picked from commit 61634ee02a8a78eb2906a88f6179f81e9d094379)
    
    Co-authored-by: George Krajcsovits <krajorama@users.noreply.github.com>

--- pkg/distributor/otlp/metrics_to_prw_generated.go
+++ pkg/distributor/otlp/metrics_to_prw_generated.go
@@ -29,6 +29,7 @@ import (
 	"go.uber.org/multierr"
 
 	prometheustranslator "github.com/prometheus/prometheus/storage/remote/otlptranslator/prometheus"
+	"github.com/prometheus/prometheus/util/annotations"
 
 	"github.com/grafana/mimir/pkg/mimirpb"
 )
@@ -58,7 +59,7 @@ func NewMimirConverter() *MimirConverter {
 }
 
 // FromMetrics converts pmetric.Metrics to Mimir remote write format.
-func (c *MimirConverter) FromMetrics(ctx context.Context, md pmetric.Metrics, settings Settings) (errs error) {
+func (c *MimirConverter) FromMetrics(ctx context.Context, md pmetric.Metrics, settings Settings) (annots annotations.Annotations, errs error) {
 	c.everyN = everyNTimes{n: 128}
 	resourceMetricsSlice := md.ResourceMetrics()
 	for i := 0; i < resourceMetricsSlice.Len(); i++ {
@@ -133,13 +134,15 @@ func (c *MimirConverter) FromMetrics(ctx context.Context, md pmetric.Metrics, se
 						errs = multierr.Append(errs, fmt.Errorf("empty data points. %s is dropped", metric.Name()))
 						break
 					}
-					if err := c.addExponentialHistogramDataPoints(
+					ws, err := c.addExponentialHistogramDataPoints(
 						ctx,
 						dataPoints,
 						resource,
 						settings,
 						promName,
-					); err != nil {
+					)
+					annots.Merge(ws)
+					if err != nil {
 						errs = multierr.Append(errs, err)
 						if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) {
 							return
@@ -165,7 +168,7 @@ func (c *MimirConverter) FromMetrics(ctx context.Context, md pmetric.Metrics, se
 		addResourceTargetInfo(resource, settings, mostRecentTimestamp, c)
 	}
 
-	return
+	return annots, errs
 }
 
 func isSameMetric(ts *mimirpb.TimeSeries, lbls []mimirpb.LabelAdapter) bool {
