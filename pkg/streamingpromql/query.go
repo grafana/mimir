1a4cb60186e8dc5152e279ed3a1fb531af6094b9
Charles Korn <charleskorn@users.noreply.github.com>
2024-09-06 10:18:12 +1000

    Mimir query engine: add support for unary negation (#9196)
    * Add failing tests
    
    * Add feature toggle
    
    * Add support for unary negation of instant vectors
    
    # Conflicts:
    #	pkg/streamingpromql/query.go
    
    * Add support for unary negation of scalars
    
    * Add changelog entry
    
    * Address PR feedback: add further test cases

--- pkg/streamingpromql/query.go
+++ pkg/streamingpromql/query.go
@@ -244,6 +244,21 @@ func (q *Query) convertToInstantVectorOperator(expr parser.Expr) (types.InstantV
 		}
 
 		return operators.NewVectorVectorBinaryOperation(lhs, rhs, *e.VectorMatching, e.Op, q.memoryConsumptionTracker, q.annotations, e.PositionRange())
+	case *parser.UnaryExpr:
+		if e.Op != parser.SUB {
+			return nil, compat.NewNotSupportedError(fmt.Sprintf("unary expression with '%s'", e.Op))
+		}
+
+		if !q.engine.featureToggles.EnableUnaryNegation {
+			return nil, compat.NewNotSupportedError("unary negation of instant vectors")
+		}
+
+		inner, err := q.convertToInstantVectorOperator(e.Expr)
+		if err != nil {
+			return nil, err
+		}
+
+		return unaryNegationOfInstantVectorOperatorFactory(inner, q.memoryConsumptionTracker, e.PositionRange()), nil
 
 	case *parser.StepInvariantExpr:
 		// One day, we'll do something smarter here.
@@ -338,6 +353,23 @@ func (q *Query) convertToScalarOperator(expr parser.Expr) (types.ScalarOperator,
 
 	case *parser.Call:
 		return q.convertFunctionCallToScalarOperator(e)
+
+	case *parser.UnaryExpr:
+		if e.Op != parser.SUB {
+			return nil, compat.NewNotSupportedError(fmt.Sprintf("unary expression with '%s'", e.Op))
+		}
+
+		if !q.engine.featureToggles.EnableUnaryNegation {
+			return nil, compat.NewNotSupportedError("unary negation of scalars")
+		}
+
+		inner, err := q.convertToScalarOperator(e.Expr)
+		if err != nil {
+			return nil, err
+		}
+
+		return operators.NewUnaryNegationOfScalar(inner, e.PositionRange()), nil
+
 	case *parser.StepInvariantExpr:
 		// One day, we'll do something smarter here.
 		return q.convertToScalarOperator(e.Expr)
