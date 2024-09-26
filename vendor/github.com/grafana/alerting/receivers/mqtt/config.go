87a0179de7d8dca3fbb4cfc8ae786aed49542c2d
Santiago <santiagohernandez.1997@gmail.com>
2024-09-26 13:39:06 -0300

    Update grafana/alerting to latest main

--- vendor/github.com/grafana/alerting/receivers/mqtt/config.go
+++ vendor/github.com/grafana/alerting/receivers/mqtt/config.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"net/url"
 
 	"github.com/grafana/alerting/receivers"
 	"github.com/grafana/alerting/templates"
@@ -16,14 +17,16 @@ const (
 )
 
 type Config struct {
-	BrokerURL          string `json:"brokerUrl,omitempty" yaml:"brokerUrl,omitempty"`
-	ClientID           string `json:"clientId,omitempty" yaml:"clientId,omitempty"`
-	Topic              string `json:"topic,omitempty" yaml:"topic,omitempty"`
-	Message            string `json:"message,omitempty" yaml:"message,omitempty"`
-	MessageFormat      string `json:"messageFormat,omitempty" yaml:"messageFormat,omitempty"`
-	Username           string `json:"username,omitempty" yaml:"username,omitempty"`
-	Password           string `json:"password,omitempty" yaml:"password,omitempty"`
-	InsecureSkipVerify bool   `json:"insecureSkipVerify,omitempty" yaml:"insecureSkipVerify,omitempty"`
+	BrokerURL     string                   `json:"brokerUrl,omitempty" yaml:"brokerUrl,omitempty"`
+	ClientID      string                   `json:"clientId,omitempty" yaml:"clientId,omitempty"`
+	Topic         string                   `json:"topic,omitempty" yaml:"topic,omitempty"`
+	Message       string                   `json:"message,omitempty" yaml:"message,omitempty"`
+	MessageFormat string                   `json:"messageFormat,omitempty" yaml:"messageFormat,omitempty"`
+	Username      string                   `json:"username,omitempty" yaml:"username,omitempty"`
+	Password      string                   `json:"password,omitempty" yaml:"password,omitempty"`
+	QoS           receivers.OptionalNumber `json:"qos,omitempty" yaml:"qos,omitempty"`
+	Retain        bool                     `json:"retain,omitempty" yaml:"retain,omitempty"`
+	TLSConfig     *receivers.TLSConfig     `json:"tlsConfig,omitempty" yaml:"tlsConfig,omitempty"`
 }
 
 func NewConfig(jsonData json.RawMessage, decryptFn receivers.DecryptFunc) (Config, error) {
@@ -56,8 +59,29 @@ func NewConfig(jsonData json.RawMessage, decryptFn receivers.DecryptFunc) (Confi
 		return Config{}, errors.New("Invalid message format, must be 'json' or 'text'")
 	}
 
-	password := decryptFn("password", settings.Password)
-	settings.Password = password
+	qos, err := settings.QoS.Int64()
+	if err != nil {
+		return Config{}, fmt.Errorf("Failed to parse QoS: %w", err)
+	}
+	if qos < 0 || qos > 2 {
+		return Config{}, fmt.Errorf("Invalid QoS level: %d. Must be 0, 1 or 2", qos)
+	}
+
+	settings.Password = decryptFn("password", settings.Password)
+
+	if settings.TLSConfig == nil {
+		settings.TLSConfig = &receivers.TLSConfig{}
+	}
+
+	settings.TLSConfig.CACertificate = decryptFn("tlsConfig.caCertificate", settings.TLSConfig.CACertificate)
+	settings.TLSConfig.ClientCertificate = decryptFn("tlsConfig.clientCertificate", settings.TLSConfig.ClientCertificate)
+	settings.TLSConfig.ClientKey = decryptFn("tlsConfig.clientKey", settings.TLSConfig.ClientKey)
+
+	parsedURL, err := url.Parse(settings.BrokerURL)
+	if err != nil {
+		return Config{}, errors.New("Failed to parse broker URL")
+	}
+	settings.TLSConfig.ServerName = parsedURL.Hostname()
 
 	return settings, nil
 }
