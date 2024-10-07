1747a64723c3c1c4ee1d351ca46c91cf27640896
Arve Knudsen <arve.knudsen@gmail.com>
2024-10-07 15:51:35 +0200

    Add tests for pkg/util/test.AssertGatherAndCompare
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/util/test/metrics.go
+++ pkg/util/test/metrics.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/testutil"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
 type ExpectedMetricsContext struct {
@@ -67,7 +67,20 @@ func (m *ExpectedMetrics) GetNames() []string {
 	return m.Names
 }
 
+// AssertGatherAndCompare asserts that metrics in expectedText are found among g's metrics.
+// If, however, any metrics are provided, the following rules apply:
+// * Provided metrics that also exist in expectedText are required to be found among g's metrics.
+// * Provided metrics that don't exist in expectedText are required to be absent from g's metrics.
 func AssertGatherAndCompare(t *testing.T, g prometheus.Gatherer, expectedText string, metrics ...string) {
+	t.Helper()
+	assert.NoError(t, gatherAndCompare(g, expectedText, metrics...))
+}
+
+func gatherAndCompare(g prometheus.Gatherer, expectedText string, metrics ...string) error {
+	if len(metrics) == 0 {
+		return testutil.GatherAndCompare(g, strings.NewReader(expectedText))
+	}
+
 	sc := bufio.NewScanner(strings.NewReader(expectedText))
 	absent := make([]string, len(metrics))
 	copy(absent, metrics)
@@ -85,17 +98,31 @@ func AssertGatherAndCompare(t *testing.T, g prometheus.Gatherer, expectedText st
 			}
 		}
 	}
-	require.Equal(t, len(metrics), len(required)+len(absent)) // Sanity check.
-	if len(required) > 0 {
-		require.NoError(t, testutil.GatherAndCompare(g, strings.NewReader(expectedText), required...), "should be present: metrics=%s", strings.Join(required, ", "))
+	// Sanity check.
+	if len(required)+len(absent) != len(metrics) {
+		panic(fmt.Errorf("length of required+absent doesn't match up with metrics"))
 	}
+
+	if len(required) > 0 {
+		if err := testutil.GatherAndCompare(g, strings.NewReader(expectedText), required...); err != nil {
+			return err
+		}
+	}
+
 	notAbsent := []string{}
 	for _, metric := range absent {
 		count, err := testutil.GatherAndCount(g, metric)
-		require.NoError(t, err)
+		if err != nil {
+			return fmt.Errorf("GatherAndCount(g, %s): %w", metric, err)
+		}
+
 		if count > 0 {
 			notAbsent = append(notAbsent, metric)
 		}
 	}
-	require.Empty(t, notAbsent, "should be absent: metrics=%s", strings.Join(notAbsent, ", "))
+	if len(notAbsent) > 0 {
+		return fmt.Errorf("should be absent: metrics=%s", strings.Join(notAbsent, ", "))
+	}
+
+	return nil
 }
