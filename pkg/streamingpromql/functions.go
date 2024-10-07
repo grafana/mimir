863f82b7cbc49090fa7966911a746391b01a7f85
Joshua Hesketh <joshua.hesketh@grafana.com>
2024-10-07 14:23:50 +1100

    Remove redundant name in LabelReplaceFunctionOperatorFactory

--- pkg/streamingpromql/functions.go
+++ pkg/streamingpromql/functions.go
@@ -141,41 +141,41 @@ func scalarToInstantVectorOperatorFactory(args []types.Operator, _ *limiting.Mem
 	return operators.NewScalarToInstantVector(inner, expressionPosition), nil
 }
 
-func LabelReplaceFunctionOperatorFactory(name string) InstantVectorFunctionOperatorFactory {
+func LabelReplaceFunctionOperatorFactory() InstantVectorFunctionOperatorFactory {
 	return func(args []types.Operator, memoryConsumptionTracker *limiting.MemoryConsumptionTracker, annotations *annotations.Annotations, expressionPosition posrange.PositionRange) (types.InstantVectorOperator, error) {
 		if len(args) != 5 {
 			// Should be caught by the PromQL parser, but we check here for safety.
-			return nil, fmt.Errorf("expected exactly 5 argument for %s, got %v", name, len(args))
+			return nil, fmt.Errorf("expected exactly 5 argument for label_replace, got %v", len(args))
 		}
 
 		inner, ok := args[0].(types.InstantVectorOperator)
 		if !ok {
 			// Should be caught by the PromQL parser, but we check here for safety.
-			return nil, fmt.Errorf("expected a range vector for 1st argument for %s, got %T", name, args[0])
+			return nil, fmt.Errorf("expected a range vector for 1st argument for label_replace, got %T", args[0])
 		}
 
 		dstLabel, ok := args[1].(types.StringOperator)
 		if !ok {
 			// Should be caught by the PromQL parser, but we check here for safety.
-			return nil, fmt.Errorf("expected a string for 2nd argument for %s, got %T", name, args[1])
+			return nil, fmt.Errorf("expected a string for 2nd argument for label_replace, got %T", args[1])
 		}
 
 		replacement, ok := args[2].(types.StringOperator)
 		if !ok {
 			// Should be caught by the PromQL parser, but we check here for safety.
-			return nil, fmt.Errorf("expected a string for 3rd argument for %s, got %T", name, args[2])
+			return nil, fmt.Errorf("expected a string for 3rd argument for label_replace, got %T", args[2])
 		}
 
 		srcLabel, ok := args[3].(types.StringOperator)
 		if !ok {
 			// Should be caught by the PromQL parser, but we check here for safety.
-			return nil, fmt.Errorf("expected a string for 4th argument for %s, got %T", name, args[3])
+			return nil, fmt.Errorf("expected a string for 4th argument for label_replace, got %T", args[3])
 		}
 
 		regex, ok := args[4].(types.StringOperator)
 		if !ok {
 			// Should be caught by the PromQL parser, but we check here for safety.
-			return nil, fmt.Errorf("expected a string for 5th argument for %s, got %T", name, args[4])
+			return nil, fmt.Errorf("expected a string for 5th argument for label_replace, got %T", args[4])
 		}
 
 		f := functions.FunctionOverInstantVector{
@@ -212,7 +212,7 @@ var instantVectorFunctionOperatorFactories = map[string]InstantVectorFunctionOpe
 	"histogram_count":   InstantVectorTransformationFunctionOperatorFactory("histogram_count", functions.HistogramCount),
 	"histogram_sum":     InstantVectorTransformationFunctionOperatorFactory("histogram_sum", functions.HistogramSum),
 	"increase":          FunctionOverRangeVectorOperatorFactory("increase", functions.Increase),
-	"label_replace":     LabelReplaceFunctionOperatorFactory("label_replace"),
+	"label_replace":     LabelReplaceFunctionOperatorFactory(),
 	"last_over_time":    FunctionOverRangeVectorOperatorFactory("last_over_time", functions.LastOverTime),
 	"max_over_time":     FunctionOverRangeVectorOperatorFactory("max_over_time", functions.MaxOverTime),
 	"min_over_time":     FunctionOverRangeVectorOperatorFactory("min_over_time", functions.MinOverTime),
