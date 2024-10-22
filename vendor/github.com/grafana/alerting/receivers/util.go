8fa5f44e0105cab67247c91fd32d40662acd1307
Tito Lins <tito.linsesilva@grafana.com>
2024-10-22 13:07:50 +0200

    Support tls config for webhook receiver (#9341)
    * use tlsConfig to send webhooks
    
    * update alerting
    
    * update alerting
    
    * use NewTLSClient from alerting
    
    * update prometheus-alertmanager
    
    * update alerting
    
    * update alerting

--- vendor/github.com/grafana/alerting/receivers/util.go
+++ vendor/github.com/grafana/alerting/receivers/util.go
@@ -76,6 +76,29 @@ func (cfg *TLSConfig) ToCryptoTLSConfig() (*tls.Config, error) {
 	return tlsCfg, nil
 }
 
+// NewTLSClient creates a new HTTP client with the provided TLS configuration or with default settings.
+func NewTLSClient(tlsConfig *tls.Config) *http.Client {
+	nc := func(tlsConfig *tls.Config) *http.Client {
+		return &http.Client{
+			Timeout: time.Second * 30,
+			Transport: &http.Transport{
+				TLSClientConfig: tlsConfig,
+				Proxy:           http.ProxyFromEnvironment,
+				Dial: (&net.Dialer{
+					Timeout: 30 * time.Second,
+				}).Dial,
+				TLSHandshakeTimeout: 5 * time.Second,
+			},
+		}
+	}
+
+	if tlsConfig == nil {
+		return nc(&tls.Config{Renegotiation: tls.RenegotiateFreelyAsClient})
+	}
+
+	return nc(tlsConfig)
+}
+
 // SendHTTPRequest sends an HTTP request.
 // Stubbable by tests.
 //
