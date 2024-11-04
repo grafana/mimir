64d5adeaa31472a15ea56a9e7b1494ea41ec71b4
Grot (@grafanabot) <43478413+grafanabot@users.noreply.github.com>
2024-11-04 08:22:36 +0100

    MQE: Fix handling of string results (#9803) (#9814)
    This would previously panic after the query was closed
    
    (cherry picked from commit dcbf3f4b6e4caf1533247493baed68271a14a514)
    
    Co-authored-by: Joshua Hesketh <joshua.hesketh@grafana.com>

--- pkg/streamingpromql/engine_test.go
+++ pkg/streamingpromql/engine_test.go
@@ -253,10 +253,12 @@ func TestNewInstantQuery_Strings(t *testing.T) {
 	q, err := mimirEngine.NewInstantQuery(ctx, storage, nil, expr, time.Now())
 	require.NoError(t, err)
 	mimir := q.Exec(context.Background())
+	q.Close()
 
 	q, err = prometheusEngine.NewInstantQuery(ctx, storage, nil, expr, time.Now())
 	require.NoError(t, err)
 	prometheus := q.Exec(context.Background())
+	q.Close()
 
 	testutils.RequireEqualResults(t, expr, prometheus, mimir)
 }
