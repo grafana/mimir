37fdc8b95790b6438dd2bdaf302024ed1a86db25
David Grant <seizethedave@gmail.com>
2024-11-21 11:26:09 -0800

    Zero insertion + OTLP testing rig.

--- development/mimir-monolithic-mode/compose-up.sh
+++ development/mimir-monolithic-mode/compose-up.sh
@@ -35,7 +35,7 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-DEFAULT_PROFILES=("--profile" "prometheus" "--profile" "grafana-agent-static")
+DEFAULT_PROFILES=("--profile" "prometheus" "--profile" "otel-collector-otlp-push")
 if [ ${#PROFILES[@]} -eq 0 ]; then
     PROFILES=("${DEFAULT_PROFILES[@]}")
 fi
@@ -45,4 +45,4 @@ fi
 CGO_ENABLED=0 GOOS=linux go build -mod=vendor -tags=netgo,stringlabels -gcflags "all=-N -l" -o "${SCRIPT_DIR}"/mimir "${SCRIPT_DIR}"/../../cmd/mimir
 
 docker_compose -f "${SCRIPT_DIR}"/docker-compose.yml build mimir-1 && \
-docker_compose -f "${SCRIPT_DIR}"/docker-compose.yml "${PROFILES[@]}" up "${ARGS[@]}"
+docker_compose -f "${SCRIPT_DIR}"/docker-compose.yml "${PROFILES[@]}" up "${ARGS[@]}" --attach otel-otlp
