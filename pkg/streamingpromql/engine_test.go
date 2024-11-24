30d61a54871354eceeb1421c211063754aa8d408
Charles Korn <charles.korn@grafana.com>
2024-11-25 09:34:16 +1100

    MQE: fix issue where subqueries could return series with no points

--- pkg/streamingpromql/engine_test.go
+++ pkg/streamingpromql/engine_test.go
@@ -905,6 +905,14 @@ func TestSubqueries(t *testing.T) {
 			Start: time.Unix(10, 0),
 		},
 		{
+			// A query where SeriesMetadata returns some series but evaluates to no samples should not return anything.
+			Query: `(metric > Inf)[20s:10s]`,
+			Start: time.Unix(30, 0),
+			Result: promql.Result{
+				Value: promql.Matrix{},
+			},
+		},
+		{
 			Query: "metric[20s:5s]",
 			Result: promql.Result{
 				Value: promql.Matrix{
