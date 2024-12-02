a46c732c66063741807120daa6dfcbdbdfcfabe8
Oleg Zaytsev <mail@olegzaytsev.com>
2024-12-02 18:48:08 +0100

    Usage-tracker: track series in the map
    This starts tracking the series from the request in the internal map.
    
    No cleanup is implemented yet. Also tests pending.
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

---
+++ pkg/usagetracker/atomicswissmap/atomic_int32_map_test.go
@@ -0,0 +1,184 @@
+package atomicswissmap
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
+)
+
+func TestAtomicInt32Map_Functionality(t *testing.T) {
+	m := New(10)
+	_, found := m.GetOrCreate(1, 1)
+	require.False(t, found)
+	av1, ok := m.Get(1)
+	require.True(t, ok)
+	require.Equal(t, int32(1), av1.Load())
+	require.Equal(t, 1, m.Count())
+
+	// Check that key 2 doesn't exist.
+	_, ok = m.Get(2)
+	require.False(t, ok)
+
+	// Create key 2.
+	_, found = m.GetOrCreate(2, 20)
+	require.False(t, found)
+	av2, ok := m.Get(2)
+	require.True(t, ok)
+	require.Equal(t, int32(20), av2.Load())
+	require.Equal(t, 2, m.Count())
+
+	// Create key 2 again, should return the existing one.
+	av2, found = m.GetOrCreate(2, 20)
+	require.True(t, found)
+	require.Equal(t, int32(20), av2.Load())
+
+	// need to get it update after the GetOrCreate call.
+	av1, ok = m.Get(1)
+	require.True(t, ok)
+	require.Equal(t, int32(1), av1.Load())
+	require.True(t, av1.CompareAndSwap(1, 10))
+
+	// make a clone and check it has the correct length.
+	m2 := m.Clone()
+	require.Equal(t, m.Count(), m2.Count())
+
+	// It's still valid to update av2.
+	require.True(t, av2.CompareAndSwap(20, 200))
+
+	m.Delete(1)
+	_, ok = m.Get(1)
+	require.False(t, ok)
+
+	// Add 1000 keys, make the map grow.
+	for i := 0; i < 1000; i++ {
+		_, found = m.GetOrCreate(uint64(i*100), int32(i))
+		require.False(t, found)
+	}
+
+	// Key 2 should have the same value, but we need to get a new reference.
+	av2, ok = m.Get(2)
+	require.True(t, ok)
+	require.Equal(t, int32(200), av2.Load())
+
+	// Check that clone was not modified.
+	require.Equal(t, 2, m2.Count())
+	av1, ok = m2.Get(1)
+	require.True(t, ok)
+	require.Equal(t, int32(10), av1.Load())
+	av2, ok = m2.Get(2)
+	require.True(t, ok)
+	require.Equal(t, int32(20), av2.Load())
+}
+
+func TestAtomicInt32Map_Concurrency(t *testing.T) {
+	const stopAtKeys = 1_000
+
+	enoughKeys := func(m *Map) bool {
+		m.LockKeys()
+		if m.Count() > stopAtKeys {
+			m.UnlockKeys()
+			return true
+		}
+		m.UnlockKeys()
+		return false
+	}
+
+	keys := make([]uint64, stopAtKeys/2)
+	for i := range keys {
+		keys[i] = rand.Uint64()
+	}
+
+	// Add half of the keys to the map, let the goroutines fight to add the other half.
+	m := New(1e6)
+	m.Lock()
+	for i := 0; i < len(keys)/2; i++ {
+		_, found := m.GetOrCreate(keys[i], 1)
+		require.False(t, found)
+	}
+	m.Unlock()
+
+	updates := atomic.NewInt64(0)
+	retries := atomic.NewInt64(0)
+	collisions := atomic.NewInt64(0)
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		wg.Done()
+		for {
+			if enoughKeys(m) {
+				return
+			}
+
+			m.Lock()
+			key := rand.Uint64()
+
+			_, found := m.GetOrCreate(key, 1)
+			if found {
+				collisions.Inc()
+			}
+			m.Unlock()
+			time.Sleep(100 * time.Microsecond)
+		}
+	}()
+	for w := 0; w < max(2, runtime.GOMAXPROCS(0)); w++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := int32(1); i < 1e6; i++ {
+				if enoughKeys(m) {
+					return
+				}
+
+				key := keys[rand.Intn(len(keys))]
+				m.LockKeys()
+				gotv, ok := m.Get(key)
+				if ok {
+					updates.Inc()
+					for val := gotv.Load(); val < i && !gotv.CompareAndSwap(val, i); val = gotv.Load() {
+						retries.Inc()
+					}
+					m.UnlockKeys()
+					continue
+				}
+				m.UnlockKeys()
+
+				m.Lock()
+				if gotv, found := m.GetOrCreate(key, i); found {
+					for val := gotv.Load(); val < i && !gotv.CompareAndSwap(val, i); val = gotv.Load() {
+					}
+				}
+				m.Unlock()
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	t.Logf("updates: %d, retries: %d, collisions: %d", updates.Load(), retries.Load(), collisions.Load())
+}
+
+func BenchmarkMap_Clone(b *testing.B) {
+	for _, elements := range []int{1e6, 10e6} {
+		b.Run(fmt.Sprintf("elements=%d", elements), func(b *testing.B) {
+			m := New(uint32(elements))
+			for i := 0; i < elements; i++ {
+				_, _ = m.GetOrCreate(rand.Uint64(), 1)
+			}
+
+			b.ResetTimer()
+			total := 0
+			for i := 0; i < b.N; i++ {
+				cloned := m.Clone()
+				total += cloned.Count()
+			}
+			require.NotZero(b, total)
+		})
+	}
+}
--- pkg/usagetracker/tracker.go
+++ pkg/usagetracker/tracker.go
@@ -5,17 +5,27 @@ package usagetracker
 import (
 	"context"
 	"flag"
+	"math"
 	"net/http"
+	"slices"
+	"sync"
+	"time"
 
+	"github.com/cespare/xxhash/v2"
 	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/grafana/dskit/ring"
 	"github.com/grafana/dskit/services"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
+	"go.uber.org/atomic"
 
+	"github.com/grafana/mimir/pkg/usagetracker/atomicswissmap"
 	"github.com/grafana/mimir/pkg/usagetracker/usagetrackerpb"
 	"github.com/grafana/mimir/pkg/util/validation"
 )
+
+const shards = 128
 
 type Config struct {
 	InstanceRing  InstanceRingConfig  `yaml:"ring"`
@@ -43,12 +53,36 @@ type UsageTracker struct {
 	// Dependencies.
 	subservices        *services.Manager
 	subservicesWatcher *services.FailureWatcher
+
+	// lock[i] locks data[i] map.
+	lock [shards]shardLock
+	// data[i] map is protected by lock[i].
+	data [shards]map[string]*atomicswissmap.Map
+
+	tenantsMtx sync.RWMutex
+	tenants    map[string]*tenantInfo
+}
+
+// shardLock is borrowed from https://github.com/prometheus/prometheus/blob/cd1f8ac129a289be8e7d98b6de57a9ba5814c406/tsdb/agent/series.go#L132-L136
+type shardLock struct {
+	sync.RWMutex
+	// Padding to avoid multiple locks being on the same cache line.
+	_ [40]byte
+}
+
+type tenantInfo struct {
+	series     atomic.Uint64
+	lastUpdate atomic.Int64
 }
 
 func NewUsageTracker(cfg Config, overrides *validation.Overrides, logger log.Logger, registerer prometheus.Registerer) (*UsageTracker, error) {
 	t := &UsageTracker{
 		overrides: overrides,
 		logger:    logger,
+		tenants:   map[string]*tenantInfo{},
+	}
+	for i := range t.data {
+		t.data[i] = make(map[string]*atomicswissmap.Map)
 	}
 
 	// Get the partition ID.
@@ -125,8 +159,141 @@ func (t *UsageTracker) run(ctx context.Context) error {
 }
 
 // TrackSeries implements usagetrackerpb.UsageTrackerServer.
-func (t *UsageTracker) TrackSeries(_ context.Context, _ *usagetrackerpb.TrackSeriesRequest) (*usagetrackerpb.TrackSeriesResponse, error) {
-	return &usagetrackerpb.TrackSeriesResponse{}, nil
+func (t *UsageTracker) TrackSeries(_ context.Context, req *usagetrackerpb.TrackSeriesRequest) (*usagetrackerpb.TrackSeriesResponse, error) {
+	return t.trackSeries(context.Background(), req, time.Now())
+}
+
+// trackSeries is used in tests so we can provide custom time.Now() value.
+func (t *UsageTracker) trackSeries(_ context.Context, req *usagetrackerpb.TrackSeriesRequest, now time.Time) (*usagetrackerpb.TrackSeriesResponse, error) {
+	info := t.getOrCreateUpdatedTenantInfo(req.UserID)
+	limit := t.localSeriesLimit(req.UserID)
+	if limit == 0 {
+		limit = math.MaxUint64
+	}
+
+	// Sort series by shard.
+	// Start each tenant on its own shard to avoid hotspots.
+	tenantStartingShard := xxhash.Sum64String(req.UserID) % shards
+	series := req.SeriesHashes
+	slices.SortFunc(series, func(a, b uint64) int {
+		return int((a%shards+tenantStartingShard)%shards) - int((b%shards+tenantStartingShard)%shards)
+	})
+
+	nowMinutes := int32(now.Sub(now.Truncate(4 * time.Hour)).Minutes())
+
+	created := make([]uint64, 0, len(series))
+	rejected := series[:0]
+	i0 := 0
+	for i := 1; i <= len(series); i++ {
+		if i == len(series) || (series[i]%shards) != (series[i0]%shards) {
+			created, rejected = t.trackShardSeries(req.UserID, info, series[i0:i], limit, created, rejected, nowMinutes)
+			i0 = i
+		}
+	}
+
+	// TODO: send the created series to Kafka events topic.
+	level.Info(t.logger).Log("msg", "tracked series", "userID", req.UserID, "received", len(series), "created", len(created), "rejected", len(rejected))
+
+	return &usagetrackerpb.TrackSeriesResponse{RejectedSeriesHashes: rejected}, nil
+}
+
+func (t *UsageTracker) trackShardSeries(userID string, info *tenantInfo, series []uint64, limit uint64, created, rejected []uint64, now int32) (_created, _rejected []uint64) {
+	shard := series[0] % shards
+	m := t.getOrCreateTenantShardMap(userID, shard, limit)
+	newSeries := 0
+	m.LockKeys()
+	for i := 0; i < len(series); i++ {
+		ts, ok := m.Get(series[i])
+		if !ok {
+			series[newSeries] = series[i]
+			newSeries++
+			continue
+		}
+
+		// CAS it if it's higher.
+		for val := ts.Load(); minutesGreater(now, val) && !ts.CompareAndSwap(val, now); val = ts.Load() {
+		}
+
+	}
+	m.UnlockKeys()
+
+	// Check if there's any need to create series first, maybe we don't need to take the mutex at all.
+	if info.series.Load() >= limit {
+		rejected = append(rejected, series[:newSeries]...)
+		return created, rejected
+	}
+
+	m.Lock()
+	for i := 0; i < newSeries && limit <= info.series.Load(); i++ {
+		ts, found := m.GetOrCreate(series[i], now)
+		if found {
+			// CAS it if it's higher.
+			for val := ts.Load(); minutesGreater(now, val) && !ts.CompareAndSwap(val, now); val = ts.Load() {
+			}
+		}
+	}
+	m.Unlock()
+
+	rejected = append(rejected, series[:newSeries]...)
+	return created, rejected
+}
+
+// minutesGreater returns true if a is greater than b as minutes values on a four-hour clock face assuming that none of the values is ever older than 1h.
+func minutesGreater(a, b int32) bool {
+	if a > b {
+		// true: 40-20 = 20
+		// true: 130-100 = 30
+		// false: 190-10 = 180 (that 190 is from the previous hour)
+		return a-b < 60
+	}
+	// true: 10, 190: 250-195 = 55
+	// false: 10, 120: 250-120 = 130
+	return a+(4*60)-b < 60
+}
+
+func (t *UsageTracker) getOrCreateTenantShardMap(id string, shard uint64, limit uint64) *atomicswissmap.Map {
+	t.lock[shard].RLock()
+	m := t.data[shard][id]
+	t.lock[shard].RUnlock()
+	if m != nil {
+		return m
+	}
+
+	t.lock[shard].Lock()
+	defer t.lock[shard].Unlock()
+	if m = t.data[shard][id]; m != nil {
+		return m
+	}
+
+	m = atomicswissmap.New(uint32(limit / shards))
+	t.data[shard][id] = m
+	return m
+}
+
+func (t *UsageTracker) getOrCreateUpdatedTenantInfo(tenantID string) *tenantInfo {
+	now := time.Now().Unix()
+
+	t.tenantsMtx.RLock()
+	if info, ok := t.tenants[tenantID]; ok {
+		// blind update here, we don't care if we overwrite the lastUpdate value with one second earlier.
+		info.lastUpdate.Store(now)
+		// don't unlock until we've updated the lastUpdate value.
+		t.tenantsMtx.RUnlock()
+		return info
+	}
+	t.tenantsMtx.RUnlock()
+
+	t.tenantsMtx.Lock()
+	defer t.tenantsMtx.Unlock()
+	if info, ok := t.tenants[tenantID]; ok {
+		return info
+	}
+
+	info := &tenantInfo{}
+	info.lastUpdate.Store(now)
+
+	t.tenants[tenantID] = info
+	return info
 }
 
 func (t *UsageTracker) InstanceRingHandler(w http.ResponseWriter, req *http.Request) {
@@ -135,4 +302,14 @@ func (t *UsageTracker) InstanceRingHandler(w http.ResponseWriter, req *http.Requ
 
 func (t *UsageTracker) PartitionRingHandler(w http.ResponseWriter, req *http.Request) {
 	t.partitionPageHandler.ServeHTTP(w, req)
+}
+
+func (t *UsageTracker) localSeriesLimit(userID string) uint64 {
+	globalLimit := t.overrides.MaxGlobalSeriesPerUser(userID) // TODO: use a new active series limit.
+	if globalLimit <= 0 {
+		return 0
+	}
+
+	// Global limit is equally distributed among all active partitions.
+	return uint64(float64(globalLimit) / float64(t.partitionWatcher.PartitionRing().ActivePartitionsCount()))
 }
