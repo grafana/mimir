1f14455887a810419f1545b7a8ef58cedb46adf2
Oleg Zaytsev <mail@olegzaytsev.com>
2024-12-05 19:19:33 +0100

    UsageTracker: register store as prometheus collector (#10148)
    * UsageTracker: register store as prometheus collector
    
    This will export the number of activer series tracker for each tenant.
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>
    
    * Consistent metric name
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>
    
    * Add license header
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>
    
    ---------
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

---
+++ pkg/usagetracker/tracker_store_collector.go
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package usagetracker
+
+import "github.com/prometheus/client_golang/prometheus"
+
+var _ prometheus.Collector = &trackerStore{}
+
+var trackerStoreMetricDesc = prometheus.NewDesc(
+	"cortex_usage_tracker_active_series",
+	"Number of active series tracker for each user.",
+	[]string{"user"}, nil,
+)
+
+func (t *trackerStore) Describe(descs chan<- *prometheus.Desc) {
+	descs <- trackerStoreMetricDesc
+}
+
+func (t *trackerStore) Collect(metrics chan<- prometheus.Metric) {
+	t.tenantsMtx.RLock()
+	defer t.tenantsMtx.RUnlock()
+	for tenantID, info := range t.tenants {
+		metrics <- prometheus.MustNewConstMetric(trackerStoreMetricDesc, prometheus.GaugeValue, float64(info.series.Load()), tenantID)
+	}
+}
