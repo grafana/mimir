7d704ed3bc1dddfb436a3b3dcabb22157c401f20
Oleg Zaytsev <mail@olegzaytsev.com>
2024-12-07 17:17:09 +0100

    Fix uint maths
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

--- pkg/usagetracker/clock/minutes.go
+++ pkg/usagetracker/clock/minutes.go
@@ -25,15 +25,16 @@ type Minutes uint8
 // sub subtracts other from m, taking into account the 2-hour clock face, assuming both values aren't more than 1h apart.
 // It does *not* return *Minutes* because it returns a duration, while Minutes is a timestamp.
 func (m Minutes) sub(other Minutes) int {
+	mi, otheri := int(m), int(other)
 	sign := 1
-	if m < other {
-		m, other, sign = other, m, -1
+	if mi < otheri {
+		mi, otheri, sign = otheri, mi, -1
 	}
 
-	if m-other < 60 {
-		return sign * int(m-other)
+	if mi-otheri < 60 {
+		return sign * (mi - otheri)
 	}
-	return sign * int(m-other-2*60)
+	return sign * (mi - otheri - 2*60)
 }
 
 // minutesGreater returns true if this value is greater than other on a four-hour clock face assuming that none of the values is ever older than 1h.
@@ -41,7 +42,7 @@ func (m Minutes) GreaterThan(other Minutes) bool {
 	if m > other {
 		return m-other < 60
 	}
-	return m+(2*60)-other < 60
+	return int(m+(2*60))-int(other) < 60
 }
 
 // greaterOrEqualThan returns true if this value is greater or equal than other on a four-hour clock face assuming that none of the values is ever older than 1h.
