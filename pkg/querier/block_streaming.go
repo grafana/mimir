56ead6b8eca6094cde453dea0cedf9d36759363d
Arve Knudsen <arve.knudsen@gmail.com>
2024-12-12 17:32:03 +0100

    Free store-gateway query buffers
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/querier/block_streaming.go
+++ pkg/querier/block_streaming.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"slices"
 	"sort"
 
 	"github.com/go-kit/log"
@@ -280,6 +281,7 @@ func (s *storeGatewayStreamReader) readStream(log *spanlogger.SpanLogger) error 
 	if err != nil {
 		return translateReceivedError(err)
 	}
+	defer msg.FreeBuffer()
 
 	estimate := msg.GetStreamingChunksEstimate()
 	msg.FreeBuffer()
@@ -336,7 +338,20 @@ func (s *storeGatewayStreamReader) readStream(log *spanlogger.SpanLogger) error 
 		s.stats.AddFetchedChunks(uint64(numChunks))
 		s.stats.AddFetchedChunkBytes(uint64(chunkBytes))
 
-		if err := s.sendBatch(msg); err != nil {
+		// Memory safe copy.
+		safeSeries := make([]*storepb.StreamingChunks, 0, len(batch.Series))
+		for _, s := range batch.Series {
+			safe := *s
+			safe.Chunks = slices.Clone(s.Chunks)
+			for i, c := range safe.Chunks {
+				safe.Chunks[i].Raw.Data = slices.Clone(c.Raw.Data)
+			}
+			safeSeries = append(safeSeries, &safe)
+		}
+		batch.Series = safeSeries
+		msg.FreeBuffer()
+
+		if err := s.sendBatch(batch); err != nil {
 			return err
 		}
 	}
