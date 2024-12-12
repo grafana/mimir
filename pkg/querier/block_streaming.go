cd7f75135d787539c818b8465492670cb3c8a4ed
Arve Knudsen <arve.knudsen@gmail.com>
2024-12-12 17:32:03 +0100

    Free store-gateway query buffers
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/querier/block_streaming.go
+++ pkg/querier/block_streaming.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"slices"
 	"sort"
 
 	"github.com/go-kit/log"
@@ -246,6 +247,7 @@ func (s *storeGatewayStreamReader) readStream(log *spanlogger.SpanLogger) error 
 	if err != nil {
 		return translateReceivedError(err)
 	}
+	defer msg.FreeBuffer()
 
 	estimate := msg.GetStreamingChunksEstimate()
 	if estimate == nil {
@@ -265,15 +267,18 @@ func (s *storeGatewayStreamReader) readStream(log *spanlogger.SpanLogger) error 
 
 		batch := msg.GetStreamingChunks()
 		if batch == nil {
+			msg.FreeBuffer()
 			return fmt.Errorf("expected to receive streaming chunks, but got message of type %T", msg.Result)
 		}
 
 		if len(batch.Series) == 0 {
+			msg.FreeBuffer()
 			continue
 		}
 
 		totalSeries += len(batch.Series)
 		if totalSeries > s.expectedSeriesCount {
+			msg.FreeBuffer()
 			return fmt.Errorf("expected to receive only %v series, but received at least %v series", s.expectedSeriesCount, totalSeries)
 		}
 
@@ -287,14 +292,29 @@ func (s *storeGatewayStreamReader) readStream(log *spanlogger.SpanLogger) error 
 		}
 		totalChunks += numChunks
 		if err := s.queryLimiter.AddChunks(numChunks); err != nil {
+			msg.FreeBuffer()
 			return err
 		}
 		if err := s.queryLimiter.AddChunkBytes(chunkBytes); err != nil {
+			msg.FreeBuffer()
 			return err
 		}
 
 		s.stats.AddFetchedChunks(uint64(numChunks))
 		s.stats.AddFetchedChunkBytes(uint64(chunkBytes))
+
+		// Memory safe copy.
+		safeSeries := make([]*storepb.StreamingChunks, 0, len(batch.Series))
+		for _, s := range batch.Series {
+			safe := *s
+			safe.Chunks = slices.Clone(s.Chunks)
+			for i, c := range safe.Chunks {
+				safe.Chunks[i].Raw.Data = slices.Clone(c.Raw.Data)
+			}
+			safeSeries = append(safeSeries, &safe)
+		}
+		batch.Series = safeSeries
+		msg.FreeBuffer()
 
 		if err := s.sendBatch(batch); err != nil {
 			return err
