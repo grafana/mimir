ca039338f55132bea809652339e3f66ee351f423
Oleg Zaytsev <mail@olegzaytsev.com>
2025-01-03 15:07:24 +0100

    Temp version of grpcrul-push-ingesters
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

---
+++ tools/grpcurl-push-ingesters/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/grafana/dskit/flagext"
+	"github.com/grafana/dskit/ring"
+	"github.com/grafana/dskit/user"
+
+	ingester_client "github.com/grafana/mimir/pkg/ingester/client"
+	"github.com/grafana/mimir/pkg/mimirpb"
+)
+
+func main() {
+	var port int
+	flag.IntVar(&port, "port", 9095, "Port to port-forward.")
+
+	var podTemplate string
+	flag.StringVar(&podTemplate, "pod-template", "ingester-zone-<zone>-<id>", "Template to generate the pod ID in the format 'ingester-zone-<zone>-<id>'")
+
+	var podMinID int
+	var podMaxID int
+	flag.IntVar(&podMinID, "pod-min-id", 0, "Min pod ID (included)")
+	flag.IntVar(&podMaxID, "pod-max-id", 0, "Max pod ID (included)")
+
+	var podZones flagext.StringSliceCSV
+	flag.Var(&podZones, "pod-zones", "Comma-separated list of zones")
+
+	var namespace string
+	flag.StringVar(&namespace, "namespace", "", "Namespace of the pods.")
+
+	var orgID string
+	flag.StringVar(&orgID, "org", "", "Organization ID to query the ingester.")
+
+	flag.Parse()
+
+	if orgID == "" {
+		log.Fatal("orgID is required")
+	}
+
+	if namespace == "" {
+		log.Fatal("namespace is required")
+	}
+
+	// Generate pod IDs.
+	pods := generatePodIDsWithinRange(podTemplate, podZones, podMinID, podMaxID)
+	log.Println("Pods:", pods)
+
+	ctx := user.InjectOrgID(context.Background(), orgID)
+	process := func(pod string, localPort int) {
+		addr := fmt.Sprintf("localhost:%d", localPort)
+
+		err := pushSeriesToIngester(ctx, pod, addr)
+		if err != nil {
+			log.Printf("failed to check ingester %s: %v", pod, err)
+		}
+	}
+
+	for _, pod := range pods {
+		if err := processPortForwarded(pod, namespace, port, process); err != nil {
+			log.Printf("Error processing pod %s in namespace %s: %v", pod, namespace, err)
+		}
+	}
+}
+
+func pushSeriesToIngester(ctx context.Context, pod, addr string) error {
+	// To keep it simple, create a gRPC client each time.
+	clientMetrics := ingester_client.NewMetrics(nil)
+	clientConfig := ingester_client.Config{}
+	flagext.DefaultValues(&clientConfig)
+
+	client, err := ingester_client.MakeIngesterClient(ring.InstanceDesc{Addr: addr}, clientConfig, clientMetrics)
+	if err != nil {
+		return err
+	}
+	wr := mimirpb.NewWriteRequest(nil, mimirpb.API).
+		AddFloatSeries([][]mimirpb.LabelAdapter{
+			{
+				{Name: "__name__", Value: "test_metric"},
+				{Name: "sent_to_pod", Value: pod},
+			},
+		}, []mimirpb.Sample{
+			{
+				TimestampMs: time.Now().UnixMilli(), Value: 1.0,
+			},
+		}, []*mimirpb.Exemplar{
+			nil,
+		})
+
+	_, err = client.Push(ctx, wr)
+	return err
+}
---
+++ tools/grpcurl-push-ingesters/util.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// processPortForwarded handles port-forwarding, HTTP request, and cleanup for a single pod
+func processPortForwarded(pod string, namespace string, podPort int, process func(pod string, localPort int)) error {
+	localPort, err := getFreePort()
+	if err != nil {
+		return fmt.Errorf("failed to get free port: %v", err)
+	}
+
+	// Start port-forwarding
+	cmd := exec.Command("kubectl", "port-forward", "--namespace", namespace,
+		fmt.Sprintf("pod/%s", pod), fmt.Sprintf("%d:%d", localPort, podPort))
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("failed to get stdout pipe: %v", err)
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("failed to get stderr pipe: %v", err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("failed to start kubectl port-forward: %v", err)
+	}
+
+	defer cmd.Process.Kill()
+
+	// Wait for port-forward to be ready
+	if err := waitForPortForward(stdout, stderr); err != nil {
+		return fmt.Errorf("port-forward failed: %v", err)
+	}
+
+	process(pod, localPort)
+
+	// Stop the port-forward
+	if err := cmd.Process.Kill(); err != nil {
+		return fmt.Errorf("failed to kill port-forward process: %v", err)
+	}
+
+	return nil
+}
+
+// getFreePort finds an available TCP port
+func getFreePort() (int, error) {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		return 0, fmt.Errorf("resolve tcp addr failed: %v", err)
+	}
+
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return 0, fmt.Errorf("tcp listen failed: %v", err)
+	}
+	defer l.Close()
+	return l.Addr().(*net.TCPAddr).Port, nil
+}
+
+// waitForPortForward waits until port-forward is ready
+func waitForPortForward(stdout, stderr io.ReadCloser) error {
+	done := make(chan error)
+	go func() {
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			line := scanner.Text()
+			log.Printf("DEBUG STDERR: %s", line)
+			if line != "" {
+				done <- fmt.Errorf("port-forward error: %s", line)
+				return
+			}
+		}
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			line := scanner.Text()
+			if line != "" {
+				if strings.Contains(line, "Forwarding from") {
+					done <- nil
+					return
+				} else {
+					log.Printf("DEBUG STDOUT: %s", line)
+				}
+			}
+		}
+	}()
+
+	select {
+	case err := <-done:
+		return err
+	case <-time.After(15 * time.Second):
+		return fmt.Errorf("timeout waiting for port-forward")
+	}
+}
+
+func generatePodIDs(template string, zones []string, ids []string) []string {
+	var permutations []string
+
+	for _, zone := range zones {
+		for _, id := range ids {
+			replacement := strings.Replace(template, "<zone>", zone, -1)
+			replacement = strings.Replace(replacement, "<id>", id, -1)
+			permutations = append(permutations, replacement)
+		}
+	}
+
+	return permutations
+}
+
+func generatePodIDsWithinRange(template string, zones []string, minID, maxID int) []string {
+	// Generate the numeric IDs as strings
+	var ids []string
+	for i := minID; i <= maxID; i++ {
+		ids = append(ids, strconv.Itoa(i))
+	}
+
+	return generatePodIDs(template, zones, ids)
+}
