97792887ee044052f50c8d0beca7c6c5a640cdbb
Jon Kartago Lamida <me@lamida.net>
2025-01-27 22:37:53 +0800

    Remove println
    Signed-off-by: Jon Kartago Lamida <me@lamida.net>

--- pkg/streamingpromql/query.go
+++ pkg/streamingpromql/query.go
@@ -208,9 +208,6 @@ func (q *Query) convertToInstantVectorOperator(expr parser.Expr, timeRange types
 			e.PosRange,
 		)
 	case *parser.Call:
-		if e.Func.Name == "absent" {
-			fmt.Println("here")
-		}
 		return q.convertFunctionCallToInstantVectorOperator(e, timeRange)
 	case *parser.BinaryExpr:
 		// We only need to handle three combinations of types here:
