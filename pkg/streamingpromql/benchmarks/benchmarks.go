0a75e52d5ec7917e3a2a7f66ec4750f231cee1ff
Charles Korn <charles.korn@grafana.com>
2025-02-05 14:17:20 +1100

    Enable benchmark

--- pkg/streamingpromql/benchmarks/benchmarks.go
+++ pkg/streamingpromql/benchmarks/benchmarks.go
@@ -255,12 +255,12 @@ func TestCases(metricSizes []int) []BenchCase {
 		//	Expr: "count_values('value', h_X)",
 		//  Steps: 100,
 		//},
-		//{
-		//	Expr: "topk(1, a_X)",
-		//},
-		//{
-		//	Expr: "topk(5, a_X)",
-		//},
+		{
+			Expr: "topk(1, a_X)",
+		},
+		{
+			Expr: "topk(5, a_X)",
+		},
 		// Combinations.
 		{
 			Expr: "rate(a_X[1m]) + rate(b_X[1m])",
