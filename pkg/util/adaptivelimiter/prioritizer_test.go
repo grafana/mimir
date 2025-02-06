f914c6c3f97f223d23325af545d6602c0113a47e
Jonathan Halterman <jonathan@grafana.com>
2025-02-06 12:44:05 -0800

    Add test for computeRejectionRate

--- pkg/util/adaptivelimiter/prioritizer_test.go
+++ pkg/util/adaptivelimiter/prioritizer_test.go
@@ -52,3 +52,56 @@ func TestPrioritizer_Calibrate(t *testing.T) {
 	require.Equal(t, .5, limiter.RejectionRate())
 	require.True(t, p.priorityThreshold.Load() > 0 && p.priorityThreshold.Load() < 200, "low priority execution should be rejected")
 }
+
+func TestComputeRejectionRate(t *testing.T) {
+	tests := []struct {
+		name               string
+		queueSize          int
+		rejectionThreshold int
+		maxQueueSize       int
+		expectedRate       float64
+	}{
+		{
+			name:               "queueSize below rejectionThreshold",
+			queueSize:          50,
+			rejectionThreshold: 60,
+			maxQueueSize:       100,
+			expectedRate:       0,
+		},
+		{
+			name:               "queueSize equal to rejectionThreshold",
+			queueSize:          60,
+			rejectionThreshold: 60,
+			maxQueueSize:       100,
+			expectedRate:       0,
+		},
+		{
+			name:               "queueSize between rejectionThreshold and maxQueueSize",
+			queueSize:          80,
+			rejectionThreshold: 60,
+			maxQueueSize:       100,
+			expectedRate:       .5,
+		},
+		{
+			name:               "queue size equal to maxQueueSize",
+			queueSize:          100,
+			rejectionThreshold: 60,
+			maxQueueSize:       100,
+			expectedRate:       1,
+		},
+		{
+			name:               "queueSize above maxQueueSize",
+			queueSize:          120,
+			rejectionThreshold: 60,
+			maxQueueSize:       100,
+			expectedRate:       1,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			rate := computeRejectionRate(tc.queueSize, tc.rejectionThreshold, tc.maxQueueSize)
+			require.Equal(t, tc.expectedRate, rate)
+		})
+	}
+}
