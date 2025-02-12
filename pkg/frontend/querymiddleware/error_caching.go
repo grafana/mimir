grafana/mimir

commit ce9463ed2d6c864b1bc7f327471f38f746d05fc5

Fix instance where context cancellation error was cached (#10537)
* Fix instance where context cancellation error was cached

This change fixes the error mapping done in the query sharding
middleware to correctly map all possible errors from the PromQL
engine to an appropriate APIError type. This fixes an issue where
`context.Canceled` was treated as an execution error and cached
for subsequent requests.

As part of this, instrumentation for the error caching middleware
was improved and Jaeger tracing in various Docker compose setups
was fixed by pinning the image used.

Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

* Changelog

Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

---------

Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

pkg/frontend/querymiddleware/error_caching.go
@@ -12,6 +12,7 @@ import (
 	"github.com/gogo/protobuf/proto"
 	"github.com/grafana/dskit/cache"
 	"github.com/grafana/dskit/tenant"
+	"github.com/grafana/dskit/tracing"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 
@@ -85,7 +86,7 @@ func (e *errorCachingHandler) Do(ctx context.Context, request MetricsQueryReques
 		return e.next.Do(ctx, request)
 	}
 
-	e.cacheLoadAttempted.Inc()
+	addWithExemplar(ctx, e.cacheLoadAttempted, 1)
 	key := e.keyGen.QueryRequestError(ctx, tenant.JoinTenantIDs(tenantIDs), request)
 	hashedKey := cacheHashKey(key)
 
@@ -103,7 +104,7 @@ func (e *errorCachingHandler) Do(ctx context.Context, request MetricsQueryReques
 
 	res, err := e.next.Do(ctx, request)
 	if err != nil {
-		e.cacheStoreAttempted.Inc()
+		addWithExemplar(ctx, e.cacheStoreAttempted, 1)
 
 		var apiErr *apierror.APIError
 		if !errors.As(err, &apiErr) {
@@ -123,6 +124,14 @@ func (e *errorCachingHandler) Do(ctx context.Context, request MetricsQueryReques
 
 		ttl := validation.MinDurationPerTenant(tenantIDs, e.limits.ResultsCacheTTLForErrors)
 		e.storeErrorToCache(key, hashedKey, ttl, apiErr, spanLog)
+
+		spanLog.DebugLog(
+			"msg", "stored API error to cache",
+			"key", key,
+			"hashed_key", hashedKey,
+			"error_type", apiErr.Type,
+			"error_message", apiErr.Message,
+		)
 	}
 
 	return res, err
@@ -163,7 +172,14 @@ func (e *errorCachingHandler) storeErrorToCache(key, hashedKey string, ttl time.
 	})
 
 	if err != nil {
-		level.Warn(spanLog).Log("msg", "unable to marshal cached error", "err", err)
+		level.Warn(spanLog).Log(
+			"msg", "unable to marshal cached error",
+			"key", key,
+			"hashed_key", hashedKey,
+			"error_type", apiErr.Type,
+			"error_message", apiErr.Message,
+			"err", err,
+		)
 		return
 	}
 
@@ -176,4 +192,11 @@ func (e *errorCachingHandler) isCacheable(apiErr *apierror.APIError) (bool, stri
 	}
 
 	return true, ""
+}
+
+func addWithExemplar(ctx context.Context, counter prometheus.Counter, val float64) {
+	// TODO: Add this to dskit as instrument.AddWithExemplar just like instrument.ObserveWithExemplar
+	if traceID, traceOK := tracing.ExtractSampledTraceID(ctx); traceOK {
+		counter.(prometheus.ExemplarAdder).AddWithExemplar(val, prometheus.Labels{"trace_id": traceID, "traceID": traceID})
+	}
 }

development/mimir-ingest-storage/docker-compose.jsonnet
@@ -253,7 +253,8 @@ std.manifestYamlDoc({
 
   jaeger:: {
     jaeger: {
-      image: 'jaegertracing/all-in-one',
+      // Use 1.62 specifically since 1.63 removes the agent which we depend on for now.
+      image: 'jaegertracing/all-in-one:1.62.0',
       ports: ['16686:16686', '14268'],
     },
   },

development/mimir-ingest-storage/docker-compose.yml
@@ -24,7 +24,7 @@
     "volumes":
       - "./config:/etc/agent-config"
   "jaeger":
-    "image": "jaegertracing/all-in-one"
+    "image": "jaegertracing/all-in-one:1.62.0"
     "ports":
       - "16686:16686"
       - "14268"

development/mimir-microservices-mode/docker-compose.jsonnet
@@ -418,7 +418,8 @@ std.manifestYamlDoc({
 
   jaeger:: {
     jaeger: {
-      image: 'jaegertracing/all-in-one',
+      // Use 1.62 specifically since 1.63 removes the agent which we depend on for now.
+      image: 'jaegertracing/all-in-one:1.62.0',
       ports: ['16686:16686', '14268'],
     },
   },

development/mimir-microservices-mode/docker-compose.yml
@@ -282,7 +282,7 @@
       - "./activity:/activity"
       - ".data-ingester-3:/tmp/mimir-tsdb-ingester:delegated"
   "jaeger":
-    "image": "jaegertracing/all-in-one"
+    "image": "jaegertracing/all-in-one:1.62.0"
     "ports":
       - "16686:16686"
       - "14268"

development/mimir-monolithic-mode-with-swift-storage/docker-compose.yml
@@ -50,7 +50,8 @@ services:
       - 9091:9091
 
   jaeger:
-    image: jaegertracing/all-in-one
+    # Use 1.62 specifically since 1.63 removes the agent which we depend on for now.
+    image: jaegertracing/all-in-one:1.62.0
     ports:
       - 16686:16686
       - "14268"

development/mimir-monolithic-mode/docker-compose.yml
@@ -71,7 +71,8 @@ services:
       - 9092:9092
 
   jaeger:
-    image: jaegertracing/all-in-one
+    # Use 1.62 specifically since 1.63 removes the agent which we depend on for now.
+    image: jaegertracing/all-in-one:1.62.0
     ports:
       - 16681:16686
       - "14268"

pkg/frontend/querymiddleware/querysharding.go
@@ -254,6 +254,13 @@ func mapEngineError(err error) error {
 		cause = err
 	}
 
+	// The engine sometimes returns context.Canceled without mapping it to one of the expected
+	// error types. Handle that specially here since we rely on the error type for errors being
+	// accurate.
+	if errors.Is(cause, context.Canceled) {
+		return apierror.New(apierror.TypeCanceled, cause.Error())
+	}
+
 	// By default, all errors returned by engine.Eval() are execution errors,
 	// This is the same as Prometheus API does: http://github.com/prometheus/prometheus/blob/076109fa1910ad2198bf2c447a174fee31114982/web/api/v1/api.go#L550-L550
 	errorType := apierror.TypeExec

commit 41ce274411f14c0a498c3f908eb91eed0bdb2807

fix: increment error caching counter when trace not sampled
Fixes an issue introduced in #10537 where a counter was only incremented
when a particular trace was sampled instead of always.

Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

pkg/frontend/querymiddleware/error_caching.go
@@ -195,8 +195,10 @@ func (e *errorCachingHandler) isCacheable(apiErr *apierror.APIError) (bool, stri
 }
 
 func addWithExemplar(ctx context.Context, counter prometheus.Counter, val float64) {
-	// TODO: Add this to dskit as instrument.AddWithExemplar just like instrument.ObserveWithExemplar
 	if traceID, traceOK := tracing.ExtractSampledTraceID(ctx); traceOK {
 		counter.(prometheus.ExemplarAdder).AddWithExemplar(val, prometheus.Labels{"trace_id": traceID, "traceID": traceID})
+	} else {
+		// If there is no trace ID, just add to the counter.
+		counter.Add(val)
 	}
 }