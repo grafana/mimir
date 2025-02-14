d12511c72d196343e9beda073010f4733da06fdc
Charles Korn <charleskorn@users.noreply.github.com>
2025-02-14 11:36:33 +1100

    Vendor latest mimir-prometheus (#10645)

--- vendor/github.com/prometheus/prometheus/tsdb/block.go
+++ vendor/github.com/prometheus/prometheus/tsdb/block.go
@@ -238,7 +238,7 @@ type BlockMetaCompaction struct {
 }
 
 func (bm *BlockMetaCompaction) SetOutOfOrder() {
-	if bm.containsHint(CompactionHintFromOutOfOrder) {
+	if bm.FromOutOfOrder() {
 		return
 	}
 	bm.Hints = append(bm.Hints, CompactionHintFromOutOfOrder)
@@ -246,16 +246,7 @@ func (bm *BlockMetaCompaction) SetOutOfOrder() {
 }
 
 func (bm *BlockMetaCompaction) FromOutOfOrder() bool {
-	return bm.containsHint(CompactionHintFromOutOfOrder)
-}
-
-func (bm *BlockMetaCompaction) containsHint(hint string) bool {
-	for _, h := range bm.Hints {
-		if h == hint {
-			return true
-		}
-	}
-	return false
+	return slices.Contains(bm.Hints, CompactionHintFromOutOfOrder)
 }
 
 const (
