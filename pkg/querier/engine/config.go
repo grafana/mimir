f603de24cf413a66c8e8996bc52322dac5b14c02
Nick Pillitteri <nick.pillitteri@grafana.com>
2025-02-14 18:01:13 -0500

    Simplify how experimental PromQL functions are enabled
    Remove experimental configuration flags
    `querier.promql-experimental-functions-enabled` and
    `query-frontend.block-promql-experimental-functions` used for
    controlling access to experimental PromQL features. Instead enable
    experimental PromQL functions at the engine level by default, block
    access to them by default, and selectively enable them at a per-user
    level using the middleware introduced in #9798.
    
    Justification for this change:
    * There are currently 3 different settings that need to changed to
      allow customers to make use experimental functions in PromQL that
      need to be applied 3 different places (queriers, query-frontend, and
      per-tenant limits).
    * The performance impact from always enabling but blocking via
      middleware experimental PromQL functions (what this PR does) is
      so small that it does not show up in profiling from what I can
      tell.
    
    Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

--- pkg/querier/engine/config.go
+++ pkg/querier/engine/config.go
@@ -31,8 +31,6 @@ type Config struct {
 	// series is considered stale.
 	LookbackDelta time.Duration `yaml:"lookback_delta" category:"advanced"`
 
-	PromQLExperimentalFunctionsEnabled bool `yaml:"promql_experimental_functions_enabled" category:"experimental"`
-
 	MimirQueryEngine streamingpromql.Features `yaml:"mimir_query_engine" category:"experimental"`
 }
 
@@ -50,14 +48,12 @@ func (cfg *Config) RegisterFlags(f *flag.FlagSet) {
 	f.IntVar(&cfg.MaxSamples, "querier.max-samples", 50e6, sharedWithQueryFrontend("Maximum number of samples a single query can load into memory."))
 	f.DurationVar(&cfg.DefaultEvaluationInterval, "querier.default-evaluation-interval", time.Minute, sharedWithQueryFrontend("The default evaluation interval or step size for subqueries."))
 	f.DurationVar(&cfg.LookbackDelta, "querier.lookback-delta", 5*time.Minute, sharedWithQueryFrontend("Time since the last sample after which a time series is considered stale and ignored by expression evaluations."))
-	f.BoolVar(&cfg.PromQLExperimentalFunctionsEnabled, "querier.promql-experimental-functions-enabled", false, sharedWithQueryFrontend("Enable experimental PromQL functions."))
 
 	cfg.MimirQueryEngine.RegisterFlags(f)
 }
 
-// NewPromQLEngineOptions returns the PromQL engine options based on the provided config and a boolean
-// to indicate whether the experimental PromQL functions should be enabled.
-func NewPromQLEngineOptions(cfg Config, activityTracker *activitytracker.ActivityTracker, logger log.Logger, reg prometheus.Registerer) (promql.EngineOpts, streamingpromql.EngineOpts, bool) {
+// NewPromQLEngineOptions returns the PromQL engine options based on the provided config.
+func NewPromQLEngineOptions(cfg Config, activityTracker *activitytracker.ActivityTracker, logger log.Logger, reg prometheus.Registerer) (promql.EngineOpts, streamingpromql.EngineOpts) {
 	commonOpts := promql.EngineOpts{
 		Logger:               util_log.SlogFromGoKit(logger),
 		Reg:                  reg,
@@ -77,5 +73,5 @@ func NewPromQLEngineOptions(cfg Config, activityTracker *activitytracker.Activit
 		Features:   cfg.MimirQueryEngine,
 	}
 
-	return commonOpts, mqeOpts, cfg.PromQLExperimentalFunctionsEnabled
+	return commonOpts, mqeOpts
 }
