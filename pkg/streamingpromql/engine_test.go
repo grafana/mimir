c4b2c57876e271bbef3e7124bb4686b9fb55471a
Charles Korn <charles.korn@grafana.com>
2025-02-19 14:19:34 +1100

    Restore one `sum` over range vector function test case

--- pkg/streamingpromql/engine_test.go
+++ pkg/streamingpromql/engine_test.go
@@ -2977,6 +2977,7 @@ func TestCompareVariousMixedMetricsVectorSelectors(t *testing.T) {
 		for _, function := range []string{"rate", "increase", "changes", "resets", "deriv", "irate", "idelta", "delta", "deriv", "stddev_over_time", "stdvar_over_time"} {
 			expressions = append(expressions, fmt.Sprintf(`%s(series{label=~"(%s)"}[45s])`, function, labelRegex))
 			expressions = append(expressions, fmt.Sprintf(`%s(series{label=~"(%s)"}[1m])`, function, labelRegex))
+			expressions = append(expressions, fmt.Sprintf(`sum(%s(series{label=~"(%s)"}[2m15s]))`, function, labelRegex))
 		}
 
 		expressions = append(expressions, fmt.Sprintf(`predict_linear(series{label=~"(%s)"}[1m], 30)`, labelRegex))
