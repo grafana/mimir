c86963a119123161ca9f1deae538f8ac281b3a8a
Nikos Angelopoulos <nikolaos.angelopoulos@grafana.com>
2025-02-21 11:36:31 +0100

    feat: synchronize cache update with KV store modification.

--- pkg/distributor/ha_tracker.go
+++ pkg/distributor/ha_tracker.go
@@ -668,9 +668,7 @@ func (h *defaultHaTracker) updateKVStore(ctx context.Context, userID, cluster, r
 	// If cache is currently empty, add the data we either stored or received from KVStore
 	if err == nil && desc != nil {
 		h.electedLock.Lock()
-		if h.clusters[userID][cluster] == nil {
-			h.updateCache(userID, cluster, desc)
-		}
+		h.updateCache(userID, cluster, desc)
 		h.electedLock.Unlock()
 	}
 	return err
