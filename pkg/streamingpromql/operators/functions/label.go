410d110f9d53647f0d535e95d7f8e5276186bc34
Joshua Hesketh <joshua.hesketh@grafana.com>
2025-02-27 00:19:35 +1100

    MQE: Add support for label_join (#10587)
    * MQE: Add support for label_join
    
    * Add label_join to deduplicate and merge test
    
    * Add label_join to benchmark
    
    * Use string builder to generate new label

--- pkg/streamingpromql/operators/functions/label.go
+++ pkg/streamingpromql/operators/functions/label.go
@@ -8,6 +8,7 @@ package functions
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/grafana/regexp"
 	"github.com/prometheus/common/model"
@@ -17,6 +18,46 @@ import (
 	"github.com/grafana/mimir/pkg/streamingpromql/types"
 )
 
+func LabelJoinFactory(dstLabelOp, separatorOp types.StringOperator, srcLabelOps []types.StringOperator) SeriesMetadataFunction {
+	return func(seriesMetadata []types.SeriesMetadata, _ *limiting.MemoryConsumptionTracker) ([]types.SeriesMetadata, error) {
+		dst := dstLabelOp.GetValue()
+		if !model.LabelName(dst).IsValid() {
+			return nil, fmt.Errorf("invalid destination label name in label_join(): %s", dst)
+		}
+		separator := separatorOp.GetValue()
+		srcLabels := make([]string, len(srcLabelOps))
+		for i, op := range srcLabelOps {
+			src := op.GetValue()
+			if !model.LabelName(src).IsValid() {
+				return nil, fmt.Errorf("invalid source label name in label_join(): %s", dst)
+			}
+			srcLabels[i] = src
+		}
+
+		lb := labels.NewBuilder(labels.EmptyLabels())
+
+		var sb strings.Builder
+		for i := range seriesMetadata {
+			sb.Reset()
+
+			for j, srcLabel := range srcLabels {
+				if j > 0 {
+					sb.WriteString(separator)
+				}
+				// Get returns an empty string for missing labels, so this is safe and gives the desired output
+				// where a series may be missing a source label.
+				sb.WriteString(seriesMetadata[i].Labels.Get(srcLabel))
+			}
+
+			lb.Reset(seriesMetadata[i].Labels)
+			lb.Set(dst, sb.String())
+			seriesMetadata[i].Labels = lb.Labels()
+		}
+
+		return seriesMetadata, nil
+	}
+}
+
 func LabelReplaceFactory(dstLabelOp, replacementOp, srcLabelOp, regexOp types.StringOperator) SeriesMetadataFunction {
 	return func(seriesMetadata []types.SeriesMetadata, _ *limiting.MemoryConsumptionTracker) ([]types.SeriesMetadata, error) {
 		regexStr := regexOp.GetValue()
@@ -25,6 +66,7 @@ func LabelReplaceFactory(dstLabelOp, replacementOp, srcLabelOp, regexOp types.St
 			return nil, fmt.Errorf("invalid regular expression in label_replace(): %s", regexStr)
 		}
 		dst := dstLabelOp.GetValue()
+		// TODO(jhesketh): Use UTF-8 validation (model.LabelName(src).IsValid()) when https://github.com/prometheus/prometheus/pull/15974 is vendored in.
 		if !model.LabelNameRE.MatchString(dst) {
 			return nil, fmt.Errorf("invalid destination label name in label_replace(): %s", dst)
 		}
