1743f5d65716c442346f4fae83247e0af457b516
Yuri Nikolic <durica.nikolic@grafana.com>
2025-02-28 16:29:07 +0100

    Allow ingester clients to use middleware.ClusterUnaryClientInterceptor
    Signed-off-by: Yuri Nikolic <durica.nikolic@grafana.com>

---
+++ pkg/util/metrics.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+const (
+	clientLabel   = "client"
+	protocolLabel = "protocol"
+	methodLabel   = "method"
+
+	GRPCProtocol = "grpc"
+	HTTPProtocol = "http"
+)
+
+func NewRequestInvalidClusterVerficationLabelsTotalCounter(reg prometheus.Registerer, client string, protocol string) *prometheus.CounterVec {
+	return promauto.With(reg).NewCounterVec(prometheus.CounterOpts{
+		Name: "cortex_client_request_invalid_cluster_validation_labels_total",
+		Help: "Number of requests with invalid cluster validation label.",
+		ConstLabels: map[string]string{
+			clientLabel:   client,
+			protocolLabel: protocol,
+		},
+	}, []string{methodLabel})
+}
