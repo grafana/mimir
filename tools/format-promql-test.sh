a5dd34f1c6d83d023fecb6547bd08661be9e594e
Charles Korn <charles.korn@grafana.com>
2025-03-03 11:17:29 +1100

    Rename script, allow formatting multiple files at once

--- tools/format-promql-test.sh
+++ tools/format-promql-test.sh
@@ -3,10 +3,10 @@
 set -euo pipefail
 
 # Ensure all lines except load, eval and clear commands and comments are indented
-sed -E -i '' '/^(load|eval|clear|.*#)/! s/^[[:space:]]*/  /g' "$1"
+sed -E -i '' '/^(load|eval|clear|.*#)/! s/^[[:space:]]*/  /g' "$@"
 
 # Convert leading whitespace to two spaces
-sed -E -i '' 's/^[[:space:]]+/  /g' "$1"
+sed -E -i '' 's/^[[:space:]]+/  /g' "$@"
 
 # Strip trailing whitespace
-sed -E -i '' 's/[[:space:]]+$//g' "$1"
+sed -E -i '' 's/[[:space:]]+$//g' "$@"
