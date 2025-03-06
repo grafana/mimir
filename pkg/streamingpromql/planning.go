408706ebcb7e9f5f5c544db5cae9700cb4126215
Charles Korn <charles.korn@grafana.com>
2025-03-06 14:09:52 +1100

    Add analysis endpoint

--- pkg/streamingpromql/planning.go
+++ pkg/streamingpromql/planning.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"time"
 
+	jsoniter "github.com/json-iterator/go"
 	"github.com/prometheus/prometheus/model/timestamp"
 	"github.com/prometheus/prometheus/promql"
 	"github.com/prometheus/prometheus/promql/parser"
@@ -16,25 +17,79 @@ import (
 	"github.com/grafana/mimir/pkg/streamingpromql/types"
 )
 
-func (e *Engine) NewQueryPlan(ctx context.Context, qs string, timeRange types.QueryTimeRange) (*planning.QueryPlan, error) {
-	expr, err := parser.ParseExpr(qs)
+type PlanningObserver interface {
+	OnASTStageComplete(stageName string, updatedExpr parser.Expr, duration time.Duration)
+	OnAllASTStagesComplete(finalExpr parser.Expr)
+	OnPlanningStageComplete(stageName string, updatedPlan *planning.QueryPlan, duration time.Duration)
+	OnAllPlanningStagesComplete(finalPlan *planning.QueryPlan)
+}
+
+// TODO: timeout
+func (e *Engine) NewQueryPlan(ctx context.Context, qs string, timeRange types.QueryTimeRange, observer PlanningObserver) (*planning.QueryPlan, error) {
+	expr, err := runASTStage("Parsing", observer, func() (parser.Expr, error) { return parser.ParseExpr(qs) })
 	if err != nil {
 		return nil, err
 	}
 
-	expr = promql.PreprocessExpr(expr, timestamp.Time(timeRange.StartT), timestamp.Time(timeRange.EndT))
+	expr, err = runASTStage("Pre-processing", observer, func() (parser.Expr, error) {
+		return promql.PreprocessExpr(expr, timestamp.Time(timeRange.StartT), timestamp.Time(timeRange.EndT)), nil
+	})
 
-	root, err := e.nodeFromExpr(expr)
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO: apply optimisations
+	// TODO: apply AST optimisations
 
-	plan := &planning.QueryPlan{
-		TimeRange: timeRange,
-		Root:      root,
+	observer.OnAllASTStagesComplete(expr)
+
+	plan, err := runPlanningStage("Original plan", observer, func() (*planning.QueryPlan, error) {
+		root, err := e.nodeFromExpr(expr)
+		if err != nil {
+			return nil, err
+		}
+
+		plan := &planning.QueryPlan{
+			TimeRange: timeRange,
+			Root:      root,
+		}
+
+		return plan, nil
+	})
+
+	if err != nil {
+		return nil, err
 	}
+
+	// TODO: apply plan optimisations
+
+	observer.OnAllPlanningStagesComplete(plan)
+
+	return plan, err
+}
+
+func runASTStage(stageName string, observer PlanningObserver, stage func() (parser.Expr, error)) (parser.Expr, error) {
+	start := time.Now()
+	expr, err := stage()
+	if err != nil {
+		return nil, err
+	}
+
+	duration := time.Since(start)
+	observer.OnASTStageComplete(stageName, expr, duration)
+
+	return expr, nil
+}
+
+func runPlanningStage(stageName string, observer PlanningObserver, stage func() (*planning.QueryPlan, error)) (*planning.QueryPlan, error) {
+	start := time.Now()
+	plan, err := stage()
+	if err != nil {
+		return nil, err
+	}
+
+	duration := time.Since(start)
+	observer.OnPlanningStageComplete(stageName, plan, duration)
 
 	return plan, nil
 }
@@ -195,7 +250,106 @@ func (e *Engine) DecodeQueryPlan(data []byte) (*planning.QueryPlan, error) {
 	return plan, nil
 }
 
+// Materialize converts a query plan into an executable query.
 func (e *Engine) Materialize(ctx context.Context, plan *planning.QueryPlan, q storage.Queryable, opts promql.QueryOpts) (promql.Query, error) {
 	// TODO
 	panic("TODO")
+}
+
+type AnalysisResult struct {
+	OriginalExpression string               `json:"originalExpression"`
+	TimeRange          types.QueryTimeRange `json:"timeRange"`
+
+	ASTStages      []ASTStage      `json:"astStages"`
+	PlanningStages []PlanningStage `json:"planningStages"`
+}
+
+type ASTStage struct {
+	Name             string         `json:"name"`
+	Duration         *time.Duration `json:"duration"` // nil if this stage has no associated duration (eg. represents final AST)
+	OutputExpression string         `json:"outputExpression"`
+}
+
+type PlanningStage struct {
+	Name       string              `json:"name"`
+	Duration   *time.Duration      `json:"duration"`   // nil if this stage has no associated duration (eg. represents final plan)
+	OutputPlan jsoniter.RawMessage `json:"outputPlan"` // Store the encoded JSON so we don't have to deal with cloning the entire query plan each time.
+}
+
+// Analyze performs query planning and produces a report on the query planning process.
+func (e *Engine) Analyze(ctx context.Context, qs string, timeRange types.QueryTimeRange) (*AnalysisResult, error) {
+	observer := NewAnalysisPlanningObserver(qs, timeRange, e)
+	_, err := e.NewQueryPlan(ctx, qs, timeRange, observer)
+	if err != nil {
+		return nil, err
+	}
+
+	return observer.Result, nil
+}
+
+type NoopPlanningObserver struct{}
+
+func (n NoopPlanningObserver) OnASTStageComplete(stageName string, updatedExpr parser.Expr, duration time.Duration) {
+	// Nothing to do.
+}
+
+func (n NoopPlanningObserver) OnAllASTStagesComplete(finalExpr parser.Expr) {
+	// Nothing to do.
+}
+
+func (n NoopPlanningObserver) OnPlanningStageComplete(stageName string, updatedPlan *planning.QueryPlan, duration time.Duration) {
+	// Nothing to do.
+}
+
+func (n NoopPlanningObserver) OnAllPlanningStagesComplete(finalPlan *planning.QueryPlan) {
+	// Nothing to do.
+}
+
+type AnalysisPlanningObserver struct {
+	Result *AnalysisResult
+	Engine *Engine
+}
+
+func NewAnalysisPlanningObserver(expr string, timeRange types.QueryTimeRange, engine *Engine) *AnalysisPlanningObserver {
+	return &AnalysisPlanningObserver{
+		Result: &AnalysisResult{
+			OriginalExpression: expr,
+			TimeRange:          timeRange,
+		},
+		Engine: engine,
+	}
+}
+
+func (o *AnalysisPlanningObserver) OnASTStageComplete(stageName string, updatedExpr parser.Expr, duration time.Duration) {
+	o.Result.ASTStages = append(o.Result.ASTStages, ASTStage{
+		Name:             stageName,
+		Duration:         &duration,
+		OutputExpression: updatedExpr.Pretty(0),
+	})
+}
+
+func (o *AnalysisPlanningObserver) OnAllASTStagesComplete(finalExpr parser.Expr) {
+	o.Result.ASTStages = append(o.Result.ASTStages, ASTStage{
+		Name:             "Final expression",
+		OutputExpression: finalExpr.Pretty(0),
+	})
+}
+
+func (o *AnalysisPlanningObserver) OnPlanningStageComplete(stageName string, updatedPlan *planning.QueryPlan, duration time.Duration) {
+	plan, _ := o.Engine.EncodeQueryPlan(updatedPlan) // TODO: what to do if encoding fails?
+
+	o.Result.PlanningStages = append(o.Result.PlanningStages, PlanningStage{
+		Name:       stageName,
+		Duration:   &duration,
+		OutputPlan: plan,
+	})
+}
+
+func (o *AnalysisPlanningObserver) OnAllPlanningStagesComplete(finalPlan *planning.QueryPlan) {
+	plan, _ := o.Engine.EncodeQueryPlan(finalPlan) // TODO: what to do if encoding fails?
+
+	o.Result.PlanningStages = append(o.Result.PlanningStages, PlanningStage{
+		Name:       "Final plan",
+		OutputPlan: plan,
+	})
 }
