61ae4d5bf3d8a24522dbad1d9845be96de7152e2
Ying WANG <ying.wang@grafana.com>
2025-03-14 16:58:06 +0100

    Ingester: export native histogram and buckets gauge metrics for cost attribution

--- pkg/costattribution/active_tracker.go
+++ pkg/costattribution/active_tracker.go
@@ -16,10 +16,18 @@ import (
 	"go.uber.org/atomic"
 )
 
+type asMetrics struct {
+	asCount     *atomic.Int64
+	nhCount     *atomic.Int64
+	nhBucketNum *atomic.Int64
+}
+
 type ActiveSeriesTracker struct {
-	userID                         string
-	activeSeriesPerUserAttribution *prometheus.Desc
-	logger                         log.Logger
+	userID                                         string
+	activeSeriesPerUserAttribution                 *prometheus.Desc
+	activeNativeHistogramSeriesPerUserAttribution  *prometheus.Desc
+	activeNativeHistogramBucketsPerUserAttribution *prometheus.Desc
+	logger                                         log.Logger
 
 	labels         []string
 	overflowLabels []string
@@ -28,10 +36,10 @@ type ActiveSeriesTracker struct {
 	cooldownDuration time.Duration
 
 	observedMtx   sync.RWMutex
-	observed      map[string]*atomic.Int64
+	observed      map[string]asMetrics
 	overflowSince time.Time
 
-	overflowCounter atomic.Int64
+	overflowCounter asMetrics
 }
 
 func NewActiveSeriesTracker(userID string, trackedLabels []string, limit int, cooldownDuration time.Duration, logger log.Logger) *ActiveSeriesTracker {
@@ -48,7 +56,7 @@ func NewActiveSeriesTracker(userID string, trackedLabels []string, limit int, co
 		userID:           userID,
 		labels:           trackedLabels,
 		maxCardinality:   limit,
-		observed:         make(map[string]*atomic.Int64),
+		observed:         make(map[string]asMetrics),
 		logger:           logger,
 		overflowLabels:   overflowLabels,
 		cooldownDuration: cooldownDuration,
@@ -60,7 +68,17 @@ func NewActiveSeriesTracker(userID string, trackedLabels []string, limit int, co
 	ast.activeSeriesPerUserAttribution = prometheus.NewDesc("cortex_ingester_attributed_active_series",
 		"The total number of active series per user and attribution.", variableLabels[:len(variableLabels)-1],
 		prometheus.Labels{trackerLabel: defaultTrackerName})
-
+	ast.activeNativeHistogramSeriesPerUserAttribution = prometheus.NewDesc("cortex_ingester_attributed_active_native_histogram_series",
+		"The total number of active native histogram series per user and attribution.", variableLabels[:len(variableLabels)-1],
+		prometheus.Labels{trackerLabel: defaultTrackerName})
+	ast.activeNativeHistogramBucketsPerUserAttribution = prometheus.NewDesc("cortex_ingester_attributed_active_native_histogram_buckets",
+		"The total number of active native histogram buckets per user and attribution.", variableLabels[:len(variableLabels)-1],
+		prometheus.Labels{trackerLabel: defaultTrackerName})
+	ast.overflowCounter = asMetrics{
+		asCount:     atomic.NewInt64(0),
+		nhCount:     atomic.NewInt64(0),
+		nhBucketNum: atomic.NewInt64(0),
+	}
 	return ast
 }
 
@@ -68,7 +86,10 @@ func (at *ActiveSeriesTracker) hasSameLabels(labels []string) bool {
 	return slices.Equal(at.labels, labels)
 }
 
-func (at *ActiveSeriesTracker) Increment(lbls labels.Labels, now time.Time) {
+// Increment increases the active series count for the given labels.
+// If nativeHistogramBucketNum is not -1, it also increments the native histogram counter and the corresponding bucket.
+// Otherwise, only the active series count is updated.
+func (at *ActiveSeriesTracker) Increment(lbls labels.Labels, now time.Time, nativeHistogramBucketNum int) {
 	if at == nil {
 		return
 	}
@@ -80,27 +101,43 @@ func (at *ActiveSeriesTracker) Increment(lbls labels.Labels, now time.Time) {
 	at.observedMtx.RLock()
 	as, ok := at.observed[string(buf.Bytes())]
 	if ok {
-		as.Inc()
+		as.asCount.Inc()
+		if nativeHistogramBucketNum >= 0 {
+			as.nhCount.Inc()
+			as.nhBucketNum.Add(int64(nativeHistogramBucketNum))
+		}
 		at.observedMtx.RUnlock()
 		return
 	}
 
 	if !at.overflowSince.IsZero() {
 		at.observedMtx.RUnlock()
-		at.overflowCounter.Inc()
+		at.overflowCounter.asCount.Inc()
+		if nativeHistogramBucketNum >= 0 {
+			at.overflowCounter.nhCount.Inc()
+			at.overflowCounter.nhBucketNum.Add(int64(nativeHistogramBucketNum))
+		}
 		return
 	}
 
 	as, ok = at.observed[string(buf.Bytes())]
 	if ok {
-		as.Inc()
+		as.asCount.Inc()
+		if nativeHistogramBucketNum >= 0 {
+			as.nhCount.Inc()
+			as.nhBucketNum.Add(int64(nativeHistogramBucketNum))
+		}
 		at.observedMtx.RUnlock()
 		return
 	}
 
 	if !at.overflowSince.IsZero() {
 		at.observedMtx.RUnlock()
-		at.overflowCounter.Inc()
+		at.overflowCounter.asCount.Inc()
+		if nativeHistogramBucketNum >= 0 {
+			at.overflowCounter.nhCount.Inc()
+			at.overflowCounter.nhBucketNum.Add(int64(nativeHistogramBucketNum))
+		}
 		return
 	}
 	at.observedMtx.RUnlock()
@@ -110,24 +147,47 @@ func (at *ActiveSeriesTracker) Increment(lbls labels.Labels, now time.Time) {
 
 	as, ok = at.observed[string(buf.Bytes())]
 	if ok {
-		as.Inc()
+		as.asCount.Inc()
+		if nativeHistogramBucketNum >= 0 {
+			as.nhCount.Inc()
+			as.nhBucketNum.Add(int64(nativeHistogramBucketNum))
+		}
 		return
 	}
 
 	if !at.overflowSince.IsZero() {
-		at.overflowCounter.Inc()
+		at.overflowCounter.asCount.Inc()
+		if nativeHistogramBucketNum >= 0 {
+			at.overflowCounter.nhCount.Inc()
+			at.overflowCounter.nhBucketNum.Add(int64(nativeHistogramBucketNum))
+		}
 		return
 	}
 
 	if len(at.observed) >= at.maxCardinality {
 		at.overflowSince = now
-		at.overflowCounter.Inc()
+		at.overflowCounter.asCount.Inc()
+		if nativeHistogramBucketNum >= 0 {
+			at.overflowCounter.nhCount.Inc()
+			at.overflowCounter.nhBucketNum.Add(int64(nativeHistogramBucketNum))
+		}
 		return
 	}
-	at.observed[string(buf.Bytes())] = atomic.NewInt64(1)
+
+	nhCount := atomic.NewInt64(0)
+	nhBucketNum := atomic.NewInt64(0)
+	if nativeHistogramBucketNum >= 0 {
+		nhCount.Inc()
+		nhBucketNum.Add(int64(nativeHistogramBucketNum))
+	}
+	at.observed[string(buf.Bytes())] = asMetrics{
+		asCount:     atomic.NewInt64(1),
+		nhCount:     nhCount,
+		nhBucketNum: nhBucketNum,
+	}
 }
 
-func (at *ActiveSeriesTracker) Decrement(lbls labels.Labels) {
+func (at *ActiveSeriesTracker) Decrement(lbls labels.Labels, nativeHistogramBucketNum int) {
 	if at == nil {
 		return
 	}
@@ -138,7 +198,11 @@ func (at *ActiveSeriesTracker) Decrement(lbls labels.Labels) {
 	at.observedMtx.RLock()
 	as, ok := at.observed[string(buf.Bytes())]
 	if ok {
-		nv := as.Dec()
+		nv := as.asCount.Dec()
+		if nativeHistogramBucketNum >= 0 && as.nhCount.Load() > 0 {
+			as.nhCount.Dec()
+			as.nhBucketNum.Sub(int64(nativeHistogramBucketNum))
+		}
 		if nv > 0 {
 			at.observedMtx.RUnlock()
 			return
@@ -146,7 +210,7 @@ func (at *ActiveSeriesTracker) Decrement(lbls labels.Labels) {
 		at.observedMtx.RUnlock()
 		at.observedMtx.Lock()
 		as, ok := at.observed[string(buf.Bytes())]
-		if ok && as.Load() == 0 {
+		if ok && as.asCount.Load() == 0 {
 			// use buf.String() instead of string(buf.Bytes()) to fix the lint issue
 			delete(at.observed, buf.String())
 		}
@@ -159,7 +223,11 @@ func (at *ActiveSeriesTracker) Decrement(lbls labels.Labels) {
 	defer at.observedMtx.RUnlock()
 
 	if !at.overflowSince.IsZero() {
-		at.overflowCounter.Dec()
+		at.overflowCounter.asCount.Dec()
+		if nativeHistogramBucketNum >= 0 && at.overflowCounter.nhCount.Load() > 0 {
+			at.overflowCounter.nhCount.Dec()
+			at.overflowCounter.nhBucketNum.Sub(int64(nativeHistogramBucketNum))
+		}
 		return
 	}
 	panic(fmt.Errorf("decrementing non-existent active series: labels=%v, cost attribution keys: %v, the current observation map length: %d, the current cost attribution key: %s", lbls, at.labels, len(at.observed), buf.String()))
@@ -169,11 +237,17 @@ func (at *ActiveSeriesTracker) Collect(out chan<- prometheus.Metric) {
 	at.observedMtx.RLock()
 	if !at.overflowSince.IsZero() {
 		var activeSeries int64
+		var nativeHistogram int64
+		var nhBucketNum int64
 		for _, as := range at.observed {
-			activeSeries += as.Load()
+			activeSeries += as.asCount.Load()
+			nativeHistogram += as.nhCount.Load()
+			nhBucketNum += as.nhBucketNum.Load()
 		}
 		at.observedMtx.RUnlock()
-		out <- prometheus.MustNewConstMetric(at.activeSeriesPerUserAttribution, prometheus.GaugeValue, float64(activeSeries+at.overflowCounter.Load()), at.overflowLabels[:len(at.overflowLabels)-1]...)
+		out <- prometheus.MustNewConstMetric(at.activeSeriesPerUserAttribution, prometheus.GaugeValue, float64(activeSeries+at.overflowCounter.asCount.Load()), at.overflowLabels[:len(at.overflowLabels)-1]...)
+		out <- prometheus.MustNewConstMetric(at.activeNativeHistogramSeriesPerUserAttribution, prometheus.GaugeValue, float64(nativeHistogram+at.overflowCounter.nhCount.Load()), at.overflowLabels[:len(at.overflowLabels)-1]...)
+		out <- prometheus.MustNewConstMetric(at.activeNativeHistogramBucketsPerUserAttribution, prometheus.GaugeValue, float64(nhBucketNum+at.overflowCounter.nhBucketNum.Load()), at.overflowLabels[:len(at.overflowLabels)-1]...)
 		return
 	}
 	// We don't know the performance of out receiver, so we don't want to hold the lock for too long
@@ -181,7 +255,11 @@ func (at *ActiveSeriesTracker) Collect(out chan<- prometheus.Metric) {
 	for key, as := range at.observed {
 		keys := strings.Split(key, string(sep))
 		keys = append(keys, at.userID)
-		prometheusMetrics = append(prometheusMetrics, prometheus.MustNewConstMetric(at.activeSeriesPerUserAttribution, prometheus.GaugeValue, float64(as.Load()), keys...))
+		prometheusMetrics = append(prometheusMetrics,
+			prometheus.MustNewConstMetric(at.activeSeriesPerUserAttribution, prometheus.GaugeValue, float64(as.asCount.Load()), keys...),
+			prometheus.MustNewConstMetric(at.activeNativeHistogramSeriesPerUserAttribution, prometheus.GaugeValue, float64(as.nhCount.Load()), keys...),
+			prometheus.MustNewConstMetric(at.activeNativeHistogramBucketsPerUserAttribution, prometheus.GaugeValue, float64(as.nhBucketNum.Load()), keys...),
+		)
 	}
 	at.observedMtx.RUnlock()
 
--- pkg/costattribution/manager_test.go
+++ pkg/costattribution/manager_test.go
@@ -56,7 +56,7 @@ func TestManager_CreateDeleteTracker(t *testing.T) {
 		manager.SampleTracker("user1").IncrementDiscardedSamples([]mimirpb.LabelAdapter{{Name: "team", Value: "bar"}}, 1, "invalid-metrics-name", time.Unix(6, 0))
 		manager.SampleTracker("user1").IncrementDiscardedSamples([]mimirpb.LabelAdapter{{Name: "team", Value: "foo"}}, 1, "invalid-metrics-name", time.Unix(12, 0))
 		manager.SampleTracker("user3").IncrementReceivedSamples(testutils.CreateRequest([]testutils.Series{{LabelValues: []string{"department", "foo", "service", "dodo"}, SamplesCount: 1}}), time.Unix(20, 0))
-		manager.ActiveSeriesTracker("user1").Increment(labels.FromStrings("team", "bar"), time.Unix(10, 0))
+		manager.ActiveSeriesTracker("user1").Increment(labels.FromStrings("team", "bar"), time.Unix(10, 0), 50)
 		expectedMetrics := `
 		# HELP cortex_discarded_attributed_samples_total The total number of samples that were discarded per attribution.
 		# TYPE cortex_discarded_attributed_samples_total counter
@@ -65,11 +65,24 @@ func TestManager_CreateDeleteTracker(t *testing.T) {
 		# HELP cortex_distributor_received_attributed_samples_total The total number of samples that were received per attribution.
 		# TYPE cortex_distributor_received_attributed_samples_total counter
 		cortex_distributor_received_attributed_samples_total{department="foo",service="dodo",tenant="user3",tracker="cost-attribution"} 1
+		# HELP cortex_ingester_attributed_active_native_histogram_buckets The total number of active native histogram buckets per user and attribution.
+        # TYPE cortex_ingester_attributed_active_native_histogram_buckets gauge
+        cortex_ingester_attributed_active_native_histogram_buckets{team="bar",tenant="user1",tracker="cost-attribution"} 50
+        # HELP cortex_ingester_attributed_active_native_histogram_series The total number of active native histogram series per user and attribution.
+        # TYPE cortex_ingester_attributed_active_native_histogram_series gauge
+        cortex_ingester_attributed_active_native_histogram_series{team="bar",tenant="user1",tracker="cost-attribution"} 1
 		# HELP cortex_ingester_attributed_active_series The total number of active series per user and attribution.
         # TYPE cortex_ingester_attributed_active_series gauge
         cortex_ingester_attributed_active_series{team="bar",tenant="user1",tracker="cost-attribution"} 1
 		`
-		assert.NoError(t, testutil.GatherAndCompare(manager.reg, strings.NewReader(expectedMetrics), "cortex_discarded_attributed_samples_total", "cortex_distributor_received_attributed_samples_total", "cortex_ingester_attributed_active_series"))
+		assert.NoError(t, testutil.GatherAndCompare(manager.reg,
+			strings.NewReader(expectedMetrics),
+			"cortex_discarded_attributed_samples_total",
+			"cortex_distributor_received_attributed_samples_total",
+			"cortex_ingester_attributed_active_series",
+			"cortex_ingester_attributed_active_native_histogram_series",
+			"cortex_ingester_attributed_active_native_histogram_buckets",
+		))
 	})
 
 	t.Run("Purge inactive attributions, only received/discarded samples are purged", func(t *testing.T) {
