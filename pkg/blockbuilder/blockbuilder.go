b5bb0771ea30869fabf8263ee3e9a6e7c1584a58
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2025-03-26 15:41:44 +0200

    Revert "Ignore G301 linter issues"
    This reverts commit ee40097abe165428b75aa6886f175aa36cbbc4d9.

--- pkg/blockbuilder/blockbuilder.go
+++ pkg/blockbuilder/blockbuilder.go
@@ -162,7 +162,7 @@ func (b *BlockBuilder) starting(context.Context) (err error) {
 	if err := os.RemoveAll(b.cfg.DataDir); err != nil {
 		return fmt.Errorf("removing data dir: %w", err)
 	}
-	if err := os.MkdirAll(b.cfg.DataDir, os.ModePerm); err != nil { // #nosec G301 -- Relies on system umask to restrict directory permissions, preserving operator flexibility
+	if err := os.MkdirAll(b.cfg.DataDir, 0750); err != nil {
 		return fmt.Errorf("creating data dir: %w", err)
 	}
 
