d29124c9abdd4347f44b1c0ae0393b2df65a6473
Marco Pracucci <marco@pracucci.com>
2025-03-31 10:24:09 +0200

    Fix TestConcurrentFetchers_fetchSingle flaky test
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/storage/ingest/fetcher_test.go
+++ pkg/storage/ingest/fetcher_test.go
@@ -1000,7 +1000,6 @@ func TestConcurrentFetchers_fetchSingle(t *testing.T) {
 
 		require.NoError(t, res.Err)
 		require.Len(t, res.Records, 0)
-		assertFetchMaxBytesMetric(t, reg, fw)
 	})
 
 	t.Run("should return an error response if the Fetch request fails", func(t *testing.T) {
