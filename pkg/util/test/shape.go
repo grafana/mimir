5f5c451ce68c9b46fe56ca7f1e44d0f85ac56a76
György Krajcsovits <gyorgy.krajcsovits@grafana.com>
2025-04-03 12:58:27 +0200

    feat(rw2): prepare for RW2 support
    Add the RW2 proto definition to mimirpb. The implementation will follow
    but it needs to patch the generated files, so we commit the proto now
    to have actually readable diff in the PR that has the functionality.
    See #10423 and especially #11100 diff.
    
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- pkg/util/test/shape.go
+++ pkg/util/test/shape.go
@@ -35,12 +35,19 @@ func prettyPrintType(t reflect.Type, ignoreName bool, ignoreXXXPrefix bool) stri
 	}
 
 	tree := treeprint.NewWithRoot("<root>")
-	addTypeToTree(t, tree, ignoreName, ignoreXXXPrefix)
+	AddTypeToTree(t, tree, ignoreName, ignoreXXXPrefix, false)
 
 	return tree.String()
 }
 
-func addTypeToTree(t reflect.Type, tree treeprint.Tree, ignoreName bool, ignoreXXXPrefix bool) {
+func AddTypeToTree(t reflect.Type, tree treeprint.Tree, ignoreName bool, ignoreXXXPrefix bool, ignoreOffset bool) {
+	offsetF := func(offset uintptr) uintptr {
+		if ignoreOffset {
+			return 0
+		}
+		return offset
+	}
+
 	if t.Kind() == reflect.Pointer {
 		fieldName := t.Name()
 		if ignoreXXXPrefix && strings.HasPrefix(fieldName, "XXX_") {
@@ -50,7 +57,7 @@ func addTypeToTree(t reflect.Type, tree treeprint.Tree, ignoreName bool, ignoreX
 			fieldName = ignoredFieldName
 		}
 		name := fmt.Sprintf("%s: *%s", fieldName, t.Elem().Kind())
-		addTypeToTree(t.Elem(), tree.AddBranch(name), ignoreName, ignoreXXXPrefix)
+		AddTypeToTree(t.Elem(), tree.AddBranch(name), ignoreName, ignoreXXXPrefix, ignoreOffset)
 		return
 	}
 
@@ -70,18 +77,21 @@ func addTypeToTree(t reflect.Type, tree treeprint.Tree, ignoreName bool, ignoreX
 
 		switch f.Type.Kind() {
 		case reflect.Pointer:
-			name := fmt.Sprintf("+%v %s: *%s", f.Offset, fieldName, f.Type.Elem().Kind())
-			addTypeToTree(f.Type.Elem(), tree.AddBranch(name), ignoreName, ignoreXXXPrefix)
+			name := fmt.Sprintf("+%v %s: *%s", offsetF(f.Offset), fieldName, f.Type.Elem().Kind())
+			AddTypeToTree(f.Type.Elem(), tree.AddBranch(name), ignoreName, ignoreXXXPrefix, ignoreOffset)
 		case reflect.Slice:
-			name := fmt.Sprintf("+%v %s: []%s", f.Offset, fieldName, f.Type.Elem().Kind())
+			name := fmt.Sprintf("+%v %s: []%s", offsetF(f.Offset), fieldName, f.Type.Elem().Kind())
 
 			if isPrimitive(f.Type.Elem().Kind()) {
 				tree.AddNode(name)
 			} else {
-				addTypeToTree(f.Type.Elem(), tree.AddBranch(name), ignoreName, ignoreXXXPrefix)
+				AddTypeToTree(f.Type.Elem(), tree.AddBranch(name), ignoreName, ignoreXXXPrefix, ignoreOffset)
 			}
+		case reflect.Struct:
+			name := fmt.Sprintf("+%v %s: struct", offsetF(f.Offset), fieldName)
+			AddTypeToTree(f.Type, tree.AddBranch(name), ignoreName, ignoreXXXPrefix, ignoreOffset)
 		default:
-			name := fmt.Sprintf("+%v %s: %s", f.Offset, fieldName, f.Type.Kind())
+			name := fmt.Sprintf("+%v %s: %s", offsetF(f.Offset), fieldName, f.Type.Kind())
 			tree.AddNode(name)
 		}
 	}
