ff2a4e1bfb75e911ff6e4603b2be2cd8eb2ee856
David Grant <seizethedave@gmail.com>
2025-04-05 21:13:13 -0700

    Add a test.

--- tools/kafkatool/offsets.go
+++ tools/kafkatool/offsets.go
@@ -11,15 +11,14 @@ import (
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
-// ConsumerdddfdGroupCommand manages Kafka consumer groups.
+// OffsetsCommand allows inspecting offsets for a given topic.
 type OffsetsCommand struct {
 	getKafkaClient func() *kgo.Client
 	printer        Printer
 
-	topic       string
-	partitionID int32
-	offsetType  string
-	milli       int64
+	topic      string
+	offsetType string
+	millis     int64
 }
 
 // Register is used to register the command to a parent command.
@@ -30,16 +29,15 @@ func (c *OffsetsCommand) Register(app *kingpin.Application, getKafkaClient func(
 	cmd := app.Command("offsets", "Describes offsets for a given topic and partition.")
 	listOffsetsCmd := cmd.Command("list", "List all offsets of a given topic and partition.").Action(c.listOffsets)
 	listOffsetsCmd.Flag("topic", "Kafka topic to dump").Required().StringVar(&c.topic)
-	listOffsetsCmd.Flag("partition", "Kafka partition to dump or import into").Required().Int32Var(&c.partitionID)
 	listOffsetsCmd.Flag("type", "offset type to list").Default("end").EnumVar(&c.offsetType, "start", "committed", "end", "after_ms")
-	listOffsetsCmd.Flag("milli", "millisecond timestamp to list offsets after").Int64Var(&c.milli)
+	listOffsetsCmd.Flag("millis", "millisecond timestamp to list offsets after").Int64Var(&c.millis)
 }
 
 func (c *OffsetsCommand) listOffsets(_ *kingpin.ParseContext) error {
 	client := c.getKafkaClient()
 	adm := kadm.NewClient(client)
 
-	offsets, err := fetchOffsets(adm, c.topic, c.offsetType, c.milli)
+	offsets, err := fetchOffsets(adm, c.topic, c.offsetType, c.millis)
 	if err != nil {
 		return err
 	}
---
+++ tools/kafkatool/offsets_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/alecthomas/kingpin/v2"
+	"github.com/grafana/mimir/pkg/util/testkafka"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/twmb/franz-go/pkg/kgo"
+)
+
+func TestOffsets(t *testing.T) {
+	const (
+		partitions = 9
+		topic      = "topic"
+		numRecords = 100
+	)
+
+	// Create a Kafka cluster
+	kafkaCluster, clusterAddr := testkafka.CreateCluster(t, partitions, topic)
+	defer kafkaCluster.Close()
+
+	// Create a Kafka client
+	clientProvider, err := createKafkaClientProvider(t, clusterAddr, "")
+	require.NoError(t, err)
+	client := clientProvider()
+
+	// Generate records and push them to the source topic. Partition p gets 10p records.
+	for p := range partitions {
+		generateAndPushRecords(t, client, topic, int32(p), p*10)
+	}
+
+	app := newTestApp()
+	_, printer := newTestOffsetsCommand(app, clientProvider)
+
+	// List offsets.
+	printer.Reset()
+	_, err = app.Parse([]string{"offsets", "list", "--topic", topic, "--type", "end"})
+	require.NoError(t, err)
+	require.Len(t, printer.Lines, partitions)
+
+	for p := range partitions {
+		assert.Regexp(t, expectedLineRegexpByComponents(
+			"Topic:", topic, "Partition:", strconv.Itoa(p), "Offset:", strconv.Itoa(p*10)), printer.Lines[p])
+	}
+}
+
+func newTestOffsetsCommand(app *kingpin.Application, getKafkaClient func() *kgo.Client) (*OffsetsCommand, *BufferedPrinter) {
+	printer := &BufferedPrinter{}
+
+	cmd := &OffsetsCommand{}
+	cmd.Register(app, getKafkaClient, printer)
+
+	return cmd, printer
+}
