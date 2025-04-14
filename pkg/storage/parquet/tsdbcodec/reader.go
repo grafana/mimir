d308bfb43cac6b0c3d55f816cec837230b86f953
francoposa <franco@francoposa.io>
2025-04-14 16:00:08 -0700

    add license header

--- pkg/storage/parquet/tsdbcodec/reader.go
+++ pkg/storage/parquet/tsdbcodec/reader.go
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// Provenance-includes-location: https://github.com/cortexproject/cortex/blob/26344c3ec7409713fcf52a9c41cd0dce537b3100/pkg/compactor/parquet_compactor.go
+// Provenance-includes-license: Apache-2.0
+// Provenance-includes-copyright: The Cortex Authors.
+
 package tsdbcodec
 
 import (
