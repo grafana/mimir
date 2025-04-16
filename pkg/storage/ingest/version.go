0454dd424140cf45778da8d6796564707fb68d67
Alex Weaver <weaver.alex.d@gmail.com>
2025-04-16 13:00:24 -0500

    simplify: remove enum and just use ints

--- pkg/storage/ingest/version.go
+++ pkg/storage/ingest/version.go
@@ -8,53 +8,41 @@ import (
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
-type RecordVersion uint32
-
-const (
-	// Record Version 0 is a valid remote write 1.0 request with no provided version header.
-	RecordVersionZero RecordVersion = iota
-	// Record Version 1 is a valid remote write 1.0 request with a record version header present.
-	// The value is identical to record version 0. It can be understood by a consumer speaking record version 0.
-	RecordVersionOne
-
-	RecordVersionHeaderKey = "X-Record-Version"
-)
+const RecordVersionHeaderKey = "X-Record-Version"
 
 func validateRecordVersion(version int) error {
-	v := RecordVersion(version)
-	switch v {
-	case RecordVersionZero:
+	switch version {
+	case 0:
 		return nil
-	case RecordVersionOne:
+	case 1:
 		return nil
 	default:
 		return fmt.Errorf("unknown record version %d", version)
 	}
 }
 
-func recordVersionHeader(version RecordVersion) kgo.RecordHeader {
+func recordVersionHeader(version int) kgo.RecordHeader {
 	return kgo.RecordHeader{
 		Key:   RecordVersionHeaderKey,
 		Value: recordVersionToHeaderValue(version),
 	}
 }
 
-func recordVersionToHeaderValue(version RecordVersion) []byte {
+func recordVersionToHeaderValue(version int) []byte {
 	var b [4]byte
 	binary.BigEndian.PutUint32(b[:], uint32(version))
 	return b[:]
 }
 
-func headerValueToRecordVersion(value []byte) RecordVersion {
-	return RecordVersion(binary.BigEndian.Uint32(value))
+func headerValueToRecordVersion(value []byte) int {
+	return int(binary.BigEndian.Uint32(value))
 }
 
 func recordSerializerFromCfg(cfg KafkaConfig) recordSerializer {
-	version := RecordVersion(cfg.ProducerSupportedRecordVersion)
-	switch version {
-	case RecordVersionZero:
+	switch cfg.ProducerSupportedRecordVersion {
+	case 0:
 		return versionZeroRecordSerializer{}
-	case RecordVersionOne:
+	case 1:
 		return versionOneRecordSerializer{}
 	default:
 		return versionZeroRecordSerializer{}
@@ -66,12 +54,17 @@ type recordSerializer interface {
 	ToRecords(partitionID int32, tenantID string, req *mimirpb.WriteRequest, maxSize int) ([]*kgo.Record, error)
 }
 
+// versionZeroRecordSerializer produces records of version 0.
+// Record Version 0 is a valid remote write 1.0 request with no provided version header.
 type versionZeroRecordSerializer struct{}
 
 func (v versionZeroRecordSerializer) ToRecords(partitionID int32, tenantID string, req *mimirpb.WriteRequest, maxSize int) ([]*kgo.Record, error) {
 	return marshalWriteRequestToRecords(partitionID, tenantID, req, maxSize)
 }
 
+// versionOneRecordSerializer produces records of version 1.
+// Record Version 1 is a valid remote write 1.0 request with a record version header present.
+// The value is identical to record version 0. It can be understood by a consumer speaking record version 0.
 type versionOneRecordSerializer struct{}
 
 func (v versionOneRecordSerializer) ToRecords(partitionID int32, tenantID string, req *mimirpb.WriteRequest, maxSize int) ([]*kgo.Record, error) {
@@ -80,7 +73,7 @@ func (v versionOneRecordSerializer) ToRecords(partitionID int32, tenantID string
 		return nil, err
 	}
 	for _, r := range records {
-		r.Headers = append(r.Headers, recordVersionHeader(RecordVersionOne))
+		r.Headers = append(r.Headers, recordVersionHeader(1))
 	}
 	return records, nil
 }
