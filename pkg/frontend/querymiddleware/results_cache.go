c056e99fe13740f0a711de0d474efb0696ee3234
Innokentii Konstantinov <konstantinovksh@gmail.com>
2025-04-16 19:13:30 +0800

    Update pkg/frontend/querymiddleware/results_cache.go
    Co-authored-by: Marco Pracucci <marco@pracucci.com>

--- pkg/frontend/querymiddleware/results_cache.go
+++ pkg/frontend/querymiddleware/results_cache.go
@@ -675,7 +675,6 @@ func approximateSamplesProcessedPerStep(start int64, end int64, step int64, samp
 	numSteps := (end-start)/step + 1
 	// approxPerStep is not float because:
 	// 1. Real per step stats always have integer values. Having int in approximation will simplify migration to per step stats from engine
-	// 2. approxPerStep < 0 doesn't make sense
 	// 3. approxPerStep is rounded with math.Ceil, because in most of the real-life scenarios, samplesProcessed / numSteps is > 1.
 	approxPerStep := int64(math.Ceil(float64(samplesProcessed) / float64(numSteps)))
 	perStepStats := make([]StepStat, 0, numSteps)
