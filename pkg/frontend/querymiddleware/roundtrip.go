8a08500af3c50763d21a78b2a08007a2abcb5af1
Justin Lei <lei.justin@gmail.com>
2025-04-23 16:55:48 -0700

    query-frontend: Move user middlewares before the caching middleware (#11284)
    * query-frontend: Move user middlewares before the caching middleware
    
    * Move user middlewares before the error caching middleware as well

--- pkg/frontend/querymiddleware/roundtrip.go
+++ pkg/frontend/querymiddleware/roundtrip.go
@@ -404,6 +404,17 @@ func newQueryMiddlewares(
 		prom2CompatMiddleware,
 	)
 
+	// Inject the extra middlewares provided by the user before the caching, query pruning, and
+	// query sharding middlewares.
+	// This is important because these extra middlewares can potentially mutate the incoming
+	// query.
+	if len(cfg.ExtraInstantQueryMiddlewares) > 0 {
+		queryInstantMiddleware = append(queryInstantMiddleware, cfg.ExtraInstantQueryMiddlewares...)
+	}
+	if len(cfg.ExtraRangeQueryMiddlewares) > 0 {
+		queryRangeMiddleware = append(queryRangeMiddleware, cfg.ExtraRangeQueryMiddlewares...)
+	}
+
 	if cfg.CacheResults && cfg.CacheErrors {
 		errorCachingMiddleware := newErrorCachingMiddleware(cacheClient, limits, resultsCacheEnabledByOption, cacheKeyGenerator, log, registerer)
 
@@ -444,15 +455,6 @@ func newQueryMiddlewares(
 		newInstrumentMiddleware("spin_off_subqueries", metrics),
 		newSpinOffSubqueriesMiddleware(limits, log, engine, registerer, splitAndCacheMiddleware, defaultStepFunc),
 	)
-
-	// Inject the extra middlewares provided by the user before the query pruning and query sharding middleware.
-	if len(cfg.ExtraInstantQueryMiddlewares) > 0 {
-		queryInstantMiddleware = append(queryInstantMiddleware, cfg.ExtraInstantQueryMiddlewares...)
-	}
-	// Inject the extra middlewares provided by the user before the query pruning and query sharding middleware.
-	if len(cfg.ExtraRangeQueryMiddlewares) > 0 {
-		queryRangeMiddleware = append(queryRangeMiddleware, cfg.ExtraRangeQueryMiddlewares...)
-	}
 
 	if cfg.ShardedQueries {
 		// Inject the cardinality estimation middleware after time-based splitting and
