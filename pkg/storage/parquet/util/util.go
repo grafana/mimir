0962282bf2e4f4e880d92a5735610d21b9927313
francoposa <franco@francoposa.io>
2025-04-28 12:49:50 -0700

    fix license headers

--- pkg/storage/parquet/util/util.go
+++ pkg/storage/parquet/util/util.go
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// Provenance-includes-location: https://github.com/prometheus-community/parquet-common/blob/306c8486441da41d2a655fa29d0e83820437cf23/util/util.go
+// Provenance-includes-license: Apache-2.0
+// Provenance-includes-copyright: The Prometheus Authors.
+
 package util
 
 import "github.com/parquet-go/parquet-go"
