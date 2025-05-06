2f3b43a243d0f2668f800636eb0b599b920ea7fc
Alexander Akhmetov <me@alx.cx>
2025-05-06 15:58:53 +0200

    Upgrade grafana/alerting 28f3388bdf46 -> 3c7640592aa8

--- vendor/github.com/grafana/alerting/receivers/googlechat/testing.go
+++ vendor/github.com/grafana/alerting/receivers/googlechat/testing.go
@@ -4,9 +4,7 @@ package googlechat
 const FullValidConfigForTesting = `{
 	"url": "http://localhost", 
 	"title": "test-title", 
-	"message": "test-message", 
-	"avatar_url" : "http://avatar", 
-	"use_discord_username": true
+	"message": "test-message"
 }`
 
 // FullValidSecretsForTesting is a string representation of JSON object that contains all fields that can be overridden from secrets.
