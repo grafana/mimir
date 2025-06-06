002dceb80531bcfaea0d0dec86ee57ea9b04e82f
Tito Lins <tito.linsesilva@grafana.com>
2025-06-06 12:52:47 +0200

    more logs

--- pkg/alertmanager/alertmanager.go
+++ pkg/alertmanager/alertmanager.go
@@ -720,7 +720,7 @@ func buildGrafanaReceiverIntegrations(emailCfg alertingReceivers.EmailSenderConf
 	if err != nil {
 		return nil, err
 	}
-	level.Warn(logger).Log("msg", "buildGrafanaReceiverIntegrations", "tmpl", fmt.Sprintf("%+v", tmpl.Text))
+	level.Warn(logger).Log("msg", "buildGrafanaReceiverIntegrations", "tmpl", fmt.Sprintf("%+v", tmpl))
 
 	emailSender, err := alertingReceivers.NewEmailSender(emailCfg)
 	if err != nil {
