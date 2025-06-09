6ff687147779b0d57e05afd21a8f45354c284095
Jon Kartago Lamida <me@lamida.net>
2025-06-09 23:10:41 +0700

    Rename MemoryConsumptionSource for labels
    Signed-off-by: Jon Kartago Lamida <me@lamida.net>
    
    # Conflicts:
    #	pkg/streamingpromql/engine_test.go

--- pkg/util/limiter/memory_consumption.go
+++ pkg/util/limiter/memory_consumption.go
@@ -59,9 +59,9 @@ const (
 	QuantileGroupSlices
 	TopKBottomKInstantQuerySeriesSlices
 	TopKBottomKRangeQuerySeriesSlices
-	StringLabels
+	SeriesMetadataLabels
 
-	memoryConsumptionSourceCount = StringLabels + 1
+	memoryConsumptionSourceCount = SeriesMetadataLabels + 1
 )
 
 const (
@@ -102,8 +102,8 @@ func (s MemoryConsumptionSource) String() string {
 		return "[]topkbottom.instantQuerySeries"
 	case TopKBottomKRangeQuerySeriesSlices:
 		return "[]topkbottom.rangeQuerySeries"
-	case StringLabels:
-		return "labels.Label"
+	case SeriesMetadataLabels:
+		return "labels.Labels"
 	default:
 		return unknownMemorySource
 	}
@@ -193,7 +193,7 @@ func (l *MemoryConsumptionTracker) CurrentEstimatedMemoryConsumptionBytes() uint
 func (l *MemoryConsumptionTracker) IncreaseMemoryConsumptionForLabels(lb labels.Labels) error {
 	for _, label := range lb {
 		// TODO: Update labels.Labels to get size of bytes directly instead of calculating it here.
-		if err := l.IncreaseMemoryConsumption(uint64(len(label.Name)+len(label.Value))*StringSize, StringLabels); err != nil {
+		if err := l.IncreaseMemoryConsumption(uint64(len(label.Name)+len(label.Value))*StringSize, SeriesMetadataLabels); err != nil {
 			return err
 		}
 	}
@@ -203,6 +203,6 @@ func (l *MemoryConsumptionTracker) IncreaseMemoryConsumptionForLabels(lb labels.
 func (l *MemoryConsumptionTracker) DecreaseMemoryConsumptionForLabels(lb labels.Labels) {
 	for _, label := range lb {
 		// TODO: Update labels.Labels to get size of bytes directly instead of calculating it here.
-		l.DecreaseMemoryConsumption(uint64(len(label.Name)+len(label.Value))*StringSize, StringLabels)
+		l.DecreaseMemoryConsumption(uint64(len(label.Name)+len(label.Value))*StringSize, SeriesMetadataLabels)
 	}
 }
