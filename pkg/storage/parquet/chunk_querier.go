452c678611c816f5a16c91abec0608ebfd08a4c7
Jesus Vazquez <jesusvazquez@users.noreply.github.com>
2025-06-24 15:34:32 +0200

    parquet-bucket-store: finish implementing Stores interface (#11772)
    We're trying to mirror the existing bucket store structure for the
    parquet implementation and in this PR i'm just trying to implement some
    of the necessary methods starting with building up the series sets for
    labels calls.
    - Series 
    - LabelNames 
    - LabelValues
    
    ---------
    
    Signed-off-by: Jesus Vazquez <jesus.vazquez@grafana.com>
    Signed-off-by: Nicolás Pazos <npazosmendez@gmail.com>
    Co-authored-by: Nicolas Pazos <nicolas.pazos-mendez@grafana.com>
    Co-authored-by: Nicolás Pazos <npazosmendez@gmail.com>

---
+++ pkg/storage/parquet/chunk_querier.go
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package parquet
+
+import (
+	"context"
+	"runtime"
+
+	"github.com/prometheus-community/parquet-common/convert"
+	"github.com/prometheus-community/parquet-common/queryable"
+	"github.com/prometheus-community/parquet-common/schema"
+	"github.com/prometheus/prometheus/model/labels"
+	prom_storage "github.com/prometheus/prometheus/storage"
+	"github.com/prometheus/prometheus/util/annotations"
+	"golang.org/x/sync/errgroup"
+)
+
+type querierOpts struct {
+	concurrency                int
+	pagePartitioningMaxGapSize int
+}
+
+var DefaultQuerierOpts = querierOpts{
+	concurrency:                runtime.GOMAXPROCS(0),
+	pagePartitioningMaxGapSize: 10 * 1024,
+}
+
+type QuerierOpts func(*querierOpts)
+
+func NewParquetChunkQuerier(d *schema.PrometheusParquetChunksDecoder, shardFinder queryable.ShardsFinderFunction, opts ...QuerierOpts) (prom_storage.ChunkQuerier, error) {
+	cfg := DefaultQuerierOpts
+
+	for _, opt := range opts {
+		opt(&cfg)
+	}
+
+	return &parquetChunkQuerier{
+		shardsFinder: shardFinder,
+		d:            d,
+		opts:         &cfg,
+	}, nil
+}
+
+type parquetChunkQuerier struct {
+	mint, maxt   int64
+	shardsFinder queryable.ShardsFinderFunction
+	d            *schema.PrometheusParquetChunksDecoder
+	opts         *querierOpts
+}
+
+func (p parquetChunkQuerier) LabelValues(ctx context.Context, name string, hints *prom_storage.LabelHints, matchers ...*labels.Matcher) ([]string, annotations.Annotations, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (p parquetChunkQuerier) LabelNames(ctx context.Context, hints *prom_storage.LabelHints, matchers ...*labels.Matcher) ([]string, annotations.Annotations, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (p parquetChunkQuerier) Close() error {
+	// TODO implement me
+	return nil
+}
+
+func (p parquetChunkQuerier) Select(ctx context.Context, sortSeries bool, hints *prom_storage.SelectHints, matchers ...*labels.Matcher) prom_storage.ChunkSeriesSet {
+	shards, err := p.queryableShards(ctx, p.mint, p.maxt)
+	if err != nil {
+		return prom_storage.ErrChunkSeriesSet(err)
+	}
+	seriesSet := make([]prom_storage.ChunkSeriesSet, len(shards))
+
+	minT, maxT := p.mint, p.maxt
+	if hints != nil {
+		minT, maxT = hints.Start, hints.End
+	}
+	skipChunks := hints != nil && hints.Func == "series"
+	errGroup, ctx := errgroup.WithContext(ctx)
+	errGroup.SetLimit(p.opts.concurrency)
+
+	for i, shard := range shards {
+		errGroup.Go(func() error {
+			ss, err := shard.Query(ctx, sortSeries, minT, maxT, skipChunks, matchers)
+			seriesSet[i] = ss
+			return err
+		})
+	}
+
+	if err := errGroup.Wait(); err != nil {
+		return prom_storage.ErrChunkSeriesSet(err)
+	}
+
+	return convert.NewMergeChunkSeriesSet(seriesSet, labels.Compare, prom_storage.NewConcatenatingChunkSeriesMerger())
+}
+
+func (p parquetChunkQuerier) queryableShards(ctx context.Context, mint, maxt int64) ([]*queryableShard, error) {
+	shards, err := p.shardsFinder(ctx, mint, maxt)
+	if err != nil {
+		return nil, err
+	}
+	qBlocks := make([]*queryableShard, len(shards))
+	for i, shard := range shards {
+		qb, err := newQueryableShard(p.opts, shard, p.d)
+		if err != nil {
+			return nil, err
+		}
+		qBlocks[i] = qb
+	}
+	return qBlocks, nil
+}
