1efcac65ce46ac7a129ee2e9c3393d4a47e57ae7
Andy Asp <andy.asp@grafana.com>
2025-06-24 17:09:08 -0400

    Basic prototyping

--- pkg/mimir/modules.go
+++ pkg/mimir/modules.go
@@ -43,6 +43,7 @@ import (
 	"github.com/grafana/mimir/pkg/blockbuilder"
 	blockbuilderscheduler "github.com/grafana/mimir/pkg/blockbuilder/scheduler"
 	"github.com/grafana/mimir/pkg/compactor"
+	compactorscheduler "github.com/grafana/mimir/pkg/compactor/scheduler"
 	"github.com/grafana/mimir/pkg/continuoustest"
 	"github.com/grafana/mimir/pkg/costattribution"
 	"github.com/grafana/mimir/pkg/distributor"
@@ -93,6 +94,7 @@ const (
 	BlockBuilder                     string = "block-builder"
 	BlockBuilderScheduler            string = "block-builder-scheduler"
 	Compactor                        string = "compactor"
+	CompactorScheduler               string = "compactor-scheduler"
 	ContinuousTest                   string = "continuous-test"
 	CostAttributionService           string = "cost-attribution-service"
 	Distributor                      string = "distributor"
@@ -1239,6 +1241,16 @@ func (t *Mimir) initCompactor() (serv services.Service, err error) {
 	return t.Compactor, nil
 }
 
+func (t *Mimir) initCompactorScheduler() (serv services.Service, err error) {
+
+	t.CompactorScheduler, err = compactorscheduler.NewCompactorScheduler(t.Cfg.Compactor, t.Cfg.CompactorScheduler, t.Cfg.BlocksStorage, util_log.Logger, t.Registerer)
+	if err != nil {
+		return
+	}
+
+	return t.CompactorScheduler, nil
+}
+
 func (t *Mimir) initStoreGateway() (serv services.Service, err error) {
 	t.Cfg.StoreGateway.ShardingRing.ListenPort = t.Cfg.Server.GRPCListenPort
 	t.StoreGateway, err = storegateway.NewStoreGateway(t.Cfg.StoreGateway, t.Cfg.BlocksStorage, t.Overrides, util_log.Logger, t.Registerer, t.ActivityTracker)
@@ -1450,6 +1462,7 @@ func (t *Mimir) setupModuleManager() error {
 	mm.RegisterModule(BlockBuilder, t.initBlockBuilder)
 	mm.RegisterModule(BlockBuilderScheduler, t.initBlockBuilderScheduler)
 	mm.RegisterModule(Compactor, t.initCompactor)
+	mm.RegisterModule(CompactorScheduler, t.initCompactorScheduler)
 	mm.RegisterModule(ContinuousTest, t.initContinuousTest)
 	mm.RegisterModule(CostAttributionService, t.initCostAttributionService, modules.UserInvisibleModule)
 	mm.RegisterModule(Distributor, t.initDistributor)
