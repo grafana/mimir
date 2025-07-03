2df988d0e69099b92e879bfe5d404d4980a374ab
Oleg Zaytsev <mail@olegzaytsev.com>
2025-07-03 14:53:24 +0000

    Implement dynamictenantshard
    Not sure yet whether I'm going to use it, but wanted to see if it's an
    option. This is up to 14% slower than fixed shards.
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

---
+++ pkg/usagetracker/dynamictenantshard/map_test.go
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package dynamictenantshard
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
+
+	"github.com/grafana/mimir/pkg/usagetracker/clock"
+)
+
+func TestMap(t *testing.T) {
+	series := atomic.NewUint64(0)
+	const events = 5
+	const seriesPerEvent = 5
+	limit := uint64(events * seriesPerEvent)
+
+	// Start small, let rehashing happen.
+	m := New(seriesPerEvent)
+
+	storedValues := map[uint64]clock.Minutes{}
+	for i := 1; i <= events; i++ {
+		refs := make([]uint64, seriesPerEvent)
+		for j := range refs {
+			refs[j] = uint64((i*100 + j) << valueBits)
+			storedValues[refs[j]] = clock.Minutes(i)
+			created, rejected := m.Put(refs[j], clock.Minutes(i), series, limit, false)
+			require.True(t, created)
+			require.False(t, rejected)
+		}
+	}
+
+	require.Equal(t, events*seriesPerEvent, m.count())
+	require.Equal(t, uint64(events*seriesPerEvent), series.Load())
+
+	{
+		// No more series will fit.
+		created, rejected := m.Put(uint64(65535)<<valueBits, 1, series, limit, true)
+		require.False(t, created)
+		require.True(t, rejected)
+	}
+
+	{
+		gotValues := map[uint64]clock.Minutes{}
+		iterator := m.Iterator()
+		iterator(
+			func(c int) { require.Equal(t, len(storedValues), c) },
+			func(key uint64, value clock.Minutes) { gotValues[key] = value },
+		)
+		require.Equal(t, storedValues, gotValues)
+	}
+
+	{
+		// Cleanup first wave of series
+		m.Cleanup(clock.Minutes(1), series)
+		expectedSeries := (events - 1) * seriesPerEvent
+
+		// It's unsafe to check m.count() after Cleanup event.
+		require.Equal(t, expectedSeries, int(series.Load()))
+	}
+}
+
+func TestMapValues(t *testing.T) {
+	const count = 10e3
+	stored := map[uint64]clock.Minutes{}
+	m := New(100)
+	total := atomic.NewUint64(0)
+	for i := 0; i < count; i++ {
+		key := rand.Uint64()
+		val := clock.Minutes(i)
+		if val == 0xff {
+			continue
+		}
+		stored[key] = val
+		m.Put(key, val, total, 0, false)
+	}
+	require.Equal(t, len(stored), m.count())
+	require.Equal(t, len(stored), int(total.Load()))
+
+	got := map[uint64]clock.Minutes{}
+	m.Iterator()(
+		func(c int) { require.Equal(t, len(stored), c) },
+		func(key uint64, value clock.Minutes) { got[key] = value },
+	)
+	require.Equal(t, stored, got)
+}
---
+++ pkg/usagetracker/tenantshard_bench_test.go
@@ -0,0 +1,66 @@
+package usagetracker
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+
+	"go.uber.org/atomic"
+
+	"github.com/grafana/mimir/pkg/usagetracker/clock"
+	"github.com/grafana/mimir/pkg/usagetracker/dynamictenantshard"
+	"github.com/grafana/mimir/pkg/usagetracker/tenantshard"
+)
+
+func BenchmarkTenantShard(b *testing.B) {
+	for _, totalSeries := range []int{1e6, 10e6, 100e6} {
+		b.Run(fmt.Sprintf("totalSeries=%d", totalSeries), func(b *testing.B) {
+			series := make([]uint64, totalSeries)
+			for i := range series {
+				series[i] = rand.Uint64() << 7
+			}
+
+			b.Run("impl=tenantshard", func(b *testing.B) {
+				m := tenantshard.New(uint32(len(series)), 0, tenantshard.Shards)
+				benchmarkWithSeries(b, m, series)
+			})
+
+			b.Run("impl=dynamictenantshard", func(b *testing.B) {
+				m := dynamictenantshard.New(uint32(len(series)))
+				benchmarkWithSeries(b, m, series)
+			})
+		})
+	}
+}
+
+type tenantShardMap interface {
+	Put(key uint64, value clock.Minutes, series *atomic.Uint64, limit uint64, track bool) (created, rejected bool)
+}
+
+func benchmarkWithSeries[T tenantShardMap](b *testing.B, m T, keys []uint64) {
+	series := atomic.NewUint64(0)
+	limit := uint64(2 * len(keys))
+	minutes := clock.Minutes(0)
+	for _, s := range keys {
+		_, _ = m.Put(s, minutes, series, limit, true)
+		if s%32 == 0 {
+			minutes++
+			if minutes >= 120 {
+				minutes = 0
+			}
+		}
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, s := range keys {
+			_, _ = m.Put(s, minutes, series, limit, true)
+			if s%32 == 0 {
+				minutes++
+				if minutes >= 120 {
+					minutes = 0
+				}
+			}
+		}
+	}
+}
