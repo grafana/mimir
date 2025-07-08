ac93dba244a0f39c57cf72c1d03ebd5c7646d413
Jeanette Tan <jeanette.tan@grafana.com>
2025-07-09 04:51:54 +0800

    Reuse astmapper in histogram agg reordering

--- pkg/streamingpromql/optimize/ast/reorder_histogram_agg.go
+++ pkg/streamingpromql/optimize/ast/reorder_histogram_agg.go
@@ -5,6 +5,7 @@ package ast
 import (
 	"context"
 
+	"github.com/grafana/mimir/pkg/frontend/querymiddleware/astmapper"
 	"github.com/prometheus/prometheus/promql/parser"
 )
 
@@ -17,88 +18,54 @@ func (r *ReorderHistogramAgg) Name() string {
 }
 
 func (r *ReorderHistogramAgg) Apply(ctx context.Context, expr parser.Expr) (parser.Expr, error) {
-	if err := ctx.Err(); err != nil {
-		return nil, err
+	mapper := &reorderHistogramAgg{
+		ctx: ctx,
 	}
-
-	return r.apply(expr), nil
+	ASTExprMapper := astmapper.NewASTExprMapper(mapper)
+	return ASTExprMapper.Map(expr)
 }
 
-func (r *ReorderHistogramAgg) apply(expr parser.Expr) parser.Expr {
-	switch e := expr.(type) {
-	case *parser.Call:
-		if !r.isSwitchableCall(e.Func) {
-			return mapChildren(e, r.apply)
-		}
-
-		if len(e.Args) != 1 {
-			return mapChildren(e, r.apply)
-		}
-
-		agg, ok := e.Args[0].(*parser.AggregateExpr)
-		if !ok || !r.isSwitchableAgg(agg.Op) {
-			return mapChildren(e, r.apply)
-		}
-
-		// Rewrite from: histogram_func(agg(...)) to: agg(histogram_func(...))
-		newExpr := &parser.AggregateExpr{
-			Op: agg.Op,
-			Expr: &parser.Call{
-				Func: e.Func,
-				Args: []parser.Expr{agg.Expr},
-			},
-			Param:    agg.Param,
-			Grouping: agg.Grouping,
-			Without:  agg.Without,
-		}
-
-		return newExpr
-
-	case *parser.AggregateExpr, *parser.MatrixSelector, *parser.SubqueryExpr,
-		*parser.NumberLiteral, *parser.StringLiteral, *parser.StepInvariantExpr, *parser.UnaryExpr, *parser.VectorSelector:
-		return mapChildren(e, r.apply)
-
-	case *parser.BinaryExpr:
-		e.LHS = r.apply(e.LHS)
-		e.RHS = r.apply(e.RHS)
-		return e
-
-	case *parser.ParenExpr:
-		e.Expr = r.apply(e.Expr)
-		return e
-	}
-
-	return expr
+type reorderHistogramAgg struct {
+	ctx context.Context
 }
 
-func (r *ReorderHistogramAgg) isSwitchableCall(callFunc *parser.Function) bool {
+func (mapper *reorderHistogramAgg) MapExpr(expr parser.Expr) (mapped parser.Expr, finished bool, err error) {
+	if err := mapper.ctx.Err(); err != nil {
+		return nil, false, err
+	}
+
+	call, ok := expr.(*parser.Call)
+	if !ok || !mapper.isSwitchableCall(call.Func) {
+		return expr, false, nil
+	}
+
+	if len(call.Args) != 1 {
+		return expr, false, nil
+	}
+
+	agg, ok := call.Args[0].(*parser.AggregateExpr)
+	if !ok || !mapper.isSwitchableAgg(agg.Op) {
+		return expr, false, nil
+	}
+
+	newExpr := &parser.AggregateExpr{
+		Op: agg.Op,
+		Expr: &parser.Call{
+			Func: call.Func,
+			Args: []parser.Expr{agg.Expr},
+		},
+		Param:    agg.Param,
+		Grouping: agg.Grouping,
+		Without:  agg.Without,
+	}
+
+	return newExpr, false, nil
+}
+
+func (*reorderHistogramAgg) isSwitchableCall(callFunc *parser.Function) bool {
 	return callFunc.Name == "histogram_sum" || callFunc.Name == "histogram_count"
 }
 
-func (r *ReorderHistogramAgg) isSwitchableAgg(op parser.ItemType) bool {
+func (*reorderHistogramAgg) isSwitchableAgg(op parser.ItemType) bool {
 	return op == parser.SUM || op == parser.AVG
-}
-
-// Helper functions for mapping AST nodes recursively
-func mapChildren(expr parser.Expr, mapFunc func(parser.Expr) parser.Expr) parser.Expr {
-	switch e := expr.(type) {
-	case *parser.Call:
-		for i, arg := range e.Args {
-			e.Args[i] = mapFunc(arg)
-		}
-	case *parser.AggregateExpr:
-		e.Expr = mapFunc(e.Expr)
-	case *parser.BinaryExpr:
-		e.LHS = mapFunc(e.LHS)
-		e.RHS = mapFunc(e.RHS)
-	case *parser.SubqueryExpr:
-		e.Expr = mapFunc(e.Expr)
-	case *parser.ParenExpr:
-		e.Expr = mapFunc(e.Expr)
-	case *parser.UnaryExpr:
-		e.Expr = mapFunc(e.Expr)
-	case *parser.StepInvariantExpr:
-		e.Expr = mapFunc(e.Expr)
-	}
-	return expr
 }
