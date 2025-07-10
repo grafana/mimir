e4e2da202dc54e5dc2c482b0a4d27b1f780c5d75
Oleg Zaytsev <mail@olegzaytsev.com>
2025-07-10 15:53:30 +0000

    Export current series limit as a metric
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

--- pkg/usagetracker/tracker_store_collector.go
+++ pkg/usagetracker/tracker_store_collector.go
@@ -8,20 +8,30 @@ var _ prometheus.Collector = &trackerStore{}
 
 const activeSeriesMetricName = "cortex_usage_tracker_active_series"
 
-var trackerStoreMetricDesc = prometheus.NewDesc(
+const currentActiveSeriesLimit = "cortex_usage_tracker_active_series_current_limit"
+
+var activeSeriesMetricDesc = prometheus.NewDesc(
 	activeSeriesMetricName,
 	"Number of active series tracker for each user.",
 	[]string{"user"}, nil,
 )
 
+var currentLimitMetricDesc = prometheus.NewDesc(
+	currentActiveSeriesLimit,
+	"Current active series limit for each user.",
+	[]string{"user"}, nil,
+)
+
 func (t *trackerStore) Describe(descs chan<- *prometheus.Desc) {
-	descs <- trackerStoreMetricDesc
+	descs <- activeSeriesMetricDesc
+	descs <- currentLimitMetricDesc
 }
 
 func (t *trackerStore) Collect(metrics chan<- prometheus.Metric) {
 	t.mtx.RLock()
 	defer t.mtx.RUnlock()
 	for tenantID, tenant := range t.tenants {
-		metrics <- prometheus.MustNewConstMetric(trackerStoreMetricDesc, prometheus.GaugeValue, float64(tenant.series.Load()), tenantID)
+		metrics <- prometheus.MustNewConstMetric(activeSeriesMetricDesc, prometheus.GaugeValue, float64(tenant.series.Load()), tenantID)
+		metrics <- prometheus.MustNewConstMetric(currentLimitMetricDesc, prometheus.GaugeValue, float64(tenant.currentLimit.Load()), tenantID)
 	}
 }
