4749c73790dcf8a7ba6697e844b4a6c02ca46588
Charles Korn <charles.korn@grafana.com>
2025-07-16 20:38:44 +1000

    Fix handling of empty results, and account for labels memory consumption when closing query

--- pkg/streamingpromql/query.go
+++ pkg/streamingpromql/query.go
@@ -41,6 +41,8 @@ type Query struct {
 	string         *promql.String
 	scalar         *promql.Scalar
 
+	resultIsVector bool
+
 	succeeded bool
 }
 
@@ -73,11 +75,16 @@ func (q *Query) Exec(ctx context.Context) *promql.Result {
 		q.engine.estimatedPeakMemoryConsumption.Observe(float64(q.memoryConsumptionTracker.PeakEstimatedMemoryConsumptionBytes()))
 	}()
 
+	_, isInstantVectorOperator := q.evaluator.root.(types.InstantVectorOperator)
+	q.resultIsVector = q.topLevelQueryTimeRange.IsInstant && isInstantVectorOperator
+
 	if err := q.evaluator.Evaluate(ctx, q); err != nil {
 		q.returnResultToPool()
 		return &promql.Result{Err: err}
 	}
 
+	// Any labels that we need to return will have been removed from q.seriesMetadata and copied into the matrix or vector,
+	// so we can safely return it now.
 	types.SeriesMetadataSlicePool.Put(&q.seriesMetadata, q.memoryConsumptionTracker)
 
 	result := &promql.Result{}
@@ -91,15 +98,24 @@ func (q *Query) Exec(ctx context.Context) *promql.Result {
 		slices.SortFunc(q.matrix, func(a, b promql.Series) int {
 			return labels.Compare(a.Metric, b.Metric)
 		})
+
 		result.Value = q.matrix
 	case q.vector != nil:
 		result.Value = q.vector
 	case q.string != nil:
-		result.Value = q.string
+		result.Value = *q.string
 	case q.scalar != nil:
-		result.Value = q.scalar
+		result.Value = *q.scalar
+	case q.resultIsVector:
+		// The result is an empty vector.
+		var err error
+		result.Value, err = types.VectorPool.Get(0, q.memoryConsumptionTracker)
+		if err != nil {
+			return &promql.Result{Err: err}
+		}
 	default:
-		return &promql.Result{Err: fmt.Errorf("unknown result type for query %q over time range %v", q.originalExpression, q.topLevelQueryTimeRange)}
+		// The result is an empty matrix.
+		result.Value = types.GetMatrix(0)
 	}
 
 	q.succeeded = true
@@ -112,15 +128,15 @@ func (q *Query) SeriesMetadataEvaluated(evaluator *Evaluator, series []types.Ser
 }
 
 func (q *Query) InstantVectorSeriesDataEvaluated(evaluator *Evaluator, seriesIndex int, seriesData types.InstantVectorSeriesData) error {
-	series := q.seriesMetadata[seriesIndex]
-	q.seriesMetadata[seriesIndex] = types.SeriesMetadata{} // Clear the original series metadata slice so we don't return the labels twice when the slice is returned later.
-
 	if len(seriesData.Floats) == 0 && len(seriesData.Histograms) == 0 {
 		types.PutInstantVectorSeriesData(seriesData, q.memoryConsumptionTracker)
 		return nil
 	}
 
-	if q.topLevelQueryTimeRange.IsInstant {
+	series := q.seriesMetadata[seriesIndex]
+	q.seriesMetadata[seriesIndex] = types.SeriesMetadata{} // Clear the original series metadata slice so we don't return the labels twice when the slice is returned later.
+
+	if q.resultIsVector {
 		// Instant query: we'll return a vector.
 		defer types.PutInstantVectorSeriesData(seriesData, q.memoryConsumptionTracker)
 		if q.vector == nil {
@@ -131,25 +147,22 @@ func (q *Query) InstantVectorSeriesDataEvaluated(evaluator *Evaluator, seriesInd
 			}
 		}
 
-		if len(seriesData.Floats)+len(seriesData.Histograms) > 1 {
+		if len(seriesData.Floats)+len(seriesData.Histograms) != 1 {
 			return fmt.Errorf("expected exactly one sample for series %s, but got %v floats, %v histograms", series.Labels.String(), len(seriesData.Floats), len(seriesData.Histograms))
 		}
 
-		ts := q.topLevelQueryTimeRange.StartT
-
-		// In addition to the two cases below, a series may also have no data points, in which case we don't need to do anything.
-		if len(seriesData.Floats) == 1 && len(seriesData.Histograms) == 0 {
+		if len(seriesData.Floats) == 1 {
 			point := seriesData.Floats[0]
 			q.vector = append(q.vector, promql.Sample{
 				Metric: series.Labels,
-				T:      ts,
+				T:      point.T,
 				F:      point.F,
 			})
-		} else if len(seriesData.Floats) == 0 && len(seriesData.Histograms) == 1 {
+		} else {
 			point := seriesData.Histograms[0]
 			q.vector = append(q.vector, promql.Sample{
 				Metric: series.Labels,
-				T:      ts,
+				T:      point.T,
 				H:      point.H,
 			})
 
@@ -180,12 +193,14 @@ func (q *Query) RangeVectorStepSamplesEvaluated(evaluator *Evaluator, seriesInde
 		return fmt.Errorf("unexpected step index for range vector result: %d", stepIndex)
 	}
 
+	if !stepData.Floats.Any() && !stepData.Histograms.Any() {
+		// Nothing to do.
+		return nil
+	}
+
 	if q.matrix == nil {
 		q.matrix = types.GetMatrix(len(q.seriesMetadata))
 	}
-
-	series := q.seriesMetadata[seriesIndex]
-	q.seriesMetadata[seriesIndex] = types.SeriesMetadata{} // Clear the original series metadata slice so we don't return the labels twice when the slice is returned later.
 
 	floats, err := stepData.Floats.CopyPoints()
 	if err != nil {
@@ -197,11 +212,8 @@ func (q *Query) RangeVectorStepSamplesEvaluated(evaluator *Evaluator, seriesInde
 		return err
 	}
 
-	if len(floats) == 0 && len(histograms) == 0 {
-		types.FPointSlicePool.Put(&floats, q.memoryConsumptionTracker)
-		types.HPointSlicePool.Put(&histograms, q.memoryConsumptionTracker)
-		return nil
-	}
+	series := q.seriesMetadata[seriesIndex]
+	q.seriesMetadata[seriesIndex] = types.SeriesMetadata{} // Clear the original series metadata slice so we don't return the labels twice when the slice is returned later.
 
 	q.matrix = append(q.matrix, promql.Series{
 		Metric:     series.Labels,
@@ -267,6 +279,7 @@ func (q *Query) returnResultToPool() {
 		for _, s := range q.matrix {
 			types.FPointSlicePool.Put(&s.Floats, q.memoryConsumptionTracker)
 			types.HPointSlicePool.Put(&s.Histograms, q.memoryConsumptionTracker)
+			q.memoryConsumptionTracker.DecreaseMemoryConsumptionForLabels(s.Metric)
 		}
 
 		types.PutMatrix(q.matrix)
@@ -275,10 +288,14 @@ func (q *Query) returnResultToPool() {
 	}
 
 	if q.vector != nil {
+		for _, s := range q.vector {
+			q.memoryConsumptionTracker.DecreaseMemoryConsumptionForLabels(s.Metric)
+		}
+
 		types.VectorPool.Put(&q.vector, q.memoryConsumptionTracker)
 	}
 
-	// Nothing to do for scalars: we already returned the slice in populateScalarFromScalarOperator.
+	// Nothing to do for scalars: we already returned the slice in ScalarEvaluated.
 	q.scalar = nil
 
 	// And nothing to do for strings: these don't come from a pool.
