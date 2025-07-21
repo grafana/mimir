36e175bc8aa3585b49ae9fcbc68c9b2b865ed762
Jesus Vazquez <jesus.vazquez@grafana.com>
2025-07-21 15:35:51 +0200

    feat(parquet): Extend caching config to cache parquet labels files

--- pkg/storage/tsdb/caching_config_test.go
+++ pkg/storage/tsdb/caching_config_test.go
@@ -39,3 +39,16 @@ func TestIsBlockIndexFile(t *testing.T) {
 	assert.True(t, isBlockIndexFile(fmt.Sprintf("%s/index", blockID.String())))
 	assert.True(t, isBlockIndexFile(fmt.Sprintf("/%s/index", blockID.String())))
 }
+
+func TestIsParquetLabelsFile(t *testing.T) {
+	blockID := ulid.MustNew(1, nil)
+
+	assert.True(t, isParquetLabelsFile(fmt.Sprintf("tenant1/%s/0.labels.parquet", blockID.String())))
+	assert.True(t, isParquetLabelsFile(fmt.Sprintf("tenant1/%s/1.labels.parquet", blockID.String())))
+	assert.False(t, isParquetLabelsFile(fmt.Sprintf("tenant1/%s/labels.parquet", blockID.String())))
+	assert.False(t, isParquetLabelsFile(fmt.Sprintf("tenant1/%s/0.labels.parquet/something", blockID.String())))
+	assert.False(t, isParquetLabelsFile(fmt.Sprintf("tenant1/0.labels.parquet/%s/index", blockID.String())))
+	assert.False(t, isParquetLabelsFile(fmt.Sprintf("tenant1/%s/0.labels.parquet.tmp", blockID.String())))
+	assert.False(t, isParquetLabelsFile(fmt.Sprintf("tenant1/%s/something.0.labels.parquet", blockID.String())))
+	assert.False(t, isParquetLabelsFile(fmt.Sprintf("tenant1/%s/0.labels.parquet.gz", blockID.String())))
+}
