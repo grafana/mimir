533a99c3020e13ad1c6ff0ac3413f7fc29cb15f7
Andy Asp <andy.asp@grafana.com>
2025-07-28 15:59:48 -0400

    Separate plan tracker, needs plan job ownership check

--- pkg/compactor/scheduler/spawner.go
+++ pkg/compactor/scheduler/spawner.go
@@ -17,17 +17,13 @@ import (
 	"github.com/grafana/mimir/pkg/util"
 )
 
-type SpawnInformation struct {
-	lastPlanSubmitted time.Time
-	lastPlanCompleted time.Time // TODO: Feedback to know this
-}
-
 type Spawner struct {
 	services.Service
 
-	planMap map[string]*SpawnInformation
+	planMap map[string]time.Time
 
 	allowedTenants *util.AllowList
+	planTracker    *JobTracker[string, struct{}]
 	rotator        *Rotator
 	bkt            objstore.Bucket
 
@@ -42,11 +38,13 @@ func NewSpawner(
 	cfg Config,
 	allowList *util.AllowList,
 	rotator *Rotator,
+	planTracker *JobTracker[string, struct{}],
 	bkt objstore.Bucket,
 	logger log.Logger) *Spawner {
 	s := &Spawner{
 		allowedTenants:       allowList,
-		rotator:              rotator,
+		rotator: rotator,
+		planTracker:          planTracker,
 		bkt:                  bkt,
 		planningInterval:     cfg.planningInterval,
 		userDiscoveryBackoff: cfg.userDiscoveryBackoff,
@@ -77,18 +75,22 @@ func (s *Spawner) iter(ctx context.Context) error {
 }
 
 func (s *Spawner) plan() {
-	for tenant, info := range s.planMap {
-		now := time.Now() // declaring inside the loop because locks are used when submitting plans
-		if now.Sub(info.lastPlanSubmitted) > s.planningInterval {
-			job := NewJob(planningJobID, &CompactionJob{}, now)
-			accepted := s.rotator.OfferJobs(tenant, []*Job[string, *CompactionJob]{job}, func(_, _ *CompactionJob) bool {
-				return false
-			})
-			if accepted == 1 {
-				info.lastPlanSubmitted = now
-			}
+	jobs := make([]*Job[string, struct{}], 0, len(s.planMap))
+	now := time.Now()
+	for tenant, lastSubmitted := range s.planMap {
+		if now.Sub(lastSubmitted) > s.planningInterval {
+			jobs = append(jobs, NewJob(tenant, struct{}{}, now))
+			s.planMap[tenant] = now
 		}
 	}
+
+	// TODO: Can track how many were actually accepted with metrics
+	s.planTracker.Offer(
+		jobs,
+		func(_ struct{}, _ struct{}) bool {
+			return false
+		},
+	)
 }
 
 func (s *Spawner) discoverTenants(ctx context.Context) error {
@@ -109,12 +111,13 @@ func (s *Spawner) discoverTenants(ctx context.Context) error {
 
 		if _, ok := s.planMap[tenant]; !ok {
 			// Discovered a new tenant
-			s.planMap[tenant] = &SpawnInformation{}
+			s.planMap[tenant] = time.Time{}
 		}
 	}
 
 	for tenant := range s.planMap {
 		if _, ok := seen[tenant]; !ok {
+			s.planTracker.RemoveForcefully(tenant)
 			s.rotator.RemoveTenant(tenant)
 			delete(s.planMap, tenant)
 		}
