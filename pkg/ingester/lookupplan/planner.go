879b2e5c5ea528f8af51ebca349a30de66fa36fd
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2025-07-30 23:01:47 +0200

    Clean up planner
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/ingester/lookupplan/planner.go
+++ pkg/ingester/lookupplan/planner.go
@@ -3,14 +3,18 @@
 package lookupplan
 
 import (
+	"cmp"
 	"context"
 	"errors"
 	"fmt"
 	"slices"
 	"time"
 
+	"github.com/go-kit/log"
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/tsdb/index"
+
+	"github.com/grafana/mimir/pkg/util/spanlogger"
 )
 
 type NoopPlanner struct{}
@@ -32,54 +36,35 @@ func NewCostBasedPlanner(metrics Metrics, statistics Statistics) *CostBasedPlann
 	}
 }
 
-func (p CostBasedPlanner) PlanIndexLookup(ctx context.Context, plan index.LookupPlan, _, _ int64) (_ index.LookupPlan, retErr error) {
+func (p CostBasedPlanner) PlanIndexLookup(ctx context.Context, inPlan index.LookupPlan, _, _ int64) (_ index.LookupPlan, retErr error) {
+	var allPlans []plan
 	defer func(start time.Time) {
 		var abortedEarly bool
 		retErr, abortedEarly = mapPlanningOutcomeError(retErr)
-		p.recordPlanningOutcome(start, abortedEarly, retErr)
+		p.recordPlanningOutcome(ctx, start, abortedEarly, retErr, allPlans)
 	}(time.Now())
 
 	// Repartition the matchers. We don't trust other planners.
 	// Allocate a new slice so that we don't mess up the slice of the caller.
-	matchers := slices.Concat(plan.IndexMatchers(), plan.ScanMatchers())
+	matchers := slices.Concat(inPlan.IndexMatchers(), inPlan.ScanMatchers())
 	if len(matchers) > 10 {
-		return plan, errTooManyMatchers
+		return inPlan, errTooManyMatchers
 	}
 
-	allPlans, err := p.generatePlans(ctx, matchers)
+	var err error
+	allPlans, err = p.generatePlans(ctx, matchers)
 	if err != nil {
 		return nil, fmt.Errorf("error generating plans: %w", err)
 	}
 
-	cheapestPlan, lowestCost := allPlans[0], allPlans[0].totalCost()
-	for _, plan := range allPlans[1:] {
-		if pCost := plan.totalCost(); pCost < lowestCost {
-			cheapestPlan, lowestCost = plan, pCost
-		}
-	}
+	slices.SortFunc(allPlans, func(a, b plan) int {
+		return cmp.Compare(a.totalCost(), b.totalCost())
+	})
 
-	return cheapestPlan, nil
+	return allPlans[0], nil
 }
 
 var errTooManyMatchers = errors.New("too many matchers to generate plans")
-
-func mapPlanningOutcomeError(err error) (mappedError error, tooManyMatchers bool) {
-	if errors.Is(err, errTooManyMatchers) {
-		return nil, true
-	}
-	return err, false
-}
-
-func (p CostBasedPlanner) recordPlanningOutcome(start time.Time, abortedEarly bool, retErr error) {
-	outcome := "success"
-	switch {
-	case abortedEarly:
-		outcome = "aborted_due_to_too_many_matchers"
-	case retErr != nil:
-		outcome = "error"
-	}
-	p.metrics.planningDuration.WithLabelValues(outcome).Observe(time.Since(start).Seconds())
-}
 
 func (p CostBasedPlanner) generatePlans(ctx context.Context, matchers []*labels.Matcher) ([]plan, error) {
 	noopPlan, err := newScanOnlyPlan(ctx, matchers, p.stats)
@@ -105,4 +90,41 @@ func generatePredicateCombinations(plans []plan, currentPlan plan, decidedPredic
 	plans = generatePredicateCombinations(plans, p, decidedPredicates+1)
 
 	return plans
+}
+
+func mapPlanningOutcomeError(err error) (mappedError error, tooManyMatchers bool) {
+	if errors.Is(err, errTooManyMatchers) {
+		return nil, true
+	}
+	return err, false
+}
+
+func (p CostBasedPlanner) recordPlanningOutcome(ctx context.Context, start time.Time, abortedEarly bool, retErr error, allPlans []plan) {
+	logger := spanlogger.FromContext(ctx, log.NewNopLogger())
+
+	var outcome string
+	switch {
+	case abortedEarly:
+		outcome = "aborted_due_to_too_many_matchers"
+		logger.DebugLog("msg", "aborted creating lookup plan because there are too many matchers")
+	case retErr != nil:
+		outcome = "error"
+		logger.DebugLog("msg", "failed to create lookup plan", "err", retErr)
+	default:
+		outcome = "success"
+		const topKPlans = 3
+		logKvs := make([]any, 0, 2 /* msg */ +topKPlans*2 /* key+value */ *(5 /* cost */ +len(allPlans[0].predicates) /* matchers */))
+		logKvs = append(logKvs,
+			"msg", "selected lookup plan",
+		)
+		for i, plan := range allPlans[:min(topKPlans, len(allPlans))] {
+			planFieldPrefix := "selected_plan"
+			if i > 0 {
+				planFieldPrefix = fmt.Sprintf("plan_%d", i+1)
+			}
+			logKvs = plan.appendLogKVs(logKvs, planFieldPrefix)
+		}
+		logger.DebugLog(logKvs...)
+	}
+	p.metrics.planningDuration.WithLabelValues(outcome).Observe(time.Since(start).Seconds())
 }
