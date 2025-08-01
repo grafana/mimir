679635abf0d0bfa0b6837b58f3ad2430303070d1
Charles Korn <charles.korn@grafana.com>
2025-08-01 11:41:51 +1000

    Fix linting

--- pkg/frontend/querymiddleware/roundtrip_test.go
+++ pkg/frontend/querymiddleware/roundtrip_test.go
@@ -614,15 +614,15 @@ func TestMiddlewaresConsistency(t *testing.T) {
 		"remote read": {
 			instances: remoteReadMiddlewares,
 			exceptions: []string{
-				"querySharding",                         // No query sharding support.
-				"splitAndCacheMiddleware",               // No time splitting and results cache support.
-				"stepAlignMiddleware",                   // Not applicable because remote read requests don't take step in account when running in Mimir.
-				"pruneMiddleware",                       // No query pruning support.
-				"experimentalFunctionsMiddleware",       // No blocking for PromQL experimental functions as it is executed remotely.
-				"durationsMiddleware",                   // No duration expressions support.
-				"prom2RangeCompatHandler",               // No rewriting Prometheus 2 subqueries to Prometheus 3
-				"spinOffSubqueriesMiddleware",           // This middleware is only for instant queries.
-				"queryLimiterMiddleware",                // This middleware is only for instant queries.
+				"querySharding",                   // No query sharding support.
+				"splitAndCacheMiddleware",         // No time splitting and results cache support.
+				"stepAlignMiddleware",             // Not applicable because remote read requests don't take step in account when running in Mimir.
+				"pruneMiddleware",                 // No query pruning support.
+				"experimentalFunctionsMiddleware", // No blocking for PromQL experimental functions as it is executed remotely.
+				"durationsMiddleware",             // No duration expressions support.
+				"prom2RangeCompatHandler",         // No rewriting Prometheus 2 subqueries to Prometheus 3
+				"spinOffSubqueriesMiddleware",     // This middleware is only for instant queries.
+				"queryLimiterMiddleware",          // This middleware is only for instant queries.
 			},
 		},
 	}
