681fe36d92c5234cd380f753092518405ffbace6
Santiago <santiagohernandez.1997@gmail.com>
2025-08-07 13:43:32 +0200

    Alertmanager: Use the current configuration to start per-tenant Alermanagers after a request (#12255)
    When strict initialization is enabled, per-tenant Alertmanagers can be
    skipped if they have default configurations. We initialize these
    Alertmanagers using the fallback config in case we receive a request for
    a tenant.
    
    Some requests might require the Alertmanager to use the actual
    configuration we have stored for the tenant. E.g., a tenant using a
    default configuration might want to try an email contact point before
    saving it. In this case, we need any custom SMTP configs to be loaded
    before we send the test notification.
    
    This PR loads the configurations (Mimir, Grafana) from the database and
    uses them to start the Alertmanager for a tenant sending requests.
    
    #### Note for reviewers
    
    I'm using `fmt.Errorf` instead of `errors.Wrap`. The former is a
    function from the standard library; the latter is part of a 3rd party
    package ([pkg/errors](https://github.com/pkg/errors)) that was archived
    in 2021. We should stop using it.

--- pkg/alertmanager/multitenant.go
+++ pkg/alertmanager/multitenant.go
@@ -69,6 +69,7 @@ var (
 	errInvalidExternalURLMissingHostname   = errors.New("the configured external URL is invalid because it's missing the hostname")
 	errZoneAwarenessEnabledWithoutZoneInfo = errors.New("the configured alertmanager has zone awareness enabled but zone is not set")
 	errNotUploadingFallback                = errors.New("not uploading fallback configuration")
+	errConfigNotFound                      = errors.New("configuration not found")
 )
 
 // MultitenantAlertmanagerConfig is the configuration for a multitenant Alertmanager.
@@ -1196,9 +1197,9 @@ func (am *MultitenantAlertmanager) serveRequest(w http.ResponseWriter, req *http
 
 	// If the Alertmanager initialization was skipped, start the Alertmanager.
 	if ok := am.lastRequestTime.CompareAndSwap(userID, time.Time{}.Unix(), time.Now().Unix()); ok {
-		userAM, err = am.startAlertmanager(userID)
+		userAM, err = am.startAlertmanager(req.Context(), userID)
 		if err != nil {
-			if errors.Is(err, errNotUploadingFallback) {
+			if errors.Is(err, errNotUploadingFallback) || errors.Is(err, errConfigNotFound) {
 				level.Warn(am.logger).Log("msg", "not initializing Alertmanager", "user", userID, "err", err)
 				http.Error(w, "Not initializing the Alertmanager", http.StatusNotAcceptable)
 				return
@@ -1236,19 +1237,28 @@ func (am *MultitenantAlertmanager) serveRequest(w http.ResponseWriter, req *http
 }
 
 // startAlertmanager will start the Alertmanager for a tenant, using the fallback configuration if no config is found.
-func (am *MultitenantAlertmanager) startAlertmanager(userID string) (*Alertmanager, error) {
+func (am *MultitenantAlertmanager) startAlertmanager(ctx context.Context, userID string) (*Alertmanager, error) {
 	// Avoid starting the Alertmanager for tenants not owned by this instance.
 	if !am.isUserOwned(userID) {
 		am.lastRequestTime.Delete(userID)
 		return nil, errors.Wrap(errNotUploadingFallback, "user not owned by this instance")
 	}
 
-	amConfig := amConfig{
-		User:            userID,
-		RawConfig:       "",
-		Templates:       nil,
-		TmplExternalURL: am.cfg.ExternalURL.URL,
+	cfgMap, err := am.store.GetAlertConfigs(ctx, []string{userID})
+	if err != nil {
+		return nil, fmt.Errorf("failed to check for existing configuration: %w", err)
 	}
+
+	cfg, ok := cfgMap[userID]
+	if !ok {
+		return nil, errConfigNotFound
+	}
+
+	amConfig, _, err := am.computeConfig(cfg) // The second value indicates whether we should start the AM or not. Ignore it.
+	if err != nil {
+		return nil, fmt.Errorf("failed to compute config: %w", err)
+	}
+
 	if err := am.setConfig(amConfig); err != nil {
 		return nil, err
 	}
