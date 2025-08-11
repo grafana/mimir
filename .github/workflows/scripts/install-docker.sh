01566da8e0ca8d1b7fdfc9456b62c0eef9555cb4
Charles Korn <charles.korn@grafana.com>
2025-08-11 13:33:14 +1000

    Install version of Docker appropriate for current architecture

--- .github/workflows/scripts/install-docker.sh
+++ .github/workflows/scripts/install-docker.sh
@@ -3,6 +3,6 @@
 
 set -x
 VER="20.10.14"
-curl -L -o /tmp/docker-$VER.tgz https://download.docker.com/linux/static/stable/x86_64/docker-$VER.tgz
+curl -L -o /tmp/docker-$VER.tgz https://download.docker.com/linux/static/stable/$(uname -m)/docker-$VER.tgz
 tar -xz -C /tmp -f /tmp/docker-$VER.tgz
 mv /tmp/docker/* /usr/bin
