a3e3cecbb6c24f0e7f306878c7799899f7c2aab9
Alex Weaver <weaver.alex.d@gmail.com>
2025-08-12 15:27:01 -0500

    Make a consumer group and jump in batches per run

--- pkg/continuoustest/ingest_storage_record.go
+++ pkg/continuoustest/ingest_storage_record.go
@@ -2,6 +2,8 @@ package continuoustest
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"time"
@@ -11,14 +13,17 @@ import (
 	"github.com/grafana/mimir/pkg/storage/ingest"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/twmb/franz-go/pkg/kadm"
+	"github.com/twmb/franz-go/pkg/kerr"
 )
 
 type IngestStorageRecordTestConfig struct {
-	Kafka ingest.KafkaConfig `yaml:"-"`
+	Kafka         ingest.KafkaConfig `yaml:"-"`
+	ConsumerGroup string             `yaml:"consumer_group"`
 }
 
 func (cfg *IngestStorageRecordTestConfig) RegisterFlags(f *flag.FlagSet) {
 	// cfg.Kafka.RegisterFlagsWithPrefix("ingest-storage.kafka.", f)
+	f.StringVar(&cfg.ConsumerGroup, "tests.ingest-storage-record.consumer-group", "ingest-storage-record", "The Kafka consumer group used for getting/setting commmitted offsets.")
 }
 
 type IngestStorageRecordTest struct {
@@ -71,5 +76,49 @@ func (t *IngestStorageRecordTest) Run(ctx context.Context, now time.Time) error 
 	for _, to := range topics {
 		level.Info(t.logger).Log("msg", "detected topic", "topic", to.Topic)
 	}
+
+	offResp, err := t.adminClient.FetchOffsetsForTopics(ctx, t.cfg.ConsumerGroup, "ingest")
+	if err != nil {
+		if errors.Is(err, kerr.GroupIDNotFound) {
+			// ignore.
+		}
+		return fmt.Errorf("fetch offsets error: %w", err)
+	}
+	if err := offResp.Error(); err != nil {
+		return fmt.Errorf("fetch offsets response error: %w", err)
+	}
+	offsets := offResp.Offsets()
+
+	endOffsetsResp, err := t.adminClient.ListEndOffsets(ctx, "ingest")
+	if err != nil {
+		return fmt.Errorf("fetch end offsets error: %w", err)
+	}
+	if endOffsetsResp.Error() != nil {
+		return fmt.Errorf("fetch end offsets response error: %w", err)
+	}
+	endOffsets := endOffsetsResp.Offsets()
+
+	// Calculate the amount of jump per partition.
+	diffMap := make(map[int32]int64)
+	for topic, partOffsets := range offsets {
+		for partition, startOffset := range partOffsets {
+			if endPartOffset, ok := endOffsets[topic]; ok {
+				if endOffset, ok := endPartOffset[partition]; ok {
+					diffMap[partition] = endOffset.At - startOffset.At
+				} else {
+					diffMap[partition] = -1
+				}
+			}
+		}
+	}
+	js, err := json.Marshal(diffMap)
+	if err != nil {
+		return fmt.Errorf("marshal error: %w", err)
+	}
+	level.Info(t.logger).Log("msg", "grabbed batch", "diffsPerPartition", string(js))
+
+	// Update to the end.
+	t.adminClient.CommitOffsets(ctx, t.cfg.ConsumerGroup, endOffsets)
+
 	return nil
 }
