ef2bedd447eac014ac85ac3c3c7ee02a1d7b5a29
Alex Weaver <weaver.alex.d@gmail.com>
2025-08-13 17:34:09 -0500

    something with timeseries comparison

--- pkg/continuoustest/ingest_storage_record.go
+++ pkg/continuoustest/ingest_storage_record.go
@@ -290,10 +290,13 @@ func (t *IngestStorageRecordTest) testRec(rec *kgo.Record) error {
 	labelsComparer := cmp.Comparer(func(x, y mimirpb.LabelAdapter) bool {
 		return string(x.Name) == string(y.Name) && string(x.Value) == string(y.Value)
 	})
+	timeseriesComparer := cmp.Comparer(func(x, y *mimirpb.TimeSeries) bool {
+		return cmp.Equal(*x, *y, labelsComparer)
+	})
 	if len(req.Timeseries) != 0 || len(v2Req.Timeseries) != 0 {
 		t.metrics.recordsWithTimeseriesProcessedTotal.WithLabelValues(tenantID).Inc()
-		if !cmp.Equal(req.Timeseries, v2Req.Timeseries, labelsComparer) {
-			diff := cmp.Diff(req.Timeseries, v2Req.Timeseries, labelsComparer)
+		if !cmp.Equal(req.Timeseries, v2Req.Timeseries, labelsComparer, timeseriesComparer) {
+			diff := cmp.Diff(req.Timeseries, v2Req.Timeseries, labelsComparer, timeseriesComparer)
 			return fmt.Errorf("Timeseries did not match. Diff: %s", diff)
 		}
 	}
