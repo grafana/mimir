6868124df61e4767f31eb85931dbfa58265be335
zenador <zenador@users.noreply.github.com>
2025-08-14 11:16:52 +0800

    Add metrics for prune toggles optimization pass (#12375)
    #### What this PR does
    
    Add metrics for prune toggles optimization pass
    
    #### Which issue(s) this PR fixes or relates to
    
    Follow up to https://github.com/grafana/mimir/pull/12303
    
    #### Checklist
    
    - [x] Tests updated.
    - [ ] Documentation added.
    - [x] `CHANGELOG.md` updated - the order of entries should be
    `[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`. If changelog entry
    is not needed, please add the `changelog-not-needed` label to the PR.
    - [ ]
    [`about-versioning.md`](https://github.com/grafana/mimir/blob/main/docs/sources/mimir/configure/about-versioning.md)
    updated with experimental features.
    
    ---------
    
    Co-authored-by: Charles Korn <charleskorn@users.noreply.github.com>

--- pkg/frontend/querymiddleware/astmapper/astmapper.go
+++ pkg/frontend/querymiddleware/astmapper/astmapper.go
@@ -192,3 +192,31 @@ func (em ASTExprMapper) Map(ctx context.Context, expr parser.Expr) (parser.Expr,
 		return nil, errors.Errorf("ASTExprMapper: unhandled expr type %T", expr)
 	}
 }
+
+// ExprMapperWithState wraps ExprMapper to include a method for checking if the mapper has changed the expression.
+type ExprMapperWithState interface {
+	ExprMapper
+	// HasChanged returns whether the mapper made any changes during mapping
+	HasChanged() bool
+}
+
+// ASTExprMapperWithState is a wrapper around an ASTExprMapper that stores the original mapper to provide a method to check if any changes were made.
+type ASTExprMapperWithState struct {
+	mapper    ExprMapperWithState
+	astMapper ASTExprMapper
+}
+
+func NewASTExprMapperWithState(mapper ExprMapperWithState) *ASTExprMapperWithState {
+	return &ASTExprMapperWithState{
+		mapper:    mapper,
+		astMapper: NewASTExprMapper(mapper),
+	}
+}
+
+func (w *ASTExprMapperWithState) Map(ctx context.Context, expr parser.Expr) (parser.Expr, error) {
+	return w.astMapper.Map(ctx, expr)
+}
+
+func (w *ASTExprMapperWithState) HasChanged() bool {
+	return w.mapper.HasChanged()
+}
