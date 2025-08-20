7946779ce57911a7e71a7646183ce08abed42932
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2025-08-20 18:04:28 +0200

    index planning: implement cost model plan (#12199)
    This is a follow-up of https://github.com/grafana/mimir/pull/12197
    
    This PR introduces the plan. The plan is a collection of predicates.
    Some of which are used when reading the index and the rest are used for
    a sequential scan.
    
    
    The code right now is not used, but will be when
    https://github.com/grafana/mimir/issues/11919 is finished
    
    ### Cost
    
    The plan has a cost model. The model consist of
    
    - **index lookup cost**: performing index lookups for all predicates
    that use the index
    - **intersection cost**: cost of intersecting posting lists from
    multiple index predicates
    - **filter cost**: sequential scan i.e. cost of applying scan predicates
    to the fetched series)
    
    
    ### Testing
    The tests here use the same pattern as the ones in
    https://github.com/grafana/mimir/pull/12197. In short:
    
    1. Hard-coded mock statistics
    3. CSV file instead of typical golang table tests.
       
    
    #### Which issue(s) this PR fixes or relates to
    
    related to https://github.com/grafana/mimir/issues/11919
    
    #### Checklist
    
    - [x] Tests updated.
    - [ ] Documentation added.
    - [ ] `CHANGELOG.md` updated - the order of entries should be
    `[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`. If changelog entry
    is not needed, please add the `changelog-not-needed` label to the PR.
    - [ ]
    [`about-versioning.md`](https://github.com/grafana/mimir/blob/main/docs/sources/mimir/configure/about-versioning.md)
    updated with experimental features.
    
    ---------
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
    Co-authored-by: chencs <casie.chen@grafana.com>

---
+++ pkg/ingester/lookupplan/plan.go
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package lookupplan
+
+import (
+	"context"
+	"fmt"
+	"slices"
+
+	"github.com/prometheus/prometheus/model/labels"
+)
+
+const (
+	costPerIteratedPosting = 0.01
+)
+
+// plan is a representation of one way of executing a vector selector.
+type plan struct {
+	// predicates contains all the matchers converted to plan predicates with their statistics.
+	predicates []planPredicate
+	// indexPredicate tracks which predicates should use index lookups vs sequential scans.
+	// indexPredicate[i] == true means predicates[i] should use the index
+	indexPredicate []bool
+	// totalSeries is the total number of series in the index.
+	totalSeries uint64
+}
+
+// newScanOnlyPlan returns a plan in which all predicates would be used to scan and none to reach from the index.
+func newScanOnlyPlan(ctx context.Context, matchers []*labels.Matcher, stats Statistics) (plan, error) {
+	p := plan{
+		predicates:     make([]planPredicate, 0, len(matchers)),
+		indexPredicate: make([]bool, 0, len(matchers)),
+		totalSeries:    stats.TotalSeries(),
+	}
+	for _, m := range matchers {
+		pred, err := newPlanPredicate(ctx, m, stats)
+		if err != nil {
+			return plan{}, fmt.Errorf("error converting matcher to plan predicate: %w", err)
+		}
+		p.predicates = append(p.predicates, pred)
+		p.indexPredicate = append(p.indexPredicate, false)
+	}
+
+	return p, nil
+}
+
+func (p plan) IndexMatchers() []*labels.Matcher {
+	var matchers []*labels.Matcher
+	for i, pred := range p.predicates {
+		if p.indexPredicate[i] {
+			matchers = append(matchers, pred.matcher)
+		}
+	}
+	return matchers
+}
+
+func (p plan) ScanMatchers() []*labels.Matcher {
+	var matchers []*labels.Matcher
+	for i, pred := range p.predicates {
+		if !p.indexPredicate[i] {
+			matchers = append(matchers, pred.matcher)
+		}
+	}
+	return matchers
+}
+
+// useIndexFor returns a copy of this plan where predicate predicateIdx is used on the index.
+func (p plan) useIndexFor(predicateIdx int) plan {
+	p.indexPredicate = slices.Clone(p.indexPredicate)
+	p.indexPredicate[predicateIdx] = true
+	return p
+}
+
+// useScanFor returns a copy of this plan where the predicate at predicateIdx is used during sequential scanning.
+//
+//nolint:unused
+func (p plan) useScanFor(predicateIdx int) plan {
+	p.indexPredicate = slices.Clone(p.indexPredicate)
+	p.indexPredicate[predicateIdx] = false
+	return p
+}
+
+// totalCost returns the sum of indexLookupCost + intersectionCost + filterCost
+func (p plan) totalCost() float64 {
+	return p.indexLookupCost() + p.intersectionCost() + p.filterCost()
+}
+
+// indexLookupCost returns the cost of performing index lookups for all predicates that use the index
+func (p plan) indexLookupCost() float64 {
+	cost := 0.0
+	for i, pr := range p.predicates {
+		if p.indexPredicate[i] {
+			cost += pr.indexLookupCost()
+		}
+	}
+	return cost
+}
+
+// intersectionCost returns the cost of intersecting posting lists from multiple index predicates
+func (p plan) intersectionCost() float64 {
+	iteratedPostings := uint64(0)
+	for i, pred := range p.predicates {
+		if !p.indexPredicate[i] {
+			continue
+		}
+
+		iteratedPostings += pred.cardinality
+	}
+
+	return float64(iteratedPostings) * costPerIteratedPosting
+}
+
+// filterCost returns the cost of applying scan predicates to the fetched series
+func (p plan) filterCost() float64 {
+	cost := 0.0
+	fetchedSeries := p.intersectionSize()
+	for i, pred := range p.predicates {
+		// In reality, we will apply all the predicates for each series and stop once one predicate doesn't match.
+		// But we calculate for the worst case where we have to run all predicates for all series.
+		if !p.indexPredicate[i] {
+			cost += pred.filterCost(fetchedSeries)
+		}
+	}
+	return cost
+}
+
+func (p plan) intersectionSize() uint64 {
+	finalSelectivity := 1.0
+	for i, pred := range p.predicates {
+		if !p.indexPredicate[i] {
+			continue
+		}
+
+		// We use the selectivity across all series instead of the selectivity across label values.
+		// For example, if {protocol=~.*} matches all values, it doesn't mean it won't reduce the result set after intersection.
+		//
+		// We also assume independence between the predicates. This is a simplification.
+		// For example, the selectivity of {pod=~prometheus.*} doesn't depend on if we have already applied {statefulset=prometheus}.
+		// While finalSelectivity is neither an upper bound nor a lower bound, assuming independence allows us to come up with cost estimates comparable between plans.
+		finalSelectivity *= float64(pred.cardinality) / float64(p.totalSeries)
+	}
+	return uint64(finalSelectivity * float64(p.totalSeries))
+}
+
+// cardinality returns an estimate of the total number of series that this plan would return.
+func (p plan) cardinality() uint64 {
+	finalSelectivity := 1.0
+	for _, pred := range p.predicates {
+		// We use the selectivity across all series instead of the selectivity across label values.
+		// For example, if {protocol=~.*} matches all values, it could still reduce the result set after intersection.
+		//
+		// We also assume independence between the predicates. This is a simplification.
+		// For example, the selectivity of {pod=~prometheus.*} doesn't depend on if we have already applied {statefulset=prometheus}.
+		finalSelectivity *= float64(pred.cardinality) / float64(p.totalSeries)
+	}
+	return uint64(finalSelectivity * float64(p.totalSeries))
+}
