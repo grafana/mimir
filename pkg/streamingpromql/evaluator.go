da0991ec48610bb68a34932de8f12fab1e12df99
Charles Korn <charleskorn@users.noreply.github.com>
2025-08-26 10:46:30 +1000

    Update pkg/streamingpromql/evaluator.go
    Co-authored-by: Jon Kartago Lamida <jon.lamida@grafana.com>

--- pkg/streamingpromql/evaluator.go
+++ pkg/streamingpromql/evaluator.go
@@ -121,7 +121,7 @@ func (e *Evaluator) Evaluate(ctx context.Context, observer EvaluationObserver) e
 	}
 
 	if e.engine.pedantic {
-		// Finalize the root operator a second time to ensure all operators behave correctly if Close is called multiple times.
+		// Finalize the root operator a second time to ensure all operators behave correctly if Finalize is called multiple times.
 		if err := e.root.Finalize(ctx); err != nil {
 			return fmt.Errorf("pedantic mode: failed to finalize operator a second time after successfully finalizing the first time: %w", err)
 		}
