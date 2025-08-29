d592ad7b464e59898e1b6e99320726cca3603c5c
Charles Korn <charles.korn@grafana.com>
2025-08-29 13:30:26 +1000

    Introduce roundtripper

--- pkg/mimir/modules.go
+++ pkg/mimir/modules.go
@@ -853,6 +853,8 @@ func (t *Mimir) initQueryFrontendTripperware() (serv services.Service, err error
 		eng,
 		promOpts,
 		t.QueryFrontendTopicOffsetsReaders,
+		t.Cfg.Frontend.EnableRemoteExecution,
+		t.QueryFrontendStreamingEngine,
 		t.Registerer,
 	)
 	if err != nil {
