5965eace9ba4b458a03fd6beb2c5be6096e670e5
Innokentii Konstantinov <innokenty.konstantinov@grafana.com>
2025-09-18 22:44:10 +0800

    Do not wrap vector-scalar comparison in DeduplicateAndMerge.
    It doesn't drop names, so it will not produce duplicates. Therefore, DeduplicateAndMerge is not needed.
    
    Signed-off-by: Innokentii Konstantinov <innokenty.konstantinov@grafana.com>

--- pkg/streamingpromql/planning.go
+++ pkg/streamingpromql/planning.go
@@ -409,6 +409,15 @@ func (p *QueryPlanner) nodeFromExpr(expr parser.Expr) (planning.Node, error) {
 			(lhsType == parser.ValueTypeScalar && rhsType == parser.ValueTypeVector)
 
 		if isVectorScalar {
+			// Comparison vector-scalar operations without bool modifier don't drop the __name__ label.
+			// So don't need to wrap in DeduplicateAndMerge.
+			isComparison := expr.Op == parser.EQLC || expr.Op == parser.NEQ ||
+				expr.Op == parser.LTE || expr.Op == parser.LSS ||
+				expr.Op == parser.GTE || expr.Op == parser.GTR
+			if isComparison && !expr.ReturnBool {
+				return binExpr, nil
+			}
+
 			return &core.DeduplicateAndMerge{
 				Inner:                      binExpr,
 				DeduplicateAndMergeDetails: &core.DeduplicateAndMergeDetails{},
