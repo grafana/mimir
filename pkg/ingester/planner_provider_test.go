139bf760766a019fd9235ecbe1961b48412759a4
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2025-09-19 12:25:55 +0200

    Use simpler mocks
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/ingester/planner_provider_test.go
+++ pkg/ingester/planner_provider_test.go
@@ -9,27 +9,31 @@ import (
 	"github.com/prometheus/prometheus/tsdb"
 	"github.com/prometheus/prometheus/tsdb/index"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/mimir/pkg/ingester/lookupplan"
 )
 
-// MockPlannerFactory is a mock implementation of iPlannerFactory
-type MockPlannerFactory struct {
-	mock.Mock
+// mockPlannerFactory is a hand-written mock implementation of iPlannerFactory
+type mockPlannerFactory struct {
+	createPlannerFunc func(meta tsdb.BlockMeta, reader tsdb.IndexReader) index.LookupPlanner
 }
 
-func (m *MockPlannerFactory) CreatePlanner(meta tsdb.BlockMeta, reader tsdb.IndexReader) index.LookupPlanner {
-	args := m.Called(meta, reader)
-	return args.Get(0).(index.LookupPlanner)
+func (m *mockPlannerFactory) CreatePlanner(meta tsdb.BlockMeta, reader tsdb.IndexReader) index.LookupPlanner {
+	return m.createPlannerFunc(meta, reader)
 }
 
 func TestPlannerProvider_getPlanner_DoesNotCachePlanners(t *testing.T) {
 	blockID := ulid.MustNew(1, nil)
 	expectedPlanner := &lookupplan.CostBasedPlanner{}
-	mockFactory := &MockPlannerFactory{}
-	mockFactory.On("CreatePlanner", mock.AnythingOfType("tsdb.BlockMeta"), mock.AnythingOfType("*ingester.mockIndex")).Return(expectedPlanner).Twice()
+	callCount := 0
+
+	mockFactory := &mockPlannerFactory{
+		createPlannerFunc: func(meta tsdb.BlockMeta, reader tsdb.IndexReader) index.LookupPlanner {
+			callCount++
+			return expectedPlanner
+		},
+	}
 
 	blockMeta := tsdb.BlockMeta{
 		ULID: blockID,
@@ -46,14 +50,21 @@ func TestPlannerProvider_getPlanner_DoesNotCachePlanners(t *testing.T) {
 	resultPlanner = provider.getPlanner(blockMeta, &mockIndex{})
 	require.NotNil(t, resultPlanner, "should return a planner")
 	assert.Equal(t, expectedPlanner, resultPlanner, "should return planner from factory")
-	mockFactory.AssertExpectations(t)
+
+	assert.Equal(t, 2, callCount, "CreatePlanner should be called twice (no caching)")
 }
 
 func TestPlannerProvider_generateAndStorePlanner_CachesPlanners(t *testing.T) {
 	blockID := ulid.MustNew(1, nil)
 	expectedPlanner := &lookupplan.CostBasedPlanner{}
-	mockFactory := &MockPlannerFactory{}
-	mockFactory.On("CreatePlanner", mock.AnythingOfType("tsdb.BlockMeta"), mock.AnythingOfType("*ingester.mockIndex")).Return(expectedPlanner).Once()
+	callCount := 0
+
+	mockFactory := &mockPlannerFactory{
+		createPlannerFunc: func(meta tsdb.BlockMeta, reader tsdb.IndexReader) index.LookupPlanner {
+			callCount++
+			return expectedPlanner
+		},
+	}
 
 	blockMeta := tsdb.BlockMeta{
 		ULID: blockID,
@@ -68,5 +79,5 @@ func TestPlannerProvider_generateAndStorePlanner_CachesPlanners(t *testing.T) {
 
 	require.NotNil(t, resultPlanner, "should return a planner")
 	assert.Equal(t, expectedPlanner, resultPlanner, "should return cached planner")
-	mockFactory.AssertNotCalled(t, "CreatePlanner")
+	assert.Equal(t, 1, callCount, "CreatePlanner should be called only once (planner is cached)")
 }
