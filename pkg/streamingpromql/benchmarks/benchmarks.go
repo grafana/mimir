12d0870492a42186eae0a2268d8c017c75842006
Jon Kartago Lamida <me@lamida.net>
2025-09-24 16:33:23 +0800

    Fix format

--- pkg/streamingpromql/benchmarks/benchmarks.go
+++ pkg/streamingpromql/benchmarks/benchmarks.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"github.com/grafana/mimir/pkg/util/limiter"
+
 	"github.com/prometheus/prometheus/promql"
 	"github.com/prometheus/prometheus/storage"
 	"github.com/stretchr/testify/require"
