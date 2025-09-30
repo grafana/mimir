grafana/mimir

commit f98392d0c12a217ee3f084bfc369781e8d237f89

Move pruning middleware into prune toggles AST optimisation pass (#12303)
#### What this PR does

Move pruning middleware into prune toggles AST optimisation pass.

#### Which issue(s) this PR fixes or relates to

Split from https://github.com/grafana/mimir/pull/11971

#### Checklist

- [x] Tests updated.
- [x] Documentation added.
- [x] `CHANGELOG.md` updated - the order of entries should be
`[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`. If changelog entry
is not needed, please add the `changelog-not-needed` label to the PR.
- [ ]
[`about-versioning.md`](https://github.com/grafana/mimir/blob/main/docs/sources/mimir/configure/about-versioning.md)
updated with experimental features.

---------

Co-authored-by: Charles Korn <charleskorn@users.noreply.github.com>

pkg/streamingpromql/optimize/ast/prune_toggles.go
@@ -1,0 +1,129 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package ast
+
+import (
+	"context"
+
+	"github.com/prometheus/prometheus/promql/parser"
+
+	"github.com/grafana/mimir/pkg/frontend/querymiddleware/astmapper"
+)
+
+// PruneToggles optimizes queries by pruning expressions that are toggled off, only
+// targeting a very specific query pattern that is commonly used to toggle between
+// classic and native histograms in our dashboards.
+// e.g. `(avg(rate(foo[1m]))) and on() (vector(1) == 1)` -> `(avg(rate(foo[1m])))`
+// e.g. `(avg(rate(foo[1m]))) and on() (vector(1) == -1)` -> `(vector(1) == -1)`
+type PruneToggles struct {
+	mapper astmapper.ASTMapper
+}
+
+func NewPruneToggles() *PruneToggles {
+	mapper := astmapper.NewASTExprMapper(&pruneToggles{})
+	return &PruneToggles{
+		mapper: mapper,
+	}
+}
+
+func (p *PruneToggles) Name() string {
+	return "Toggled off expressions pruning"
+}
+
+func (p *PruneToggles) Apply(ctx context.Context, expr parser.Expr) (parser.Expr, error) {
+	return p.mapper.Map(ctx, expr)
+}
+
+type pruneToggles struct{}
+
+func (mapper *pruneToggles) MapExpr(ctx context.Context, expr parser.Expr) (mapped parser.Expr, finished bool, err error) {
+	e, ok := expr.(*parser.BinaryExpr)
+	if !ok {
+		return expr, false, nil
+	}
+
+	if e.Op != parser.LAND || e.VectorMatching == nil ||
+		!e.VectorMatching.On || len(e.VectorMatching.MatchingLabels) != 0 {
+		// Return if not "<lhs> and on() <rhs>"
+		return expr, false, nil
+	}
+
+	isConst, isEmpty := mapper.isConst(e.RHS)
+	if !isConst {
+		return expr, false, nil
+	}
+	if isEmpty {
+		// The right hand side is empty, so the whole expression is empty due to
+		// "and on()", return the right hand side.
+		return e.RHS, false, nil
+	}
+	// The right hand side is const and not empty, so the whole expression is
+	// just the left side.
+	return e.LHS, false, nil
+}
+
+func (mapper *pruneToggles) isConst(expr parser.Expr) (isConst, isEmpty bool) {
+	var lhs, rhs parser.Expr
+	switch e := expr.(type) {
+	case *parser.ParenExpr:
+		return mapper.isConst(e.Expr)
+	case *parser.BinaryExpr:
+		if e.Op != parser.EQLC || e.ReturnBool {
+			return false, false
+		}
+		lhs = e.LHS
+		rhs = e.RHS
+	default:
+		return false, false
+	}
+
+	if vectorAndNumber, equals := mapper.isVectorAndNumberEqual(lhs, rhs); vectorAndNumber {
+		return true, !equals
+	}
+	if vectorAndNumber, equals := mapper.isVectorAndNumberEqual(rhs, lhs); vectorAndNumber {
+		return true, !equals
+	}
+	return false, false
+}
+
+// isVectorAndNumberEqual returns whether the lhs is a const vector like
+// "vector(5)"" and the right hand size is a number like "2". Also returns
+// if the values are equal.
+func (mapper *pruneToggles) isVectorAndNumberEqual(lhs, rhs parser.Expr) (bool, bool) {
+	lIsVector, lValue := mapper.isConstVector(lhs)
+	if !lIsVector {
+		return false, false
+	}
+	rIsConst, rValue := mapper.isNumber(rhs)
+	if !rIsConst {
+		return false, false
+	}
+	return true, rValue == lValue
+}
+
+func (mapper *pruneToggles) isConstVector(expr parser.Expr) (isVector bool, value float64) {
+	switch e := expr.(type) {
+	case *parser.ParenExpr:
+		return mapper.isConstVector(e.Expr)
+	case *parser.Call:
+		if e.Func.Name != "vector" || len(e.Args) != 1 {
+			return false, 0
+		}
+		lit, ok := e.Args[0].(*parser.NumberLiteral)
+		if !ok {
+			return false, 0
+		}
+		return true, lit.Val
+	}
+	return false, 0
+}
+
+func (mapper *pruneToggles) isNumber(expr parser.Expr) (isNumber bool, value float64) {
+	switch e := expr.(type) {
+	case *parser.ParenExpr:
+		return mapper.isNumber(e.Expr)
+	case *parser.NumberLiteral:
+		return true, e.Val
+	}
+	return false, 0
+}

pkg/streamingpromql/optimize/ast/prune_toggles_test.go
@@ -1,0 +1,87 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package ast_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/prometheus/prometheus/promql/parser"
+	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/mimir/pkg/streamingpromql/optimize/ast"
+)
+
+var testCasesPruneToggles = map[string]string{
+	// Non-prunable expressions.
+	`foo`:                `foo`,
+	`foo[2m]`:            `foo[2m]`,
+	`foo{bar="baz"}[2m]`: `foo{bar="baz"}[2m]`,
+	`foo{bar="baz"}`:     `foo{bar="baz"}`,
+	`quantile(0.9,foo)`:  `quantile(0.9,foo)`,
+	`histogram_quantile(0.5, rate(bar1{baz="blip"}[30s]))`: `histogram_quantile(0.5, rate(bar1{baz="blip"}[30s]))`,
+	`count(up)`:          `count(up)`,
+	`avg(rate(foo[2m]))`: `avg(rate(foo[2m]))`,
+	`vector(0)`:          `vector(0)`,
+	`avg(rate(foo[2m])) or avg(rate(bar[2m]))`: `avg(rate(foo[2m])) or avg(rate(bar[2m]))`,
+	// The const expression is on the wrong side.
+	`(vector(0) == 1) and on() (avg(rate(foo[2m])))`: `(vector(0) == 1) and on() (avg(rate(foo[2m])))`,
+	// Matching on labels.
+	`(avg(rate(foo[2m]))) and on(id) (vector(0) == 1)`: `(avg(rate(foo[2m]))) and on(id) (vector(0) == 1)`,
+	// Not "on" expression.
+	`(avg(rate(foo[2m]))) and ignoring() (vector(0) == 1)`: `(avg(rate(foo[2m]))) and ignoring() (vector(0) == 1)`,
+	// Pruned expressions.
+	`(avg(rate(foo[2m]))) and on() (vector(0) == 1)`:     `(vector(0) == 1)`,
+	`(avg(rate(foo[2m]))) and on() (vector(1) == 1)`:     `(avg(rate(foo[2m])))`,
+	`(avg(rate(foo[2m]))) and on() (vector(3) == 4.5)`:   `(vector(3) == 4.5)`,
+	`(avg(rate(foo[2m]))) and on() (vector(5.5) == 5.5)`: `(avg(rate(foo[2m])))`,
+	// "and on()" is not on top level, "or" has lower precedence.
+	// This could be further reduced by dropping the vector(0)==1 from
+	// the "or", but it is intentionally not done as that would
+	// complicate the algorithm and isn't required to reduce chunks
+	// loading.
+	`(avg(rate(foo[2m]))) and on() (vector(0) == 1) or avg(rate(bar[2m]))`: `(vector(0) == 1) or avg(rate(bar[2m]))`,
+	// "and on()" is not on top level, due to left-right associativity.
+	`(avg(rate(foo[2m]))) and on() (vector(0) == 1) and avg(rate(bar[2m]))`: `(vector(0) == 1) and avg(rate(bar[2m]))`,
+	// "and on()" is not on top level.
+	// This could be further reduced by dropping the vector(0)==1 from
+	// the "or", but it is intentionally not done as that would
+	// complicate the algorithm and isn't required to reduce chunks
+	// loading.
+	`(avg(rate(foo[2m]))) and on() (vector(0) == 1) or avg(rate(bar[2m])) and on() (vector(1) == 1)`: `(vector(0) == 1) or avg(rate(bar[2m]))`,
+}
+
+func TestPruneToggles(t *testing.T) {
+	optimizer := ast.NewPruneToggles()
+	ctx := context.Background()
+
+	for input, expected := range testCasesPruneToggles {
+		t.Run(input, func(t *testing.T) {
+			expectedExpr, err := parser.ParseExpr(expected)
+			require.NoError(t, err)
+
+			inputExpr, err := parser.ParseExpr(input)
+			require.NoError(t, err)
+			outputExpr, err := optimizer.Apply(ctx, inputExpr)
+			require.NoError(t, err)
+
+			require.Equal(t, expectedExpr.String(), outputExpr.String())
+		})
+	}
+}
+
+func TestPruneTogglesWithData(t *testing.T) {
+	testASTOptimizationPassWithData(t, `
+		load 1m
+			foo{series="1"} 0+1x<num samples>
+			foo{series="2"} 0+2x<num samples>
+			foo{series="3"} 0+3x<num samples>
+			foo{series="4"} 0+4x<num samples>
+			foo{series="5"} 0+5x<num samples>
+			bar{series="1"} 0+6x<num samples>
+			bar{series="2"} 0+7x<num samples>
+			bar{series="3"} 0+8x<num samples>
+			bar{series="4"} 0+9x<num samples>
+			bar{series="5"} 0+10x<num samples>
+	`, testCasesPruneToggles)
+}

pkg/streamingpromql/optimize/ast/util_test.go
@@ -1,0 +1,60 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package ast_test
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/grafana/dskit/user"
+	"github.com/prometheus/prometheus/model/timestamp"
+	"github.com/prometheus/prometheus/promql"
+	"github.com/prometheus/prometheus/promql/promqltest"
+	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/mimir/pkg/streamingpromql"
+	"github.com/grafana/mimir/pkg/streamingpromql/testutils"
+)
+
+func testASTOptimizationPassWithData(t *testing.T, loadTemplate string, testCases map[string]string) {
+	numSamples := 100
+	replacer := strings.NewReplacer("<num samples>", fmt.Sprintf("%d", numSamples))
+	data := replacer.Replace(loadTemplate)
+
+	const step = 20 * time.Second
+
+	queryable := promqltest.LoadedStorage(t, data)
+
+	// Use Prometheus's query engine to execute the queries, ensuring that our query rewriting
+	// produces the same results as the original queries without any added optimizations
+	// on the query engine.
+	engine := promql.NewEngine(streamingpromql.NewTestEngineOpts().CommonOpts)
+
+	for input, expected := range testCases {
+		if input == expected {
+			continue
+		}
+		t.Run(input, func(t *testing.T) {
+			ctx := user.InjectOrgID(context.Background(), "test")
+			startTime := timestamp.Time(0)
+			endTime := startTime.Add(time.Duration(numSamples) * time.Minute)
+
+			qInput, err := engine.NewRangeQuery(ctx, queryable, nil, input, startTime, endTime, step)
+			require.NoError(t, err)
+			t.Cleanup(qInput.Close)
+			resInput := qInput.Exec(context.Background())
+			require.NoError(t, resInput.Err)
+
+			qRewritten, err := engine.NewRangeQuery(ctx, queryable, nil, expected, startTime, endTime, step)
+			require.NoError(t, err)
+			t.Cleanup(qRewritten.Close)
+			resRewritten := qRewritten.Exec(context.Background())
+			require.NoError(t, resRewritten.Err)
+
+			testutils.RequireEqualResults(t, "", resInput, resRewritten, true)
+		})
+	}
+}

cmd/mimir/help-all.txt.tmpl
@@ -2071,6 +2071,8 @@ Usage of ./cmd/mimir/mimir:
     	[experimental] Enable common subexpression elimination when evaluating queries. (default true)
   -querier.mimir-query-engine.enable-common-subexpression-elimination-for-range-vector-expressions-in-instant-queries
     	[experimental] Enable common subexpression elimination for range vector expressions when evaluating instant queries. This has no effect if common subexpression elimination is disabled. (default true)
+  -querier.mimir-query-engine.enable-prune-toggles
+    	[experimental] Enable pruning query expressions that are toggled off with constants. (default true)
   -querier.mimir-query-engine.enable-skipping-histogram-decoding
     	[experimental] Enable skipping decoding native histograms when evaluating queries that do not require full histograms. (default true)
   -querier.minimize-ingester-requests
@@ -2285,8 +2287,6 @@ Usage of ./cmd/mimir/mimir:
     	True to enable query sharding.
   -query-frontend.prom2-range-compat
     	[experimental] Rewrite queries using the same range selector and resolution [X:X] which don't work in Prometheus 3.0 to a nearly identical form that works with Prometheus 3.0 semantics
-  -query-frontend.prune-queries
-    	[experimental] True to enable pruning dead code (eg. expressions that cannot produce any results) and simplifying expressions (eg. expressions that can be evaluated immediately) in queries.
   -query-frontend.querier-forget-delay duration
     	[experimental] If a querier disconnects without sending notification about graceful shutdown, the query-frontend will keep the querier in the tenant's shard until the forget delay has passed. This feature is useful to reduce the blast radius when shuffle-sharding is enabled.
   -query-frontend.query-engine string

pkg/frontend/querymiddleware/astmapper/astmapper.go
@@ -169,6 +169,14 @@ func (em ASTExprMapper) Map(ctx context.Context, expr parser.Expr) (parser.Expr,
 		e.Expr = mapped
 		return e, nil
 
+	case *parser.StepInvariantExpr:
+		mapped, err := em.Map(ctx, e.Expr)
+		if err != nil {
+			return nil, err
+		}
+		e.Expr = mapped
+		return e, nil
+
 	case *parser.MatrixSelector:
 		mapped, err := em.Map(ctx, e.VectorSelector)
 		if err != nil {

pkg/frontend/querymiddleware/astmapper/pruning.go
@@ -1,113 +1,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-
-package astmapper
-
-import (
-	"context"
-
-	"github.com/go-kit/log"
-	"github.com/prometheus/prometheus/promql/parser"
-)
-
-func NewQueryPruner(logger log.Logger) ASTMapper {
-	pruner := &queryPruner{
-		logger: logger,
-	}
-	return NewASTExprMapper(pruner)
-}
-
-type queryPruner struct {
-	logger log.Logger
-}
-
-func (pruner *queryPruner) MapExpr(ctx context.Context, expr parser.Expr) (mapped parser.Expr, finished bool, err error) {
-	e, ok := expr.(*parser.BinaryExpr)
-	if !ok {
-		return expr, false, nil
-	}
-
-	if e.Op != parser.LAND || e.VectorMatching == nil ||
-		!e.VectorMatching.On || len(e.VectorMatching.MatchingLabels) != 0 {
-		// Return if not "<lhs> and on() <rhs>"
-		return expr, false, nil
-	}
-
-	isConst, isEmpty := pruner.isConst(e.RHS)
-	if !isConst {
-		return expr, false, nil
-	}
-	if isEmpty {
-		// The right hand side is empty, so the whole expression is empty due to
-		// "and on()", return the right hand side.
-		return e.RHS, false, nil
-	}
-	// The right hand side is const and not empty, so the whole expression is
-	// just the left side.
-	return e.LHS, false, nil
-}
-
-func (pruner *queryPruner) isConst(expr parser.Expr) (isConst, isEmpty bool) {
-	var lhs, rhs parser.Expr
-	switch e := expr.(type) {
-	case *parser.ParenExpr:
-		return pruner.isConst(e.Expr)
-	case *parser.BinaryExpr:
-		if e.Op != parser.EQLC || e.ReturnBool {
-			return false, false
-		}
-		lhs = e.LHS
-		rhs = e.RHS
-	default:
-		return false, false
-	}
-
-	if vectorAndNumber, equals := pruner.isVectorAndNumberEqual(lhs, rhs); vectorAndNumber {
-		return true, !equals
-	}
-	if vectorAndNumber, equals := pruner.isVectorAndNumberEqual(rhs, lhs); vectorAndNumber {
-		return true, !equals
-	}
-	return false, false
-}
-
-// isVectorAndNumberEqual returns whether the lhs is a const vector like
-// "vector(5)"" and the right hand size is a number like "2". Also returns
-// if the values are equal.
-func (pruner *queryPruner) isVectorAndNumberEqual(lhs, rhs parser.Expr) (bool, bool) {
-	lIsVector, lValue := pruner.isConstVector(lhs)
-	if !lIsVector {
-		return false, false
-	}
-	rIsConst, rValue := pruner.isNumber(rhs)
-	if !rIsConst {
-		return false, false
-	}
-	return true, rValue == lValue
-}
-
-func (pruner *queryPruner) isConstVector(expr parser.Expr) (isVector bool, value float64) {
-	switch e := expr.(type) {
-	case *parser.ParenExpr:
-		return pruner.isConstVector(e.Expr)
-	case *parser.Call:
-		if e.Func.Name != "vector" || len(e.Args) != 1 {
-			return false, 0
-		}
-		lit, ok := e.Args[0].(*parser.NumberLiteral)
-		if !ok {
-			return false, 0
-		}
-		return true, lit.Val
-	}
-	return false, 0
-}
-
-func (pruner *queryPruner) isNumber(expr parser.Expr) (isNumber bool, value float64) {
-	switch e := expr.(type) {
-	case *parser.ParenExpr:
-		return pruner.isNumber(e.Expr)
-	case *parser.NumberLiteral:
-		return true, e.Val
-	}
-	return false, 0
-}

pkg/frontend/querymiddleware/astmapper/pruning_test.go
@@ -1,189 +1,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-
-package astmapper
-
-import (
-	"context"
-	"testing"
-
-	"github.com/go-kit/log"
-	"github.com/prometheus/prometheus/promql/parser"
-	"github.com/stretchr/testify/require"
-)
-
-func TestQueryPruner(t *testing.T) {
-	pruner := NewQueryPruner(log.NewNopLogger())
-	ctx := context.Background()
-
-	for _, tt := range []struct {
-		in  string
-		out string
-	}{
-		// Non-prunable expressions.
-		{
-			`foo`,
-			`foo`,
-		},
-		{
-			`foo[1m]`,
-			`foo[1m]`,
-		},
-		{
-			`foo{bar="baz"}[1m]`,
-			`foo{bar="baz"}[1m]`,
-		},
-		{
-			`foo{bar="baz"}`,
-			`foo{bar="baz"}`,
-		},
-		{
-			`quantile(0.9,foo)`,
-			`quantile(0.9,foo)`,
-		},
-		{
-			`histogram_quantile(0.5, rate(bar1{baz="blip"}[30s]))`,
-			`histogram_quantile(0.5, rate(bar1{baz="blip"}[30s]))`,
-		},
-		{
-			`count(up)`,
-			`count(up)`,
-		},
-		{
-			`avg(rate(foo[1m]))`,
-			`avg(rate(foo[1m]))`,
-		},
-		{
-			`vector(0)`,
-			`vector(0)`,
-		},
-		{
-			`up < -Inf`,
-			`up < -Inf`,
-		},
-		{
-			`avg(rate(foo[1m])) < (-Inf)`,
-			`avg(rate(foo[1m])) < (-Inf)`,
-		},
-		{
-			`Inf * -1`,
-			`Inf * -1`,
-		},
-		{
-			`avg(rate(foo[1m])) < (-1 * +Inf)`,
-			`avg(rate(foo[1m])) < (-1 * +Inf)`,
-		},
-		{
-			`(-1 * -Inf) < avg(rate(foo[1m]))`,
-			`(-1 * -Inf) < avg(rate(foo[1m]))`,
-		},
-		{
-			`vector(0) < -Inf or avg(rate(foo[1m]))`,
-			`vector(0) < -Inf or avg(rate(foo[1m]))`,
-		},
-		{
-			`avg(rate(foo[1m])) or vector(0) < -Inf`,
-			`avg(rate(foo[1m])) or vector(0) < -Inf`,
-		},
-		{
-			`avg(rate(foo[1m])) or (vector(0) < -Inf)`,
-			`avg(rate(foo[1m])) or (vector(0) < -Inf)`,
-		},
-		{
-			`((-1 * -Inf) < avg(rate(foo[2m]))) or avg(rate(foo[1m]))`,
-			`((-1 * -Inf) < avg(rate(foo[2m]))) or avg(rate(foo[1m]))`,
-		},
-		{
-			`(avg(rate(foo[2m])) < (+1 * -Inf)) or avg(rate(foo[1m]))`,
-			`(avg(rate(foo[2m])) < (+1 * -Inf)) or avg(rate(foo[1m]))`,
-		},
-		{
-			`((-1 * -Inf) == avg(rate(foo[2m]))) or avg(rate(foo[1m]))`,
-			`((-1 * -Inf) == avg(rate(foo[2m]))) or avg(rate(foo[1m]))`,
-		},
-		{
-			`avg(rate(foo[1m])) unless ((-1 * -Inf) < avg(rate(foo[2m])))`,
-			`avg(rate(foo[1m])) unless ((-1 * -Inf) < avg(rate(foo[2m])))`,
-		},
-		{
-			`((2 * +Inf) < avg(rate(foo[2m]))) or avg(rate(foo[1m]))`,
-			`((2 * +Inf) < avg(rate(foo[2m]))) or avg(rate(foo[1m]))`,
-		},
-		{
-			`(((-1 * -Inf) < avg(rate(foo[3m]))) unless avg(rate(foo[2m]))) or avg(rate(foo[1m]))`,
-			`(((-1 * -Inf) < avg(rate(foo[3m]))) unless avg(rate(foo[2m]))) or avg(rate(foo[1m]))`,
-		},
-		{
-			`((((-1 * -Inf) < avg(rate(foo[4m]))) unless avg(rate(foo[3m]))) and avg(rate(foo[2m]))) or avg(rate(foo[1m]))`,
-			`((((-1 * -Inf) < avg(rate(foo[4m]))) unless avg(rate(foo[3m]))) and avg(rate(foo[2m]))) or avg(rate(foo[1m]))`,
-		},
-		{
-			`avg(rate(foo[1m])) or avg(rate(bar[1m]))`,
-			`avg(rate(foo[1m])) or avg(rate(bar[1m]))`,
-		},
-		{ // The const expression is on the wrong side.
-			`(vector(0) == 1) and on() (avg(rate(foo[1m])))`,
-			`(vector(0) == 1) and on() (avg(rate(foo[1m])))`,
-		},
-		{ // Matching on labels.
-			`(avg(rate(foo[1m]))) and on(id) (vector(0) == 1)`,
-			`(avg(rate(foo[1m]))) and on(id) (vector(0) == 1)`,
-		},
-		{ // Not "on" expression.
-			`(avg(rate(foo[1m]))) and ignoring() (vector(0) == 1)`,
-			`(avg(rate(foo[1m]))) and ignoring() (vector(0) == 1)`,
-		},
-		// Pruned expressions.
-		{
-			`(avg(rate(foo[1m]))) and on() (vector(0) == 1)`,
-			`(vector(0) == 1)`,
-		},
-		{
-			`(avg(rate(foo[1m]))) and on() (vector(1) == 1)`,
-			`(avg(rate(foo[1m])))`,
-		},
-		{
-			`(avg(rate(foo[1m]))) and on() (vector(3) == 4.5)`,
-			`(vector(3) == 4.5)`,
-		},
-		{
-			`(avg(rate(foo[1m]))) and on() (vector(5.5) == 5.5)`,
-			`(avg(rate(foo[1m])))`,
-		},
-		{
-			// "and on()" is not on top level, "or" has lower precedence.
-			// This could be further reduced by dropping the vector(0)==1 from
-			// the "or", but it is intentionally not done as that would
-			// complicate the algorithm and isn't required to reduce chunks
-			// loading.
-			`(avg(rate(foo[1m]))) and on() (vector(0) == 1) or avg(rate(bar[1m]))`,
-			`(vector(0) == 1) or avg(rate(bar[1m]))`,
-		},
-		{
-			// "and on()" is not on top level, due to left-right associativity.
-			`(avg(rate(foo[1m]))) and on() (vector(0) == 1) and avg(rate(bar[1m]))`,
-			`(vector(0) == 1) and avg(rate(bar[1m]))`,
-		},
-		{
-			// "and on()" is not on top level.
-			// This could be further reduced by dropping the vector(0)==1 from
-			// the "or", but it is intentionally not done as that would
-			// complicate the algorithm and isn't required to reduce chunks
-			// loading.
-			`(avg(rate(foo[1m]))) and on() (vector(0) == 1) or avg(rate(bar[1m])) and on() (vector(1) == 1)`,
-			`(vector(0) == 1) or avg(rate(bar[1m]))`,
-		},
-	} {
-		tt := tt
-
-		t.Run(tt.in, func(t *testing.T) {
-			expr, err := parser.ParseExpr(tt.in)
-			require.NoError(t, err)
-			out, err := parser.ParseExpr(tt.out)
-			require.NoError(t, err)
-
-			mapped, err := pruner.Map(ctx, expr)
-			require.NoError(t, err)
-			require.Equal(t, out.String(), mapped.String())
-		})
-	}
-}

commit 81da512fe5db5281ee8448d8ff758da68e1ee70d

Fix bug with pruning toggles not working in query planner (#12783)
#### What this PR does

Pruning query toggles was broken with
https://github.com/grafana/mimir/pull/12303 when it moved to AST
optimization passes, as the query planner preprocess step wraps some
parts of the expression in StepInvariantExpressions which we did not
account for in the code and unit tests working on it in isolation
instead of as part of the query plan.

This fixes that bug and adds preprocessing to the unit tests for some
AST optimization passes. Will do testing with more of the query plan in
another [PR](https://github.com/grafana/mimir/pull/12784) as that
requires bigger changes to the planning code.

#### Which issue(s) this PR fixes or relates to

Follow up to https://github.com/grafana/mimir/pull/12303

#### Checklist

- [x] Tests updated.
- [ ] Documentation added.
- [x] `CHANGELOG.md` updated - the order of entries should be
`[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`. If changelog entry
is not needed, please add the `changelog-not-needed` label to the PR.
- [ ]
[`about-versioning.md`](https://github.com/grafana/mimir/blob/main/docs/sources/mimir/configure/about-versioning.md)
updated with experimental features.

---------

Co-authored-by: Taylor C <41653732+tacole02@users.noreply.github.com>

pkg/streamingpromql/optimize/ast/propagate_matchers.go
@@ -120,8 +120,9 @@ func (mapper *propagateMatchers) extractVectorSelectors(expr parser.Expr) ([]*en
 		return mapper.propagateMatchersInBinaryExpr(e)
 	// Explicitly define what is not handled to avoid confusion.
 	case *parser.StepInvariantExpr:
-		// Used only for optimizations and not produced directly by parser.
-		return nil, nil
+		// Used only for optimizations and not produced directly by parser,
+		// but may be added in preprocessing step.
+		return mapper.extractVectorSelectors(e.Expr)
 	default:
 		return nil, nil
 	}

pkg/streamingpromql/optimize/ast/prune_toggles.go
@@ -93,6 +93,8 @@ func (mapper *pruneToggles) MapExpr(ctx context.Context, expr parser.Expr) (mapp
 func (mapper *pruneToggles) isConst(expr parser.Expr) (isConst, isEmpty bool) {
 	var lhs, rhs parser.Expr
 	switch e := expr.(type) {
+	case *parser.StepInvariantExpr:
+		return mapper.isConst(e.Expr)
 	case *parser.ParenExpr:
 		return mapper.isConst(e.Expr)
 	case *parser.BinaryExpr:
@@ -131,6 +133,8 @@ func (mapper *pruneToggles) isVectorAndNumberEqual(lhs, rhs parser.Expr) (bool, 
 
 func (mapper *pruneToggles) isConstVector(expr parser.Expr) (isVector bool, value float64) {
 	switch e := expr.(type) {
+	case *parser.StepInvariantExpr:
+		return mapper.isConstVector(e.Expr)
 	case *parser.ParenExpr:
 		return mapper.isConstVector(e.Expr)
 	case *parser.Call:
@@ -148,6 +152,8 @@ func (mapper *pruneToggles) isConstVector(expr parser.Expr) (isVector bool, valu
 
 func (mapper *pruneToggles) isNumber(expr parser.Expr) (isNumber bool, value float64) {
 	switch e := expr.(type) {
+	case *parser.StepInvariantExpr:
+		return mapper.isNumber(e.Expr)
 	case *parser.ParenExpr:
 		return mapper.isNumber(e.Expr)
 	case *parser.NumberLiteral:

pkg/streamingpromql/optimize/ast/reorder_histogram_aggregation.go
@@ -90,6 +90,8 @@ func vectorSelectorContainsNonExactMetricNameMatcher(expr parser.Expr) bool {
 		return false
 	case *parser.MatrixSelector:
 		return vectorSelectorContainsNonExactMetricNameMatcher(e.VectorSelector)
+	case *parser.StepInvariantExpr:
+		return vectorSelectorContainsNonExactMetricNameMatcher(e.Expr)
 	case *parser.ParenExpr:
 		return vectorSelectorContainsNonExactMetricNameMatcher(e.Expr)
 	case *parser.UnaryExpr:

pkg/streamingpromql/optimize/ast/propagate_matchers_test.go
@@ -68,6 +68,7 @@ var testCasesPropagateMatchers = map[string]string{
 	`rate(up{foo="bar"}[2m]) + delta(down{baz="fob"}[4m])`:                     `rate(up{foo="bar", baz="fob"}[2m]) + delta(down{foo="bar", baz="fob"}[4m])`,
 	`up * -down{foo="bar"}`:                                                    `up{foo="bar"} * -down{foo="bar"}`,
 	`up{foo="bar"} @ 1 + down @ 2`:                                             `up{foo="bar"} @ 1 + down{foo="bar"} @ 2`,
+	`up{foo="bar"} @ 1 + down`:                                                 `up{foo="bar"} @ 1 + down{foo="bar"}`,
 	`minute() * down{baz="fob"}`:                                               `minute() * down{baz="fob"}`,
 	`minute(up{foo="bar"}) * down{baz="fob"}`:                                  `minute(up{foo="bar", baz="fob"}) * down{foo="bar", baz="fob"}`,
 	`round(up{foo="bar"}) * down{baz="fob"}`:                                   `round(up{foo="bar", baz="fob"}) * down{foo="bar", baz="fob"}`,
@@ -138,6 +139,9 @@ func TestPropagateMatchers(t *testing.T) {
 
 			inputExpr, err := parser.ParseExpr(input)
 			require.NoError(t, err)
+			inputExpr, err = preprocessQuery(t, inputExpr)
+			require.NoError(t, err)
+
 			optimizer := ast.NewPropagateMatchersMapper()
 			outputExpr, err := optimizer.Map(ctx, inputExpr)
 			require.NoError(t, err)

pkg/streamingpromql/optimize/ast/prune_toggles_test.go
@@ -64,6 +64,8 @@ func TestPruneToggles(t *testing.T) {
 			require.NoError(t, err)
 			inputExpr, err := parser.ParseExpr(input)
 			require.NoError(t, err)
+			inputExpr, err = preprocessQuery(t, inputExpr)
+			require.NoError(t, err)
 
 			reg := prometheus.NewPedanticRegistry()
 			optimizer := ast.NewPruneToggles(reg)

pkg/streamingpromql/optimize/ast/reorder_histogram_aggregation_test.go
@@ -37,6 +37,7 @@ var testCasesReorderHistogramAggregation = map[string]string{
 	"histogram_sum(rate(foo[2m]))":              "histogram_sum(rate(foo[2m]))",
 	`3 + (((histogram_sum(sum(foo)))))`:         `3 + (((sum(histogram_sum(foo)))))`,
 	`vector(3) + (((histogram_sum(sum(foo)))))`: `vector(3) + (((sum(histogram_sum(foo)))))`,
+	`histogram_sum(sum(foo @ 1234 * bar))`:      `sum(histogram_sum(foo @ 1234 * bar))`,
 
 	// Unsupported aggregations
 	`histogram_sum(max(foo))`:     `histogram_sum(max(foo))`,
@@ -64,6 +65,9 @@ func TestReorderHistogramAggregation(t *testing.T) {
 
 			inputExpr, err := parser.ParseExpr(input)
 			require.NoError(t, err)
+			inputExpr, err = preprocessQuery(t, inputExpr)
+			require.NoError(t, err)
+
 			optimizer := ast.NewReorderHistogramAggregationMapper()
 			outputExpr, err := optimizer.Map(ctx, inputExpr)
 			require.NoError(t, err)

pkg/streamingpromql/optimize/ast/util_test.go
@@ -12,12 +12,19 @@ import (
 	"github.com/grafana/dskit/user"
 	"github.com/prometheus/prometheus/model/timestamp"
 	"github.com/prometheus/prometheus/promql"
+	"github.com/prometheus/prometheus/promql/parser"
 	"github.com/prometheus/prometheus/promql/promqltest"
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/mimir/pkg/streamingpromql"
 	"github.com/grafana/mimir/pkg/streamingpromql/testutils"
+	"github.com/grafana/mimir/pkg/streamingpromql/types"
 )
+
+func preprocessQuery(t *testing.T, expr parser.Expr) (parser.Expr, error) {
+	dummyTimeRange := types.NewInstantQueryTimeRange(timestamp.Time(1000))
+	return promql.PreprocessExpr(expr, timestamp.Time(dummyTimeRange.StartT), timestamp.Time(dummyTimeRange.EndT), 0)
+}
 
 func testASTOptimizationPassWithData(t *testing.T, loadTemplate string, testCases map[string]string) {
 	numSamples := 100