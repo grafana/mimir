4f070e86236b98ade53844d4e161a9b5df7bc4fe
Jon Kartago Lamida <me@lamida.net>
2025-10-02 18:31:21 +0800

    MemoryConsumptionTracker for promql.Engine must be passed in Query not in Query constructor

--- pkg/util/limiter/memory_consumption_promql_engine_compat.go
+++ pkg/util/limiter/memory_consumption_promql_engine_compat.go
@@ -10,6 +10,19 @@ import (
 	"github.com/prometheus/prometheus/storage"
 )
 
+type UnlimitedMemoryTrackingQuery struct {
+	promql.Query
+}
+
+func (u *UnlimitedMemoryTrackingQuery) Exec(ctx context.Context) *promql.Result {
+	ctx = ContextWithNewUnlimitedMemoryConsumptionTracker(ctx)
+	return u.Query.Exec(ctx)
+}
+
+func NewUnlimitedMemoryTrackingQuery(query promql.Query) *UnlimitedMemoryTrackingQuery {
+	return &UnlimitedMemoryTrackingQuery{query}
+}
+
 // UnlimitedMemoryTrackerPromqlEngine wraps promql.Engine so that this can
 // pass an unlimited MemoryConsumptionTracker.
 //
@@ -24,10 +37,16 @@ func NewUnlimitedMemoryTrackerPromqlEngine(inner *promql.Engine) UnlimitedMemory
 }
 
 func (p UnlimitedMemoryTrackerPromqlEngine) NewInstantQuery(ctx context.Context, q storage.Queryable, opts promql.QueryOpts, qs string, ts time.Time) (promql.Query, error) {
-	ctx = ContextWithNewUnlimitedMemoryConsumptionTracker(ctx)
-	return p.inner.NewInstantQuery(ctx, q, opts, qs, ts)
+	qry, err := p.inner.NewInstantQuery(ctx, q, opts, qs, ts)
+	if err != nil {
+		return nil, err
+	}
+	return NewUnlimitedMemoryTrackingQuery(qry), nil
 }
 func (p UnlimitedMemoryTrackerPromqlEngine) NewRangeQuery(ctx context.Context, q storage.Queryable, opts promql.QueryOpts, qs string, start, end time.Time, interval time.Duration) (promql.Query, error) {
-	ctx = ContextWithNewUnlimitedMemoryConsumptionTracker(ctx)
-	return p.inner.NewRangeQuery(ctx, q, opts, qs, start, end, interval)
+	qry, err := p.inner.NewRangeQuery(ctx, q, opts, qs, start, end, interval)
+	if err != nil {
+		return nil, err
+	}
+	return NewUnlimitedMemoryTrackingQuery(qry), nil
 }
