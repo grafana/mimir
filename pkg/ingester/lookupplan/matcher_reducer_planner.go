dab5999335b6e00e22a1d3fe52bfbb5af4186afd
Casie Chen <casie.chen@grafana.com>
2025-10-03 18:00:22 -0700

    Incorporate feedback

--- pkg/ingester/lookupplan/matcher_reducer_planner.go
+++ pkg/ingester/lookupplan/matcher_reducer_planner.go
@@ -44,14 +44,17 @@ func (p MatcherReducerPlanner) PlanIndexLookup(ctx context.Context, inPlan index
 	matchers := slices.Concat(inPlan.IndexMatchers(), inPlan.ScanMatchers())
 	matchers = setReduceMatchers(matchers)
 
-	allowedOutMatchers := make(map[labels.Matcher]bool, len(matchers))
+	// allowedOutMatchers is used to check which matchers can be returned.
+	// We use the matcher string as the key because we can't rely on value equality for regex matchers.
+	// If a matcher string's value is false, it hasn't been added to any result set.
+	allowedOutMatchers := make(map[string]bool, len(matchers))
 	for _, m := range matchers {
-		allowedOutMatchers[*m] = false
+		allowedOutMatchers[m.String()] = false
 	}
 
 	// Rebuild the index and scan matchers in the input order, less the filtered/deduplicated matchers
-	droppedMatchers := make([]*labels.Matcher, 0)
-	outIndexMatchers, allowedOutMatchers, droppedMatchers := buildOutMatchers(inPlan.IndexMatchers(), allowedOutMatchers, droppedMatchers)
+	// droppedMatchers is used for logging purposes to record all matchers have been removed from the plan.
+	outIndexMatchers, allowedOutMatchers, droppedMatchers := buildOutMatchers(inPlan.IndexMatchers(), allowedOutMatchers, nil)
 	outScanMatchers, _, droppedMatchers := buildOutMatchers(inPlan.ScanMatchers(), allowedOutMatchers, droppedMatchers)
 
 	if traceSampled && len(droppedMatchers) > 0 {
@@ -66,18 +69,17 @@ func (p MatcherReducerPlanner) PlanIndexLookup(ctx context.Context, inPlan index
 }
 
 // buildOutMatchers takes a slice of index or scan matchers, and returns the same slice in order,
-// less any duplicate matchers. allowedOutMatchers contains the set of matchers which can be returned.
-// If dedupedMatchers[*m] is false, the matcher has not been added to any result set.
-// droppedMatchers is used for logging purposes to record which matchers have been removed from the plan.
-func buildOutMatchers(inMatchers []*labels.Matcher, allowedOutMatchers map[labels.Matcher]bool, droppedMatchers []*labels.Matcher) ([]*labels.Matcher, map[labels.Matcher]bool, []*labels.Matcher) {
+// less any duplicate matchers.
+func buildOutMatchers(inMatchers []*labels.Matcher, allowedOutMatchers map[string]bool, droppedMatchers []*labels.Matcher) ([]*labels.Matcher, map[string]bool, []*labels.Matcher) {
 	outMatchers := make([]*labels.Matcher, 0, len(inMatchers))
 	for _, m := range inMatchers {
-		// dedupedMatchers is used to both keep track of all unique matchers (evidenced by existence in the map),
-		// and whether the matcher has already been added to a set of output matchers (evidenced by the value in the map).
+		matcherStr := m.String()
+		// allowedOutMatchers is used to both keep track of all unique matchers (evidenced by existence in the map),
+		// and whether the matcher has already been seen and added to a set of output matchers (evidenced by the value in the map).
 		// We only want to add the matcher if it hasn't already been added to an output slice.
-		if _, ok := allowedOutMatchers[*m]; ok && !allowedOutMatchers[*m] {
+		if matcherAlreadySeen, ok := allowedOutMatchers[matcherStr]; ok && !matcherAlreadySeen {
 			outMatchers = append(outMatchers, m)
-			allowedOutMatchers[*m] = true
+			allowedOutMatchers[matcherStr] = true
 		} else {
 			droppedMatchers = append(droppedMatchers, m)
 		}
@@ -85,6 +87,13 @@ func buildOutMatchers(inMatchers []*labels.Matcher, allowedOutMatchers map[label
 	return outMatchers, allowedOutMatchers, droppedMatchers
 }
 
+// setReduceMatchers takes a slice of matchers, and returns only those matchers which would reduce the result set size.
+// For each label name, matchers are dropped from the result set based on criteria for their type:
+//   - equals matchers are never dropped, but if there is more than one unique equals matcher,
+//     all other matchers for the label name are dropped because unique equals matchers have non-intersecting result sets.
+//   - not-equals matchers are dropped if they match any equals matcher value,
+//     or if any not-regex matchers also exclude the not-equals matcher value.
+//   - regex and not-regex matchers are dropped if they match any equals matcher value.
 func setReduceMatchers(ms []*labels.Matcher) []*labels.Matcher {
 	// Group matchers by their label names so we can evaluate each label name independently
 	matchersByName := make(map[string][]*labels.Matcher)
@@ -92,25 +101,37 @@ func setReduceMatchers(ms []*labels.Matcher) []*labels.Matcher {
 		if _, ok := matchersByName[m.Name]; !ok {
 			matchersByName[m.Name] = make([]*labels.Matcher, 0, 1)
 		}
-		// Always drop wildcard matchers
-		if m.Type == labels.MatchRegexp && m.Value == ".*" {
-			continue
-		}
 		matchersByName[m.Name] = append(matchersByName[m.Name], m)
 	}
 	outMatchers := make([]*labels.Matcher, 0, len(ms))
 	for _, matchers := range matchersByName {
-		matchers = dedupeMatchers(matchers)
 		matchersByType := groupMatchersByType(matchers)
-		outMatchers = append(outMatchers, matchersByType[labels.MatchEqual]...)
+		equalsMatchers := dedupeEqualsMatchers(matchersByType)
+		outMatchers = append(outMatchers, equalsMatchers...)
 		// If we have more than one unique equals matcher, we can just return those;
 		// we know we'll only return an empty set
-		if len(matchersByType[labels.MatchEqual]) > 1 {
+		if len(equalsMatchers) > 1 {
 			continue
 		}
 		outMatchers = append(outMatchers, filterRegexMatchers(matchersByType, labels.MatchRegexp)...)
 		outMatchers = append(outMatchers, filterNotEqualsMatchers(matchersByType)...)
 		outMatchers = append(outMatchers, filterRegexMatchers(matchersByType, labels.MatchNotRegexp)...)
+	}
+	return outMatchers
+}
+
+func dedupeEqualsMatchers(mf map[labels.MatchType][]*labels.Matcher) []*labels.Matcher {
+	return dedupeMatchers(mf[labels.MatchEqual])
+}
+
+func dedupeMatchers(ms []*labels.Matcher) []*labels.Matcher {
+	deduped := make(map[labels.Matcher]*labels.Matcher, len(ms))
+	for _, m := range ms {
+		deduped[*m] = m
+	}
+	outMatchers := make([]*labels.Matcher, 0, len(deduped))
+	for _, ptr := range deduped {
+		outMatchers = append(outMatchers, ptr)
 	}
 	return outMatchers
 }
@@ -126,7 +147,14 @@ func matcherMatchesAnyValues(matcher *labels.Matcher, matchers []*labels.Matcher
 }
 
 // filterRegexMatchers returns a subset of regex matchers which would actually reduce the result set size for either positive or negative regex matchers.
-// If a regex matcher matches any equals matchers, it is dropped, since the equals matcher will match a strict subset of values that the regex matcher would.
+// A regex matcher is dropped if:
+//   - it matches all values (foo=~".*")
+//   - it matches any equals matches, since the equals matcher will match a strict subset of values that the regex matcher would.
+//
+// Examples:
+//   - {foo=~".*bar.*", foo="bar"}, foo=~".*bar.*" is dropped because foo="bar" is a subset of foo=~".*bar.*"
+//   - {foo!~".*bar.*", foo="bar"}, foo!~".*bar.*" is not dropped because it covers a different set of values than foo="bar"
+//   - {foo!~".*baz.*", foo="bar"}, foo!~".*baz.*" is dropped because foo="bar" is a subset of foo!~".*baz.*"
 func filterRegexMatchers(mf map[labels.MatchType][]*labels.Matcher, regexType labels.MatchType) []*labels.Matcher {
 	var matchers []*labels.Matcher
 	switch regexType {
@@ -136,12 +164,16 @@ func filterRegexMatchers(mf map[labels.MatchType][]*labels.Matcher, regexType la
 		matchers = mf[labels.MatchNotRegexp]
 	default:
 		// Do not filter anything if passed a non-regex type.
-		return mf[regexType]
+		panic("cannot filter unknown type regex matchers")
 	}
 	outMatchers := make([]*labels.Matcher, 0, len(matchers))
 	for _, m := range matchers {
-		// If a regex matcher matches any equals matcher, that equals matcher is a subset of the regex,
-		// and we should not add it to outMatchers.
+		// Always drop wildcard matchers
+		if m.Value == ".*" {
+			continue
+		}
+		// If m matches any equals matcher, that equals matcher is a subset of the regex,
+		// and we should not add m to outMatchers.
 		if !matcherMatchesAnyValues(m, mf[labels.MatchEqual]) {
 			outMatchers = append(outMatchers, m)
 		}
@@ -151,9 +183,14 @@ func filterRegexMatchers(mf map[labels.MatchType][]*labels.Matcher, regexType la
 
 // filterNotEqualsMatchers returns a subset of not-equals matchers which should actually reduce the result set size.
 // This subset is determined by comparing each not-equals matchers against equals and not-regex matchers.
-// A not-equals matcher is dropped if:
-// - there exists any not-regex matcher which would also remove the not-equals matcher value from the result set, OR
-// - there exists any equals matcher for a value that is matched by the not-equals matcher
+// A not-equals matcher is dropped on any of these conditions:
+//   - there are any not-regex matchers which would also remove the not-equals matcher value from the result set
+//   - any equals matcher for a value that is matched by the not-equals matcher
+//
+// Examples:
+//   - for the matcher set {foo!="bar", foo="b"}, foo!="bar" does match the value "b", so foo!="bar" is dropped.
+//   - for the matcher set {foo!="bar", foo="bar"}, foo!="bar" matches the value "bar", so foo!="bar" is not dropped.
+//   - for the matcher set {foo!="bar", foo!~".*bar.*"}, foo!~".*bar.*" will exclude more values from the result set than foo!="bar", so foo!="bar" is dropped.
 func filterNotEqualsMatchers(mf map[labels.MatchType][]*labels.Matcher) []*labels.Matcher {
 	outMatchers := make([]*labels.Matcher, 0, len(mf[labels.MatchNotEqual]))
 	// If the value of a not-equals matcher is matched by the inverse of any not-regex matcher,
@@ -161,11 +198,11 @@ func filterNotEqualsMatchers(mf map[labels.MatchType][]*labels.Matcher) []*label
 	// Using the example of x!="a" and x!~"a.*",
 	// x!~"a.*" will exclude more values than x!="a".
 	for _, m := range mf[labels.MatchNotEqual] {
-		matchesNoRegex := true
+		noRegexMatchesValue := true
 		for _, nr := range mf[labels.MatchNotRegexp] {
 			if inv, err := nr.Inverse(); err == nil {
 				if inv.Matches(m.Value) {
-					matchesNoRegex = false
+					noRegexMatchesValue = false
 					break
 				}
 			}
@@ -174,7 +211,7 @@ func filterNotEqualsMatchers(mf map[labels.MatchType][]*labels.Matcher) []*label
 		// because the not-equals will not "subtract" anything from the final set.
 		matchesNoEquals := !matcherMatchesAnyValues(m, mf[labels.MatchEqual])
 
-		if matchesNoRegex && matchesNoEquals {
+		if noRegexMatchesValue && matchesNoEquals {
 			outMatchers = append(outMatchers, m)
 		}
 	}
@@ -187,16 +224,4 @@ func groupMatchersByType(ms []*labels.Matcher) map[labels.MatchType][]*labels.Ma
 		outGroups[m.Type] = append(outGroups[m.Type], m)
 	}
 	return outGroups
-}
-
-func dedupeMatchers(ms []*labels.Matcher) []*labels.Matcher {
-	deduped := make(map[labels.Matcher]*labels.Matcher, len(ms))
-	for _, m := range ms {
-		deduped[*m] = m
-	}
-	outMatchers := make([]*labels.Matcher, 0, len(deduped))
-	for _, ptr := range deduped {
-		outMatchers = append(outMatchers, ptr)
-	}
-	return outMatchers
 }
--- pkg/ingester/lookupplan/matcher_reducer_planner_test.go
+++ pkg/ingester/lookupplan/matcher_reducer_planner_test.go
@@ -13,191 +13,159 @@ import (
 
 func TestMatcherReducerPlanner_PlanIndexLookup(t *testing.T) {
 	ctxPlanningDisabled := ContextWithDisabledPlanning(context.Background())
-
-	equalsMatcher, _ := labels.NewMatcher(labels.MatchEqual, "foo", "bar")
-	otherEqualsMatcher, _ := labels.NewMatcher(labels.MatchEqual, "foo", "baz")
-	supersetRegexpMatcher, _ := labels.NewMatcher(labels.MatchRegexp, "foo", ".*bar.*")
-	nonSupersetRegexpMatcher, _ := labels.NewMatcher(labels.MatchRegexp, "foo", "baz.*")
-
-	wildcardMatcher, _ := labels.NewMatcher(labels.MatchRegexp, "foo", ".*")
-	emptyStringNotRegexpMatcher, _ := labels.NewMatcher(labels.MatchNotRegexp, "foo", "")
-	emptyStringNotEqualsMatcher, _ := labels.NewMatcher(labels.MatchNotEqual, "foo", "")
-	notEqualsMatcher, _ := labels.NewMatcher(labels.MatchNotEqual, "foo", "bar")
-
-	otherLabelEmptyStringNotEqualsMatcher, _ := labels.NewMatcher(labels.MatchNotRegexp, "baz", "")
-
 	tests := []struct {
-		name         string
-		ctx          context.Context
-		inPlan       concreteLookupPlan
-		expectedPlan concreteLookupPlan
+		name                  string
+		ctx                   context.Context
+		inIndexMatchers       []string
+		inScanMatchers        []string
+		expectedIndexMatchers []string
+		expectedScanMatchers  []string
 	}{
 		{
-			name: "planning disabled should not alter plan",
-			ctx:  ctxPlanningDisabled,
-			inPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{equalsMatcher, wildcardMatcher, emptyStringNotEqualsMatcher},
-				scanMatchers:  []*labels.Matcher{equalsMatcher, emptyStringNotRegexpMatcher},
-			},
-			expectedPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{equalsMatcher, wildcardMatcher, emptyStringNotEqualsMatcher},
-				scanMatchers:  []*labels.Matcher{equalsMatcher, emptyStringNotRegexpMatcher},
-			},
+			name:                  "planning disabled should not alter plan",
+			ctx:                   ctxPlanningDisabled,
+			inIndexMatchers:       []string{`foo="bar"`, `foo=~".*"`, `foo!=""`},
+			expectedIndexMatchers: []string{`foo="bar"`, `foo=~".*"`, `foo!=""`},
+			inScanMatchers:        []string{`foo="bar"`, `foo!=""`},
+			expectedScanMatchers:  []string{`foo="bar"`, `foo!=""`},
 		},
 		{
-			name: "deduplicate index matchers",
-			ctx:  context.Background(),
-			inPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{equalsMatcher, equalsMatcher},
-				scanMatchers:  []*labels.Matcher{},
-			},
-			expectedPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{equalsMatcher},
-				scanMatchers:  []*labels.Matcher{},
-			},
+			name:                  "deduplicate index matchers",
+			ctx:                   context.Background(),
+			inIndexMatchers:       []string{`foo="bar"`, `foo="bar"`},
+			expectedIndexMatchers: []string{`foo="bar"`},
 		},
 		{
-			name: "deduplicate scan matchers",
-			ctx:  context.Background(),
-			inPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{},
-				scanMatchers:  []*labels.Matcher{supersetRegexpMatcher, supersetRegexpMatcher},
-			},
-			expectedPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{},
-				scanMatchers:  []*labels.Matcher{supersetRegexpMatcher},
-			},
+			name:                 "deduplicate scan matchers",
+			ctx:                  context.Background(),
+			inScanMatchers:       []string{`foo=~".*bar.*"`, `foo=~".*bar.*"`, `foo=~".*bar.*"`},
+			expectedScanMatchers: []string{`foo=~".*bar.*"`},
 		},
 		{
-			name: "duplicate matchers across index and scan should be deduplicated to only index matcher",
-			ctx:  context.Background(),
-			inPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{equalsMatcher},
-				scanMatchers:  []*labels.Matcher{equalsMatcher},
-			},
-			expectedPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{equalsMatcher},
-				scanMatchers:  []*labels.Matcher{},
-			},
+			name:                  "duplicate matchers across index and scan should be deduplicated to only index matcher",
+			ctx:                   context.Background(),
+			inIndexMatchers:       []string{`foo="bar"`},
+			expectedIndexMatchers: []string{`foo="bar"`},
+			inScanMatchers:        []string{`foo="bar"`},
 		},
 		{
-			name: "multiple equals matchers should only return equals matchers",
-			ctx:  context.Background(),
-			inPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{equalsMatcher},
-				scanMatchers:  []*labels.Matcher{otherEqualsMatcher, supersetRegexpMatcher},
-			},
-			expectedPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{equalsMatcher},
-				scanMatchers:  []*labels.Matcher{otherEqualsMatcher},
-			},
+			name:            "multiple unique equals matchers should only return equals matchers",
+			ctx:             context.Background(),
+			inIndexMatchers: []string{`foo="bar"`},
+			// Even though the regex matcher matches neither equals matcher,
+			// a query with multiple equals matchers for the same label name is already guaranteed to return an empty set
+			inScanMatchers:        []string{`foo="baz"`, `foo=~".*bananas.*"`},
+			expectedIndexMatchers: []string{`foo="bar"`},
+			expectedScanMatchers:  []string{`foo="baz"`},
 		},
 		{
-			name: "planning should remove a regex matcher if it is a superset of an equals matcher",
-			ctx:  context.Background(),
-			inPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{equalsMatcher},
-				scanMatchers:  []*labels.Matcher{supersetRegexpMatcher},
-			},
-			expectedPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{equalsMatcher},
-				scanMatchers:  []*labels.Matcher{},
-			},
+			name:                  "planning should remove a regex matcher if it is a superset of an equals matcher",
+			ctx:                   context.Background(),
+			inIndexMatchers:       []string{`foo="bar"`},
+			inScanMatchers:        []string{`foo=~".*bar.*"`},
+			expectedIndexMatchers: []string{`foo="bar"`},
 		},
 		{
-			name: "planning should not remove a regex matcher if it is not a superset of an equals matcher",
-			ctx:  context.Background(),
-			inPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{equalsMatcher, equalsMatcher},
-				scanMatchers:  []*labels.Matcher{nonSupersetRegexpMatcher},
-			},
-			expectedPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{equalsMatcher},
-				scanMatchers:  []*labels.Matcher{nonSupersetRegexpMatcher},
-			},
+			name:                  "planning should preserve a regex matcher if it is not a superset of an equals matcher",
+			ctx:                   context.Background(),
+			inIndexMatchers:       []string{`foo="bar"`, `foo="bar"`},
+			inScanMatchers:        []string{`foo=~".*baz.*"`},
+			expectedIndexMatchers: []string{`foo="bar"`},
+			expectedScanMatchers:  []string{`foo=~".*baz.*"`},
 		},
 		{
-			name: "always drop wildcard matcher, even if it is the only matcher",
-			ctx:  context.Background(),
-			inPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{wildcardMatcher},
-				scanMatchers:  []*labels.Matcher{},
-			},
-			expectedPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{},
-				scanMatchers:  []*labels.Matcher{},
-			},
+			name:                  "always drop wildcard matcher, even if it is the only matcher",
+			ctx:                   context.Background(),
+			inIndexMatchers:       []string{`foo=~".*"`},
+			expectedIndexMatchers: []string{},
 		},
 		{
-			name: "single matcher should not be dropped",
-			ctx:  context.Background(),
-			inPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{emptyStringNotEqualsMatcher},
-				scanMatchers:  []*labels.Matcher{},
-			},
-			expectedPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{emptyStringNotEqualsMatcher},
-				scanMatchers:  []*labels.Matcher{},
-			},
+			name:                  "single non-wildcard matcher should not be dropped",
+			ctx:                   context.Background(),
+			inIndexMatchers:       []string{`foo!=""`},
+			expectedIndexMatchers: []string{`foo!=""`},
 		},
 		{
-			name: "if there is an equals matcher, remove all matchers that match supersets of the equals matcher",
-			ctx:  context.Background(),
-			inPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{supersetRegexpMatcher},
-				scanMatchers:  []*labels.Matcher{equalsMatcher, emptyStringNotEqualsMatcher, emptyStringNotRegexpMatcher},
-			},
-			expectedPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{},
-				scanMatchers:  []*labels.Matcher{equalsMatcher},
-			},
+			name:                  "drop all matchers that match supersets of an equals matcher",
+			ctx:                   context.Background(),
+			inIndexMatchers:       []string{`foo=~".*bar.*"`},
+			inScanMatchers:        []string{`foo="bar"`, `foo!=""`, `foo!~""`, `foo!="baz"`},
+			expectedIndexMatchers: []string{},
+			expectedScanMatchers:  []string{`foo="bar"`},
 		},
 		{
-			name: "keep a matcher that excludes empty strings if it is the most selective matcher",
-			ctx:  context.Background(),
-			inPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{wildcardMatcher},
-				scanMatchers:  []*labels.Matcher{emptyStringNotRegexpMatcher, emptyStringNotRegexpMatcher},
-			},
-			expectedPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{},
-				scanMatchers:  []*labels.Matcher{emptyStringNotRegexpMatcher},
-			},
+			name:                 "keep one matcher of ones that reduce the set size equivalently",
+			ctx:                  context.Background(),
+			inIndexMatchers:      []string{`foo=~".*"`},
+			inScanMatchers:       []string{`foo!~""`, `foo!=""`},
+			expectedScanMatchers: []string{`foo!~""`},
 		},
 		{
-			name: "keep at least one matcher for each label name",
-			ctx:  context.Background(),
-			inPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{wildcardMatcher, otherLabelEmptyStringNotEqualsMatcher},
-				scanMatchers:  []*labels.Matcher{emptyStringNotRegexpMatcher, emptyStringNotRegexpMatcher},
-			},
-			expectedPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{otherLabelEmptyStringNotEqualsMatcher},
-				scanMatchers:  []*labels.Matcher{emptyStringNotRegexpMatcher},
-			},
+			name:                  "keep at least one matcher for each label name",
+			ctx:                   context.Background(),
+			inIndexMatchers:       []string{`foo=~".*"`, `baz!=""`},
+			inScanMatchers:        []string{`foo!~""`, `foo!=""`},
+			expectedIndexMatchers: []string{`baz!=""`},
+			expectedScanMatchers:  []string{`foo!~""`},
 		},
 		{
-			name: "keep matcher that subtracts empty strings if no other matcher does so",
-			ctx:  context.Background(),
-			inPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{notEqualsMatcher, emptyStringNotEqualsMatcher},
-				scanMatchers:  []*labels.Matcher{},
-			},
-			expectedPlan: concreteLookupPlan{
-				indexMatchers: []*labels.Matcher{notEqualsMatcher, emptyStringNotEqualsMatcher},
-				scanMatchers:  []*labels.Matcher{},
-			},
+			name:                  "keep matcher that excludes empty strings if no other matcher does so",
+			ctx:                   context.Background(),
+			inIndexMatchers:       []string{`foo!="bar"`, `foo!=""`},
+			expectedIndexMatchers: []string{`foo!="bar"`, `foo!=""`},
+		},
+		{
+			name:                  "not equals matcher should not be removed if it doesn't match equals matcher value",
+			ctx:                   context.Background(),
+			inIndexMatchers:       []string{`foo != "bar"`, `foo="bar"`},
+			expectedIndexMatchers: []string{`foo!="bar"`, `foo="bar"`},
+		},
+		{
+			name:                  "not equals matcher should be removed if it does match equals matcher value",
+			ctx:                   context.Background(),
+			inIndexMatchers:       []string{`foo!="bar"`, `foo="baz"`},
+			expectedIndexMatchers: []string{`foo="baz"`},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			planner := MatcherReducerPlanner{}
-			outPlan, err := planner.PlanIndexLookup(tt.ctx, tt.inPlan, 0, 0)
-			require.NoError(t, err)
-			assert.Equal(t, tt.expectedPlan.IndexMatchers(), outPlan.IndexMatchers())
-			assert.Equal(t, tt.expectedPlan.ScanMatchers(), outPlan.ScanMatchers())
+			inIndex := parseMatchers(t, tt.inIndexMatchers)
+			inScan := parseMatchers(t, tt.inScanMatchers)
+			inPlan := concreteLookupPlan{inIndex, inScan}
 
+			planner := MatcherReducerPlanner{}
+			outPlan, err := planner.PlanIndexLookup(tt.ctx, inPlan, 0, 0)
+			require.NoError(t, err)
+
+			expectedIndex := parseMatchers(t, tt.expectedIndexMatchers)
+			expectedScan := parseMatchers(t, tt.expectedScanMatchers)
+			expectedPlan := concreteLookupPlan{expectedIndex, expectedScan}
+			assertEqualMatchers(t, expectedPlan.IndexMatchers(), outPlan.IndexMatchers())
+			assertEqualMatchers(t, expectedPlan.ScanMatchers(), outPlan.ScanMatchers())
 		})
 	}
+}
+
+func assertEqualMatchers(t *testing.T, expected, actual []*labels.Matcher) {
+	assert.Equal(t, len(expected), len(actual))
+	for i, m := range expected {
+		switch m.Type {
+		case labels.MatchEqual, labels.MatchNotEqual:
+			assert.Equal(t, m, actual[i])
+		case labels.MatchRegexp, labels.MatchNotRegexp:
+			// We can't rely on the FastRegexMatcher pointer to be the same for all regex matchers
+			assert.Equal(t, m.Type, actual[i].Type)
+			assert.Equal(t, m.Name, actual[i].Name)
+			assert.Equal(t, m.Value, actual[i].Value)
+		}
+	}
+}
+
+func parseMatchers(t *testing.T, ms []string) []*labels.Matcher {
+	outMatchers := make([]*labels.Matcher, 0, len(ms))
+	for _, m := range ms {
+		outMatchers = append(outMatchers, parseMatcher(t, m))
+	}
+	return outMatchers
 }
