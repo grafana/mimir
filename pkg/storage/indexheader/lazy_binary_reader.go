874e7ceb2d8d3da95d66edf02e9aafb087a77865
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2025-10-08 15:56:44 +0200

    Move block directory creation to NewLazyStreamBinaryReader
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/storage/indexheader/lazy_binary_reader.go
+++ pkg/storage/indexheader/lazy_binary_reader.go
@@ -135,8 +135,17 @@ func NewLazyBinaryReader(
 	onClosed func(*LazyBinaryReader),
 	lazyLoadingGate gate.Gate,
 ) (*LazyBinaryReader, error) {
-	indexHeaderPath := filepath.Join(dir, id.String(), block.IndexHeaderFilename)
-	sparseHeaderPath := filepath.Join(dir, id.String(), block.SparseIndexHeaderFilename)
+	dir = filepath.Join(dir, id.String())
+	indexHeaderPath := filepath.Join(dir, block.IndexHeaderFilename)
+	sparseHeaderPath := filepath.Join(dir, block.SparseIndexHeaderFilename)
+
+	if df, err := os.Open(dir); err != nil && os.IsNotExist(err) {
+		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+			return nil, fmt.Errorf("cannot create index-header dir: %w", err)
+		}
+	} else {
+		_ = df.Close()
+	}
 
 	g := errgroup.Group{}
 	g.Go(func() error {
@@ -181,16 +190,6 @@ func tryDownloadSparseHeader(ctx context.Context, logger log.Logger, bkt objstor
 	if err != nil {
 		level.Info(logger).Log("msg", "could not download sparse index-header from bucket; will reconstruct when the block is queried", "err", err)
 		return
-	}
-
-	dir := filepath.Dir(sparseHeaderPath)
-	if df, err := os.Open(dir); err != nil && os.IsNotExist(err) {
-		if err := os.MkdirAll(dir, os.ModePerm); err != nil {
-			level.Warn(logger).Log("msg", "could not create directory for sparse index-header; will reconstruct when the block is queried", "err", err)
-			return
-		}
-	} else {
-		_ = df.Close()
 	}
 
 	err = atomicfs.CreateFile(sparseHeaderPath, bytes.NewReader(bucketSparseHeaderBytes))
