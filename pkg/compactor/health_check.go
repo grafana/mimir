deb563858e3cc8388a085cb2c8b6cef216c15a41
Aaron Goss <aaron.goss@grafana.com>
2025-10-15 14:34:02 -0600

    Support multiple attempts to write to /data
    Add a simple check to the health_check.go touch process which attempts
    to touch a file in /data a certain # of times before it fails.

--- pkg/compactor/health_check.go
+++ pkg/compactor/health_check.go
@@ -2,6 +2,7 @@ package compactor
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"path/filepath"
 	"time"
@@ -9,6 +10,11 @@ import (
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/grafana/dskit/services"
+)
+
+const (
+	// TODO: balance between a const and another config value?
+	MaxAttempts = 3
 )
 
 type HealthCheckService struct {
@@ -29,30 +35,38 @@ func NewHealthCheck(dataDir string, logger log.Logger) HealthCheckService {
 // run performs a simple 'touch' operation on the /data volume, ensuring our health check includes
 // the ability to read/write to this partition
 func (hc HealthCheckService) run(ctx context.Context) error {
-	testfile := filepath.Join("/data", ".rw-test")
 	ticker := time.NewTicker(30 * time.Second)
 
 	// cleanup the file if it exists once the health check stops running
 	defer func() {
-		st, err := os.Stat(testfile)
+		st, err := os.Stat(hc.testfile)
 		if st != nil && err == nil {
-			_ = os.Remove(testfile)
+			_ = os.Remove(hc.testfile)
 		}
 		ticker.Stop()
 	}()
 
+	attempts := 0
 	for {
 		select {
 		case <-ctx.Done():
 			return nil
 		case <-ticker.C:
-			if err := os.WriteFile(testfile, []byte(""), 0o644); err != nil {
-				level.Error(hc.logger).Log("msg", "error while touching test file in /data volume", "err", err)
-				return err
+
+			// TODO: no check of the error
+			if err := os.WriteFile(hc.testfile, []byte(""), 0o644); err != nil {
+				level.Warn(hc.logger).Log("msg", fmt.Sprintf("error writing test file %s", hc.testfile), "err", err)
+
+				if attempts >= MaxAttempts {
+					level.Error(hc.logger).Log("msg", fmt.Sprintf("failed to write test file %s after %d attempts", hc.testfile, attempts), "err", err)
+					return nil
+				}
+				attempts++
 			}
-			if err := os.Remove(testfile); err != nil {
-				level.Error(hc.logger).Log("msg", "error while removing test file in /data volume", "err", err)
-				return err
+
+			// TODO: if this fails, should we include it in the attempts?
+			if err := os.Remove(hc.testfile); err != nil {
+				level.Warn(hc.logger).Log("msg", fmt.Sprintf("error removing test file %s volume", hc.testfile), "err", err)
 			}
 		}
 	}
