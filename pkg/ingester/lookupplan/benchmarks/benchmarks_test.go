fd0d2010dec6122c9d23831c51ef0514efb7f7c2
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2025-10-15 15:50:34 +0200

    Remove useless benchmark

--- pkg/ingester/lookupplan/benchmarks/benchmarks_test.go
+++ pkg/ingester/lookupplan/benchmarks/benchmarks_test.go
@@ -10,33 +10,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// BenchmarkStartIngesterAndLoadBlocks benchmarks the ingester startup with block loading
-func BenchmarkStartIngesterAndLoadBlocks(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		b.StopTimer()
-
-		// Create temporary directory
-		tempDir, err := os.MkdirTemp("", "benchmark-ingester-*")
-		require.NoError(b, err)
-
-		// Create data subdirectory
-		dataDir := filepath.Join(tempDir, "data")
-		err = os.MkdirAll(dataDir, 0755)
-		require.NoError(b, err)
-
-		b.StartTimer()
-
-		// Benchmark the ingester startup
-		addr, cleanup, err := StartIngesterAndLoadBlocks(tempDir, nil)
-		require.NoError(b, err)
-		require.NotEmpty(b, addr)
-
-		b.StopTimer()
-		cleanup()
-		os.RemoveAll(tempDir)
-	}
-}
-
 // BenchmarkQueryExecution benchmarks query execution against a running ingester
 func BenchmarkQueryExecution(b *testing.B) {
 	// Create temporary directory
