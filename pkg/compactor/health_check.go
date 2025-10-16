b68539ff1c4a609bb111e1bf53b814fcd58f0f80
Aaron Goss <aaron.goss@grafana.com>
2025-10-16 09:32:48 -0600

    Update health_check to include stat/read ops
    Add two more operations to the /ready health check:
    * If writing is successful, stat the file (test existence)
    * If stat is successful, read the file
    
    Also ensure we return an error if the # of failures exceeds the limit
    and reorder the operations so we don't triple-count failures.

--- pkg/compactor/health_check.go
+++ pkg/compactor/health_check.go
@@ -13,8 +13,7 @@ import (
 )
 
 const (
-	// TODO: balance between a const and another config value?
-	MaxAttempts = 3
+	MaxDiskCheckAttempts = 5
 )
 
 type HealthCheckService struct {
@@ -32,15 +31,17 @@ func NewHealthCheck(dataDir string, logger log.Logger) HealthCheckService {
 	return svc
 }
 
-// run performs a simple 'touch' operation on the /data volume, ensuring our health check includes
-// the ability to read/write to this partition
+// run performs a simple 'touch' operation on a local filesystem, ensuring our health check includes
+// the ability to write/read to the mounted volume.  If the compactor is unable to interact with the
+// mounted volume, it should fail the pod's /ready check.
+// We track three types of operations as potential failures: write a test file, stat the test file,
+// and read from the test file.  We also attempt to remove the file, but don't count this as an
+// error if it fails.
 func (hc HealthCheckService) run(ctx context.Context) error {
 	ticker := time.NewTicker(30 * time.Second)
 
-	// cleanup the file if it exists once the health check stops running
 	defer func() {
-		st, err := os.Stat(hc.testfile)
-		if st != nil && err == nil {
+		if _, err := os.Stat(hc.testfile); err == nil {
 			_ = os.Remove(hc.testfile)
 		}
 		ticker.Stop()
@@ -53,18 +54,27 @@ func (hc HealthCheckService) run(ctx context.Context) error {
 			return nil
 		case <-ticker.C:
 
-			// TODO: no check of the error
 			if err := os.WriteFile(hc.testfile, []byte(""), 0o644); err != nil {
 				level.Warn(hc.logger).Log("msg", fmt.Sprintf("error writing test file %s", hc.testfile), "err", err)
 
-				if attempts >= MaxAttempts {
+				if attempts >= MaxDiskCheckAttempts {
 					level.Error(hc.logger).Log("msg", fmt.Sprintf("failed to write test file %s after %d attempts", hc.testfile, attempts), "err", err)
 					return nil
 				}
 				attempts++
+				continue
 			}
 
-			// TODO: if this fails, should we include it in the attempts?
+			if _, err := os.Stat(hc.testfile); err != nil {
+				level.Warn(hc.logger).Log("msg", "failed to stat test file %s after it was created", "err", err)
+				attempts++
+			} else {
+				if _, err := os.ReadFile(hc.testfile); err != nil {
+					level.Warn(hc.logger).Log("msg", "failed to read test file %s after it was created", "err", err)
+					attempts++
+				}
+			}
+
 			if err := os.Remove(hc.testfile); err != nil {
 				level.Warn(hc.logger).Log("msg", fmt.Sprintf("error removing test file %s volume", hc.testfile), "err", err)
 			}
