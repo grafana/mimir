1d80734187da7fc2b70dc2d7fb4b88fcda2ead18
Charles Korn <charles.korn@grafana.com>
2025-10-16 12:16:31 +1100

    Fix another issue where unshardable expressions are sharded if the "max sharded queries" limit is 0

--- pkg/frontend/querymiddleware/querysharding.go
+++ pkg/frontend/querymiddleware/querysharding.go
@@ -377,25 +377,29 @@ func (s *QuerySharder) getShardsForQuery(ctx context.Context, tenantIDs []string
 		}
 	}
 
+	// Calculate how many legs are shardable.
+	analyzer := astmapper.NewShardingAnalyzer(s.logger)
+	result, err := analyzer.Analyze(queryExpr)
+	if err != nil {
+		return 0, err
+	}
+
+	spanLog.DebugLog(
+		"msg", "computed number of shardable legs for the query",
+		"shardable legs", result.ShardedSelectors,
+		"can shard all selectors", result.WillShardAllSelectors,
+	)
+
+	if !result.WillShardAllSelectors || result.ShardedSelectors == 0 {
+		// If we can't shard all selectors, then we won't shard the query at all.
+		return 1, nil
+	}
+
+	numShardableLegs := result.ShardedSelectors
+
 	// If total queries is provided through hints, then we adjust the number of shards for the query
 	// based on the configured max sharded queries limit.
 	if maxShardedQueries := validation.SmallestPositiveIntPerTenant(tenantIDs, s.limit.QueryShardingMaxShardedQueries); maxShardedQueries > 0 {
-		// Calculate how many legs are shardable.
-		analyzer := astmapper.NewShardingAnalyzer(s.logger)
-		result, err := analyzer.Analyze(queryExpr)
-		if err != nil {
-			return 0, err
-		}
-		numShardableLegs := 1
-		if result.WillShardAllSelectors && result.ShardedSelectors > 0 {
-			numShardableLegs = result.ShardedSelectors
-		}
-
-		spanLog.DebugLog(
-			"msg", "computed number of shardable legs for the query",
-			"shardable legs", numShardableLegs,
-		)
-
 		prevTotalShards := totalShards
 		totalShards = max(1, min(totalShards, (maxShardedQueries/int(totalQueries))/numShardableLegs))
 
