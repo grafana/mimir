433e9b04cf9f7feb7970963e714c74ef26af4470
Charles Korn <charleskorn@users.noreply.github.com>
2025-10-23 16:12:06 +1100

    Fix issue where log messages emitted by `queryResponseWriter` aren't attached to the request's trace (#13124)
    #### What this PR does
    
    This PR fixes an issue where the log messages emitted by
    `queryResponseWriter` aren't attached to the request's trace.
    
    I've chosen not to add a changelog entry given this is not a user-facing
    change.
    
    #### Which issue(s) this PR fixes or relates to
    
    (none)
    
    #### Checklist
    
    - [n/a] Tests updated.
    - [n/a] Documentation added.
    - [n/a] `CHANGELOG.md` updated - the order of entries should be
    `[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`. If changelog entry
    is not needed, please add the `changelog-not-needed` label to the PR.
    - [n/a]
    [`about-versioning.md`](https://github.com/grafana/mimir/blob/main/docs/sources/mimir/configure/about-versioning.md)
    updated with experimental features.

--- pkg/querier/dispatcher.go
+++ pkg/querier/dispatcher.go
@@ -36,6 +36,7 @@ import (
 	"github.com/grafana/mimir/pkg/streamingpromql/types"
 	"github.com/grafana/mimir/pkg/util/limiter"
 	"github.com/grafana/mimir/pkg/util/propagation"
+	"github.com/grafana/mimir/pkg/util/spanlogger"
 )
 
 var evaluateQueryRequestMessageName = proto.MessageName(&querierpb.EvaluateQueryRequest{})
@@ -72,7 +73,8 @@ func NewDispatcher(engine *streamingpromql.Engine, queryable storage.Queryable, 
 }
 
 func (d *Dispatcher) HandleProtobuf(ctx context.Context, req *prototypes.Any, metadata propagation.Carrier, stream frontendv2pb.QueryResultStream) {
-	writer := newQueryResponseWriter(stream, d.querierMetrics, d.serverMetrics, d.logger)
+	logger := spanlogger.FromContext(ctx, d.logger)
+	writer := newQueryResponseWriter(stream, d.querierMetrics, d.serverMetrics, logger)
 	messageName, err := prototypes.AnyMessageName(req)
 	writer.Start(messageName, len(req.Value)) // We deliberately call this before checking the error below, so that we still get stats for malformed requests.
 	defer writer.Finish()
@@ -180,7 +182,7 @@ type queryResponseWriter struct {
 	stream         frontendv2pb.QueryResultStream
 	querierMetrics *RequestMetrics
 	serverMetrics  *server.Metrics
-	logger         log.Logger
+	logger         *spanlogger.SpanLogger
 
 	querierInflightRequests     prometheus.Gauge
 	serverInflightRequests      prometheus.Gauge
@@ -192,7 +194,7 @@ type queryResponseWriter struct {
 	tenantID                    string
 }
 
-func newQueryResponseWriter(stream frontendv2pb.QueryResultStream, querierMetrics *RequestMetrics, serverMetrics *server.Metrics, logger log.Logger) *queryResponseWriter {
+func newQueryResponseWriter(stream frontendv2pb.QueryResultStream, querierMetrics *RequestMetrics, serverMetrics *server.Metrics, logger *spanlogger.SpanLogger) *queryResponseWriter {
 	return &queryResponseWriter{
 		stream:         stream,
 		querierMetrics: querierMetrics,
