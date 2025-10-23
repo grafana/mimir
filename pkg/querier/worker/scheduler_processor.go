grafana/mimir

commit 1bf3bbf65e8aa74d63b95cd3e99099ca22cacb2c

Remote execution: improve logging of errors when streaming results from querier (#13099)
#### What this PR does

This PR improves the logging from queriers when sending a message to
query-frontends fails.

It also fixes an issue where a non-client-generated error (eg.
networking issue) while sending a message to query-frontends results in
queriers logging an "EOF" error. Instead, we need to call `RecvMsg()` to
discover the error.

#### Which issue(s) this PR fixes or relates to

https://github.com/grafana/mimir/pull/12551

#### Checklist

- [n/a] Tests updated.
- [n/a] Documentation added.
- [x] `CHANGELOG.md` updated - the order of entries should be
`[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`. If changelog entry
is not needed, please add the `changelog-not-needed` label to the PR.
- [n/a]
[`about-versioning.md`](https://github.com/grafana/mimir/blob/main/docs/sources/mimir/configure/about-versioning.md)
updated with experimental features.

pkg/querier/worker/scheduler_processor.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 	"time"
@@ -516,9 +517,9 @@ func (g *grpcStreamWriter) Write(ctx context.Context, msg *frontendv2pb.QueryRes
 	if g.stream != nil {
 		// We already have a stream, use it.
 		// If this fails, don't retry, as we can't know if the message made it to the frontend or not, which could lead to incorrect query results.
-		if err := g.stream.Send(msg); err != nil {
-			g.clientPool.RemoveClient(g.client, g.frontendAddress)
+		if err := g.writeMessageToStream(g.stream, g.client, msg); err != nil {
 			g.failed = true
+			level.Warn(g.logger).Log("msg", "attempt to send subsequent message to query-frontend failed", "err", err, "frontendAddress", g.frontendAddress, "queryID", msg.QueryID)
 			return err
 		}
 
@@ -532,21 +533,21 @@ func (g *grpcStreamWriter) Write(ctx context.Context, msg *frontendv2pb.QueryRes
 	bof := createFrontendBackoff(ctx)
 	var err error
 	for bof.Ongoing() {
-		err = g.tryToStartStreamAndSendFirstMessage(ctx, msg)
+		err = g.tryToStartStreamAndWriteFirstMessage(ctx, msg)
 		if err == nil {
 			return nil
 		}
 
-		level.Warn(g.logger).Log("msg", "attempt to send message to query-frontend failed", "err", err, "frontendAddress", g.frontendAddress, "queryID", msg.QueryID, "attempt", bof.NumRetries()+1)
+		level.Warn(g.logger).Log("msg", "attempt to send initial message to query-frontend failed", "err", err, "frontendAddress", g.frontendAddress, "queryID", msg.QueryID, "attempt", bof.NumRetries()+1)
 		bof.Wait()
 	}
 
-	level.Error(g.logger).Log("msg", "abandoned attempt to send message to query-frontend", "lastErr", err, "frontendAddress", g.frontendAddress, "queryID", msg.QueryID)
+	level.Error(g.logger).Log("msg", "abandoned attempt to send initial message to query-frontend", "lastErr", err, "frontendAddress", g.frontendAddress, "queryID", msg.QueryID)
 	g.failed = true
 	return err
 }
 
-func (g *grpcStreamWriter) tryToStartStreamAndSendFirstMessage(ctx context.Context, msg *frontendv2pb.QueryResultStreamRequest) error {
+func (g *grpcStreamWriter) tryToStartStreamAndWriteFirstMessage(ctx context.Context, msg *frontendv2pb.QueryResultStreamRequest) error {
 	client, err := g.clientPool.GetClientFor(g.frontendAddress)
 	if err != nil {
 		return err
@@ -558,8 +559,7 @@ func (g *grpcStreamWriter) tryToStartStreamAndSendFirstMessage(ctx context.Conte
 		return err
 	}
 
-	if err := stream.Send(msg); err != nil {
-		g.clientPool.RemoveClient(client, g.frontendAddress)
+	if err := g.writeMessageToStream(stream, client, msg); err != nil {
 		return err
 	}
 
@@ -567,6 +567,29 @@ func (g *grpcStreamWriter) tryToStartStreamAndSendFirstMessage(ctx context.Conte
 	g.stream = stream
 
 	return nil
+}
+
+func (g *grpcStreamWriter) writeMessageToStream(stream frontendv2pb.FrontendForQuerier_QueryResultStreamClient, client client.PoolClient, msg *frontendv2pb.QueryResultStreamRequest) error {
+	err := stream.Send(msg)
+
+	if err == nil {
+		return nil
+	}
+
+	g.clientPool.RemoveClient(client, g.frontendAddress)
+
+	if errors.Is(err, io.EOF) {
+		// If Send returns EOF, then that means the error was not generated by the client (ie. the querier) and we need to call RecvMsg to get that error.
+		// See the docs for grpc.ClientStream for more details.
+		receivedErr := stream.RecvMsg(nil)
+		if receivedErr == nil {
+			return fmt.Errorf("writing message to stream failed with EOF error (ie. not generated by client), and RecvMsg returned no error")
+		} else {
+			return fmt.Errorf("writing message to stream failed due to error not generated by client: %w", receivedErr)
+		}
+	}
+
+	return err
 }
 
 func (g *grpcStreamWriter) Close(ctx context.Context) {
@@ -590,8 +613,9 @@ func (g *grpcStreamWriter) Close(ctx context.Context) {
 	}
 
 	if g.stream != nil {
-		// If this fails, there's not much we can do.
-		_, _ = g.stream.CloseAndRecv()
+		if _, err := g.stream.CloseAndRecv(); err != nil {
+			level.Warn(g.logger).Log("msg", "error closing query-frontend stream", "err", err)
+		}
 	}
 }
 

pkg/querier/dispatcher.go
@@ -243,7 +243,12 @@ func (w *queryResponseWriter) Write(ctx context.Context, r querierpb.EvaluateQue
 		},
 	}
 
-	return w.write(ctx, resp)
+	if err := w.write(ctx, resp); err != nil {
+		level.Warn(w.logger).Log("msg", "failed to write message to stream", "err", err)
+		return err
+	}
+
+	return nil
 }
 
 func (w *queryResponseWriter) WriteError(ctx context.Context, fallbackType apierror.Type, err error) {
@@ -269,7 +274,7 @@ func (w *queryResponseWriter) WriteError(ctx context.Context, fallbackType apier
 	}
 
 	if err := w.write(ctx, resp); err != nil {
-		level.Debug(w.logger).Log("msg", "failed to write error", "writeErr", err, "originalErr", msg)
+		level.Warn(w.logger).Log("msg", "failed to write error to stream", "writeErr", err, "originalErr", msg)
 	}
 }
 

commit 4c1a6ea02c5ed4760e8318a6dd5f1c3a2c5ce234

Remote execution: fix panic in querier if writing message to stream fails (#13121)
#### What this PR does

This PR fixes an issue introduced in
https://github.com/grafana/mimir/pull/13099.

If writing a message to the stream fails and returns `EOF`, calling
`RecvMsg` with a `nil` target causes the querier to panic.

#### Which issue(s) this PR fixes or relates to

#13099

#### Checklist

- [n/a] Tests updated.
- [n/a] Documentation added.
- [x] `CHANGELOG.md` updated - the order of entries should be
`[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`. If changelog entry
is not needed, please add the `changelog-not-needed` label to the PR.
- [n/a]
[`about-versioning.md`](https://github.com/grafana/mimir/blob/main/docs/sources/mimir/configure/about-versioning.md)
updated with experimental features.

pkg/querier/worker/scheduler_processor.go
@@ -581,7 +581,7 @@ func (g *grpcStreamWriter) writeMessageToStream(stream frontendv2pb.FrontendForQ
 	if errors.Is(err, io.EOF) {
 		// If Send returns EOF, then that means the error was not generated by the client (ie. the querier) and we need to call RecvMsg to get that error.
 		// See the docs for grpc.ClientStream for more details.
-		receivedErr := stream.RecvMsg(nil)
+		receivedErr := stream.RecvMsg(&frontendv2pb.QueryResultResponse{})
 		if receivedErr == nil {
 			return fmt.Errorf("writing message to stream failed with EOF error (ie. not generated by client), and RecvMsg returned no error")
 		} else {