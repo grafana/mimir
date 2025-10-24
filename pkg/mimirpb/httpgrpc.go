497ef4b48ca78fad9e3c437ff5ada6cc1aa84a0b
Toni Cárdenas <toni.cardenasvargas@grafana.com>
2025-10-24 17:54:52 +0200

    Add BufferHolder to frontendpb.QueryResultRequest.

--- pkg/mimirpb/httpgrpc.go
+++ pkg/mimirpb/httpgrpc.go
@@ -11,3 +11,11 @@ type HTTPRequest struct {
 	BufferHolder
 	httpgrpc.HTTPRequest
 }
+
+// HTTPResponse is a wrapper around httpgrpc that also embeds BufferHolder.
+//
+// Use this if you're using HTTPResponse as a request message in a service.
+type HTTPResponse struct {
+	BufferHolder
+	httpgrpc.HTTPResponse
+}
