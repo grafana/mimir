grafana/mimir

commit ed30de76b5f480159c212a04fa7316d3a9bb760a

Remote execution: querier backwards compatibility support (#13017)
#### What this PR does

This PR changes the behaviour of query-frontends to only generate query
plans that the running set of queriers support.

This ensures that query-frontends don't start generating plans that
queriers don't support during rollouts until all queriers have upgraded.

Queriers now join a ring and report the maximum supported query plan
version as part of their instance description, and query-frontends then
pass this to the query planner.

Optimisations passes can then choose to skip optimisations that
introduce functionality that is not supported by the running set of
queriers.

Not included in this PR, but will be added separately: alerts to warn
when the cluster is in an inconsistent state (eg. query-frontends are
reporting a maximum supported version that does not match what queriers
support).

Don't be put off by the large number of files changed: most of these are
the regenerated Helm or Jsonnet test files, or files that needed to be
updated due to the changed signature of `AddIngester` from the dskit PR.

:warning: This PR depends on https://github.com/grafana/dskit/pull/757.

#### Which issue(s) this PR fixes or relates to

https://github.com/grafana/mimir/pull/12808,
https://github.com/grafana/mimir/pull/12961

#### Checklist

- [x] Tests updated.
- [n/a] Documentation added.
- [x] `CHANGELOG.md` updated - the order of entries should be
`[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`. If changelog entry
is not needed, please add the `changelog-not-needed` label to the PR.
- [x]
[`about-versioning.md`](https://github.com/grafana/mimir/blob/main/docs/sources/mimir/configure/about-versioning.md)
updated with experimental features.

pkg/mimir/modules.go
@@ -106,7 +106,9 @@ const (
 	Overrides                        string = "overrides"
 	OverridesExporter                string = "overrides-exporter"
 	Querier                          string = "querier"
+	QuerierLifecycler                string = "querier-lifecycler"
 	QuerierQueryPlanner              string = "querier-query-planner"
+	QuerierRing                      string = "querier-ring"
 	QueryFrontend                    string = "query-frontend"
 	QueryFrontendCodec               string = "query-frontend-codec"
 	QueryFrontendQueryPlanner        string = "query-frontend-query-planner"
@@ -229,6 +231,7 @@ func (t *Mimir) initVault() (services.Service, error) {
 	t.Cfg.Ingester.IngesterRing.KVStore.Etcd.TLS.Reader = t.Vault
 	t.Cfg.MemberlistKV.TCPTransport.TLS.Reader = t.Vault
 	t.Cfg.OverridesExporter.Ring.Common.KVStore.Etcd.TLS.Reader = t.Vault
+	t.Cfg.Querier.Ring.Common.KVStore.Etcd.TLS.Reader = t.Vault
 	t.Cfg.QueryScheduler.ServiceDiscovery.SchedulerRing.KVStore.Etcd.TLS.Reader = t.Vault
 	t.Cfg.Ruler.Ring.Common.KVStore.Etcd.TLS.Reader = t.Vault
 	t.Cfg.StoreGateway.ShardingRing.KVStore.Etcd.TLS.Reader = t.Vault
@@ -457,6 +460,7 @@ func (t *Mimir) initRuntimeConfig() (services.Service, error) {
 	t.Cfg.Ingester.IngesterPartitionRing.KVStore.Multi.ConfigProvider = multiClientRuntimeConfigChannel(t.RuntimeConfig)
 	t.Cfg.Ingester.IngesterRing.KVStore.Multi.ConfigProvider = multiClientRuntimeConfigChannel(t.RuntimeConfig)
 	t.Cfg.OverridesExporter.Ring.Common.KVStore.Multi.ConfigProvider = multiClientRuntimeConfigChannel(t.RuntimeConfig)
+	t.Cfg.Querier.Ring.Common.KVStore.Multi.ConfigProvider = multiClientRuntimeConfigChannel(t.RuntimeConfig)
 	t.Cfg.QueryScheduler.ServiceDiscovery.SchedulerRing.KVStore.Multi.ConfigProvider = multiClientRuntimeConfigChannel(t.RuntimeConfig)
 	t.Cfg.Ruler.Ring.Common.KVStore.Multi.ConfigProvider = multiClientRuntimeConfigChannel(t.RuntimeConfig)
 	t.Cfg.StoreGateway.ShardingRing.KVStore.Multi.ConfigProvider = multiClientRuntimeConfigChannel(t.RuntimeConfig)
@@ -573,6 +577,30 @@ func (t *Mimir) initTenantFederation() (serv services.Service, err error) {
 		t.MetadataSupplier = tenantfederation.NewMetadataSupplier(t.MetadataSupplier, t.Cfg.TenantFederation.MaxConcurrent, util_log.Logger)
 	}
 	return nil, nil
+}
+
+func (t *Mimir) initQuerierLifecycler() (services.Service, error) {
+	t.Cfg.Querier.Ring.Common.ListenPort = t.Cfg.Server.GRPCListenPort
+
+	lifecycler, err := querier.NewLifecycler(t.Cfg.Querier.Ring, util_log.Logger, t.Registerer)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create querier lifecycler: %w", err)
+	}
+
+	t.QuerierLifecycler = lifecycler
+
+	return lifecycler, nil
+}
+
+func (t *Mimir) initQuerierRing() (services.Service, error) {
+	r, err := querier.NewRing(t.Cfg.Querier.Ring, util_log.Logger, t.Registerer)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create querier ring: %w", err)
+	}
+
+	t.QuerierRing = r
+
+	return r, nil
 }
 
 // initQuerier registers an internal HTTP router with a Prometheus API backed by the
@@ -964,8 +992,12 @@ func (t *Mimir) initQuerierQueryPlanner() (services.Service, error) {
 	reg := prometheus.WrapRegistererWith(prometheus.Labels{"component": "querier"}, t.Registerer)
 	_, mqeOpts := engine.NewPromQLEngineOptions(t.Cfg.Querier.EngineConfig, t.ActivityTracker, util_log.Logger, reg)
 
+	// The query plan generated by this querier will only be used in this process, so we can
+	// allow anything this version of Mimir supports.
+	versionProvider := streamingpromql.NewMaximumSupportedVersionQueryPlanVersionProvider()
+
 	var err error
-	t.QuerierQueryPlanner, err = streamingpromql.NewQueryPlanner(mqeOpts)
+	t.QuerierQueryPlanner, err = streamingpromql.NewQueryPlanner(mqeOpts, versionProvider)
 	if err != nil {
 		return nil, err
 	}
@@ -984,8 +1016,19 @@ func (t *Mimir) initQueryFrontendQueryPlanner() (services.Service, error) {
 	reg := prometheus.WrapRegistererWith(prometheus.Labels{"component": "query-frontend"}, t.Registerer)
 	_, mqeOpts := engine.NewPromQLEngineOptions(t.Cfg.Querier.EngineConfig, t.ActivityTracker, util_log.Logger, reg)
 
+	var versionProvider streamingpromql.QueryPlanVersionProvider
+
+	if t.Cfg.Frontend.QueryMiddleware.EnableRemoteExecution {
+		versionProvider = querier.NewRingQueryPlanVersionProvider(t.QuerierRing, t.Registerer)
+	} else {
+		// If remote execution is not enabled, then the query plans we generate in the query-frontend will
+		// only be used in this process, so we can generate query plans up to whatever the maximum supported
+		// version is.
+		versionProvider = streamingpromql.NewMaximumSupportedVersionQueryPlanVersionProvider()
+	}
+
 	var err error
-	t.QueryFrontendQueryPlanner, err = streamingpromql.NewQueryPlanner(mqeOpts)
+	t.QueryFrontendQueryPlanner, err = streamingpromql.NewQueryPlanner(mqeOpts, versionProvider)
 	if err != nil {
 		return nil, err
 	}
@@ -1235,6 +1278,7 @@ func (t *Mimir) initMemberlistKV() (services.Service, error) {
 	t.Cfg.Ingester.IngesterPartitionRing.KVStore.MemberlistKV = t.MemberlistKV.GetMemberlistKV
 	t.Cfg.Ingester.IngesterRing.KVStore.MemberlistKV = t.MemberlistKV.GetMemberlistKV
 	t.Cfg.OverridesExporter.Ring.Common.KVStore.MemberlistKV = t.MemberlistKV.GetMemberlistKV
+	t.Cfg.Querier.Ring.Common.KVStore.MemberlistKV = t.MemberlistKV.GetMemberlistKV
 	t.Cfg.QueryScheduler.ServiceDiscovery.SchedulerRing.KVStore.MemberlistKV = t.MemberlistKV.GetMemberlistKV
 	t.Cfg.Ruler.Ring.Common.KVStore.MemberlistKV = t.MemberlistKV.GetMemberlistKV
 	t.Cfg.StoreGateway.ShardingRing.KVStore.MemberlistKV = t.MemberlistKV.GetMemberlistKV
@@ -1405,7 +1449,9 @@ func (t *Mimir) setupModuleManager() error {
 	mm.RegisterModule(Overrides, t.initOverrides, modules.UserInvisibleModule)
 	mm.RegisterModule(OverridesExporter, t.initOverridesExporter)
 	mm.RegisterModule(Querier, t.initQuerier)
+	mm.RegisterModule(QuerierLifecycler, t.initQuerierLifecycler, modules.UserInvisibleModule)
 	mm.RegisterModule(QuerierQueryPlanner, t.initQuerierQueryPlanner, modules.UserInvisibleModule)
+	mm.RegisterModule(QuerierRing, t.initQuerierRing, modules.UserInvisibleModule)
 	mm.RegisterModule(QueryFrontend, t.initQueryFrontend)
 	mm.RegisterModule(QueryFrontendCodec, t.initQueryFrontendCodec, modules.UserInvisibleModule)
 	mm.RegisterModule(QueryFrontendQueryPlanner, t.initQueryFrontendQueryPlanner, modules.UserInvisibleModule)
@@ -1449,10 +1495,12 @@ func (t *Mimir) setupModuleManager() error {
 		MemberlistKV:                     {API, Vault},
 		Overrides:                        {RuntimeConfig},
 		OverridesExporter:                {Overrides, MemberlistKV, Vault},
-		Querier:                          {TenantFederation, Vault},
+		Querier:                          {TenantFederation, Vault, QuerierLifecycler},
+		QuerierLifecycler:                {API, RuntimeConfig, MemberlistKV, Vault},
 		QuerierQueryPlanner:              {API, ActivityTracker},
+		QuerierRing:                      {API, RuntimeConfig, MemberlistKV, Vault},
 		QueryFrontend:                    {QueryFrontendTripperware, MemberlistKV, Vault},
-		QueryFrontendQueryPlanner:        {API, ActivityTracker, Overrides},
+		QueryFrontendQueryPlanner:        {API, ActivityTracker, Overrides, QuerierRing},
 		QueryFrontendTopicOffsetsReaders: {IngesterPartitionRing},
 		QueryFrontendTripperware:         {API, Overrides, QueryFrontendCodec, QueryFrontendTopicOffsetsReaders, QueryFrontendQueryPlanner},
 		QueryScheduler:                   {API, Overrides, MemberlistKV, Vault},

pkg/querier/querier_ring.go
@@ -1,0 +1,179 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package querier
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"math"
+	"net"
+	"strconv"
+	"time"
+
+	"github.com/go-kit/log"
+	"github.com/grafana/dskit/kv"
+	"github.com/grafana/dskit/ring"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+
+	"github.com/grafana/mimir/pkg/streamingpromql"
+	"github.com/grafana/mimir/pkg/streamingpromql/planning"
+	"github.com/grafana/mimir/pkg/util"
+)
+
+const (
+	// ringNumTokens is how many tokens each querier should have in the ring.
+	// Queriers use a ring because they need to share the maximum query plan version
+	// they support with query-frontends.
+	ringNumTokens = 1
+
+	querierRingKey = "querier"
+
+	MaximumSupportedQueryPlanVersion = 1
+)
+
+var statusPageConfig = ring.StatusPageConfig{
+	HideTokensUIElements: true,
+	ShowVersions:         true,
+	ComponentNames: map[uint64]string{
+		MaximumSupportedQueryPlanVersion: "maximum supported query plan version",
+	},
+}
+
+// RingConfig strips the ring lifecycler configuration down to the minimum required for the querier ring.
+// The ring lifecycler config contains many options not needed by the querier ring.
+type RingConfig struct {
+	Common util.CommonRingConfig `yaml:",inline"`
+
+	AutoForgetUnhealthyPeriods int `yaml:"auto_forget_unhealthy_periods" category:"advanced"`
+}
+
+func (cfg *RingConfig) RegisterFlags(f *flag.FlagSet, logger log.Logger) {
+	cfg.Common.RegisterFlags("querier.ring.", "collectors/", "queriers", f, logger)
+	f.IntVar(&cfg.AutoForgetUnhealthyPeriods, "querier.ring.auto-forget-unhealthy-periods", 10, "Number of consecutive timeout periods an unhealthy instance in the ring is automatically removed after. Set to 0 to disable auto-forget.")
+}
+
+func (cfg *RingConfig) ToBasicLifecyclerConfig(logger log.Logger) (ring.BasicLifecyclerConfig, error) {
+	instanceAddr, err := ring.GetInstanceAddr(cfg.Common.InstanceAddr, cfg.Common.InstanceInterfaceNames, logger, cfg.Common.EnableIPv6)
+	if err != nil {
+		return ring.BasicLifecyclerConfig{}, err
+	}
+
+	instancePort := ring.GetInstancePort(cfg.Common.InstancePort, cfg.Common.ListenPort)
+
+	return ring.BasicLifecyclerConfig{
+		ID:                              cfg.Common.InstanceID,
+		Addr:                            net.JoinHostPort(instanceAddr, strconv.Itoa(instancePort)),
+		HeartbeatPeriod:                 cfg.Common.HeartbeatPeriod,
+		HeartbeatTimeout:                cfg.Common.HeartbeatTimeout,
+		TokensObservePeriod:             0,
+		NumTokens:                       ringNumTokens,
+		KeepInstanceInTheRingOnShutdown: false,
+		StatusPageConfig:                statusPageConfig,
+		Versions: ring.InstanceVersions{
+			MaximumSupportedQueryPlanVersion: planning.MaximumSupportedQueryPlanVersion,
+		},
+	}, nil
+}
+
+func (cfg *RingConfig) toRingConfig() ring.Config {
+	rc := cfg.Common.ToRingConfig()
+	rc.ReplicationFactor = 1
+	rc.StatusPageConfig = statusPageConfig
+
+	return rc
+}
+
+func NewLifecycler(cfg RingConfig, logger log.Logger, reg prometheus.Registerer) (*ring.BasicLifecycler, error) {
+	reg = prometheus.WrapRegistererWithPrefix("cortex_", reg)
+	kvStore, err := kv.NewClient(cfg.Common.KVStore, ring.GetCodec(), kv.RegistererWithKVName(reg, "querier-lifecycler"), logger)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize querier KV store: %w", err)
+	}
+
+	lifecyclerCfg, err := cfg.ToBasicLifecyclerConfig(logger)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build querier lifecycler config: %w", err)
+	}
+
+	var delegate ring.BasicLifecyclerDelegate
+	delegate = ring.NewInstanceRegisterDelegate(ring.ACTIVE, lifecyclerCfg.NumTokens)
+	delegate = ring.NewLeaveOnStoppingDelegate(delegate, logger)
+	if cfg.AutoForgetUnhealthyPeriods > 0 {
+		delegate = ring.NewAutoForgetDelegate(time.Duration(cfg.AutoForgetUnhealthyPeriods)*cfg.Common.HeartbeatTimeout, delegate, logger)
+	}
+
+	lifecycler, err := ring.NewBasicLifecycler(lifecyclerCfg, "querier", querierRingKey, kvStore, delegate, logger, reg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize querier lifecycler: %w", err)
+	}
+
+	promauto.With(reg).NewGauge(prometheus.GaugeOpts{
+		Name: "cortex_querier_maximum_supported_query_plan_version",
+		Help: "The maximum supported query plan version this process was compiled to support.",
+	}).Set(float64(planning.MaximumSupportedQueryPlanVersion))
+
+	return lifecycler, nil
+}
+
+func NewRing(cfg RingConfig, logger log.Logger, reg prometheus.Registerer) (*ring.Ring, error) {
+	reg = prometheus.WrapRegistererWithPrefix("cortex_", reg)
+	r, err := ring.New(cfg.toRingConfig(), "querier", querierRingKey, logger, reg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize querier ring client: %w", err)
+	}
+
+	return r, nil
+}
+
+type RingQueryPlanVersionProvider struct {
+	ring ring.ReadRing
+}
+
+func NewRingQueryPlanVersionProvider(ring ring.ReadRing, reg prometheus.Registerer) streamingpromql.QueryPlanVersionProvider {
+	provider := &RingQueryPlanVersionProvider{ring: ring}
+
+	// The metrics below are only expected to be exposed by query-frontends, hence the cortex_query_frontend_ prefix.
+	promauto.With(reg).NewGaugeFunc(prometheus.GaugeOpts{
+		Name: "cortex_query_frontend_querier_ring_calculated_maximum_supported_query_plan_version",
+		Help: "The maximum supported query plan version calculated from the querier ring.",
+	}, func() float64 {
+		version, err := provider.GetMaximumSupportedQueryPlanVersion(context.Background())
+		if err != nil {
+			return -1
+		}
+
+		return float64(version)
+	})
+
+	promauto.With(reg).NewGauge(prometheus.GaugeOpts{
+		Name: "cortex_query_frontend_querier_ring_expected_maximum_supported_query_plan_version",
+		Help: "The maximum supported query plan version this process was compiled to support.",
+	}).Set(float64(planning.MaximumSupportedQueryPlanVersion))
+
+	return provider
+}
+
+var queryPlanVersioningOp = ring.NewOp([]ring.InstanceState{ring.ACTIVE, ring.PENDING, ring.JOINING, ring.LEAVING}, nil)
+var errQuerierHasNoSupportedQueryPlanVersion = fmt.Errorf("one or more queriers in the ring is not reporting a supported query plan version")
+
+func (r *RingQueryPlanVersionProvider) GetMaximumSupportedQueryPlanVersion(_ context.Context) (uint64, error) {
+	instances, err := r.ring.GetAllHealthy(queryPlanVersioningOp)
+	if err != nil {
+		return 0, fmt.Errorf("could not get all queriers from the ring: %w", err)
+	}
+
+	lowestVersionSeen := uint64(math.MaxUint64)
+
+	for _, instance := range instances.Instances {
+		version, ok := instance.Versions[MaximumSupportedQueryPlanVersion]
+		if !ok {
+			return 0, errQuerierHasNoSupportedQueryPlanVersion
+		}
+
+		lowestVersionSeen = min(lowestVersionSeen, version)
+	}
+
+	return lowestVersionSeen, nil
+}

commit ff4298ed415c3e813b487ea3b2e6f1a4e8fee340

Remote execution: fix incorrect metric name, and log when computing maximum supported query plan version fails (#13164)
#### What this PR does

This PR fixes two issues introduced in
https://github.com/grafana/mimir/pull/13017:

* the `cortex_querier_maximum_supported_query_plan_version` metric
incorrectly had the `cortex_` prefix twice
* failures computing the maximum supported query plan version were not
logged, making it difficult to investigate why the
`cortex_query_frontend_querier_ring_calculated_maximum_supported_query_plan_version`
metric was -1

#### Which issue(s) this PR fixes or relates to

https://github.com/grafana/mimir/pull/13017

#### Checklist

- [x] Tests updated.
- [n/a] Documentation added.
- [x] `CHANGELOG.md` updated - the order of entries should be
`[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`. If changelog entry
is not needed, please add the `changelog-not-needed` label to the PR.
- [n/a]
[`about-versioning.md`](https://github.com/grafana/mimir/blob/main/docs/sources/mimir/configure/about-versioning.md)
updated with experimental features.

pkg/mimir/modules.go
@@ -1019,7 +1019,7 @@ func (t *Mimir) initQueryFrontendQueryPlanner() (services.Service, error) {
 	var versionProvider streamingpromql.QueryPlanVersionProvider
 
 	if t.Cfg.Frontend.QueryMiddleware.EnableRemoteExecution {
-		versionProvider = querier.NewRingQueryPlanVersionProvider(t.QuerierRing, t.Registerer)
+		versionProvider = querier.NewRingQueryPlanVersionProvider(t.QuerierRing, t.Registerer, util_log.Logger)
 	} else {
 		// If remote execution is not enabled, then the query plans we generate in the query-frontend will
 		// only be used in this process, so we can generate query plans up to whatever the maximum supported

pkg/querier/querier_ring.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/grafana/dskit/kv"
 	"github.com/grafana/dskit/ring"
 	"github.com/prometheus/client_golang/prometheus"
@@ -110,7 +111,7 @@ func NewLifecycler(cfg RingConfig, logger log.Logger, reg prometheus.Registerer)
 	}
 
 	promauto.With(reg).NewGauge(prometheus.GaugeOpts{
-		Name: "cortex_querier_maximum_supported_query_plan_version",
+		Name: "querier_maximum_supported_query_plan_version",
 		Help: "The maximum supported query plan version this process was compiled to support.",
 	}).Set(float64(planning.MaximumSupportedQueryPlanVersion))
 
@@ -131,7 +132,7 @@ type RingQueryPlanVersionProvider struct {
 	ring ring.ReadRing
 }
 
-func NewRingQueryPlanVersionProvider(ring ring.ReadRing, reg prometheus.Registerer) streamingpromql.QueryPlanVersionProvider {
+func NewRingQueryPlanVersionProvider(ring ring.ReadRing, reg prometheus.Registerer, logger log.Logger) streamingpromql.QueryPlanVersionProvider {
 	provider := &RingQueryPlanVersionProvider{ring: ring}
 
 	// The metrics below are only expected to be exposed by query-frontends, hence the cortex_query_frontend_ prefix.
@@ -141,6 +142,7 @@ func NewRingQueryPlanVersionProvider(ring ring.ReadRing, reg prometheus.Register
 	}, func() float64 {
 		version, err := provider.GetMaximumSupportedQueryPlanVersion(context.Background())
 		if err != nil {
+			level.Warn(logger).Log("msg", "failed to compute maximum supported query plan version", "err", err)
 			return -1
 		}
 
@@ -161,7 +163,7 @@ var errQuerierHasNoSupportedQueryPlanVersion = fmt.Errorf("one or more queriers 
 func (r *RingQueryPlanVersionProvider) GetMaximumSupportedQueryPlanVersion(ctx context.Context) (planning.QueryPlanVersion, error) {
 	instances, err := r.ring.GetAllHealthy(queryPlanVersioningOp)
 	if err != nil {
-		return 0, fmt.Errorf("could not get all queriers from the ring: %w", err)
+		return 0, fmt.Errorf("could not compute maximum supported query plan version: could not get all queriers from the ring: %w", err)
 	}
 
 	lowestVersionSeen := uint64(math.MaxUint64)
@@ -169,7 +171,7 @@ func (r *RingQueryPlanVersionProvider) GetMaximumSupportedQueryPlanVersion(ctx c
 	for _, instance := range instances.Instances {
 		version, ok := instance.Versions[MaximumSupportedQueryPlanVersion]
 		if !ok {
-			return 0, errQuerierHasNoSupportedQueryPlanVersion
+			return 0, fmt.Errorf("could not compute maximum supported query plan version: %w", errQuerierHasNoSupportedQueryPlanVersion)
 		}
 
 		lowestVersionSeen = min(lowestVersionSeen, version)

pkg/querier/querier_ring_test.go
@@ -36,7 +36,7 @@ func TestRingQueryPlanVersionProvider(t *testing.T) {
 		expectedError   string
 	}{
 		"no instances in the ring": {
-			expectedError: "could not get all queriers from the ring: empty ring",
+			expectedError: "could not compute maximum supported query plan version: could not get all queriers from the ring: empty ring",
 		},
 		"one instance in the ring, has no version": {
 			instances: []mockInstance{
@@ -44,7 +44,7 @@ func TestRingQueryPlanVersionProvider(t *testing.T) {
 					state: ring.ACTIVE,
 				},
 			},
-			expectedError: "one or more queriers in the ring is not reporting a supported query plan version",
+			expectedError: "could not compute maximum supported query plan version: one or more queriers in the ring is not reporting a supported query plan version",
 		},
 		"one instance in the ring, has version and is active": {
 			instances: []mockInstance{
@@ -94,7 +94,7 @@ func TestRingQueryPlanVersionProvider(t *testing.T) {
 					state: ring.ACTIVE,
 				},
 			},
-			expectedError: "one or more queriers in the ring is not reporting a supported query plan version",
+			expectedError: "could not compute maximum supported query plan version: one or more queriers in the ring is not reporting a supported query plan version",
 		},
 		"many instances in the ring, some have no version": {
 			instances: []mockInstance{
@@ -109,7 +109,7 @@ func TestRingQueryPlanVersionProvider(t *testing.T) {
 					state: ring.ACTIVE,
 				},
 			},
-			expectedError: "one or more queriers in the ring is not reporting a supported query plan version",
+			expectedError: "could not compute maximum supported query plan version: one or more queriers in the ring is not reporting a supported query plan version",
 		},
 		"many instances in the ring, all have the same version": {
 			instances: []mockInstance{
@@ -175,7 +175,7 @@ func TestRingQueryPlanVersionProvider(t *testing.T) {
 			require.NoError(t, services.StartAndAwaitRunning(ctx, r))
 			t.Cleanup(func() { _ = services.StopAndAwaitTerminated(ctx, r) })
 
-			versionProvider := NewRingQueryPlanVersionProvider(r, reg)
+			versionProvider := NewRingQueryPlanVersionProvider(r, reg, log.NewNopLogger())
 			version, err := versionProvider.GetMaximumSupportedQueryPlanVersion(ctx)
 			if testCase.expectedError != "" {
 				require.EqualError(t, err, testCase.expectedError)