c9714a57765f4488e5dac87d151f7a780750b527
Jesus Vazquez <jesus.vazquez@grafana.com>
2025-10-29 12:36:05 +0100

    fix(query-tee): Include /api/v1/read as an existing route

--- cmd/query-tee/main.go
+++ cmd/query-tee/main.go
@@ -122,6 +122,7 @@ func mimirReadRoutes(cfg Config) []querytee.Route {
 	}
 
 	return []querytee.Route{
+		{Path: prefix + "/api/v1/read", RouteName: "api_v1_read", Methods: []string{"POST"}, ResponseComparator: nil},
 		{Path: prefix + "/api/v1/query", RouteName: "api_v1_query", Methods: []string{"GET", "POST"}, ResponseComparator: samplesComparator, RequestTransformers: instantQueryTransformers},
 		{Path: prefix + "/api/v1/query_range", RouteName: "api_v1_query_range", Methods: []string{"GET", "POST"}, ResponseComparator: samplesComparator},
 		{Path: prefix + "/api/v1/query_exemplars", RouteName: "api_v1_query_exemplars", Methods: []string{"GET", "POST"}, ResponseComparator: nil},
