43f736f69919aeca6a522109ef7a3448ebf2c181
Oleg Zaytsev <mail@olegzaytsev.com>
2025-11-12 10:45:58 +0000

    Update the default usage-tracker.user-close-to-limit-percentage-threshold
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

--- pkg/usagetracker/tracker.go
+++ pkg/usagetracker/tracker.go
@@ -127,7 +127,7 @@ func (c *Config) RegisterFlags(f *flag.FlagSet, logger log.Logger) {
 
 	f.IntVar(&c.MaxEventsFetchSize, "usage-tracker.max-events-fetch-size", 100, "Maximum number of events to fetch from Kafka in a single request. This is used to limit the memory usage when fetching events.")
 
-	f.IntVar(&c.UserCloseToLimitPercentageThreshold, "usage-tracker.user-close-to-limit-percentage-threshold", 85, "Percentage of the local series limit after which a user is considered close to the limit. A user is close to the limit if their series count is above this percentage of their local limit.")
+	f.IntVar(&c.UserCloseToLimitPercentageThreshold, "usage-tracker.user-close-to-limit-percentage-threshold", 90, "Percentage of the local series limit after which a user is considered close to the limit. A user is close to the limit if their series count is above this percentage of their local limit.")
 }
 
 func (c *Config) ValidateForClient() error {
