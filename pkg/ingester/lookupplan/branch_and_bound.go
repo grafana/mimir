8ef618cd011e6f30a8e020c53bb4bf00c1e4ee5d
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2025-11-15 11:22:13 +0100

    WIP proper branch and bound
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov@grafana.com>

--- pkg/ingester/lookupplan/branch_and_bound.go
+++ pkg/ingester/lookupplan/branch_and_bound.go
@@ -161,3 +161,129 @@ func (p CostBasedPlanner) generatePlansBranchAndBound(ctx context.Context, stati
 
 	return morePlans.Iterator()
 }
+
+func (p partialPlan) LowerBoundCost() float64 {
+	return p.indexLookupCost() + p.intersectionCost() + p.seriesRetrievalCost() + p.filterCost()
+}
+
+// indexLookupCost returns the cost of performing index lookups for all predicates that use the index
+func (p partialPlan) indexLookupCost() float64 {
+	cost := 0.0
+	for i := range p.predicates {
+		pr, ok := p.pretendDecidedPredicate(i)
+		if ok {
+			cost += pr.indexLookupCost()
+		}
+	}
+	return cost
+}
+
+// pretendDecidedPredicate returns the predicate at idx and whether it's a scan predicate.
+// The first undecided predicate is always treated as a decided index predicate for lower bound calculation purposes.
+// All other undecided predicates are treated as decided scan predicates.
+func (p partialPlan) pretendDecidedPredicate(idx int) (planPredicate, bool) {
+	if p.decidedPredicates[idx] {
+		return p.predicates[idx], p.indexPredicate[idx]
+	}
+
+	pretendPredicate := p.predicates[idx]
+	pretendPredicate.singleMatchCost = 1
+	pretendPredicate.cardinality = 1
+	pretendPredicate.labelNameUniqueVals = 1
+	pretendPredicate.selectivity = 1
+	pretendPredicate.indexScanCost = 1
+
+	return pretendPredicate, idx == p.firstUndecidedPredicateIdx()
+}
+
+// intersectionCost returns the cost of intersecting posting lists from multiple index predicates
+// This includes retrieving the series' labels from the index.
+func (p partialPlan) intersectionCost() float64 {
+	iteratedPostings := uint64(0)
+	for i := range p.predicates {
+		pred, ok := p.pretendDecidedPredicate(i)
+		if !ok {
+			continue
+		}
+
+		iteratedPostings += pred.cardinality
+	}
+
+	return float64(iteratedPostings) * p.config.RetrievedPostingCost
+}
+
+// seriesRetrievalCost returns the cost of retrieving series from the index after intersecting posting lists.
+// This includes retrieving the series' labels from the index and checking if the series belongs to the query's shard.
+// Realistically we don't retrieve every series because we have the series hash cache, but we ignore that for simplicity.
+func (p partialPlan) seriesRetrievalCost() float64 {
+	return float64(p.NumSelectedPostings()) * p.config.RetrievedSeriesCost
+}
+
+// filterCost returns the cost of applying scan predicates to the fetched series.
+// The sequence is: intersection → retrieve series → check shard → apply scan matchers.
+func (p partialPlan) filterCost() float64 {
+	cost := 0.0
+	seriesToFilter := p.numSelectedPostingsInOurShard()
+	for i := range p.predicates {
+		// In reality, we will apply all the predicates for each series and stop once one predicate doesn't match.
+		// But we calculate for the worst case where we have to run all predicates for all series.
+		pred, ok := p.pretendDecidedPredicate(i)
+		if !ok {
+			cost += pred.filterCost(seriesToFilter)
+		}
+	}
+	return cost
+}
+
+func (p partialPlan) numSelectedPostingsInOurShard() uint64 {
+	return shardedCardinality(p.NumSelectedPostings(), p.shard)
+}
+
+func (p partialPlan) NumSelectedPostings() uint64 {
+	finalSelectivity := 1.0
+	for i := range p.predicates {
+		pred, ok := p.pretendDecidedPredicate(i)
+		if !ok {
+			continue
+		}
+
+		// We use the selectivity across all series instead of the selectivity across label values.
+		// For example, if {protocol=~.*} matches all values, it doesn't mean it won't reduce the result set after intersection.
+		//
+		// We also assume independence between the predicates. This is a simplification.
+		// For example, the selectivity of {pod=~prometheus.*} doesn't depend on if we have already applied {statefulset=prometheus}.
+		// While finalSelectivity is neither an upper bound nor a lower bound, assuming independence allows us to come up with cost estimates comparable between plans.
+		finalSelectivity *= float64(pred.cardinality) / float64(p.totalSeries)
+	}
+	return uint64(finalSelectivity * float64(p.totalSeries))
+}
+
+// nonShardedCardinality returns an estimate of the total number of series before query sharding is applied.
+// This is the base cardinality considering only the selectivity of all predicates.
+func (p partialPlan) nonShardedCardinality() uint64 {
+	finalSelectivity := 1.0
+	for i := range p.predicates {
+		pred, _ := p.pretendDecidedPredicate(i)
+		// We use the selectivity across all series instead of the selectivity across label values.
+		// For example, if {protocol=~.*} matches all values, it could still reduce the result set after intersection.
+		//
+		// We also assume independence between the predicates. This is a simplification.
+		// For example, the selectivity of {pod=~prometheus.*} doesn't depend on if we have already applied {statefulset=prometheus}.
+		finalSelectivity *= float64(pred.cardinality) / float64(p.totalSeries)
+	}
+	return uint64(finalSelectivity * float64(p.totalSeries))
+}
+
+// FinalCardinality returns an estimate of the total number of series that this plan would return.
+func (p partialPlan) FinalCardinality() uint64 {
+	return shardedCardinality(p.nonShardedCardinality(), p.shard)
+}
+
+func (p partialPlan) firstUndecidedPredicateIdx() int {
+	for i, decided := range p.decidedPredicates {
+		if !decided {
+			return i
+		}
+	}
+	return len(p.decidedPredicates)
+}
