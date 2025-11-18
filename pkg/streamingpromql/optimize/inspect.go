930958e65f51bd9a58c5b30a4dd260f95b06bd42
Nick Pillitteri <nick.pillitteri@grafana.com>
2025-11-18 12:25:23 -0500

    MQE: Skip using labels for binary expression hints based on children
    Change the logic introduced in #13499 to only skip using particular
    labels for hints in binary expressions when the label is created by
    a function call that is a child of that binary expression. This allows
    us to apply hints in more cases when it is safe to do so.
    
    Related #13499
    
    Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

--- pkg/streamingpromql/optimize/inspect.go
+++ pkg/streamingpromql/optimize/inspect.go
@@ -7,7 +7,6 @@ import (
 	"github.com/prometheus/prometheus/model/labels"
 
 	"github.com/grafana/mimir/pkg/frontend/querymiddleware/astmapper"
-	"github.com/grafana/mimir/pkg/streamingpromql/operators/functions"
 	"github.com/grafana/mimir/pkg/streamingpromql/planning"
 	"github.com/grafana/mimir/pkg/streamingpromql/planning/core"
 )
@@ -18,8 +17,6 @@ type InspectResult struct {
 	// IsRewrittenByMiddleware indicates if the query has been rewritten by query sharding or by
 	// subquery spin-off middlewares.
 	IsRewrittenByMiddleware bool
-	// CreatedLabels is a set of labels that are the result of a function that creates labels.
-	CreatedLabels map[string]struct{}
 }
 
 // Inspect traverses a tree of Nodes and returns a result that indicates if the query
@@ -33,14 +30,6 @@ func Inspect(node planning.Node) InspectResult {
 
 func crawlPlanFromNode(node planning.Node, res *InspectResult) {
 	switch e := node.(type) {
-	case *core.FunctionCall:
-		if lbl := createdLabel(e); lbl != "" {
-			if res.CreatedLabels == nil {
-				res.CreatedLabels = make(map[string]struct{})
-			}
-
-			res.CreatedLabels[lbl] = struct{}{}
-		}
 	case *core.MatrixSelector:
 		res.HasSelectors = true
 		res.IsRewrittenByMiddleware = res.IsRewrittenByMiddleware || isSpunOff(e.Matchers)
@@ -52,16 +41,6 @@ func crawlPlanFromNode(node planning.Node, res *InspectResult) {
 	for c := range planning.ChildrenIter(node) {
 		crawlPlanFromNode(c, res)
 	}
-}
-
-func createdLabel(v *core.FunctionCall) string {
-	if (v.Function == functions.FUNCTION_LABEL_REPLACE || v.Function == functions.FUNCTION_LABEL_JOIN) && len(v.Args) > 1 {
-		if lbl, ok := v.Args[1].(*core.StringLiteral); ok {
-			return lbl.Value
-		}
-	}
-
-	return ""
 }
 
 func isSharded(v *core.VectorSelector) bool {
