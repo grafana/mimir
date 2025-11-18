grafana/mimir

commit 028ee04d79dee7ac3971fc31b41a85e646205db5

chore: remove use of github.com/pkg/errors in some packages (#13408)
Remove use of deprecated and unmaintained errors package in favor of the
stdlib errors package. Enforce that it is not reintroduced with a lint.

This change only migrates `pkg/util`, `cmd`, and `integration` since our
use of it is quite widespread. Other packages will be migrated in future
PRs.

pkg/util/limiter/utilization.go
@@ -12,7 +12,6 @@ import (
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/grafana/dskit/services"
-	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/procfs"
@@ -43,7 +42,7 @@ type combinedScanner struct {
 func (s combinedScanner) Scan() (float64, uint64, error) {
 	ps, err := s.proc.Stat()
 	if err != nil {
-		return 0, 0, errors.Wrap(err, "failed to get process stats")
+		return 0, 0, fmt.Errorf("failed to get process stats: %w", err)
 	}
 
 	metrics.Read(s.memoryTotalSample)
@@ -133,7 +132,7 @@ func (l *UtilizationBasedLimiter) LimitingReason() string {
 func (l *UtilizationBasedLimiter) starting(_ context.Context) error {
 	var err error
 	l.utilizationScanner, err = newCombinedScanner()
-	return errors.Wrap(err, "unable to detect CPU/memory utilization, unsupported platform. Please disable utilization based limiting")
+	return fmt.Errorf("unable to detect CPU/memory utilization, unsupported platform. Please disable utilization based limiting: %w", err)
 }
 
 func (l *UtilizationBasedLimiter) update(_ context.Context) error {

Makefile
@@ -329,6 +329,11 @@ lint: check-makefiles check-merge-conflicts
 	# Ensure all errors are reported as APIError
 	faillint -paths "github.com/weaveworks/common/httpgrpc.{Errorf}=github.com/grafana/mimir/pkg/api/error.Newf" ./pkg/frontend/querymiddleware/...
 
+	# Gradually removing use of the github.com/pkg/errors package entirely. It's unmaintained and all functionality
+	# now exists in the standard library errors package. Prevent it from being used in packages that have already
+	# completely migrated.
+	faillint -paths "github.com/pkg/errors=errors" ./pkg/util/... ./cmd/... ./integration/...
+
 	# errors.Cause() only work on errors wrapped by github.com/pkg/errors, while it doesn't work
 	# on errors wrapped by golang standard errors package. In Mimir we currently use github.com/pkg/errors
 	# but other vendors we depend on (e.g. Prometheus) just uses the standard errors package.

cmd/metaconvert/main.go
@@ -21,7 +21,6 @@ import (
 	"github.com/go-kit/log/level"
 	"github.com/grafana/dskit/flagext"
 	dslog "github.com/grafana/dskit/log"
-	"github.com/pkg/errors"
 	"github.com/thanos-io/objstore"
 
 	"github.com/grafana/mimir/pkg/storage/bucket"
@@ -142,7 +141,7 @@ func convertTenantBlocks(ctx context.Context, userBucketClient objstore.Bucket, 
 		level.Info(logger).Log("msg", "changes required, uploading meta.json file", "block", blockID.String())
 
 		if err := uploadMetadata(ctx, userBucketClient, meta, path.Join(blockID.String(), block.MetaFilename)); err != nil {
-			return errors.Wrapf(err, "failed to upload meta.json for block %s", blockID.String())
+			return fmt.Errorf("failed to upload meta.json for block %s: %w", blockID.String(), err)
 		}
 
 		level.Info(logger).Log("msg", "meta.json file uploaded successfully", "block", blockID.String())
@@ -153,7 +152,7 @@ func convertTenantBlocks(ctx context.Context, userBucketClient objstore.Bucket, 
 func uploadMetadata(ctx context.Context, bkt objstore.Bucket, meta block.Meta, path string) error {
 	var body bytes.Buffer
 	if err := meta.Write(&body); err != nil {
-		return errors.Wrap(err, "encode meta.json")
+		return fmt.Errorf("failed to encode meta.json: %w", err)
 	}
 
 	return bkt.Upload(ctx, path, bytes.NewReader(body.Bytes()))

cmd/mimir/main.go
@@ -21,7 +21,6 @@ import (
 	"github.com/grafana/dskit/ballast"
 	"github.com/grafana/dskit/flagext"
 	"github.com/grafana/dskit/tracing"
-	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"gopkg.in/yaml.v3"
 
@@ -282,7 +281,7 @@ func parseConfigFileParameter(args []string) (configFile string, expandEnv bool)
 func LoadConfig(filename string, expandEnv bool, cfg *mimir.Config) error {
 	buf, err := os.ReadFile(filename)
 	if err != nil {
-		return errors.Wrap(err, "Error reading config file")
+		return fmt.Errorf("error reading config file: %w", err)
 	}
 
 	// create a sha256 hash of the config before expansion and expose it via
@@ -300,7 +299,7 @@ func LoadConfig(filename string, expandEnv bool, cfg *mimir.Config) error {
 
 	// Unmarshal with common config unmarshaler.
 	if err := dec.Decode((*mimir.ConfigWithCommon)(cfg)); err != nil {
-		return errors.Wrap(err, "Error parsing config file")
+		return fmt.Errorf("error parsing config file: %w", err)
 	}
 
 	return nil

pkg/util/activitytracker/activity_tracker.go
@@ -4,7 +4,9 @@ package activitytracker
 
 import (
 	"encoding/binary"
+	"errors"
 	"flag"
+	"fmt"
 	"io"
 	"os"
 	"strings"
@@ -13,7 +15,6 @@ import (
 
 	"github.com/edsrzf/mmap-go"
 	"github.com/grafana/dskit/multierror"
-	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
@@ -174,7 +175,7 @@ func trimEntryToSize(entry string, size int) string {
 func getMappedFile(filename string, filesize int) (*os.File, mmap.MMap, error) {
 	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0600)
 	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed to create activity file")
+		return nil, nil, fmt.Errorf("failed to create activity file: %w", err)
 	}
 
 	closeOnReturn := true
@@ -186,12 +187,12 @@ func getMappedFile(filename string, filesize int) (*os.File, mmap.MMap, error) {
 
 	err = file.Truncate(int64(filesize))
 	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed to truncate activity file")
+		return nil, nil, fmt.Errorf("failed to truncate activity file: %w", err)
 	}
 
 	fileAsBytes, err := mmap.Map(file, mmap.RDWR, 0)
 	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed to mmap activity file")
+		return nil, nil, fmt.Errorf("failed to mmap activity file: %w", err)
 	}
 
 	closeOnReturn = false

pkg/util/globalerror/grpc_test.go
@@ -4,6 +4,8 @@ package globalerror
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"io"
 	"net"
 	"testing"
@@ -16,7 +18,6 @@ import (
 	"github.com/grafana/dskit/httpgrpc"
 	"github.com/grafana/dskit/middleware"
 	dskitserver "github.com/grafana/dskit/server"
-	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
@@ -52,14 +53,14 @@ func TestWrapContextError(t *testing.T) {
 			"wrapped gogo Canceled error": {
 				ctxCanceled:         true,
 				ctxDeadlineExceeded: false,
-				origErr:             errors.Wrap(status.Error(codes.Canceled, context.Canceled.Error()), "custom message"),
+				origErr:             fmt.Errorf("custom message: %w", status.Error(codes.Canceled, context.Canceled.Error())),
 				expectedGrpcCode:    codes.Canceled,
 				expectedContextErr:  context.Canceled,
 			},
 			"wrapped gRPC Canceled error": {
 				ctxCanceled:         true,
 				ctxDeadlineExceeded: false,
-				origErr:             errors.Wrap(grpcstatus.Error(codes.Canceled, context.Canceled.Error()), "custom message"),
+				origErr:             fmt.Errorf("custom message: %w", grpcstatus.Error(codes.Canceled, context.Canceled.Error())),
 				expectedGrpcCode:    codes.Canceled,
 				expectedContextErr:  context.Canceled,
 			},
@@ -80,14 +81,14 @@ func TestWrapContextError(t *testing.T) {
 			"wrapped gogo DeadlineExceeded error": {
 				ctxCanceled:         false,
 				ctxDeadlineExceeded: true,
-				origErr:             errors.Wrap(status.Error(codes.DeadlineExceeded, context.DeadlineExceeded.Error()), "custom message"),
+				origErr:             fmt.Errorf("custom message: %w", status.Error(codes.DeadlineExceeded, context.DeadlineExceeded.Error())),
 				expectedGrpcCode:    codes.DeadlineExceeded,
 				expectedContextErr:  context.DeadlineExceeded,
 			},
 			"wrapped gRPC DeadlineExceeded error": {
 				ctxCanceled:         false,
 				ctxDeadlineExceeded: true,
-				origErr:             errors.Wrap(grpcstatus.Error(codes.DeadlineExceeded, context.DeadlineExceeded.Error()), "custom message"),
+				origErr:             fmt.Errorf("custom message: %w", grpcstatus.Error(codes.DeadlineExceeded, context.DeadlineExceeded.Error())),
 				expectedGrpcCode:    codes.DeadlineExceeded,
 				expectedContextErr:  context.DeadlineExceeded,
 			},

pkg/util/http.go
@@ -25,7 +25,6 @@ import (
 	"github.com/grafana/dskit/flagext"
 	"github.com/klauspost/compress/zstd"
 	"github.com/pierrec/lz4/v4"
-	"github.com/pkg/errors"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc/codes"
@@ -235,7 +234,7 @@ func decompressRequest(buffers *RequestBuffers, reader io.Reader, expectedSize, 
 	case Gzip:
 		gzReader, err := gzip.NewReader(reader)
 		if err != nil {
-			return nil, errors.Wrap(err, "create gzip reader")
+			return nil, fmt.Errorf("create gzip reader: %w", err)
 		}
 
 		defer func() {
@@ -269,15 +268,15 @@ func decompressRequest(buffers *RequestBuffers, reader io.Reader, expectedSize, 
 
 	if _, err := buf.ReadFrom(reader); err != nil {
 		if compression == Gzip {
-			return nil, errors.Wrap(err, "decompress gzip")
+			return nil, fmt.Errorf("decompress gzip: %w", err)
 		}
 		if compression == Lz4 {
-			return nil, errors.Wrap(err, "decompress lz4")
+			return nil, fmt.Errorf("decompress lz4: %w", err)
 		}
 		if compression == Zstd {
-			return nil, errors.Wrap(err, "decompress zstd")
+			return nil, fmt.Errorf("decompress zstd: %w", err)
 		}
-		return nil, errors.Wrap(err, "read body")
+		return nil, fmt.Errorf("read body: %w", err)
 	}
 	sp.AddEvent("util.ParseProtoReader[finished_reading]")
 
@@ -296,7 +295,7 @@ func decompressSnappyFromBuffer(buffers *RequestBuffers, buffer *bytes.Buffer, m
 
 	size, err := snappy.DecodedLen(buffer.Bytes())
 	if err != nil {
-		return nil, errors.Wrap(err, "getting snappy decoded length")
+		return nil, fmt.Errorf("getting snappy decoded length: %w", err)
 	}
 	if size > maxSize {
 		return nil, MsgSizeTooLargeErr{Actual: size, Limit: maxSize}
@@ -308,7 +307,7 @@ func decompressSnappyFromBuffer(buffers *RequestBuffers, buffer *bytes.Buffer, m
 
 	decoded, err := snappy.Decode(decBufBytes, buffer.Bytes())
 	if err != nil {
-		return nil, errors.Wrap(err, "decompress snappy")
+		return nil, fmt.Errorf("decompress snappy: %w", err)
 	}
 
 	return decoded, nil
@@ -340,7 +339,7 @@ func SerializeProtoResponse(w http.ResponseWriter, resp proto.Message, compressi
 	case RawSnappy:
 		data, err = snappyEncoding(nil, data)
 		if err != nil {
-			err = errors.Wrap(err, "snappy encoding")
+			err = fmt.Errorf("snappy encoding: %w", err)
 			break
 		}
 	case Gzip:
@@ -348,11 +347,11 @@ func SerializeProtoResponse(w http.ResponseWriter, resp proto.Message, compressi
 		buf.Grow(len(data))
 		wr := gzip.NewWriter(&buf)
 		if _, err = wr.Write(data); err != nil {
-			err = errors.Wrap(err, "write gzip")
+			err = fmt.Errorf("write gzip: %w", err)
 			break
 		}
 		if err = wr.Close(); err != nil {
-			err = errors.Wrap(err, "close gzip writer")
+			err = fmt.Errorf("close gzip writer: %w", err)
 			break
 		}
 		data = buf.Bytes()

pkg/util/net/firewall_dialer.go
@@ -7,11 +7,11 @@ package net
 
 import (
 	"context"
+	"errors"
 	"net"
 	"syscall"
 
 	"github.com/grafana/dskit/flagext"
-	"github.com/pkg/errors"
 )
 
 var errBlockedAddress = errors.New("blocked address")

commit 5b7a8343fbe09b622332851bc96f17a492f523ba

Fix issue where utilization-based limiter always fails to start (#13541)
#### What this PR does

This PR fixes an issue introduced in
https://github.com/grafana/mimir/pull/13408 where the utilization-based
limiter always fails to start with `unable to detect CPU/memory
utilization, unsupported platform. Please disable utilization based
limiting: %!w(<nil>)`

I've chosen not to add a changelog entry given #13408 does not have a
changelog entry and this change has not been part of a numbered release
yet.

#### Which issue(s) this PR fixes or relates to

#13408

#### Checklist

- [n/a] Tests updated.
- [n/a] Documentation added.
- [n/a] `CHANGELOG.md` updated - the order of entries should be
`[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`. If changelog entry
is not needed, please add the `changelog-not-needed` label to the PR.
- [n/a]
[`about-versioning.md`](https://github.com/grafana/mimir/blob/main/docs/sources/mimir/configure/about-versioning.md)
updated with experimental features.

pkg/util/limiter/utilization.go
@@ -132,7 +132,11 @@ func (l *UtilizationBasedLimiter) LimitingReason() string {
 func (l *UtilizationBasedLimiter) starting(_ context.Context) error {
 	var err error
 	l.utilizationScanner, err = newCombinedScanner()
-	return fmt.Errorf("unable to detect CPU/memory utilization, unsupported platform. Please disable utilization based limiting: %w", err)
+	if err != nil {
+		return fmt.Errorf("unable to detect CPU/memory utilization, unsupported platform. Please disable utilization based limiting: %w", err)
+	}
+
+	return nil
 }
 
 func (l *UtilizationBasedLimiter) update(_ context.Context) error {