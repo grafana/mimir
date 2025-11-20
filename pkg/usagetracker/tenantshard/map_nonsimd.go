5656ca3158605027d3fa4b4791068567393e5681
David Grant <seizethedave@gmail.com>
2025-11-20 14:40:54 -0800

    Docs and tests.

--- pkg/usagetracker/tenantshard/map_nonsimd.go
+++ pkg/usagetracker/tenantshard/map_nonsimd.go
@@ -15,28 +15,39 @@ const (
 	// maxAvgGroupLoad was 7 in dolthub/swiss, but we trade in some memory for less CPU by having to check less entries.
 	maxAvgGroupLoad = 4
 
-	loBits uint64 = 0b0000000100000001000000010000000100000001000000010000000100000001
-	hiBits uint64 = 0b1000000010000000100000001000000010000000100000001000000010000000
+	loBits uint64 = 0x0101010101010101
+	hiBits uint64 = 0x8080808080808080
 )
 
 type bitset uint64
 
+// metaMatchH2 searches the given index for whole bytes that match the given prefix.
 func metaMatchH2(m *index, p prefix) bitset {
-	// https://graphics.stanford.edu/~seander/bithacks.html##ValueInWord
-	return hasZeroByte(castUint64(m) ^ (loBits * uint64(p)))
+	// See: https://graphics.stanford.edu/~seander/bithacks.html##ValueInWord
+	// e.g.,
+	// if m = 0x123456c9c9777777 and p = 0xc9, then:
+	// (loBits * p) == 0xc9c9c9c9c9c9c9c9
+	// and B = m ^ (loBits * p) == 0x1234560000777777
+	// then findZeroBytes(B) ==    0x0000008080000000
+	return findZeroBytes(castUint64(m) ^ (loBits * uint64(p)))
 }
 
 func metaMatchEmpty(m *index) bitset {
-	return hasZeroByte(castUint64(m))
+	return findZeroBytes(castUint64(m))
 }
 
+// nextMatch clears and returns the index corresponding to the next set bit in
+// the given bitset.
 func nextMatch(b *bitset) uint32 {
 	s := uint32(bits.TrailingZeros64(uint64(*b)))
-	*b &= ^(1 << s) // clear bit |s|
-	return s >> 3   // div by 8
+	*b &= ^(1 << s) // clear bit s+1 from the right.
+	return s >> 3   // div by 8 to obtain index [0, 8).
 }
 
-func hasZeroByte(x uint64) bitset {
+// findZeroBytes locates all zero bytes in the given word. Their presence is
+// indicated by the returned bitset: if the i'th bit is set, then the byte
+// starting at the i'th bit is zero.
+func findZeroBytes(x uint64) bitset {
 	return bitset(((x - loBits) & ^(x)) & hiBits)
 }
 
--- pkg/usagetracker/tenantshard/map_nonsimd_test.go
+++ pkg/usagetracker/tenantshard/map_nonsimd_test.go
@@ -6,12 +6,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestHasZeroByte(t *testing.T) {
-	require.Equal(t, bitset(0x0000000000000000), hasZeroByte(castUint64(&index{7, 8, 9, 10, 11, 12, 13, 14})))
-	require.Equal(t, bitset(0x8080808080808080), hasZeroByte(castUint64(&index{0, 0, 0, 0, 0, 0, 0, 0})))
-	require.Equal(t, bitset(0x8000000000000080), hasZeroByte(castUint64(&index{0, 11, 12, 13, 14, 15, 16, 0})))
+func TestMetaMatchH2(t *testing.T) {
+	m := &index{0x12, 0x34, 0x56, 0xc9, 0xc9, 0x77, 0x77, 0x77}
+	p := prefix(0xc9)
+	require.Equal(t, bitset(0x0000008080000000), metaMatchH2(m, p))
+}
+
+func TestFindZeroBytes(t *testing.T) {
+	require.Equal(t, bitset(0x0000000000000000), findZeroBytes(castUint64(&index{7, 8, 9, 10, 11, 12, 13, 14})))
+	require.Equal(t, bitset(0x8080808080808080), findZeroBytes(castUint64(&index{0, 0, 0, 0, 0, 0, 0, 0})))
+	require.Equal(t, bitset(0x8000000000000080), findZeroBytes(castUint64(&index{0, 11, 12, 13, 14, 15, 16, 0})))
 	// Tombstones are a special case that also count as zero bytes.
-	require.Equal(t, bitset(0x0000000000000000), hasZeroByte(castUint64(&index{tombstone, tombstone, tombstone, tombstone, tombstone, tombstone, tombstone, tombstone})))
+	require.Equal(t, bitset(0x0000000000000000), findZeroBytes(castUint64(&index{tombstone, tombstone, tombstone, tombstone, tombstone, tombstone, tombstone, tombstone})))
 }
 
 func TestNextMatch(t *testing.T) {
@@ -24,4 +30,12 @@ func TestNextMatch(t *testing.T) {
 	require.Equal(t, bitset(0x8080808080000000), b)
 	require.Equal(t, uint32(3), nextMatch(&b))
 	require.Equal(t, bitset(0x8080808000000000), b)
+	require.Equal(t, uint32(4), nextMatch(&b))
+	require.Equal(t, bitset(0x8080800000000000), b)
+	require.Equal(t, uint32(5), nextMatch(&b))
+	require.Equal(t, bitset(0x8080000000000000), b)
+	require.Equal(t, uint32(6), nextMatch(&b))
+	require.Equal(t, bitset(0x8000000000000000), b)
+	require.Equal(t, uint32(7), nextMatch(&b))
+	require.Equal(t, bitset(0x0000000000000000), b)
 }
