220f5dd19cdb296ebca42a82ba3257d4178334ef
David Grant <seizethedave@gmail.com>
2025-11-20 11:30:10 -0800

    TestNextMatch

--- pkg/usagetracker/tenantshard/map_nonsimd_test.go
+++ pkg/usagetracker/tenantshard/map_nonsimd_test.go
@@ -3,17 +3,8 @@ package tenantshard
 import (
 	"testing"
 
-	"github.com/grafana/mimir/pkg/usagetracker/clock"
 	"github.com/stretchr/testify/require"
 )
-
-func TestMapNonsimd(t *testing.T) {
-	m := New(100)
-	for i := 0; i < 100; i++ {
-		m.Put(uint64(i), clock.Minutes(i), nil, nil, false)
-	}
-	require.Equal(t, 100, m.Count())
-}
 
 func TestHasZeroByte(t *testing.T) {
 	require.Equal(t, bitset(0x0000000000000000), hasZeroByte(castUint64(&index{7, 8, 9, 10, 11, 12, 13, 14})))
@@ -21,4 +12,16 @@ func TestHasZeroByte(t *testing.T) {
 	require.Equal(t, bitset(0x8000000000000080), hasZeroByte(castUint64(&index{0, 11, 12, 13, 14, 15, 16, 0})))
 	// Tombstones are a special case that also count as zero bytes.
 	require.Equal(t, bitset(0x0000000000000000), hasZeroByte(castUint64(&index{tombstone, tombstone, tombstone, tombstone, tombstone, tombstone, tombstone, tombstone})))
+}
+
+func TestNextMatch(t *testing.T) {
+	b := bitset(0x8080808080808080)
+	require.Equal(t, uint32(0), nextMatch(&b))
+	require.Equal(t, bitset(0x8080808080808000), b)
+	require.Equal(t, uint32(1), nextMatch(&b))
+	require.Equal(t, bitset(0x8080808080800000), b)
+	require.Equal(t, uint32(2), nextMatch(&b))
+	require.Equal(t, bitset(0x8080808080000000), b)
+	require.Equal(t, uint32(3), nextMatch(&b))
+	require.Equal(t, bitset(0x8080808000000000), b)
 }
