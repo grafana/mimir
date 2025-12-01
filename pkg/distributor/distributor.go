f299ee0ccca37a91c8541f3687be419b125aced1
juliette.orain <juliette.orain@ovhcloud.com>
2025-12-01 15:06:57 +0100

    Clone string in haReplica to avoid gRPC buffer issues
    Signed-off-by: juliette.orain <juliette.orain@ovhcloud.com>

--- pkg/distributor/distributor.go
+++ pkg/distributor/distributor.go
@@ -1239,8 +1239,8 @@ func (d *Distributor) prePushHaDedupeMiddleware(next PushFunc) PushFunc {
 		getReplicaForSample := func(i int) haReplica {
 			cluster, replica := findHALabels(haReplicaLabel, haClusterLabel, req.Timeseries[i].Labels)
 			return haReplica{
-				cluster: cluster,
-				replica: replica,
+				cluster: strings.Clone(cluster),
+				replica: strings.Clone(replica),
 			}
 		}
 
@@ -1301,9 +1301,11 @@ func (d *Distributor) prePushHaDedupeMiddleware(next PushFunc) PushFunc {
 		}
 		for replicaKey, info := range replicaInfos {
 			if info.state == replicaRejectedUnknown {
-				state, err := d.replicaObserved(ctx, userID, replicaKey, sampleTimestamp)
+				state, replicaErr := d.replicaObserved(ctx, userID, replicaKey, sampleTimestamp)
 				info.state = state
-				errs.Add(err)
+				if replicaErr != nil {
+					errs.Add(replicaErr)
+				}
 			}
 			samplesPerState[info.state] += info.sampleCount
 		}
@@ -1342,8 +1344,9 @@ func (d *Distributor) prePushHaDedupeMiddleware(next PushFunc) PushFunc {
 		pushReq.AddCleanup(sliceUnacceptedRequests(req, lastAccepted))
 
 		if len(req.Timeseries) > 0 {
-			err = next(ctx, pushReq)
-			errs.Add(err)
+			if pushErr := next(ctx, pushReq); pushErr != nil {
+				errs.Add(pushErr)
+			}
 		}
 
 		return errs.Err()
