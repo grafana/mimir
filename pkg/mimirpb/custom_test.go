25fef1814a320f021dd8a92adbb092c769c2c58a
Toni Cárdenas <toni.cardenasvargas@grafana.com>
2025-12-02 11:33:45 +0100

    Don't wrap encoding.CodecV2.

--- pkg/mimirpb/custom_test.go
+++ pkg/mimirpb/custom_test.go
@@ -13,9 +13,6 @@ import (
 	"github.com/prometheus/prometheus/model/histogram"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc/encoding"
-	"google.golang.org/grpc/encoding/proto"
-	"google.golang.org/grpc/mem"
 
 	"github.com/grafana/mimir/pkg/util/test"
 )
@@ -197,7 +194,7 @@ func TestIsFloatHistogram(t *testing.T) {
 }
 
 func TestCodecV2_Unmarshal(t *testing.T) {
-	c := codecV2{codec: fakeCodecV2{}}
+	c := codecV2{}
 
 	var origReq WriteRequest
 	data, err := c.Marshal(&origReq)
@@ -210,14 +207,6 @@ func TestCodecV2_Unmarshal(t *testing.T) {
 
 	require.NotNil(t, req.Buffer())
 	req.FreeBuffer()
-}
-
-type fakeCodecV2 struct {
-	encoding.CodecV2
-}
-
-func (c fakeCodecV2) Marshal(v any) (mem.BufferSlice, error) {
-	return encoding.GetCodecV2(proto.Name).Marshal(v)
 }
 
 func TestHistogram_BucketsCount(t *testing.T) {
