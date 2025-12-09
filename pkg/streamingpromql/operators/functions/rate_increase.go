2edc0086c375e3992db8dc99dc2718613519f9f7
Andrew Hall <andrew.hall@grafana.com>
2025-12-09 11:11:11 +0800

    Update pkg/streamingpromql/operators/functions/rate_increase.go
    Co-authored-by: Charles Korn <charleskorn@users.noreply.github.com>

--- pkg/streamingpromql/operators/functions/rate_increase.go
+++ pkg/streamingpromql/operators/functions/rate_increase.go
@@ -209,7 +209,7 @@ func floatRate(isRate bool, fCount int, fHead []promql.FPoint, fTail []promql.FP
 			lastPoint = smoothedBasisForTailPoint
 		}
 
-		// We are essentially replacing the last point in the slices with the smoothed tail point
+		// We are essentially replacing the last point in the slices with the basis for the smoothed tail point
 		// We could achieve the same thing by setting the last value.F in the slice to the smoothedBasisForTailPoint.F,
 		// and not pruning the slice. This would then avoid the need for the extra delta addition after the accumulations.
 		// However, we probably do not want to edit values in these slices.
