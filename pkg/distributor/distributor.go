83b82acb7b50f8382cae863ad687be93410c3b3c
Jon Kartago Lamida <me@lamida.net>
2025-12-15 17:33:12 +0800

    Put labels memory tracking inside AddSeries

--- pkg/distributor/distributor.go
+++ pkg/distributor/distributor.go
@@ -3268,7 +3268,7 @@ respsLoop:
 
 	result := make([]labels.Labels, 0, len(metrics))
 	for _, m := range metrics {
-		uniqueSeriesLabels, err := queryLimiter.AddSeries(m)
+		uniqueSeriesLabels, err := queryLimiter.AddSeries(m, mimir_limiter.NoopMemoryTracker{})
 		if err != nil {
 			return nil, err
 		}
