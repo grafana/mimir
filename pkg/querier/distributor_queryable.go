5499a664fe31936c7ed61f877fe6c139aa72308e
Jon Kartago Lamida <me@lamida.net>
2025-12-22 13:36:31 +0800

    Get memoryTracker from context one time and reuse the usage

--- pkg/querier/distributor_queryable.go
+++ pkg/querier/distributor_queryable.go
@@ -115,22 +115,22 @@ func (q *distributorQuerier) Select(ctx context.Context, _ bool, sp *storage.Sel
 	now := time.Now().UnixMilli()
 	minT = clampMinTime(spanLog, minT, now, -queryIngestersWithin, "query ingesters within")
 
+	memoryTracker, err := limiter.MemoryConsumptionTrackerFromContext(ctx)
+	if err != nil {
+		return storage.ErrSeriesSet(err)
+	}
 	if sp != nil && sp.Func == "series" {
 		ms, err := q.distributor.MetricsForLabelMatchers(ctx, model.Time(minT), model.Time(maxT), sp, matchers...)
-		if err != nil {
-			return storage.ErrSeriesSet(err)
-		}
-		memoryTracker, err := limiter.MemoryConsumptionTrackerFromContext(ctx)
 		if err != nil {
 			return storage.ErrSeriesSet(err)
 		}
 		return series.NewMemoryTrackingSeriesSet(series.LabelsToSeriesSet(ms), memoryTracker)
 	}
 
-	return q.streamingSelect(ctx, minT, maxT, matchers)
+	return q.streamingSelect(ctx, minT, maxT, matchers, memoryTracker)
 }
 
-func (q *distributorQuerier) streamingSelect(ctx context.Context, minT, maxT int64, matchers []*labels.Matcher) storage.SeriesSet {
+func (q *distributorQuerier) streamingSelect(ctx context.Context, minT, maxT int64, matchers []*labels.Matcher, memoryTracker *limiter.MemoryConsumptionTracker) storage.SeriesSet {
 	results, err := q.distributor.QueryStream(ctx, q.queryMetrics, model.Time(minT), model.Time(maxT), matchers...)
 	if err != nil {
 		return storage.ErrSeriesSet(err)
@@ -175,10 +175,6 @@ func (q *distributorQuerier) streamingSelect(ctx context.Context, minT, maxT int
 	// Store the stream readers so we can free their buffers when we're done using this Querier.
 	q.streamReaders = append(q.streamReaders, results.StreamReaders...)
 
-	memoryTracker, err := limiter.MemoryConsumptionTrackerFromContext(ctx)
-	if err != nil {
-		return storage.ErrSeriesSet(err)
-	}
 	return series.NewMemoryTrackingSeriesSet(series.NewConcreteSeriesSetFromSortedSeries(streamingSeries), memoryTracker)
 }
 
