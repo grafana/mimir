faa26ebf0dec28fe74f7ca2dd9fd0d64d0d11f23
Toni Cárdenas <toni.cardenasvargas@grafana.com>
2025-12-23 16:32:01 +0100

    Add missing FreeBuffer in test.

--- pkg/storage/ingest/version_test.go
+++ pkg/storage/ingest/version_test.go
@@ -126,6 +126,7 @@ func TestDeserializeRecordContent(t *testing.T) {
 
 		require.NoError(t, err)
 		wr.ClearTimeseriesUnmarshalData()
+		defer wr.FreeBuffer()
 		wr.BufferHolder = mimirpb.BufferHolder{} // We don't want to compare this.
 		require.Equal(t, reqv1, &wr)
 	})
