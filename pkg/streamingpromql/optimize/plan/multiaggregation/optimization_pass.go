01740a24a4d7096c7f06d939e0511f998a9bef64
Charles Korn <charles.korn@grafana.com>
2026-01-19 15:59:08 +1100

    Expand tests

--- pkg/streamingpromql/optimize/plan/multiaggregation/optimization_pass.go
+++ pkg/streamingpromql/optimize/plan/multiaggregation/optimization_pass.go
@@ -4,8 +4,10 @@ package multiaggregation
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/grafana/mimir/pkg/streamingpromql/planning"
+	"github.com/grafana/mimir/pkg/streamingpromql/planning/core"
 )
 
 type OptimizationPass struct{}
@@ -20,4 +22,41 @@ func (o *OptimizationPass) Name() string {
 
 func (o *OptimizationPass) Apply(ctx context.Context, plan *planning.QueryPlan, maximumSupportedQueryPlanVersion planning.QueryPlanVersion) (*planning.QueryPlan, error) {
 	return plan, nil
+}
+
+func IsSupportedAggregationOperation(o core.AggregationOperation) (bool, error) {
+	switch o {
+	case core.AGGREGATION_SUM:
+		return true, nil
+	case core.AGGREGATION_COUNT:
+		return true, nil
+	case core.AGGREGATION_MIN:
+		return true, nil
+	case core.AGGREGATION_MAX:
+		return true, nil
+	case core.AGGREGATION_AVG:
+		return true, nil
+	case core.AGGREGATION_GROUP:
+		return true, nil
+	case core.AGGREGATION_STDVAR:
+		return true, nil
+	case core.AGGREGATION_STDDEV:
+		return true, nil
+
+	case core.AGGREGATION_QUANTILE:
+		return false, nil
+	case core.AGGREGATION_COUNT_VALUES:
+		return false, nil
+	case core.AGGREGATION_TOPK:
+		return false, nil
+	case core.AGGREGATION_BOTTOMK:
+		return false, nil
+	case core.AGGREGATION_LIMITK:
+		return false, nil
+	case core.AGGREGATION_LIMIT_RATIO:
+		return false, nil
+
+	default:
+		return false, fmt.Errorf("multiaggregation.IsSupportedAggregationOperation: unknown aggregation operation: %s", o.String())
+	}
 }
