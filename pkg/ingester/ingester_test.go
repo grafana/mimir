57ab0a983467578d06ff5b0e2638d8d42e85fce0
Toni Cárdenas <toni@tcardenas.me>
2026-01-23 09:51:53 +0100

    Update pkg/ingester/ingester_test.go
    Co-authored-by: Arve Knudsen <arve.knudsen@gmail.com>

--- pkg/ingester/ingester_test.go
+++ pkg/ingester/ingester_test.go
@@ -11492,7 +11492,6 @@ func TestIngester_PushWithSampledErrors(t *testing.T) {
 				cortex_discarded_samples_total{group="",reason="labels-not-sorted",user="user-2"} 1
 			`,
 		},
-
 		"should soft fail on series with duplicate labels with native histogram": {
 			reqs: []*mimirpb.WriteRequest{
 				{
