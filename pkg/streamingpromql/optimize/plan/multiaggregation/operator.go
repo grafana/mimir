grafana/mimir

commit 05ef3852472fe062216ee75914001bfca461e4ee

MQE: add support for evaluating multiple aggregations over the same input without buffering (#14123)
#### What this PR does

This PR adds a new optimisation pass to MQE ("multi-aggregation") that
evaluates multiple aggregations over the same input without buffering.

For example, let's say we're evaluating the expression `max(foo) -
min(foo)`. Previously, `foo` would have been identified as a common
subexpression and so the selector would have only been evaluated once,
with the data buffered as it was read by `max` so that it could later be
read by `min`. If `foo` selects a large amount of data, then this causes
a spike in memory usage, and can potentially lead to queries failing due
to hitting the per-query memory consumption limit.

With the optimisation pass from this PR in place, this buffering is
eliminated: as data is read by one aggregation, it is also passed to the
other aggregations over the same data, so that the accumulated state for
the appropriate aggregation group can be updated. This means that in the
worst case, we'll hold accumulated state for each aggregation group in
memory, which generally consumes far less memory than buffering the
input data. (In the example above, we have just one aggregation group
for each of `min` and `max`, regardless of the number of input series.)

This is particularly helpful for sharded `avg()` aggregations in
conjunction with https://github.com/grafana/mimir/pull/14001. `avg(foo)`
is rewritten as `sum(foo) / count(foo)` to allow the `sum` and `count`
to be sharded, and then remote execution grouping logic from #14001
would group the corresponding `sum` and `count` shards into the same
request. With the optimisation from this PR, this grouping of shards
into the same request won't result in buffering data in queriers.

Benchmark results show a significant reduction in peak memory
consumption for benchmarks that select a large amount of data:

```
goos: darwin
goarch: arm64

pkg/streamingpromql/optimize/plan/multiaggregation/operator.go
@@ -1,0 +1,213 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package multiaggregation
+
+import (
+	"context"
+
+	"github.com/prometheus/prometheus/promql/parser"
+	"github.com/prometheus/prometheus/promql/parser/posrange"
+	"github.com/prometheus/prometheus/util/annotations"
+
+	"github.com/grafana/mimir/pkg/streamingpromql/operators/aggregations"
+	"github.com/grafana/mimir/pkg/streamingpromql/types"
+	"github.com/grafana/mimir/pkg/util/limiter"
+)
+
+type MultiAggregatorGroupEvaluator struct {
+	inner                    types.InstantVectorOperator
+	memoryConsumptionTracker *limiter.MemoryConsumptionTracker
+
+	instances []*MultiAggregatorInstanceOperator
+
+	prepareCalled      bool
+	afterPrepareCalled bool
+}
+
+func NewMultiAggregatorGroupEvaluator(
+	inner types.InstantVectorOperator,
+	memoryConsumptionTracker *limiter.MemoryConsumptionTracker,
+) *MultiAggregatorGroupEvaluator {
+	return &MultiAggregatorGroupEvaluator{
+		inner:                    inner,
+		memoryConsumptionTracker: memoryConsumptionTracker,
+	}
+}
+
+func (m *MultiAggregatorGroupEvaluator) AddInstance() *MultiAggregatorInstanceOperator {
+	instance := &MultiAggregatorInstanceOperator{group: m}
+	m.instances = append(m.instances, instance)
+	return instance
+}
+
+func (m *MultiAggregatorGroupEvaluator) Prepare(ctx context.Context, params *types.PrepareParams) error {
+	if m.prepareCalled {
+		return nil
+	}
+
+	m.prepareCalled = true
+	return m.inner.Prepare(ctx, params)
+}
+
+func (m *MultiAggregatorGroupEvaluator) AfterPrepare(ctx context.Context) error {
+	if m.afterPrepareCalled {
+		return nil
+	}
+
+	m.afterPrepareCalled = true
+	return m.inner.AfterPrepare(ctx)
+}
+
+func (m *MultiAggregatorGroupEvaluator) ComputeOutputSeriesForAllInstances(ctx context.Context) error {
+	// We can't pass any matchers to the inner operator because different consumers may provide different matchers.
+	innerSeries, err := m.inner.SeriesMetadata(ctx, types.Matchers{})
+	if err != nil {
+		return err
+	}
+
+	defer types.SeriesMetadataSlicePool.Put(&innerSeries, m.memoryConsumptionTracker)
+
+	for _, instance := range m.instances {
+		groups, err := instance.aggregator.ComputeGroups(innerSeries)
+		if err != nil {
+			return err
+		}
+
+		instance.outputSeriesMetadata = groups
+	}
+
+	return nil
+}
+
+func (m *MultiAggregatorGroupEvaluator) ReadNextSeries(ctx context.Context) error {
+	data, err := m.inner.NextSeries(ctx)
+	if err != nil {
+		return err
+	}
+
+	lastIndex := len(m.instances) - 1
+	for idx, instance := range m.instances {
+		isLastInstance := idx == lastIndex
+		if err := instance.aggregator.AccumulateNextInnerSeries(data, isLastInstance); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m *MultiAggregatorGroupEvaluator) Finalize(ctx context.Context) error {
+	// Only finalize the inner operator if all instances have been finalized.
+	for _, instance := range m.instances {
+		if !instance.finalized {
+			return nil
+		}
+	}
+
+	return m.inner.Finalize(ctx)
+}
+
+func (m *MultiAggregatorGroupEvaluator) Close() {
+	// Only close the inner operator if all instances have been closed.
+	for _, instance := range m.instances {
+		if !instance.closed {
+			return
+		}
+	}
+
+	m.inner.Close()
+}
+
+type MultiAggregatorInstanceOperator struct {
+	group              *MultiAggregatorGroupEvaluator
+	expressionPosition posrange.PositionRange
+	aggregator         *aggregations.Aggregator
+
+	outputSeriesMetadata []types.SeriesMetadata
+
+	finalized bool
+	closed    bool
+}
+
+var _ types.InstantVectorOperator = (*MultiAggregatorInstanceOperator)(nil)
+
+func (m *MultiAggregatorInstanceOperator) Configure(
+	op parser.ItemType,
+	grouping []string,
+	without bool,
+	memoryConsumptionTracker *limiter.MemoryConsumptionTracker,
+	annotations *annotations.Annotations,
+	timeRange types.QueryTimeRange,
+	expressionPosition posrange.PositionRange,
+) error {
+	var err error
+	m.aggregator, err = aggregations.NewAggregator(op, grouping, without, memoryConsumptionTracker, annotations, timeRange, m.group.inner.ExpressionPosition())
+	if err != nil {
+		return err
+	}
+
+	m.expressionPosition = expressionPosition
+
+	return nil
+}
+
+func (m *MultiAggregatorInstanceOperator) Prepare(ctx context.Context, params *types.PrepareParams) error {
+	return m.group.Prepare(ctx, params)
+}
+
+func (m *MultiAggregatorInstanceOperator) AfterPrepare(ctx context.Context) error {
+	return m.group.AfterPrepare(ctx)
+}
+
+func (m *MultiAggregatorInstanceOperator) SeriesMetadata(ctx context.Context, matchers types.Matchers) ([]types.SeriesMetadata, error) {
+	if m.outputSeriesMetadata == nil {
+		if err := m.group.ComputeOutputSeriesForAllInstances(ctx); err != nil {
+			return nil, err
+		}
+	}
+
+	series := m.outputSeriesMetadata
+	m.outputSeriesMetadata = nil
+	return series, nil
+}
+
+func (m *MultiAggregatorInstanceOperator) NextSeries(ctx context.Context) (types.InstantVectorSeriesData, error) {
+	if !m.aggregator.HasMoreOutputSeries() {
+		return types.InstantVectorSeriesData{}, types.EOS
+	}
+
+	for !m.aggregator.IsNextOutputSeriesComplete() {
+		if err := m.group.ReadNextSeries(ctx); err != nil {
+			return types.InstantVectorSeriesData{}, err
+		}
+	}
+
+	return m.aggregator.ComputeNextOutputSeries()
+}
+
+func (m *MultiAggregatorInstanceOperator) Finalize(ctx context.Context) error {
+	if m.finalized {
+		return nil
+	}
+
+	m.finalized = true
+	return m.group.Finalize(ctx)
+}
+
+func (m *MultiAggregatorInstanceOperator) Close() {
+	if m.closed {
+		return
+	}
+
+	m.closed = true
+
+	if m.aggregator != nil {
+		m.aggregator.Close()
+	}
+
+	m.group.Close()
+}
+
+func (m *MultiAggregatorInstanceOperator) ExpressionPosition() posrange.PositionRange {
+	return m.expressionPosition
+}

pkg/streamingpromql/testdata/ours/multi_aggregation.test
@@ -1,0 +1,75 @@
+# SPDX-License-Identifier: AGPL-3.0-only
+
+# The goal of these tests is not to ensure that opportunities to apply multi-aggregation are correctly identified
+# (this is tested in the tests for the optimization pass), but to ensure that expressions containing
+# multi-aggregations are correctly evaluated.
+
+load 1m
+  float_metric{idx="1", group="a", subgroup="1"} 1+1x10
+  float_metric{idx="2", group="b", subgroup="1"} 2+2x10
+  float_metric{idx="3", group="c", subgroup="1"} 3+3x10
+  float_metric{idx="4", group="a", subgroup="2"} 4+4x10
+  float_metric{idx="5", group="b", subgroup="2"} 5+5x10
+  histogram_metric{idx="1", group="a", subgroup="1"} {{count:1}}+{{count:1}}x10
+  histogram_metric{idx="2", group="b", subgroup="1"} {{count:2}}+{{count:2}}x10
+  histogram_metric{idx="3", group="c", subgroup="1"} {{count:3}}+{{count:3}}x10
+  histogram_metric{idx="4", group="a", subgroup="2"} {{count:4}}+{{count:4}}x10
+  histogram_metric{idx="5", group="b", subgroup="2"} {{count:5}}+{{count:5}}x10
+
+eval instant at 0 sum(float_metric) + count(float_metric)
+  {} 20
+
+eval range from 0 to 10m step 1m sum(float_metric) + count(float_metric)
+  {} 20+15x10
+
+eval instant at 0 sum(histogram_metric) + avg(histogram_metric)
+  {} {{count:18}}
+
+eval range from 0 to 10m step 1m sum(histogram_metric) + avg(histogram_metric)
+  {} {{count:18}}+{{count:18}}x10
+
+# Same as above, but in the other order, to ensure there are no issues with FloatHistogram instance reuse in either aggregation implementaion.
+eval instant at 0 avg(histogram_metric) + sum(histogram_metric)
+  {} {{count:18}}
+
+eval range from 0 to 10m step 1m avg(histogram_metric) + sum(histogram_metric)
+  {} {{count:18}}+{{count:18}}x10
+
+# Same thing, but with a matrix selector.
+eval instant at 4m sum(rate(float_metric[3m])) + count(rate(float_metric[3m]))
+  {} 5.25
+
+eval range from 0 to 10m step 1m sum(rate(float_metric[3m])) + count(rate(float_metric[3m]))
+  {} _ 5.125 5.25+0x8
+
+eval instant at 4m sum(rate(histogram_metric[3m])) + avg(rate(histogram_metric[3m]))
+  {} {{count:0.3}}
+
+eval range from 0 to 10m step 1m sum(rate(histogram_metric[3m])) + avg(rate(histogram_metric[3m]))
+  {} _ {{count:0.15}} {{count:0.3}}+{{count:0}}x8
+
+# Test that grouping by labels behaves as expected with different aggregation operations.
+eval instant at 0 sum by (group) (float_metric) + count by (group) (float_metric)
+  {group="a"} 7
+  {group="b"} 9
+  {group="c"} 4
+
+eval instant at 0 sum by (group) (histogram_metric) + avg by (group) (histogram_metric)
+  {group="a"} {{count:7.5}}
+  {group="b"} {{count:10.5}}
+  {group="c"} {{count:6}}
+
+# Same thing, but with the same aggregation operation and different grouping labels.
+eval instant at 0 label_replace(sum by (group) (float_metric), "subgroup", "1", "group", ".*") + sum by (group, subgroup) (float_metric)
+  {group="a", subgroup="1"} 6
+  {group="b", subgroup="1"} 9
+  {group="c", subgroup="1"} 6
+
+eval instant at 0 label_replace(sum by (group) (histogram_metric), "subgroup", "1", "group", ".*") + sum by (group, subgroup) (histogram_metric)
+  {group="a", subgroup="1"} {{count:6}}
+  {group="b", subgroup="1"} {{count:9}}
+  {group="c", subgroup="1"} {{count:6}}
+
+# Make sure that running a supported and unsupported aggregation in the same expression works.
+eval range from 0 to 10m step 1m sum(float_metric) + quantile(0.9, float_metric)
+  {} 19.6+19.6x10

cmd/mimir/help-all.txt.tmpl
@@ -2157,6 +2157,8 @@ Usage of ./cmd/mimir/mimir:
     	[experimental] Enable common subexpression elimination for range vector expressions when evaluating instant queries. This has no effect if common subexpression elimination is disabled. (default true)
   -querier.mimir-query-engine.enable-eliminate-deduplicate-and-merge
     	[experimental] Enable eliminating redundant DeduplicateAndMerge nodes from the query plan when it can be proven that each input series produces a unique output series.
+  -querier.mimir-query-engine.enable-multi-aggregation
+    	[experimental] Enable computing multiple aggregations over the same data without buffering. Requires common subexpression elimination to be enabled. (default true)
   -querier.mimir-query-engine.enable-narrow-binary-selectors
     	[experimental] Enable generating selectors for one side of a binary expression based on results from the other side.
   -querier.mimir-query-engine.enable-projection-pushdown

pkg/streamingpromql/config.go
@@ -37,6 +37,7 @@ type EngineOpts struct {
 	EnableEliminateDeduplicateAndMerge                                            bool `yaml:"enable_eliminate_deduplicate_and_merge" category:"experimental"`
 	EnableReduceMatchers                                                          bool `yaml:"enable_reduce_matchers" category:"experimental"`
 	EnableProjectionPushdown                                                      bool `yaml:"enable_projection_pushdown" category:"experimental"`
+	EnableMultiAggregation                                                        bool `yaml:"enable_multi_aggregation" category:"experimental"`
 }
 
 func (o *EngineOpts) RegisterFlags(f *flag.FlagSet) {
@@ -48,6 +49,7 @@ func (o *EngineOpts) RegisterFlags(f *flag.FlagSet) {
 	f.BoolVar(&o.EnableEliminateDeduplicateAndMerge, "querier.mimir-query-engine.enable-eliminate-deduplicate-and-merge", false, "Enable eliminating redundant DeduplicateAndMerge nodes from the query plan when it can be proven that each input series produces a unique output series.")
 	f.BoolVar(&o.EnableReduceMatchers, "querier.mimir-query-engine.enable-reduce-matchers", true, "Enable eliminating duplicate or redundant matchers that are part of selector expressions.")
 	f.BoolVar(&o.EnableProjectionPushdown, "querier.mimir-query-engine.enable-projection-pushdown", false, "Enable projection pushdown to only fetch labels required for the query from storage.")
+	f.BoolVar(&o.EnableMultiAggregation, "querier.mimir-query-engine.enable-multi-aggregation", true, "Enable computing multiple aggregations over the same data without buffering. Requires common subexpression elimination to be enabled.")
 }
 
 func NewTestEngineOpts() EngineOpts {
@@ -73,5 +75,6 @@ func NewTestEngineOpts() EngineOpts {
 		EnableEliminateDeduplicateAndMerge:                                            true,
 		EnableReduceMatchers:                                                          true,
 		EnableProjectionPushdown:                                                      true,
+		EnableMultiAggregation:                                                        true,
 	}
 }

pkg/streamingpromql/engine.go
@@ -26,6 +26,7 @@ import (
 	apierror "github.com/grafana/mimir/pkg/api/error"
 	"github.com/grafana/mimir/pkg/querier/stats"
 	"github.com/grafana/mimir/pkg/streamingpromql/optimize/plan/commonsubexpressionelimination"
+	"github.com/grafana/mimir/pkg/streamingpromql/optimize/plan/multiaggregation"
 	"github.com/grafana/mimir/pkg/streamingpromql/planning"
 	"github.com/grafana/mimir/pkg/streamingpromql/planning/core"
 	"github.com/grafana/mimir/pkg/streamingpromql/types"
@@ -81,8 +82,10 @@ func NewEngine(opts EngineOpts, limitsProvider QueryLimitsProvider, metrics *sta
 		planning.NODE_TYPE_DEDUPLICATE_AND_MERGE: planning.NodeMaterializerFunc[*core.DeduplicateAndMerge](core.MaterializeDeduplicateAndMerge),
 		planning.NODE_TYPE_DROP_NAME:             planning.NodeMaterializerFunc[*core.DropName](core.MaterializeDropName),
 
-		planning.NODE_TYPE_DUPLICATE:                 planning.NodeMaterializerFunc[*commonsubexpressionelimination.Duplicate](commonsubexpressionelimination.MaterializeDuplicate),
-		planning.NODE_TYPE_STEP_INVARIANT_EXPRESSION: planning.NodeMaterializerFunc[*core.StepInvariantExpression](core.MaterializeStepInvariantExpression),
+		planning.NODE_TYPE_DUPLICATE:                  planning.NodeMaterializerFunc[*commonsubexpressionelimination.Duplicate](commonsubexpressionelimination.MaterializeDuplicate),
+		planning.NODE_TYPE_STEP_INVARIANT_EXPRESSION:  planning.NodeMaterializerFunc[*core.StepInvariantExpression](core.MaterializeStepInvariantExpression),
+		planning.NODE_TYPE_MULTI_AGGREGATION_GROUP:    planning.NodeMaterializerFunc[*multiaggregation.MultiAggregationGroup](multiaggregation.MaterializeMultiAggregationGroup),
+		planning.NODE_TYPE_MULTI_AGGREGATION_INSTANCE: planning.NodeMaterializerFunc[*multiaggregation.MultiAggregationInstance](multiaggregation.MaterializeMultiAggregationInstance),
 	}
 
 	return &Engine{

pkg/streamingpromql/optimize/plan/multiaggregation/node.go
@@ -1,0 +1,242 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package multiaggregation
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/prometheus/prometheus/promql/parser"
+	"github.com/prometheus/prometheus/promql/parser/posrange"
+
+	"github.com/grafana/mimir/pkg/streamingpromql/planning"
+	"github.com/grafana/mimir/pkg/streamingpromql/types"
+)
+
+func init() {
+	planning.RegisterNodeFactory(func() planning.Node {
+		return &MultiAggregationGroup{MultiAggregationGroupDetails: &MultiAggregationGroupDetails{}}
+	})
+	planning.RegisterNodeFactory(func() planning.Node {
+		return &MultiAggregationInstance{MultiAggregationInstanceDetails: &MultiAggregationInstanceDetails{}}
+	})
+}
+
+type MultiAggregationGroup struct {
+	*MultiAggregationGroupDetails
+	Inner planning.Node
+}
+
+func (g *MultiAggregationGroup) Details() proto.Message {
+	return g.MultiAggregationGroupDetails
+}
+
+func (g *MultiAggregationGroup) NodeType() planning.NodeType {
+	return planning.NODE_TYPE_MULTI_AGGREGATION_GROUP
+}
+
+func (g *MultiAggregationGroup) Child(idx int) planning.Node {
+	if idx != 0 {
+		panic(fmt.Sprintf("node of type MultiAggregationGroup supports 1 child, but attempted to get child at index %d", idx))
+	}
+
+	return g.Inner
+}
+
+func (g *MultiAggregationGroup) ChildCount() int {
+	return 1
+}
+
+func (g *MultiAggregationGroup) SetChildren(children []planning.Node) error {
+	if len(children) != 1 {
+		return fmt.Errorf("node of type MultiAggregationGroup supports 1 child, but got %d", len(children))
+	}
+
+	g.Inner = children[0]
+	return nil
+}
+
+func (g *MultiAggregationGroup) ReplaceChild(idx int, node planning.Node) error {
+	if idx != 0 {
+		return fmt.Errorf("node of type MultiAggregationGroup supports 1 child, but attempted to replace child at index %d", idx)
+	}
+
+	g.Inner = node
+	return nil
+}
+
+func (g *MultiAggregationGroup) EquivalentToIgnoringHintsAndChildren(other planning.Node) bool {
+	_, ok := other.(*MultiAggregationGroup)
+
+	return ok
+}
+
+func (g *MultiAggregationGroup) MergeHints(other planning.Node) error {
+	_, ok := other.(*MultiAggregationGroup)
+	if !ok {
+		return fmt.Errorf("cannot merge hints from %T into %T", other, g)
+	}
+
+	return nil
+}
+
+func (g *MultiAggregationGroup) Describe() string {
+	return ""
+}
+
+func (g *MultiAggregationGroup) ChildrenLabels() []string {
+	return []string{""}
+}
+
+func (g *MultiAggregationGroup) ChildrenTimeRange(parentTimeRange types.QueryTimeRange) types.QueryTimeRange {
+	return parentTimeRange
+}
+
+func (g *MultiAggregationGroup) ResultType() (parser.ValueType, error) {
+	return g.Inner.ResultType()
+}
+
+func (g *MultiAggregationGroup) QueriedTimeRange(queryTimeRange types.QueryTimeRange, lookbackDelta time.Duration) (planning.QueriedTimeRange, error) {
+	return g.Inner.QueriedTimeRange(queryTimeRange, lookbackDelta)
+}
+
+func (g *MultiAggregationGroup) ExpressionPosition() (posrange.PositionRange, error) {
+	return g.Inner.ExpressionPosition()
+}
+
+func (g *MultiAggregationGroup) MinimumRequiredPlanVersion() planning.QueryPlanVersion {
+	return planning.QueryPlanV5
+}
+
+type MultiAggregationInstance struct {
+	*MultiAggregationInstanceDetails
+	Group *MultiAggregationGroup
+}
+
+func (a *MultiAggregationInstance) Details() proto.Message {
+	return a.MultiAggregationInstanceDetails
+}
+
+func (a *MultiAggregationInstance) NodeType() planning.NodeType {
+	return planning.NODE_TYPE_MULTI_AGGREGATION_INSTANCE
+}
+
+func (a *MultiAggregationInstance) Child(idx int) planning.Node {
+	if idx != 0 {
+		panic(fmt.Sprintf("node of type MultiAggregationInstance supports 1 child, but attempted to get child at index %d", idx))
+	}
+
+	return a.Group
+}
+
+func (a *MultiAggregationInstance) ChildCount() int {
+	return 1
+}
+
+func (a *MultiAggregationInstance) SetChildren(children []planning.Node) error {
+	if len(children) != 1 {
+		return fmt.Errorf("node of type MultiAggregationInstance supports 1 child, but got %d", len(children))
+	}
+
+	return a.ReplaceChild(0, children[0])
+}
+
+func (a *MultiAggregationInstance) ReplaceChild(idx int, node planning.Node) error {
+	if idx != 0 {
+		return fmt.Errorf("node of type MultiAggregationInstance supports 1 child, but attempted to replace child at index %d", idx)
+	}
+
+	group, ok := node.(*MultiAggregationGroup)
+	if !ok {
+		return fmt.Errorf("node of type MultiAggregationInstance requires child of type MultiAggregationGroup, but got %T", node)
+	}
+
+	a.Group = group
+	return nil
+}
+
+func (a *MultiAggregationInstance) EquivalentToIgnoringHintsAndChildren(other planning.Node) bool {
+	otherInstance, ok := other.(*MultiAggregationInstance)
+
+	return ok && a.Aggregation.EquivalentTo(otherInstance.Aggregation)
+}
+
+func (a *MultiAggregationInstance) MergeHints(other planning.Node) error {
+	otherInstance, ok := other.(*MultiAggregationInstance)
+	if !ok {
+		return fmt.Errorf("cannot merge hints from %T into %T", other, a)
+	}
+
+	return a.Aggregation.MergeHints(otherInstance.Aggregation)
+}
+
+func (a *MultiAggregationInstance) Describe() string {
+	return a.Aggregation.Describe()
+}
+
+func (a *MultiAggregationInstance) ChildrenLabels() []string {
+	return []string{""}
+}
+
+func (a *MultiAggregationInstance) ChildrenTimeRange(parentTimeRange types.QueryTimeRange) types.QueryTimeRange {
+	return parentTimeRange
+}
+
+func (a *MultiAggregationInstance) ResultType() (parser.ValueType, error) {
+	return a.Group.ResultType()
+}
+
+func (a *MultiAggregationInstance) QueriedTimeRange(queryTimeRange types.QueryTimeRange, lookbackDelta time.Duration) (planning.QueriedTimeRange, error) {
+	return a.Group.QueriedTimeRange(queryTimeRange, lookbackDelta)
+}
+
+func (a *MultiAggregationInstance) ExpressionPosition() (posrange.PositionRange, error) {
+	return a.Group.ExpressionPosition()
+}
+
+func (a *MultiAggregationInstance) MinimumRequiredPlanVersion() planning.QueryPlanVersion {
+	return planning.QueryPlanV5
+}
+
+func MaterializeMultiAggregationGroup(node *MultiAggregationGroup, materializer *planning.Materializer, timeRange types.QueryTimeRange, params *planning.OperatorParameters) (planning.OperatorFactory, error) {
+	inner, err := materializer.ConvertNodeToInstantVectorOperator(node.Inner, timeRange)
+	if err != nil {
+		return nil, err
+	}
+
+	evaluator := NewMultiAggregatorGroupEvaluator(inner, params.MemoryConsumptionTracker)
+
+	return &MultiAggregationInstanceFactory{group: evaluator}, nil
+}
+
+type MultiAggregationInstanceFactory struct {
+	group *MultiAggregatorGroupEvaluator
+}
+
+func (m *MultiAggregationInstanceFactory) Produce() (types.Operator, error) {
+	return m.group.AddInstance(), nil
+}
+
+func MaterializeMultiAggregationInstance(node *MultiAggregationInstance, materializer *planning.Materializer, timeRange types.QueryTimeRange, params *planning.OperatorParameters) (planning.OperatorFactory, error) {
+	operator, err := materializer.ConvertNodeToInstantVectorOperator(node.Group, timeRange)
+	if err != nil {
+		return nil, err
+	}
+
+	instance, ok := operator.(*MultiAggregatorInstanceOperator)
+	if !ok {
+		return nil, fmt.Errorf("expected MultiAggregatorInstanceOperator, got %T", operator)
+	}
+
+	op, ok := node.Aggregation.Op.ToItemType()
+	if !ok {
+		return nil, fmt.Errorf("unknown aggregation operation %s", node.Aggregation.Op.String())
+	}
+
+	if err := instance.Configure(op, node.Aggregation.Grouping, node.Aggregation.Without, params.MemoryConsumptionTracker, params.Annotations, timeRange, node.Aggregation.ExpressionPosition.ToPrometheusType()); err != nil {
+		return nil, err
+	}
+
+	return planning.NewSingleUseOperatorFactory(instance), nil
+}

commit b66747841d87ff1da9ee468e19337be2fa42e849

Fix issue where queries can fail with `should not call Selector.SeriesMetadata() multiple times` if multi-aggregation optimisation applies (#14174)
#### What this PR does

This PR fixes an issue in the optimisation pass introduced in
https://github.com/grafana/mimir/pull/14123. In an expression like `sum
by (env) (foo) / scalar(sum(foo))`, if `foo` returns no series, the
query would fail with `should not call Selector.SeriesMetadata()
multiple times`.

#### Which issue(s) this PR fixes or relates to

https://github.com/grafana/mimir/pull/14123

#### Checklist

- [x] Tests updated.
- [n/a] Documentation added.
- [x] `CHANGELOG.md` updated - the order of entries should be
`[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`. If changelog entry
is not needed, please add the `changelog-not-needed` label to the PR.
- [n/a]
[`about-versioning.md`](https://github.com/grafana/mimir/blob/main/docs/sources/mimir/configure/about-versioning.md)
updated with experimental features.

<!-- CURSOR_SUMMARY -->
---

> [!NOTE]
> Fixes failure in multi-aggregation optimization when the inner
selector returns no series.
> 
> - Track `haveComputedSeriesMetadata` in
`MultiAggregatorGroupEvaluator` and gate `SeriesMetadata` to compute
once across instances
> - Adjust instance `SeriesMetadata` to rely on group-level computation
and clear per-instance cache safely
> - Add tests covering empty-input case (`sum by (foo) (does_not_exist)
/ scalar(sum(does_not_exist))`) and update `CHANGELOG.md` entry with
additional reference
> 
> <sup>Written by [Cursor
Bugbot](https://cursor.com/dashboard?tab=bugbot) for commit
da40477ce7873193371972a9226c78c47cd88aa0. This will update automatically
on new commits. Configure
[here](https://cursor.com/dashboard?tab=bugbot).</sup>
<!-- /CURSOR_SUMMARY -->

pkg/streamingpromql/optimize/plan/multiaggregation/operator.go
@@ -20,8 +20,9 @@ type MultiAggregatorGroupEvaluator struct {
 
 	instances []*MultiAggregatorInstanceOperator
 
-	prepareCalled      bool
-	afterPrepareCalled bool
+	haveComputedSeriesMetadata bool
+	prepareCalled              bool
+	afterPrepareCalled         bool
 }
 
 func NewMultiAggregatorGroupEvaluator(
@@ -65,6 +66,7 @@ func (m *MultiAggregatorGroupEvaluator) ComputeOutputSeriesForAllInstances(ctx c
 		return err
 	}
 
+	m.haveComputedSeriesMetadata = true
 	defer types.SeriesMetadataSlicePool.Put(&innerSeries, m.memoryConsumptionTracker)
 
 	for _, instance := range m.instances {
@@ -160,7 +162,7 @@ func (m *MultiAggregatorInstanceOperator) AfterPrepare(ctx context.Context) erro
 }
 
 func (m *MultiAggregatorInstanceOperator) SeriesMetadata(ctx context.Context, matchers types.Matchers) ([]types.SeriesMetadata, error) {
-	if m.outputSeriesMetadata == nil {
+	if !m.group.haveComputedSeriesMetadata {
 		if err := m.group.ComputeOutputSeriesForAllInstances(ctx); err != nil {
 			return nil, err
 		}

pkg/streamingpromql/testdata/ours/multi_aggregation.test
@@ -73,3 +73,6 @@ eval instant at 0 label_replace(sum by (group) (histogram_metric), "subgroup", "
 # Make sure that running a supported and unsupported aggregation in the same expression works.
 eval range from 0 to 10m step 1m sum(float_metric) + quantile(0.9, float_metric)
   {} 19.6+19.6x10
+
+eval instant at 0 sum by (foo) (does_not_exist) / scalar(sum(does_not_exist))
+  # Should return no results.