08e04daf92ba77acf21d4a44ed9f1d47b41ac3b6
Franco Posa <franco@francoposa.io>
2026-01-27 08:32:27 -0800

    fix(ingester): revert to only shut down ring lifecycler on startup failure (#14180)
    <!--  Thanks for sending a pull request!  Before submitting:
    
    1. Read our CONTRIBUTING.md guide
    2. Rebase your PR if it gets out of sync with main
    -->
    
    #### What this PR does
    
    Follow-up to https://github.com/grafana/mimir/pull/14025 and
    https://github.com/grafana/mimir/pull/14134.
    
    We originally tried to have a nice clean shutdown in order to pass the
    tests with `VerifyNoLeak`, but there are issues with circular
    dependencies blocking the process as we shutdown various services.
    
    We are going back to only shutting down the ring lifecycler and ignoring
    the hanging goroutines in tests.
    We are keeping the context deadline for the shutdown we introduced in an
    attempt to prevent stuck shutdowns.
    
    #### Which issue(s) this PR fixes or relates to
    
    Fixes #<issue number>
    
    #### Checklist
    
    - [x] Tests updated.
    - [ ] Documentation added.
    - [x] `CHANGELOG.md` updated - the order of entries should be
    `[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`. If changelog entry
    is not needed, please add the `changelog-not-needed` label to the PR.
    - [ ]
    [`about-versioning.md`](https://github.com/grafana/mimir/blob/main/docs/sources/mimir/configure/about-versioning.md)
    updated with experimental features.
    
    <!-- CURSOR_SUMMARY -->
    ---
    
    > [!NOTE]
    > Simplifies ingester startup-failure cleanup and stabilizes tests.
    >
    > - Starting failure handler now stops only the `lifecycler` (timeout
    reduced to 1m); removal of broad subservice shutdown and watcher close
    from `starting()` defer
    > - Tests updated to tolerate specific goroutine leaks via `goleak`
    options and to pass these to `VerifyNoLeak`
    > - CHANGELOG bugfix line amended to include additional PR reference
    (#14180)
    >
    > <sup>Written by [Cursor
    Bugbot](https://cursor.com/dashboard?tab=bugbot) for commit
    44a120eb50bab9d265484790ed3006325b9579c4. This will update automatically
    on new commits. Configure
    [here](https://cursor.com/dashboard?tab=bugbot).</sup>
    <!-- /CURSOR_SUMMARY -->
    
    (cherry picked from commit ac8a2e3166c6203f4bb6ef84fc9b6d7fddf4ddcf)

--- pkg/ingester/ingester_ingest_storage_test.go
+++ pkg/ingester/ingester_ingest_storage_test.go
@@ -37,6 +37,7 @@ import (
 	"github.com/twmb/franz-go/pkg/kgo"
 	"github.com/twmb/franz-go/pkg/kmsg"
 	"go.uber.org/atomic"
+	"go.uber.org/goleak"
 
 	"github.com/grafana/mimir/pkg/mimirpb"
 	"github.com/grafana/mimir/pkg/querier/api"
@@ -47,8 +48,15 @@ import (
 	"github.com/grafana/mimir/pkg/util/validation"
 )
 
+var goleakOpts = []goleak.Option{
+	// TestIngester_Startup_PartitionRingActiveBlocksOnInstanceRingActive
+	// tests ingester failure on startup, which does not clean up all goroutines.
+	// This goroutine will be detected by other tests running in parallel with VerifyNoLeak.
+	goleak.IgnoreAnyFunction("github.com/grafana/dskit/services.funcBasedListener.Failed"),
+}
+
 func TestIngester_Startup_PartitionRingActiveBlocksOnInstanceRingActive(t *testing.T) {
-	util_test.VerifyNoLeak(t)
+	util_test.VerifyNoLeak(t, goleakOpts...)
 	var err error
 
 	cfg := defaultIngesterTestConfig(t)
@@ -159,7 +167,7 @@ func TestIngester_Startup_PartitionRingActiveBlocksOnInstanceRingActive(t *testi
 }
 
 func TestIngester_Start(t *testing.T) {
-	util_test.VerifyNoLeak(t)
+	util_test.VerifyNoLeak(t, goleakOpts...)
 
 	t.Run("should replay the partition at startup (after a restart) and then join the ingesters and partitions ring", func(t *testing.T) {
 		var (
