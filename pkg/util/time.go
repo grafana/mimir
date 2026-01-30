1aef6d37dead02dee8a36945572a84cafe092851
Marco Pracucci <marco@pracucci.com>
2026-01-30 18:27:41 +0100

    Add -memberlist.rejoin-seed-nodes support (#14208)
    #### What this PR does
    
    Update dskit to get a couple of changes to memberlist client:
    - https://github.com/grafana/dskit/pull/888
    - https://github.com/grafana/dskit/pull/892
    
    #### Which issue(s) this PR fixes or relates to
    
    N/A
    
    #### Checklist
    
    - [ ] Tests updated.
    - [x] Documentation added.
    - [x] `CHANGELOG.md` updated - the order of entries should be
    `[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`. If changelog entry
    is not needed, please add the `changelog-not-needed` label to the PR.
    - [ ]
    [`about-versioning.md`](https://github.com/grafana/mimir/blob/main/docs/sources/mimir/configure/about-versioning.md)
    updated with experimental features.
    
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- pkg/util/time.go
+++ pkg/util/time.go
@@ -9,7 +9,6 @@ import (
 	"math"
 	"math/rand"
 	"strconv"
-	"sync"
 	"time"
 
 	"github.com/efficientgo/core/errors"
@@ -109,60 +108,6 @@ func NewDisableableTicker(interval time.Duration) (func(), <-chan time.Time) {
 
 	tick := time.NewTicker(interval)
 	return func() { tick.Stop() }, tick.C
-}
-
-// NewVariableTicker wrap time.Ticker to Reset() the ticker with the next duration (picked from
-// input durations) after each tick. The last configured duration is the one that will be preserved
-// once previous ones have been applied.
-//
-// Returns a function for stopping the ticker, and the ticker channel.
-func NewVariableTicker(durations ...time.Duration) (func(), <-chan time.Time) {
-	if len(durations) == 0 {
-		panic("at least 1 duration required")
-	}
-
-	// Init the ticker with the 1st duration.
-	ticker := time.NewTicker(durations[0])
-	durations = durations[1:]
-
-	// If there was only 1 duration we can simply return the built-in ticker.
-	if len(durations) == 0 {
-		return ticker.Stop, ticker.C
-	}
-
-	// Create a channel over which our ticks will be sent.
-	ticks := make(chan time.Time, 1)
-
-	// Create a channel used to signal once this ticker is stopped.
-	stopped := make(chan struct{})
-
-	go func() {
-		for {
-			select {
-			case ts := <-ticker.C:
-				if len(durations) > 0 {
-					ticker.Reset(durations[0])
-					durations = durations[1:]
-				}
-
-				ticks <- ts
-
-			case <-stopped:
-				// Interrupt the loop once stopped.
-				return
-			}
-		}
-	}()
-
-	stopOnce := sync.Once{}
-	stop := func() {
-		stopOnce.Do(func() {
-			ticker.Stop()
-			close(stopped)
-		})
-	}
-
-	return stop, ticks
 }
 
 // UnixSeconds is Unix timestamp with seconds precision.
