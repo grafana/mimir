836e28778c946e18fd5edda295cc865b5a9815e6
Yuri Nikolic <durica.nikolic@grafana.com>
2026-02-03 13:31:07 +0100

    Limits: add configurable limit for max additional custom trackers cardinality
    Signed-off-by: Yuri Nikolic <durica.nikolic@grafana.com>

--- pkg/ingester/activeseries/model/custom_trackers_config_test.go
+++ pkg/ingester/activeseries/model/custom_trackers_config_test.go
@@ -442,3 +442,76 @@ func TestMergeCustomTrackersConfig(t *testing.T) {
 		})
 	}
 }
+
+func TestCustomTrackersConfig_ValidateCardinality(t *testing.T) {
+	tests := map[string]struct {
+		config         map[string]string
+		maxCardinality int
+		expectError    bool
+		errorContains  string
+	}{
+		"within limit": {
+			config: map[string]string{
+				"tracker1": `{foo="bar"}`,
+				"tracker2": `{baz="qux"}`,
+			},
+			maxCardinality: 5,
+			expectError:    false,
+		},
+		"at limit": {
+			config: map[string]string{
+				"tracker1": `{foo="bar"}`,
+				"tracker2": `{baz="qux"}`,
+				"tracker3": `{hello="world"}`,
+			},
+			maxCardinality: 3,
+			expectError:    false,
+		},
+		"exceeds limit": {
+			config: map[string]string{
+				"tracker1": `{foo="bar"}`,
+				"tracker2": `{baz="qux"}`,
+				"tracker3": `{hello="world"}`,
+			},
+			maxCardinality: 2,
+			expectError:    true,
+			errorContains:  "the number of custom trackers [3] exceeds the configured limit [2]",
+		},
+		"unlimited (maxCardinality = 0)": {
+			config: map[string]string{
+				"tracker1": `{foo="bar"}`,
+				"tracker2": `{baz="qux"}`,
+				"tracker3": `{hello="world"}`,
+			},
+			maxCardinality: 0,
+			expectError:    false,
+		},
+		"invalid negative limit": {
+			config: map[string]string{
+				"tracker1": `{foo="bar"}`,
+			},
+			maxCardinality: -1,
+			expectError:    true,
+			errorContains:  "invalid max cardinality: -1",
+		},
+		"empty config with limit": {
+			config:         map[string]string{},
+			maxCardinality: 5,
+			expectError:    false,
+		},
+	}
+
+	for testName, testData := range tests {
+		t.Run(testName, func(t *testing.T) {
+			config := mustNewCustomTrackersConfigFromMap(t, testData.config)
+			err := config.ValidateCardinality(testData.maxCardinality)
+
+			if testData.expectError {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), testData.errorContains)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
