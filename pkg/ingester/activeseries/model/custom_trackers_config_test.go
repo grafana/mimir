a9a82b77aa6739e31150dc9a7e339d8b0a8186d2
Yuri Nikolic <durica.nikolic@grafana.com>
2026-02-05 07:47:26 +0100

    Fixing a failing test
    Signed-off-by: Yuri Nikolic <durica.nikolic@grafana.com>

--- pkg/ingester/activeseries/model/custom_trackers_config_test.go
+++ pkg/ingester/activeseries/model/custom_trackers_config_test.go
@@ -492,7 +492,7 @@ func TestCustomTrackersConfig_Validate(t *testing.T) {
 			},
 			length:        -1,
 			expectError:   true,
-			errorContains: "invalid max cardinality: -1",
+			errorContains: "invalid number of custom trackers: -1",
 		},
 		"empty config with limit": {
 			config:      map[string]string{},
