28fd4119f11caa928332d622f0f35ccd8f59ce08
Jon Kartago Lamida <me@lamida.net>
2026-02-11 09:56:28 +0800

    Remove outdated comment

--- pkg/util/limiter/series_labels_deduplicator.go
+++ pkg/util/limiter/series_labels_deduplicator.go
@@ -38,20 +38,6 @@ func NewSeriesLabelsDeduplicator() SeriesLabelsDeduplicator {
 }
 
 // ContextWithNewSeriesLabelsDeduplicator adds a new SeriesLabelsDeduplicator to the context.
-//
-// The scope at which a SeriesLabelsDeduplicator is added determines the level of deduplication:
-//
-//  1. Per-Select scope (default): Created by MemoryTrackingQueryable for each Select() call.
-//     This is used by both MQE streaming queries and Prometheus engine queries, including
-//     federated queries.
-//
-//  2. Per-query scope (special cases): Created for ruler queries and other custom queryable
-//     implementations that don't use MemoryTrackingQueryable.
-//
-// The deduplicator must be added to the context before series selection begins, as it
-// tracks unique labels and increases memory consumption during the Select() operation.
-//
-// See the function usage for more documentation on different scopes of SeriesLabelsDeduplicator.
 func ContextWithNewSeriesLabelsDeduplicator(ctx context.Context) context.Context {
 	return context.WithValue(ctx, sdCtxKey, NewSeriesLabelsDeduplicator())
 }
