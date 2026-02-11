8591064fb5b37fd9bc1d11ccc73fff037308f123
Jon Kartago Lamida <me@lamida.net>
2026-02-11 22:52:13 +0800

    Create MemoryTrackingQueryable only for MQE engine

--- pkg/querier/querier.go
+++ pkg/querier/querier.go
@@ -202,16 +202,8 @@ func New(
 		},
 	})
 
-	queryable := NewMemoryTrackingQueryable(newQueryable(queryables, cfg, limits, queryMetrics, logger), createUnlimitedMemoryTracker)
+	queryable := newQueryable(queryables, cfg, limits, queryMetrics, logger)
 	exemplarQueryable := newDistributorExemplarQueryable(distributor, logger)
-
-	lazyQueryable := storage.QueryableFunc(func(minT int64, maxT int64) (storage.Querier, error) {
-		querier, err := queryable.Querier(minT, maxT)
-		if err != nil {
-			return nil, err
-		}
-		return lazyquery.NewLazyQuerier(querier), nil
-	})
 
 	opts, mqeOpts := engine.NewPromQLEngineOptions(cfg.EngineConfig, tracker, logger, reg)
 
@@ -244,9 +236,19 @@ func New(
 		} else {
 			eng = streamingEngine
 		}
+		// Wrap queryable with memory tracking and optionally create UnlimitedMemoryTracker.
+		queryable = NewMemoryTrackingQueryable(queryable, createUnlimitedMemoryTracker)
 	default:
 		panic(fmt.Sprintf("invalid config not caught by validation: unknown PromQL engine '%s'", cfg.QueryEngine))
 	}
+
+	lazyQueryable := storage.QueryableFunc(func(minT int64, maxT int64) (storage.Querier, error) {
+		querier, err := queryable.Querier(minT, maxT)
+		if err != nil {
+			return nil, err
+		}
+		return lazyquery.NewLazyQuerier(querier), nil
+	})
 
 	return NewSampleAndChunkQueryable(lazyQueryable), exemplarQueryable, eng, streamingEngine, nil
 }
