9eff670e3f3354ef3392faff910b2bbda49b1ed8
Marco Pracucci <marco@pracucci.com>
2026-02-11 19:11:27 +0100

    Fix linter
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- tools/writetee/proxy_test.go
+++ tools/writetee/proxy_test.go
@@ -64,9 +64,9 @@ func TestNewProxy_Validation(t *testing.T) {
 		{
 			name: "valid single backend with preferred",
 			cfg: ProxyConfig{
-				BackendMirroredEndpoints:    "http://backend1:8080",
-				PreferredBackend:            "backend1",
-				AsyncMaxInFlightPerBackend:  1000,
+				BackendMirroredEndpoints:   "http://backend1:8080",
+				PreferredBackend:           "backend1",
+				AsyncMaxInFlightPerBackend: 1000,
 			},
 			routes:      []Route{},
 			expectedErr: "",
@@ -74,9 +74,9 @@ func TestNewProxy_Validation(t *testing.T) {
 		{
 			name: "valid multiple backends with preferred",
 			cfg: ProxyConfig{
-				BackendMirroredEndpoints:    "http://backend1:8080,http://backend2:8080",
-				PreferredBackend:            "backend1",
-				AsyncMaxInFlightPerBackend:  1000,
+				BackendMirroredEndpoints:   "http://backend1:8080,http://backend2:8080",
+				PreferredBackend:           "backend1",
+				AsyncMaxInFlightPerBackend: 1000,
 			},
 			routes:      []Route{},
 			expectedErr: "",
@@ -84,9 +84,9 @@ func TestNewProxy_Validation(t *testing.T) {
 		{
 			name: "negative async max in-flight",
 			cfg: ProxyConfig{
-				BackendMirroredEndpoints:    "http://backend1:8080",
-				PreferredBackend:            "backend1",
-				AsyncMaxInFlightPerBackend:  -1,
+				BackendMirroredEndpoints:   "http://backend1:8080",
+				PreferredBackend:           "backend1",
+				AsyncMaxInFlightPerBackend: -1,
 			},
 			routes:      []Route{},
 			expectedErr: "backend.async-max-in-flight must be greater than 0",
@@ -94,9 +94,9 @@ func TestNewProxy_Validation(t *testing.T) {
 		{
 			name: "zero async max in-flight",
 			cfg: ProxyConfig{
-				BackendMirroredEndpoints:    "http://backend1:8080",
-				PreferredBackend:            "backend1",
-				AsyncMaxInFlightPerBackend:  0,
+				BackendMirroredEndpoints:   "http://backend1:8080",
+				PreferredBackend:           "backend1",
+				AsyncMaxInFlightPerBackend: 0,
 			},
 			routes:      []Route{},
 			expectedErr: "backend.async-max-in-flight must be greater than 0",
