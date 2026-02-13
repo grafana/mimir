51c7102602d8d4b509f359da17dbe4b41347a0ef
Vladimir Varankin <vladimir.varankin@grafana.com>
2026-02-13 15:02:27 +0100

    missing copyright
    Signed-off-by: Vladimir Varankin <vladimir.varankin@grafana.com>

--- pkg/storage/tsdb/tsdb_metrics_test.go
+++ pkg/storage/tsdb/tsdb_metrics_test.go
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// Provenance-includes-location: https://github.com/cortexproject/cortex/blob/master/pkg/ingester/metrics_test.go
+// Provenance-includes-license: Apache-2.0
+// Provenance-includes-copyright: The Cortex Authors.
+
 package tsdb_test
 
 import (
