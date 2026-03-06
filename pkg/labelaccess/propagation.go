bda4d890c47923ef2ab8eaa1ea872dd994a90b3d
Jon Kartago Lamida <me@lamida.net>
2026-03-06 17:23:43 +0800

    Copy propagation.go

---
+++ pkg/labelaccess/propagation.go
@@ -0,0 +1,208 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// Provenance-includes-location: https://github.com/grafana/backend-enterprise/pkg/labelaccess/propagation.go
+// Provenance-includes-location: https://github.com/grafana/backend-enterprise/pkg/labelaccess/inject_hook.go
+// Provenance-includes-license: Apache-2.0
+// Provenance-includes-copyright: Grafana Labs
+
+package labelaccess
+
+import (
+	"context"
+	"crypto/sha1"
+	"encoding/base32"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"sort"
+	"strings"
+
+	"github.com/prometheus/prometheus/model/labels"
+
+	"github.com/grafana/mimir/pkg/util/promqlext"
+)
+
+type key int
+
+const (
+	// HTTPHeaderKey is the HTTP header used to carry label policy values.
+	HTTPHeaderKey = "X-Prom-Label-Policy"
+
+	contextKey key = 0
+)
+
+var (
+	errNoMatcherSource     = errors.New("no label matcher source")
+	errInvalidHeaderParse  = errors.New("invalid label policy header value, unable to parse tenant and selector")
+	errInvalidHeaderEscape = errors.New("invalid label policy header value unable to url escape selector string")
+	errInvalidSelector     = errors.New("invalid label policy header value unable to parse selector string")
+)
+
+// LabelPolicy holds a set of Prometheus label matchers representing one access policy selector.
+type LabelPolicy struct {
+	Selector []*labels.Matcher
+}
+
+// LabelPolicySet maps tenant IDs to their label policies.
+type LabelPolicySet map[string][]*LabelPolicy
+
+// GetLabelPolicySet implements LabelPolicies.
+func (l LabelPolicySet) GetLabelPolicySet() (LabelPolicySet, error) {
+	return l, nil
+}
+
+// Hash iterates through the tenant map in ascending order and returns a base32-encoded
+// SHA1 hash of all tenants and their label policies.
+func (l LabelPolicySet) Hash() string {
+	tenants := make([]string, 0, len(l))
+	for t := range l {
+		tenants = append(tenants, t)
+	}
+	h := sha1.New()
+	sort.Strings(tenants)
+	for _, t := range tenants {
+		_, _ = h.Write([]byte(t))
+		for _, p := range l[t] {
+			for i := range p.Selector {
+				_, _ = h.Write([]byte(p.Selector[i].String()))
+			}
+		}
+	}
+	return base32.StdEncoding.EncodeToString(h.Sum(nil))
+}
+
+func policyToHeaderValue(instanceName string, policy *LabelPolicy) (string, error) {
+	matchers := make([]string, len(policy.Selector))
+	for i := range policy.Selector {
+		matchers[i] = policy.Selector[i].String()
+	}
+	return instanceName + ":" + url.PathEscape("{"+strings.Join(matchers, ", ")+"}"), nil
+}
+
+func policyFromHeaderValue(headerString string) (string, *LabelPolicy, error) {
+	components := strings.SplitN(headerString, ":", 2)
+	if len(components) != 2 {
+		return "", nil, errInvalidHeaderParse
+	}
+	selectorString, err := url.PathUnescape(components[1])
+	if err != nil {
+		return "", nil, fmt.Errorf("%w, '%v'", errInvalidHeaderEscape, err)
+	}
+	matchers, err := promqlext.NewPromQLParser().ParseMetricSelector(selectorString)
+	if err != nil {
+		return "", nil, fmt.Errorf("%w '%s', error: '%v'", errInvalidSelector, selectorString, err)
+	}
+
+	for _, m := range matchers {
+		if m.Name == "" {
+			return "", nil, fmt.Errorf("%w '%s', error: 'a label name was empty'", errInvalidSelector, selectorString)
+		}
+	}
+
+	return components[0], &LabelPolicy{Selector: matchers}, nil
+}
+
+// InjectLabelMatchersHTTP encodes the policy set as X-Prom-Label-Policy header values on r.
+// Any existing header values are replaced.
+func InjectLabelMatchersHTTP(r *http.Request, instancePolicyMap LabelPolicySet) error {
+	r.Header.Del(HTTPHeaderKey)
+
+	values, err := InjectLabelMatchersSlice(instancePolicyMap)
+	if err != nil {
+		return err
+	}
+
+	r.Header[HTTPHeaderKey] = values
+	return nil
+}
+
+// InjectLabelMatchersSlice encodes the policy set as a slice of header value strings.
+func InjectLabelMatchersSlice(instancePolicyMap LabelPolicySet) ([]string, error) {
+	var values []string
+	for instanceName, policies := range instancePolicyMap {
+		for _, policy := range policies {
+			encodedPolicy, err := policyToHeaderValue(instanceName, policy)
+			if err != nil {
+				return nil, err
+			}
+			values = append(values, encodedPolicy)
+		}
+	}
+	return values, nil
+}
+
+// ExtractLabelMatchersHTTP parses X-Prom-Label-Policy header values from r into a LabelPolicySet.
+func ExtractLabelMatchersHTTP(r *http.Request) (LabelPolicySet, error) {
+	return ExtractLabelMatchersSlice(r.Header.Values(HTTPHeaderKey))
+}
+
+// ExtractLabelMatchersSlice parses a slice of X-Prom-Label-Policy header values into a LabelPolicySet.
+func ExtractLabelMatchersSlice(values []string) (LabelPolicySet, error) {
+	instancePolicyMap := LabelPolicySet{}
+	for _, headerValue := range values {
+		for _, v := range strings.Split(headerValue, ",") {
+			instanceName, policy, err := policyFromHeaderValue(v)
+			if err != nil {
+				return nil, err
+			}
+			instancePolicyMap[instanceName] = append(instancePolicyMap[instanceName], policy)
+		}
+	}
+	return instancePolicyMap, nil
+}
+
+type matcherSource interface {
+	GetMatchers() LabelPolicySet
+}
+
+type labelMatcherCarrier struct {
+	matcher LabelPolicySet
+}
+
+func (l *labelMatcherCarrier) GetMatchers() LabelPolicySet {
+	return l.matcher
+}
+
+// ExtractLabelMatchersContext retrieves the label policy set embedded in ctx.
+func ExtractLabelMatchersContext(ctx context.Context) (LabelPolicySet, error) {
+	source, ok := ctx.Value(contextKey).(matcherSource)
+	if !ok {
+		return nil, errNoMatcherSource
+	}
+	return source.GetMatchers(), nil
+}
+
+// InjectLabelMatchersContext returns a derived context containing matchers.
+func InjectLabelMatchersContext(ctx context.Context, matchers LabelPolicySet) context.Context {
+	return context.WithValue(ctx, interface{}(contextKey), &labelMatcherCarrier{matchers})
+}
+
+// LabelPolicies is the interface for types that can provide a LabelPolicySet.
+type LabelPolicies interface {
+	GetLabelPolicySet() (LabelPolicySet, error)
+}
+
+// InjectMatchers sets X-Prom-Label-Policy on req from labelPolicies.
+// If labelPolicies returns an empty set the header is left unchanged.
+func InjectMatchers(req *http.Request, labelPolicies LabelPolicies) error {
+	labelPolicySet, err := labelPolicies.GetLabelPolicySet()
+	if err != nil {
+		return fmt.Errorf("unable to parse label policies: %w", err)
+	}
+
+	noPolicies := true
+	for _, policies := range labelPolicySet {
+		if len(policies) != 0 {
+			noPolicies = false
+			break
+		}
+	}
+	if noPolicies {
+		return nil
+	}
+
+	if err := InjectLabelMatchersHTTP(req, labelPolicySet); err != nil {
+		return fmt.Errorf("unable to inject matchers: %w", err)
+	}
+	return nil
+}
