7f2fd312aba6ede6c138df468411380941b170b4
Jon Kartago Lamida <me@lamida.net>
2026-03-09 11:15:21 +0800

    Remove grafana labs provenance

--- pkg/labelaccess/propagation_test.go
+++ pkg/labelaccess/propagation_test.go
@@ -1,7 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-// Provenance-includes-location: https://github.com/grafana/backend-enterprise/pkg/labelaccess/propagation_test.go
-// Provenance-includes-license: Apache-2.0
-// Provenance-includes-copyright: Grafana Labs
 
 package labelaccess
 
