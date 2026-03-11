6218dcb5ba7ee3026aa20a8c479172500ee76cfd
Marco Pracucci <marco@pracucci.com>
2026-03-11 20:38:28 +0100

    Fix
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- tools/kafkatool/create_topic.go
+++ tools/kafkatool/create_topic.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/alecthomas/kingpin/v2"
@@ -37,16 +38,14 @@ func (c *CreateTopicCommand) createTopic(_ *kingpin.ParseContext) error {
 	// Check if the topic already exists.
 	if _, err := getTopicDetails(adm, c.topic); err == nil {
 		return fmt.Errorf("topic %s already exists", c.topic)
-	}
-
-	// Create the topic. Replication factor -1 means use broker default.
-	responses, err := adm.CreateTopic(context.Background(), int32(c.numPartitions), -1, nil, c.topic)
-	if err != nil {
+	} else if !errors.Is(err, errTopicNotFound) {
 		return err
 	}
 
-	if responses.Err != nil {
-		return responses.Err
+	// Create the topic. Replication factor -1 means use broker default.
+	_, err := adm.CreateTopic(context.Background(), int32(c.numPartitions), -1, nil, c.topic)
+	if err != nil {
+		return err
 	}
 
 	c.printer.PrintLine(fmt.Sprintf("Created topic %s with %d partitions", c.topic, c.numPartitions))
