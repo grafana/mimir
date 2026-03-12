752ad99ccb1720ddd14dac0b1e000312b8a9587d
Jon Kartago Lamida <me@lamida.net>
2026-03-12 17:23:04 +0800

    Follow backend enterprise structure

--- pkg/labelaccess/propagation.go
+++ pkg/labelaccess/propagation.go
@@ -185,33 +185,3 @@ func ExtractLabelMatchersContext(ctx context.Context) (LabelPolicySet, error) {
 func InjectLabelMatchersContext(ctx context.Context, matchers LabelPolicySet) context.Context {
 	return context.WithValue(ctx, interface{}(contextKey), &labelMatcherCarrier{matchers})
 }
-
-// LabelPolicies is the interface for types that can provide a LabelPolicySet.
-type LabelPolicies interface {
-	GetLabelPolicySet() (LabelPolicySet, error)
-}
-
-// InjectMatchers sets X-Prom-Label-Policy on req from labelPolicies.
-// If labelPolicies returns an empty set the header is left unchanged.
-func InjectMatchers(req *http.Request, labelPolicies LabelPolicies) error {
-	labelPolicySet, err := labelPolicies.GetLabelPolicySet()
-	if err != nil {
-		return fmt.Errorf("unable to parse label policies: %w", err)
-	}
-
-	noPolicies := true
-	for _, policies := range labelPolicySet {
-		if len(policies) != 0 {
-			noPolicies = false
-			break
-		}
-	}
-	if noPolicies {
-		return nil
-	}
-
-	if err := InjectLabelMatchersHTTP(req, labelPolicySet); err != nil {
-		return fmt.Errorf("unable to inject matchers: %w", err)
-	}
-	return nil
-}
