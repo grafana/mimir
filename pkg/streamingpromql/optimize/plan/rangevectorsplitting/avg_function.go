2e135997c693a08c018cd5417cc7bdf0aa4db96a
Nikolai Tikhonov <nikolai.tikhonov@grafana.com>
2026-03-18 11:52:36 +0100

    Fixed review comments:   * Switch histogram to sum+count approach instead weighted average

--- pkg/streamingpromql/optimize/plan/rangevectorsplitting/avg_function.go
+++ pkg/streamingpromql/optimize/plan/rangevectorsplitting/avg_function.go
@@ -142,7 +142,7 @@ func avgOverTimeCombine(pieces []AvgOverTimeIntermediate, _ int64, _ int64, emit
 				}
 			}
 
-			countH = float64(p.CountH)
+			countH += float64(p.CountH)
 		}
 
 		// There are two modes used to combine intermediate pieces depending on whether overflow has been encountered.
