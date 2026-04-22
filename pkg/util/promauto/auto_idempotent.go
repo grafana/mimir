df137fd58333cacfe85bcf977ff864e8f9b2e5d6
francoposa <franco@francoposa.io>
2026-04-22 11:33:26 -0700

    License header

--- pkg/util/promauto/auto_idempotent.go
+++ pkg/util/promauto/auto_idempotent.go
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+// Provenance-includes-location: https://github.com/prometheus/client_golang/prometheus/promauto/auto.go
+// Provenance-includes-license: Apache-2.0
+// Provenance-includes-copyright: The Prometheus Authors.
+
 package promauto
 
 import (
