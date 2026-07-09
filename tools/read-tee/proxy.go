5ff5c0ad10389c432cd1e597534891a8b2bae5ca
Jonathan Halterman <jonathan@grafana.com>
2026-07-09 10:16:37 -0700

    Rename readtee dir to read-tee

--- tools/read-tee/proxy.go
+++ tools/read-tee/proxy.go
@@ -21,7 +21,7 @@ import (
 	"go.opentelemetry.io/otel"
 )
 
-var tracer = otel.Tracer("pkg/tools/readtee")
+var tracer = otel.Tracer("pkg/tools/read-tee")
 
 const (
 	defaultReportGRPCCodesInInstrumentationLabel = true
