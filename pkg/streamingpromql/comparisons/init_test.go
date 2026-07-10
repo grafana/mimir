505b747d98817feafc12861eacdc4d849c2dfe57
Charles Korn <charleskorn@users.noreply.github.com>
2026-07-10 10:21:26 +1000

    MQE: ensure slice mangling is enabled for all tests by default, and fix race in unit tests (#16000)
    #### What this PR does
    
    This PR enables slice mangling for tests in all MQE packages by default,
    and adds a script to enforce this. This would help catch issues like
    https://github.com/grafana/mimir/pull/15999 as part of unit testing.
    
    This uncovered a race that only affects unit tests:
    `histogramGrouper.releaseGroup` could modify a `promql.Buckets` slice
    that had already been returned to `bucketSliceBucketedPool`, because
    `group.pointBuckets[i]` was not cleared before mangling of the
    `[]promql.Buckets` slice occured when returning `group.pointBuckets` to
    `pointBucketPool`.
    
    This does not affect production query correctness, as both pools had
    `clearOnGet` set, and so queries running in parallel could not access or
    modify data from one another.
    
    #### Which issue(s) this PR fixes or relates to
    
    (none)
    
    #### Checklist
    
    - [n/a] Tests updated.
    - [n/a] Documentation added.
    - [n/a] `CHANGELOG.md` updated - the order of entries should be
    `[CHANGE]`, `[FEATURE]`, `[ENHANCEMENT]`, `[BUGFIX]`. If changelog entry
    is not needed, please add the `changelog-not-needed` label to the PR.
    - [n/a]
    [`about-versioning.md`](https://github.com/grafana/mimir/blob/main/docs/sources/mimir/configure/about-versioning.md)
    updated with experimental features.

---
+++ pkg/streamingpromql/comparisons/init_test.go
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+package comparisons
+
+import (
+	"github.com/grafana/mimir/pkg/streamingpromql/types"
+)
+
+func init() {
+	types.EnableManglingReturnedSlices = true
+}
