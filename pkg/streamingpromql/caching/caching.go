17c77cc6d3c56ba3ba5dd6f36eeabe54baa616e1
Charles Korn <charles.korn@grafana.com>
2026-08-03 13:16:40 +1000

    Add support for `SetMultiAsync` in `caching.Backend`

--- pkg/streamingpromql/caching/caching.go
+++ pkg/streamingpromql/caching/caching.go
@@ -14,6 +14,7 @@ import (
 type Backend interface {
 	GetMulti(ctx context.Context, keys []string, opts ...cache.Option) (map[string][]byte, error)
 	SetAsync(ctx context.Context, key string, value []byte, ttl time.Duration) error
+	SetMultiAsync(ctx context.Context, entries map[string][]byte, ttl time.Duration) error
 }
 
 // HashCacheKey returns a hashed version of key that is small enough to fit within the Memcached key limit
